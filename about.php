4ac5b8f83f3b27b508be0cdf907eb8b083c251e5
z0rglub <z0rglub@piwigo.org>
2003-05-26 19:24:08 +0000

    *** empty log message ***
    git-svn-id: http://piwigo.org/svn/trunk@18 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+/***************************************************************************
+ *                                about.php                                *
+ *                            ------------------                           *
+ *   application          : PhpWebGallery 1.3                              *
+ *   author               : Pierrick LE GALL <pierrick@z0rglub.com>        *
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation;                                         *
+ *                                                                         *
+ ***************************************************************************/
+
+//----------------------------------------------------------- personnal include
+include_once( './include/init.inc.php' );
+//----------------------------------------------------- template initialization
+$vtp = new VTemplate;
+$handle = $vtp->Open( './template/'.$user['template'].'/about.vtp' );
+initialize_template();
+
+$tpl = array('about_page_title','about_title','about_message','about_return');
+templatize_array( $tpl, 'lang', $handle );
+
+$url = './category.php?'.$_SERVER['QUERY_STRING'];
+$vtp->setVar( $handle, 'back_url', add_session_id( $url ) );
+//----------------------------------------------------------- html code display
+$code = $vtp->Display( $handle, 0 );
+echo $code;
+?>
