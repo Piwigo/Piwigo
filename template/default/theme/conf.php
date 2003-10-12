2308b8a873394dbba6ab3ce3c7861b7b352fc962
z0rglub <z0rglub@piwigo.org>
2003-10-12 15:15:27 +0000

    Using zelabo dark theme as default
    git-svn-id: http://piwigo.org/svn/trunk@203 68402e56-0260-453c-a942-63ccdbb3a9ee

--- template/default/theme/conf.php
+++ template/default/theme/conf.php
@@ -1,7 +1,23 @@
 <?php
-	/*---------------couleur de fond pour le template agjimmy - lightred------------------*/
-	/* ce template a été "adapté" par thierry                                         */
-	$user['couleur_fond'] = "#FFFFFF";
-	$user['couleur_text_fond'] = "black";
-	$user['couleur_text'] = "black";
+/***************************************************************************
+ *                                  conf.php                               *
+ *                            -------------------                          *
+ *   application   : PhpWebGallery 1.3 <http://phpwebgallery.net>          *
+ *   author        : Jerome <http://www.ze-labo.com>                       *
+ *                                                                         *
+ *   $Id$
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation;                                         *
+ *                                                                         *
+ ***************************************************************************/
+
+$user['couleur_fond'] = '#444444';
+$user['couleur_text_fond'] = 'white';
+$user['couleur_text'] = 'white';
 ?>
