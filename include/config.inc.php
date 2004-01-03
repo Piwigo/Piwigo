b8996a04bb598b60e6651d1e233c0926dfea505c
z0rglub <z0rglub@piwigo.org>
2004-01-03 19:18:05 +0000

    change version number, add max_LOV_categories used for numerous categories galleries
    git-svn-id: http://piwigo.org/svn/branches/release-1_3@238 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/config.inc.php
+++ include/config.inc.php
@@ -23,7 +23,7 @@ $user = array();
 $lang = array();
 
 include_once( PREFIX_INCLUDE.'./include/functions.inc.php' );
-include_once( PREFIX_INCLUDE.'./include/vtemplate.class.php' );
+include( PREFIX_INCLUDE.'./include/vtemplate.class.php' );
 // How to change the order of display for images in a category ?
 //
 // You have to modify $conf['order_by'].
@@ -44,13 +44,14 @@ $conf['nb_image_row']       = array(4,5,6,7,8);
 $conf['nb_row_page']        = array(2,3,4,5,6,7,10,20,1000);
 $conf['slideshow_period']   = array(2,5,10);
 $conf['last_days']          = array(1,2,3,10,30,365);
-$conf['version']            = '1.3';
+$conf['version']            = '1.3.1 [devel]';
 $conf['site_url']           = 'http://www.phpwebgallery.net';
 $conf['forum_url']          = 'http://forum.phpwebgallery.net';
 $conf['picture_ext']        = array('jpg','JPG','gif','GIF','png','PNG');
 $conf['document_ext']       = array('doc','pdf','zip');
 $conf['top_number']         = 10;
 $conf['anti-flood_time']    = 60; // seconds between 2 comments : 0 to disable
+$conf['max_LOV_categories'] = 50;
 
 database_connection();
 // rertieving the configuration informations for site
