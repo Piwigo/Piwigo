eea989f019f21fbd7ae4aa8e2f4a1503992c23bf
gweltas <gweltas@piwigo.org>
2004-02-01 22:09:22 +0000

    no message
    git-svn-id: http://piwigo.org/svn/trunk@344 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ include/constant.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************************
+ *                              constant.php                             *
+ *                            -------------------                          *
+ *   application   : PhpWebGallery 1.4 <http://phpwebgallery.net>          *
+ *   author        : Pierrick LE GALL <pierrick@z0rglub.com>               *
+ *                                                                         *
+ *   $Id$
+ *                                                                         *
+ ***************************************************************************
+
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation;                                         *
+ *                                                                         *
+ ***************************************************************************/
+
+define( 'PREFIX_INCLUDE', '' );
+  
+  // Table names
+define('CATEGORIES_TABLE', $table_prefix.'categories');
+define('COMMENTS_TABLE', $table_prefix.'comments');
+define('CONFIG_TABLE', $table_prefix.'config');
+define('FAVORITES_TABLE', $table_prefix.'favorites');
+define('GROUPS_ACCESS_TABLE', $table_prefix.'group_access');
+define('GROUPS_TABLE', $table_prefix.'groups');
+define('HISTORY_TABLE', $table_prefix.'history');
+define('IMAGE_CATEGORY_TABLE', $table_prefix.'image_category');
+define('IMAGES_TABLE', $table_prefix.'images');
+define('SESSIONS_TABLE', $table_prefix.'sessions');
+define('SITES_TABLE', $table_prefix.'sites');
+define('USER_ACCESS_TABLE', $table_prefix.'user_access');
+define('USER_CATEGORY_TABLE', $table_prefix.'user_category');
+define('USER_GROUP_TABLE', $table_prefix.'user_group');
+define('USERS_TABLE', $table_prefix.'users');
+define('WAITING_TABLE', $table_prefix.'waiting');
+
+?>
