c19070a07dae1ea3722e673ca75f8bcdf0f7bf14
z0rglub <z0rglub@piwigo.org>
2004-02-05 23:16:54 +0000

    - adding file header
     - echoing $output var
    
    
    git-svn-id: http://piwigo.org/svn/branches/release-1_3@349 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/page_tail.php
+++ include/page_tail.php
@@ -1,6 +1,23 @@
 <?php
-$handle = $vtp->Open( './template/'.$user['template'].'/footer.vtp' );
+/***************************************************************************
+ *                              page_footer.php                            *
+ *                            -------------------                          *
+ *   application   : PhpWebGallery 1.3 <http://phpwebgallery.net>          *
+ *   author        : Pierrick LE GALL <pierrick@z0rglub.com>               *
+ *                                                                         *
+ *   $Id$
+ *                                                                         *
+ ***************************************************************************
 
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation;                                         *
+ *                                                                         *
+ ***************************************************************************/
+
+$handle = $vtp->Open( './template/'.$user['template'].'/footer.vtp' );
 //------------------------------------------------------------- generation time
 $time = get_elapsed_time( $t2, get_moment() );
 $vtp->setGlobalVar( $handle, 'time', $time );
@@ -8,12 +25,13 @@ $vtp->setGlobalVar( $handle, 'time', $time );
 $vtp->setGlobalVar( $handle, 'generation_time', $lang['generation_time'] );
 $vtp->setGlobalVar( $handle, 'version', $conf['version'] );
 $vtp->setGlobalVar( $handle, 'site_url', $conf['site_url'] );
-$vtp->setVarF( $handle, 'footer', './template/'.$user['template'].'/footer.htm' );
+$vtp->setVarF( $handle, 'footer',
+               './template/'.$user['template'].'/footer.htm' );
 
 //
 // Generate the page
 //
 
-$code = $vtp->Display( $handle, 0 );
-echo $code;
+$output.= $vtp->Display( $handle, 0 );
+echo $output;
 ?>
