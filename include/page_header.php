ec7cb287665976870844eed44c4eb1f97a24d8e6
z0rglub <z0rglub@piwigo.org>
2004-02-05 23:16:40 +0000

    - adding file header
     - initializing uniq $output var
    
    
    git-svn-id: http://piwigo.org/svn/branches/release-1_3@348 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/page_header.php
+++ include/page_header.php
@@ -1,30 +1,47 @@
 <?php
+/***************************************************************************
+ *                              page_header.php                            *
+ *                            -------------------                          *
+ *   application   : PhpWebGallery 1.3 <http://phpwebgallery.net>          *
+ *   author        : Pierrick LE GALL <pierrick@z0rglub.com>               *
+ *                                                                         *
+ *   $Id$
+ *                                                                         *
+ ***************************************************************************
 
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation;                                         *
+ *                                                                         *
+ ***************************************************************************/
 //
 // Start output of page
 //
 $vtp = new VTemplate;
 $handle = $vtp->Open( './template/'.$user['template'].'/header.vtp' );
 $vtp->setGlobalVar( $handle, 'charset', $lang['charset'] );
-$vtp->setGlobalVar( $handle, 'style', './template/'.$user['template'].'/'.$user['template'].'.css');
+$vtp->setGlobalVar( $handle, 'style',
+                    './template/'.$user['template'].'/default.css');
 
-  // refresh
-  if ( isset( $refresh ) && $refresh >0 && isset($url_link))
-  {
-    $vtp->addSession( $handle, 'refresh' );
-    $vtp->setVar( $handle, 'refresh.time', $refresh );
-    $url = $url_link.'&amp;slideshow='.$refresh;
-    $vtp->setVar( $handle, 'refresh.url', add_session_id( $url ) );
-    $vtp->closeSession( $handle, 'refresh' );
-  }
-  
+// refresh
+if ( isset( $refresh ) and $refresh > 0 and isset( $url_link ) )
+{
+  $vtp->addSession( $handle, 'refresh' );
+  $vtp->setVar( $handle, 'refresh.time', $refresh );
+  $url = $url_link.'&amp;slideshow='.$refresh;
+  $vtp->setVar( $handle, 'refresh.url', add_session_id( $url ) );
+  $vtp->closeSession( $handle, 'refresh' );
+}
+
 $vtp->setGlobalVar( $handle, 'title', $title );
-$vtp->setVarF( $handle, 'header', './template/'.$user['template'].'/header.htm' );
+$vtp->setVarF( $handle, 'header',
+               './template/'.$user['template'].'/header.htm' );
 
 //
 // Generate the page
 //
 
-$code = $vtp->Display( $handle, 0 );
-echo $code;
+$output = $vtp->Display( $handle, 0 );
 ?>
