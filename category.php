6300caa15cc47b026421a428c26d289b37591887
z0rglub <z0rglub@piwigo.org>
2004-02-07 15:31:01 +0000

    correct coding header
    git-svn-id: http://piwigo.org/svn/trunk@352 68402e56-0260-453c-a942-63ccdbb3a9ee

--- category.php
+++ category.php
@@ -1,9 +1,9 @@
 <?php
 // +-----------------------------------------------------------------------+
-// |                           category.php                           |
+// |                           category.php                                |
 // +-----------------------------------------------------------------------+
 // | application   : PhpWebGallery <http://phpwebgallery.net>              |
-// | branch        : 1.4                                           |
+// | branch        : 1.4                                                   |
 // | author        : Pierrick LE GALL <pierrick@z0rglub.com>               |
 // +-----------------------------------------------------------------------+
 // | file          : $RCSfile$
@@ -13,10 +13,20 @@
 // +-----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or modify  |
 // | it under the terms of the GNU General Public License as published by  |
-// | the Free Software Foundation;                                         |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 
-//----------------------------------------------------------- include
+//--------------------------------------------------------------------- include
 $phpwg_root_path = './';
 include_once( $phpwg_root_path.'common.php' );
 //---------------------------------------------------------------------- logout
