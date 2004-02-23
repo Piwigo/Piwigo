29a07c00d083b7687331cd7cc19f097a8849a3ea
z0rglub <z0rglub@piwigo.org>
2004-02-23 22:02:41 +0000

    code refactoring
    git-svn-id: http://piwigo.org/svn/trunk@371 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/page_header.php
+++ include/page_header.php
@@ -30,36 +30,38 @@
 //
 $template->set_filenames(array('header'=>'header.tpl'));
 
+$css = './template/'.$user['template'].'/'.$user['template'].'.css';
+
 $template->assign_vars(array(
-	'S_CONTENT_ENCODING' => $lang['charset'],
-	'T_STYLE' =>  './template/'.$user['template'].'/'.$user['template'].'.css', 
-	'PAGE_TITLE' => $title)
-	);
+                         'S_CONTENT_ENCODING' => $lang['charset'],
+                         'T_STYLE' => $css, 
+                         'PAGE_TITLE' => $title
+                         ));
 
 // refresh
 if ( isset( $refresh ) && $refresh >0 && isset($url_link))
-  {
-    $url = $url_link.'&amp;slideshow='.$refresh;
-	$template->assign_vars(array(
-	'S_REFRESH_TIME' => $refresh,
-	'U_REFRESH' => add_session_id( $url )
-	)
-	);
-  	$template->assign_block_vars('refresh', array());
-  }
+{
+  $url = $url_link.'&amp;slideshow='.$refresh;
+  $template->assign_vars(array(
+                           'S_REFRESH_TIME' => $refresh,
+                           'U_REFRESH' => add_session_id( $url )
+                           ));
+  $template->assign_block_vars('refresh', array());
+}
 
 // Work around for "current" Apache 2 + PHP module which seems to not
 // cope with private cache control setting
-if (!empty($HTTP_SERVER_VARS['SERVER_SOFTWARE']) && strstr($HTTP_SERVER_VARS['SERVER_SOFTWARE'], 'Apache/2'))
+if (!empty( $_SERVER['SERVER_SOFTWARE'] )
+    and strstr( $_SERVER['SERVER_SOFTWARE'], 'Apache/2'))
 {
-	header ('Cache-Control: no-cache, pre-check=0, post-check=0, max-age=0');
+  header( 'Cache-Control: no-cache, pre-check=0, post-check=0, max-age=0' );
 }
 else
 {
-	header ('Cache-Control: private, pre-check=0, post-check=0, max-age=0');
+  header( 'Cache-Control: private, pre-check=0, post-check=0, max-age=0' );
 }
-header ('Expires: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
-header ('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header( 'Expires: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT' );
+header( 'Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT' );
 
 $template->pparse('header');
 $vtp=new VTemplate;
