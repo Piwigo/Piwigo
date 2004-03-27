6b1aee3dadcc2600967866613ac138b0932d0b4c
gweltas <gweltas@piwigo.org>
2004-03-27 13:25:07 +0000

    Default configuration
    git-svn-id: http://piwigo.org/svn/trunk@399 68402e56-0260-453c-a942-63ccdbb3a9ee

--- install/config.sql
+++ install/config.sql
@@ -3,11 +3,12 @@
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('prefix_thumbnail','TN-','thumbnails filename prefix');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('webmaster','','webmaster login');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('mail_webmaster','','webmaster mail');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('default_lang','en_EN','Default gallery language');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('default_style','default','Default gallery style');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('access','free','access type to your gallery (free|restricted)');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('session_id_size','4','length of session identifiers');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('session_keyword','','improves the encoding of the session identifier');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('session_time','30','number of minutes for validity of sessions');
-INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('max_user_listbox','10','maximum number of users for displaying a listbox on identification page');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('show_comments','true','display the users comments');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('nb_comment_page','10','number of comments to display on each page');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('upload_available','false','authorizing the upload of pictures by users');
@@ -21,3 +22,9 @@ INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('comments_validat
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('comments_forall','false','even guest not registered can post comments');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('authorize_cookies','false','users can create cookies');
 INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('mail_notification','false','automated mail notification for adminsitrators');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('nb_image_line','5','Number of images displayed per row');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('nb_line_page','3','Number of rows displayed per page');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('short_period','7','Period within which pictures are displayed as new (in days)');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('long_period','14','Long period within which pictures are displayed as new (in days)');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('auto_expand','false','Auto expand of the category tree');
+INSERT INTO phpwebgallery_config (param,value,comment) VALUES ('show_nb_comments','true','Show the number of comments under the thumbnails');
