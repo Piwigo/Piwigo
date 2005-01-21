2f50745d1412a3bdb4c507bf50e8fe5d1ac9f48a
plegall <plg@piwigo.org>
2005-01-21 22:09:58 +0000

    - version moved to 1.4.0RC2
    git-svn-id: http://piwigo.org/svn/trunk@718 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -26,7 +26,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '1.4.0RC1');
+define('PHPWG_VERSION', '1.4.0RC2');
 define('PHPWG_URL', 'http://www.phpwebgallery.net');
 define('PHPWG_FORUM_URL', 'http://forum.phpwebgallery.net');
 
