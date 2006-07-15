61ca501bcf3227b581127bf53d2614d941464a2a
chrisaga <chrisaga@piwigo.org>
2006-07-15 14:29:16 +0000

    fix bug 193 : pb with quote in tooltips with IE
    git-svn-id: http://piwigo.org/svn/trunk@1472 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/pngfix.js
+++ include/pngfix.js
@@ -1,6 +1,9 @@
 
 // Correctly handle PNG transparency in Win IE 5.5 or higher.
 // http://homepage.ntlworld.com/bobosola. Updated 02-March-2004
+// 15-Jully-2006 : chrisaga use \" instead of ' in imgTitle
+//		 : to fix ' display in tooltips
+//		 : keep the alt attribute
 
 function correctPNG() 
    {
@@ -14,7 +17,9 @@ function correctPNG()
 	     {
 		 var imgID = (img.id) ? "id='" + img.id + "' " : ""
 		 var imgClass = (img.className) ? "class='" + img.className + "' " : ""
-		 var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+		 //var imgTitle = (img.title) ? "title=\"" + img.title + "\" " : "alt=\"" + img.alt + "\" "
+		 var imgTitle = (img.title) ? "title=\"" + img.title + "\" " : "";
+		 imgTitle = imgTitle + (img.alt) ? "title=\"" + img.alt + "\" " : "";
 		 var imgStyle = "display:inline-block;" + img.style.cssText 
 		 if (img.align == "left") imgStyle = "float:left;" + imgStyle
 		 if (img.align == "right") imgStyle = "float:right;" + imgStyle
