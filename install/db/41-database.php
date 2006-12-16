5fe8be40c2d5d7bf35fb276e3630395726e7813f
vdigital <vdigital@piwigo.org>
2006-12-16 11:31:04 +0000

    Updrade_feed corrected
    git-svn-id: http://piwigo.org/svn/trunk@1667 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ install/db/41-database.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PhpWebGallery - a PHP based picture gallery                           |
+// | Copyright (C) 2002-2003 Pierrick LE GALL - pierrick@phpwebgallery.net |
+// | Copyright (C) 2003-2005 PhpWebGallery Team - http://phpwebgallery.net |
+// +-----------------------------------------------------------------------+
+// | branch        : BSF (Best So Far)
+// | file          : $RCSfile$
+// | last update   : $Date: 2005-09-21 00:04:57 +0200 (mer, 21 sep 2005) $
+// | last modifier : $Author: plg $
+// | revision      : $Revision: 870 $
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+if (!defined('PHPWG_ROOT_PATH'))
+{
+  die('Hacking attempt!');
+}
+
+$upgrade_description = 'Web service access table creation';
+
+include_once(PHPWG_ROOT_PATH.'include/constants.php');
+
+// +-----------------------------------------------------------------------+
+// |                            Upgrade content                            |
+// +-----------------------------------------------------------------------+
+
+echo "Create table ".WEB_SERVICES_ACCESS_TABLE;
+$query = '
+CREATE TABLE '.WEB_SERVICES_ACCESS_TABLE.' (
+  id smallint(5) unsigned NOT NULL auto_increment,
+  name varchar(32) NOT NULL default '',
+  access varchar(255) default NULL,
+  `start` datetime default NULL,
+  `end` datetime default NULL,
+  request varchar(255) default NULL,
+  high enum(\'true\',\'false\') NOT NULL default \'true\',
+  normal enum(\'true\',\'false\') NOT NULL default \'true\',
+  `limit` smallint(5) unsigned default NULL,
+  `comment` varchar(255) default NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY name (name)
+) ENGINE=MyISAM COMMENT=\'Access for Web Services\';';
+pwg_query($query);
+
+echo
+"\n"
+.'"'.$upgrade_description.'"'.' ended'
+."\n"
+;
+
+?>
