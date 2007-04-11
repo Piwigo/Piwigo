38ca856ea4adf9daf7cdb91e580a70ab176eb6f4
plegall <plg@piwigo.org>
2007-04-11 08:33:18 +0000

    New: trunk code name changes from "Alligator" to "Butterfly".
    git-svn-id: http://piwigo.org/svn/trunk@1954 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -31,7 +31,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = 'Alligator';
+$conf['version'] = 'Butterfly';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
