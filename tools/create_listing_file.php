38dbeabe43b1b307014ad1b696ffb3a21fbc5fc6
plegall <plg@piwigo.org>
2007-05-04 22:43:06 +0000

    New version 1.7.0 hard coded (not replaced at release creation time).
    git-svn-id: http://piwigo.org/svn/tags/release-1_7_0@2002 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -31,7 +31,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = 'branch 1.7';
+$conf['version'] = '1.7.0';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
