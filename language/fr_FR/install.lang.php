a34f8c67b07c861212346252b766cf6131b65885
plegall <plg@piwigo.org>
2007-10-09 21:39:15 +0000

    Part of feature 32: fr_FR.iso-8859-1 and en_UK.iso-8859-1 contents become utf-8 encoded and the directory names lose encoding information as the only supported encoding becomes utf-8.
    git-svn-id: http://piwigo.org/svn/trunk@2130 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fr_FR/install.lang.php
+++ language/fr_FR/install.lang.php
@@ -26,45 +26,45 @@
 
 $lang['install_message'] = 'Message';
 $lang['Initial_config'] = 'Configuration de Base';
-$lang['Default_lang'] = 'Langue par défaut de la galerie';
-$lang['step1_title'] = 'Configuration de la Base de données';
+$lang['Default_lang'] = 'Langue par dÃ©faut de la galerie';
+$lang['step1_title'] = 'Configuration de la Base de donnÃ©es';
 $lang['step2_title'] = 'Configuration du compte Administrateur';
-$lang['Start_Install'] = 'Démarrer l\'installation';
-$lang['reg_err_mail_address'] = 'L\'adresse mail doit être de la forme xxx@yyy.eee (exemple : jack@altern.org)';
+$lang['Start_Install'] = 'DÃ©marrer l\'installation';
+$lang['reg_err_mail_address'] = 'L\'adresse mail doit Ãªtre de la forme xxx@yyy.eee (exemple : jack@altern.org)';
 
 $lang['install_webmaster'] = 'Administrateur';
-$lang['install_webmaster_info'] = 'Cet identifiant apparaîtra à tous vos visiteurs. Il vous sert pour administrer le site';
+$lang['install_webmaster_info'] = 'Cet identifiant apparaÃ®tra Ã  tous vos visiteurs. Il vous sert pour administrer le site';
 
-$lang['step1_confirmation'] = 'Les paramètres rentrés sont corrects';
-$lang['step1_err_db'] = 'La connexion au serveur est OK, mais impossible de se connecter à cette base de données';
+$lang['step1_confirmation'] = 'Les paramÃ¨tres rentrÃ©s sont corrects';
+$lang['step1_err_db'] = 'La connexion au serveur est OK, mais impossible de se connecter Ã  cette base de donnÃ©es';
 $lang['step1_err_server'] = 'Impossible de se connecter au serveur';
 
-$lang['step1_host'] = 'Hôte MySQL';
+$lang['step1_host'] = 'HÃ´te MySQL';
 $lang['step1_host_info'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
 $lang['step1_user'] = 'Utilisateur';
-$lang['step1_user_info'] = 'nom d\'utilisateur pour votre hébergeur';
+$lang['step1_user_info'] = 'nom d\'utilisateur pour votre hÃ©bergeur';
 $lang['step1_pass'] = 'Mot de passe';
-$lang['step1_pass_info'] = 'celui fourni par votre hébergeur';
+$lang['step1_pass_info'] = 'celui fourni par votre hÃ©bergeur';
 $lang['step1_database'] = 'Nom de la base';
-$lang['step1_database_info'] = 'celui fourni par votre hébergeur';
-$lang['step1_prefix'] = 'Préfixe des noms de table';
-$lang['step1_prefix_info'] = 'le nom des tables apparaîtra avec ce préfixe (permet de mieux gérer sa base de données)';
+$lang['step1_database_info'] = 'celui fourni par votre hÃ©bergeur';
+$lang['step1_prefix'] = 'PrÃ©fixe des noms de table';
+$lang['step1_prefix_info'] = 'le nom des tables apparaÃ®tra avec ce prÃ©fixe (permet de mieux gÃ©rer sa base de donnÃ©es)';
 $lang['step2_err_login1'] = 'veuillez rentrer un pseudo pour le webmaster';
-$lang['step2_err_login3'] = 'le pseudo du webmaster ne doit pas comporter les caractère " et \'';
+$lang['step2_err_login3'] = 'le pseudo du webmaster ne doit pas comporter les caractÃ¨re " et \'';
 $lang['step2_err_pass'] = 'veuillez retaper votre mot de passe';
-$lang['install_end_title'] = 'Installation terminée';
+$lang['install_end_title'] = 'Installation terminÃ©e';
 $lang['step2_pwd'] = 'Mot de passe';
-$lang['step2_pwd_info'] = 'Il doit rester confidentiel, il permet d\'accéder au panneau d\'administration.';
+$lang['step2_pwd_info'] = 'Il doit rester confidentiel, il permet d\'accÃ©der au panneau d\'administration.';
 $lang['step2_pwd_conf'] = 'Mot de passe [ Confirmer ]';
-$lang['step2_pwd_conf_info'] = 'Vérification';
-$lang['step1_err_copy'] = 'Copiez le texte en bleu entre les tirets et collez-le dans le fichier mysql.inc.php qui se trouve dans le répertoire "include" à la base de l\'endroit où vous avez installé PhpWebGallery (le fichier mysql.inc.php ne doit comporter QUE ce qui est en bleu entre les tirets, aucun retour à la ligne ou espace n\'est autorisé)';
+$lang['step2_pwd_conf_info'] = 'VÃ©rification';
+$lang['step1_err_copy'] = 'Copiez le texte en bleu entre les tirets et collez-le dans le fichier mysql.inc.php qui se trouve dans le rÃ©pertoire "include" Ã  la base de l\'endroit oÃ¹ vous avez installÃ© PhpWebGallery (le fichier mysql.inc.php ne doit comporter QUE ce qui est en bleu entre les tirets, aucun retour Ã  la ligne ou espace n\'est autorisÃ©)';
 $lang['install_help'] = 'Besoin d\'aide ? Posez votre question sur le <a href="%s">forum de PhpWebGallery</a>.';
-$lang['install_end_message'] = 'La configuration de l\'application s\'est correctement déroulée, place à la prochaine étape<br /><br />
-Par mesure de sécurité, merci de supprimer le fichier "install.php"<br />
-Un fois ce fichier supprimé, veuillez suivre ces indications :
+$lang['install_end_message'] = 'La configuration de l\'application s\'est correctement dÃ©roulÃ©e, place Ã  la prochaine Ã©tape<br /><br />
+Par mesure de sÃ©curitÃ©, merci de supprimer le fichier "install.php"<br />
+Un fois ce fichier supprimÃ©, veuillez suivre ces indications :
 <ul>
-<li>allez sur la page d\'identification : [ <a href="./identification.php">identification</a> ] et connectez-vous avec le pseudo donné pour le webmaster</li>
-<li>celui-ci vous permet d\'accéder à la partie administration et aux instructions pour placer les images dans les répertoires.</li>
+<li>allez sur la page d\'identification : [ <a href="./identification.php">identification</a> ] et connectez-vous avec le pseudo donnÃ© pour le webmaster</li>
+<li>celui-ci vous permet d\'accÃ©der Ã  la partie administration et aux instructions pour placer les images dans les rÃ©pertoires.</li>
 </ul>';
 $lang['conf_mail_webmaster'] = 'Adresse e-mail de l\'Administrateur';
 $lang['conf_mail_webmaster_info'] = 'Les visiteurs pourront vous contacter par ce mail';
