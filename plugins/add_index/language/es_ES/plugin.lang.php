80021710902188049a57138fadc207d031f302c8
rub <rub@piwigo.org>
2007-10-22 21:08:58 +0000

    Plugin AddIndex: Add Spanish translations from ddtddt .
    git-svn-id: http://piwigo.org/svn/trunk@2148 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/add_index/language/es_ES/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PhpWebGallery - a PHP based picture gallery                           |
+// | Copyright (C) 2002-2003 Pierrick LE GALL - pierrick@phpwebgallery.net |
+// | Copyright (C) 2003-2007 PhpWebGallery Team - http://phpwebgallery.net |
+// | Copyright (C) 2007 Ruben ARNAUD - team@phpwebgallery.net              |
+// +-----------------------------------------------------------------------+
+// | branch        : BSF (Best So Far)
+// | file          : $RCSfile$
+// | last update   : $Date$
+// | last modifier : $Author$
+// | revision      : $Revision$
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add_Index'] = 'Adición de los ficheros "index"';
+$lang['Advanced_Add_Index'] = 'Adición y aplastamiento de los ficheros "index" para todos los sitios locales';
+$lang['Manager_Add_Index'] = 'adición de "index"';
+
+$lang['add_index_file_copied'] = '%s copiado';
+$lang['add_index_file_not_copied'] = '%s no copiado';
+$lang['add_index_not_local_site'] = '%s [%d] es un sitio distante, no es compatible con la adición de ficheros "index"';
+$lang['add_index_nb_copied_file'] = '%d fichero copiado';
+$lang['add_index_nb_copied_files'] = '%d ficheros copiados';
+$lang['add_index_nb_skipped_file'] = '%d fichero que debe tratarse';
+$lang['add_index_nb_skipped_files'] = '%d ficheros que deben tratarse';
+$lang['add_index_nb_not_copied_file'] = '%d fichero no copiado';
+$lang['add_index_nb_not_copied_files'] = '%d ficheros no copiados';
+$lang['add_index_src_file_dont_exists'] = 'El fichero fuente no existe, la duplicación de este fichero no es posible';
+
+?>
