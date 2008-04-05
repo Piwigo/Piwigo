1458e65a1ca097036ae48b4ec4cc2738af6c3efc
vdigital <vdigital@piwigo.org>
2008-04-05 11:14:37 +0000

    Tweak: Comments update
    git-svn-id: http://piwigo.org/svn/trunk@2298 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/language_switch/main.inc.php
+++ plugins/language_switch/main.inc.php
@@ -20,6 +20,7 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
+/*
 Plugin Name: Language Switch
 Version: 1.8
 Description: Switch to another language from flags on your gallery home page.
@@ -27,30 +28,6 @@ Plugin URI: http://phpwebgallery.net/ext/extension_view.php?eid=123
 Author: PhpWebGallery team
 Author URI: http://www.phpwebgallery.net
 */
-// +-----------------------------------------------------------------------+
-// | PhpWebGallery - a PHP based picture gallery                           |
-// | Copyright (C) 2002-2003 Pierrick LE GALL - pierrick@phpwebgallery.net |
-// | Copyright (C) 2003-2007 PhpWebGallery Team - http://phpwebgallery.net |
-// +-----------------------------------------------------------------------+
-// | file          : $Id$
-// | last update   : $Date$
-// | last modifier : $Author$
-// | revision      : $Revision$
-// +-----------------------------------------------------------------------+
-// | This program is free software; you can redistribute it and/or modify  |
-// | it under the terms of the GNU General Public License as published by  |
-// | the Free Software Foundation                                          |
-// |                                                                       |
-// | This program is distributed in the hope that it will be useful, but   |
-// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
-// | General Public License for more details.                              |
-// |                                                                       |
-// | You should have received a copy of the GNU General Public License     |
-// | along with this program; if not, write to the Free Software           |
-// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
-// | USA.                                                                  |
-// +-----------------------------------------------------------------------+
 include_once(PHPWG_PLUGINS_PATH.'language_switch/language_switch.inc.php');
 add_event_handler('loading_lang', 'language_switch' );
 add_event_handler('loc_end_index', 'Lang_flags' );
