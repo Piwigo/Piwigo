d0e86480c46b6f48dfb37284b20b99b9ab862e9a
riopwg <riopwg@piwigo.org>
2008-07-01 15:44:18 +0000

    language/it_IT
    git-svn-id: http://piwigo.org/svn/trunk@2412 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/install.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['install_message'] = 'Messaggio';
+$lang['Initial_config'] = 'Configurazione di base';
+$lang['Default_lang'] = 'Linguaggio di default della galleria';
+$lang['step1_title'] = 'Configurazione della base dati';
+$lang['step2_title'] = 'Configurazione del utente "Amministratore"';
+$lang['Start_Install'] = 'Inizia l\'installazione';
+$lang['reg_err_mail_address'] = 'L\'indirizzo email deve essere del tipo xxx@yyy.eee (ad esempio: cippalippa@libero.rio)';
+
+$lang['install_webmaster'] = 'Amministratore';
+$lang['install_webmaster_info'] = 'Verrà mostrato ai visitatori. È necessario per l\'amministrazione del sito';
+
+$lang['step1_confirmation'] = 'I parametri sono corretti';
+$lang['step1_err_db'] = 'Connessione al server riuscita. Non è stato però possibile connettersi alla base dati';
+$lang['step1_err_server'] = 'Non è stato possibile connettersi al server';
+
+$lang['step1_host'] = 'MySQL host';
+$lang['step1_host_info'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
+$lang['step1_user'] = 'Utente';
+$lang['step1_user_info'] = 'nome utente di login alla base dati fornito dal tuo provider';
+$lang['step1_pass'] = 'Password';
+$lang['step1_pass_info'] = 'La password di accesso alla base dati fornita dal tuo provider';
+$lang['step1_database'] = 'Nome della base dati';
+$lang['step1_database_info'] = 'fornitovi dal provider';
+$lang['step1_prefix'] = 'Prefisso delle tabelle della base dati';
+$lang['step1_prefix_info'] = 'Le tabelle della base dati avranno come prefisso quello specificato (permette di gestire meglio le tabelle)';
+$lang['step2_err_login1'] = 'Inserire un nome utente per il webmaster';
+$lang['step2_err_login3'] = 'Il nome utente del webmaster non può contenere caratteri come \' o "';
+$lang['step2_err_pass'] = 'Reinserire la password';
+$lang['install_end_title'] = 'Installazione completata';
+$lang['step2_pwd'] = 'Password';
+$lang['step2_pwd_info'] = 'Da conservare con cura. Permette l\'accesso al pannello di amministrazione';
+$lang['step2_pwd_conf'] = 'Password [confermare]';
+$lang['step2_pwd_conf_info'] = 'Verifica';
+$lang['step1_err_copy'] = 'Copiate il testo in blu tra i trattini e mettetelo nel file mysql.inc.php che si trova nella directory "include" alla base del vostro sito dove aveto installato PhpWebGallery (il file mysql.inc.php non deve contenere altro che ciò che è in blu tra i trattini, nessun ritorno a capo o spazio è autorizzato)';
+$lang['install_help'] = 'Bisogno di aiuto? Visitate il <a href="%s">forum di PhpWebGallery</a>.';
+$lang['install_end_message'] = 'La configurazione di PhpWebGallery è conclusa. Procedete al prossimo step<br /><br />
+Per ragioni di sicurezza vi consigliamo di cancellare il file "install.php"<br />
+Una volta che il file è stato cancellato seguire le istruzioni:
+<ul>
+<li>collegatevi alla pagina di accesso: [ <a href="identification.php">Accedi ora</a> ] e usate il vostro nome utente e password del Webmaster</li>
+<li>a questo punto sarete abilitati all\'accesso al pannello di amministrazione in cui troverete le istruzioni per l\'inserimento delle immagini nelle vostre directory</li>
+</ul>';
+$lang['conf_mail_webmaster'] = 'Indirizzo email del Amministratore';
+$lang['conf_mail_webmaster_info'] = 'I visitatori potranno contattarvi utilizzando questo indirizzo email';
+?>
