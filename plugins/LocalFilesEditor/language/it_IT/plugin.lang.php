90002e45dd9b83c7654f1a756006d7af39dab926
riopwg <riopwg@piwigo.org>
2008-07-20 23:47:18 +0000

    language/it_IT
    git-svn-id: http://piwigo.org/svn/trunk@2443 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/it_IT/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'Configurazione locale';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'Templates';
+$lang['locfiledit_onglet_lang'] = 'Lingue';
+$lang['locfiledit_onglet_plug'] = 'Plugin Personale';
+$lang['locfiledit_cant_save'] = 'Impossibile di scrivere il file. Verificare i permessi (chmod) della directory.';
+$lang['locfiledit_newfile'] = "/* Il file non esiste e sarà creato durante la fase di registrazione con LocalFiles Editor. */";
+$lang['locfiledit_save_config'] = 'Il file è stato salvato.';
+$lang['locfiledit_show_default'] = 'Visualizzare il file ';
+$lang['locfiledit_save_bak'] = 'Una copia di backup del file sarà creata durante la fase di registrazione';
+$lang['locfiledit_saved_bak'] = 'Una copia di backup è stata creata (%s)';
+$lang['locfiledit_save_file'] = 'Registrare il file';
+$lang['locfiledit_choose_file'] = 'Scegliere un file da editare';
+$lang['locfiledit_edit'] = 'Editare';
+$lang['locfiledit_restore'] = 'Caricare il file di backup';
+$lang['locfiledit_restore_confirm'] = 'Volete caricare il file di backup? Dovrete in seguito cliccare sù Registrare per restaurarlo.';
+$lang['locfiledit_bak_loaded1'] = 'Copia di backup caricata';
+$lang['locfiledit_bak_loaded2'] = 'Dovete registrarlo per salvarlo.';
+$lang['locfiledit_syntax_error'] = 'Errore di sintassi! Impossible di registrare il file.';
+
+?>
