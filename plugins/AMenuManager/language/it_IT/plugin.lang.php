5e3d71c9cab75c7796c566823df96765522c8965
riopwg <riopwg@piwigo.org>
2008-08-03 22:24:22 +0000

    Translate \language\it_IT\help\extend_for_templates.html Add italian lang \plugins\AMenuManager\ Corrections
    git-svn-id: http://piwigo.org/svn/trunk@2467 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/AMenuManager/language/it_IT/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Il plugin <b>Grum Plugin Classes</b> non è installato';
+
+$lang['g002_title_page'] = 'Gestion avanzata dei menu';
+$lang['g002_version'] = 'v';
+
+$lang['g002_error_invalid_ajax_call'] = "Chiamata alla funzione invalida !";
+
+$lang['g002_setmenu'] = 'Gestione del menu';
+$lang['g002_addlinks'] = 'Links';
+$lang['g002_randompict'] = 'Immagine aleatoira';
+$lang['g002_personnalblock'] = 'Menu personalizzato';
+
+$lang['g002_setmenu_nfo'] = 'Gestione della visualizzazione delle sezioni visualizzate nel menu';
+$lang['g002_addlinks_nfo'] = 'Sezione che permette la visualizzazione nel menu di una lista d\'hyperlinks verso altri siti internet';
+$lang['g002_randompict_nfo'] = 'Sezione che permette la visualizzazione nel menu d\'un immagine presa a caso nella galleria';
+$lang['g002_personnalblock_nfo'] = 'Visualizzare nel menu delle sezioni con un contenuto personalizzato';
+
+
+$lang['g002_mode_new_window'] = 'Nuova finestra';
+$lang['g002_mode_current_window'] = 'Finestra attuale';
+$lang['g002_addlink'] = 'Aggiungere un link';
+$lang['g002_label'] = 'Etichetta';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Modo';
+$lang['g002_icon'] = 'Immagine';
+$lang['g002_visible'] = 'Visibile';
+$lang['g002_linkslist'] = 'Lista dei links';
+$lang['g002_confirm_delete_link'] = 'Cancellare il link ?';
+$lang['g002_nolinks'] = 'Nessun\' link';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'links';
+
+$lang['g002_createoflink'] = 'Aggiungere un link';
+$lang['g002_editoflink'] = 'Modifica di un link';
+$lang['g002_createthelink'] = 'Aggiungere un link';
+$lang['g002_editthelink'] = 'Modifica di un link';
+
+$lang['g002_configlinks'] = 'Configurazione dei links';
+$lang['g002_setting_link_block_menu'] = 'Integrazione nel menu';
+$lang['g002_setting_link_links']='Links';
+$lang['g002_setting_link_show_icon'] = 'Visualizzare le icône dei links';
+$lang['g002_setting_link_block_active'] = 'Visualizzare la sezione nel menu';
+$lang['g002_setting_link_block_title'] = 'Titolo della sezione nel menu';
+$lang['g002_apply'] = 'Applicare';
+
+$lang['g002_sectionslist'] = 'Sezioni del menu';
+
+$lang['g002_yesno_y'] = 'Si';
+$lang['g002_yesno_n'] = 'No';
+
+$lang['g002_owner'] = 'Proprietario';
+$lang['g002_sectionid'] = 'Id';
+$lang['g002_name'] = 'Etichetta';
+
+
+?>
