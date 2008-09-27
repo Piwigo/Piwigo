50c4f0d6828d3b96cbc3abd368e8e21f7a2728aa
plegall <plg@piwigo.org>
2008-09-27 21:18:14 +0000

    Release candidate "2.0.0RC1" hard coded.
    git-svn-id: http://piwigo.org/svn/tags/release-2_0_0RC1@2617 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = 'branch 1.7';
+$conf['version'] = '2.0.0RC1';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
