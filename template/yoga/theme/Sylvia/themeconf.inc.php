756946289589a8146101265c603d4bf4a234b137
vdigital <vdigital@piwigo.org>
2008-10-04 09:05:36 +0000

    Gallery side icons for Sylvia (Some are missing, your browser cache could hide that). + No borders for pics in Sylvia.
    git-svn-id: http://piwigo.org/svn/trunk@2650 68402e56-0260-453c-a942-63ccdbb3a9ee

--- template/yoga/theme/Sylvia/themeconf.inc.php
+++ template/yoga/theme/Sylvia/themeconf.inc.php
@@ -2,9 +2,9 @@
 $themeconf = array(
   'template' => 'yoga',
   'theme' => 'Sylvia',
-  'icon_dir' => 'template/yoga/icon',
+  'icon_dir' => 'template/yoga/theme/Sylvia/icon',
   'admin_icon_dir' => 'template/yoga/icon/admin',
-  'mime_icon_dir' => 'template/yoga/icon/mimetypes/',
+  'mime_icon_dir' => 'template/yoga/theme/Sylvia/icon/mimetypes/',
   'local_head' => '',
 );
 ?>
