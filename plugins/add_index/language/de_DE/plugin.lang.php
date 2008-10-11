f2b0b09a0a6e4ee442325953c534bfffe185d981
patdenice <patdenice@piwigo.org>
2008-10-11 10:21:33 +0000

    Merge from 2710: - Move filter button back to menubar_categories.tpl. - Add dutch plugins language.
    git-svn-id: http://piwigo.org/svn/branches/2.0@2711 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/add_index/language/de_DE/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add_Index'] = 'Hinzufügen "Index"-Dateien';
+$lang['Advanced_Add_Index'] = 'Hinzufügen und überschreiben "Index"-Dateien für alle lokalen Standorte';
+$lang['Manager_Add_Index'] = 'Hinzufügen "Index"';
+
+$lang['add_index_file_copied'] = '%s kopiert';
+$lang['add_index_file_not_copied'] = '%s nicht kopiert';
+$lang['add_index_not_local_site'] = '%s [%d] ist eine Remote-Website. Es ist kompatibel mit hinzufügen "Index"-Dateien.';
+$lang['add_index_nb_copied_file'] = '%d Datei kopiert';
+$lang['add_index_nb_copied_files'] = '%d Dateien kopiert';
+$lang['add_index_nb_skipped_file'] = '%d Datei übersprungen';
+$lang['add_index_nb_skipped_files'] = '%d Dateien übersprungen';
+$lang['add_index_nb_not_copied_file'] = '%d Datei nicht kopiert';
+$lang['add_index_nb_not_copied_files'] = '%d Dateien nicht kopiert';
+$lang['add_index_src_file_dont_exists'] = 'Quelldatei %s ist nicht vorhanden, kann nicht duplizieren diese Datei';
+
+?>
