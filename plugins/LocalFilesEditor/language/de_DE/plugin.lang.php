da532c3979c6adb7e4e599d6ebb8baa71ddea255
patdenice <patdenice@piwigo.org>
2008-10-12 19:12:33 +0000

    Dutch language pack for LocalFiles Editor.
    git-svn-id: http://piwigo.org/svn/trunk@2726 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/de_DE/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'Lokale Konfiguration';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'Vorlagen';
+$lang['locfiledit_onglet_lang'] = 'Sprachen';
+$lang['locfiledit_onglet_plug'] = 'Persönliche Plugin';
+$lang['locfiledit_cant_save'] = 'Aktuelle Datei ist nicht änderbar. Prüfen Sie ob ein Verzeichnis "include/" ist änderbar (chmod)';
+$lang['locfiledit_newfile'] = 'Die Datei ist nicht vorhanden und wird bei der Registrierung von localfile Editor';
+$lang['locfiledit_save_config'] = 'Datei geschrieben erfolgreich.';
+$lang['locfiledit_show_default'] = 'Anzeige Referenz-Datei: ';
+$lang['locfiledit_save_bak'] = 'Backup-Kopie wird auf Speichern.';
+$lang['locfiledit_saved_bak'] = 'Backup-Datei erstellt (%s)';
+$lang['locfiledit_save_file'] = 'Datei speichern';
+$lang['locfiledit_choose_file'] = 'Wählen die Datei auf der bearbeitet werden';
+$lang['locfiledit_edit'] = 'Bearbeiten';
+$lang['locfiledit_restore'] = 'Wiederherstellung der Backup-Datei';
+$lang['locfiledit_restore_confirm'] = 'Bitte bestätigen?
+Wiederherstellung wird nicht wirksam sein bis zum nächsten speichern.';
+$lang['locfiledit_bak_loaded1'] = 'Backup-Datei geladen werden.';
+$lang['locfiledit_bak_loaded2'] = 'Sie müssen Datei speichern zur Wiederherstellung es.';
+$lang['locfiledit_syntax_error'] = 'Syntax-Fehler! Datei kann nicht gespeichert werden.';
+$lang['locfiledit_enable_editarea'] = 'Aktivieren Bearbeiten Bereich';
+$lang['locfiledit_disable_editarea'] = 'Deaktivieren Bearbeiten Bereich';
+$lang['locfiledit_new_tpl'] = 'Erstellen eine neue Datei';
+$lang['locfiledit_new_filename'] = 'Dateiname';
+$lang['locfiledit_model'] = 'Modell';
+$lang['locfiledit_empty_page'] = 'Leere Seite';
+$lang['locfiledit_parent_directory'] = 'Eltern-Verzeichnis';
+$lang['locfiledit_filename_error'] = 'Verboten Zeichen in Dateinamen.';
+$lang['locfiledit_file_already_exists'] = 'Datei ist bereits vorhanden.';
+$lang['locfiledit_model_error'] = 'Sie müssen ein Modell wählen.';
+
+?>
