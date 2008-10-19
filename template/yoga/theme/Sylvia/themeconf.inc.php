4e6e78d04cdb685a535974d3a3b2721dac15a4c1
vdigital <vdigital@piwigo.org>
2008-10-19 20:55:23 +0000

    CSS 2.1 cannot be reached (14 errors left). IE rule set is created (including IE hack).
    git-svn-id: http://piwigo.org/svn/branches/2.0@2778 68402e56-0260-453c-a942-63ccdbb3a9ee

--- template/yoga/theme/Sylvia/themeconf.inc.php
+++ template/yoga/theme/Sylvia/themeconf.inc.php
@@ -5,6 +5,10 @@ $themeconf = array(
   'icon_dir' => 'template/yoga/theme/Sylvia/icon',
   'admin_icon_dir' => 'template/yoga/icon/admin',
   'mime_icon_dir' => 'template/yoga/theme/Sylvia/icon/mimetypes/',
-  'local_head' => '',
+  'local_head' => '
+<!--[if IE]> <-->
+	<link rel="stylesheet" href="template/yoga/theme/Sylvia/theme-ie.css" type="text/css">
+<!--> <![endif]-->
+  ',
 );
 ?>
