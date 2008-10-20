c31581b44249485ad977f5a8c7377dfadd1b8f86
riopwg <riopwg@piwigo.org>
2008-10-20 00:13:01 +0000

    Lang file from iso8859 to utf8
    git-svn-id: http://piwigo.org/svn/trunk@2781 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/AMenuManager/language/de_DE/plugin.lang.php
+++ plugins/AMenuManager/language/de_DE/plugin.lang.php
@@ -5,64 +5,64 @@ $lang['Grum Plugin Classes is not installed'] = 'Das Plugin <b>Grum Plugin Klass
 $lang['g002_title_page'] = 'Advanced Menu Management';
 $lang['g002_version'] = 'r';
 
-$lang['g002_error_invalid_ajax_call'] = "Ungültige Funktion Anruf!";
+$lang['g002_error_invalid_ajax_call'] = "UngÃ¼ltige Funktion Anruf!";
 
-$lang['g002_setmenu'] = 'Menü-Management';
+$lang['g002_setmenu'] = 'MenÃ¼-Management';
 $lang['g002_addlinks'] = 'Links';
 $lang['g002_randompict'] = 'Zufallsbild';
-$lang['g002_personnalblock'] = 'Personalisierte Menü';
+$lang['g002_personnalblock'] = 'Personalisierte MenÃ¼';
 
-$lang['g002_setmenu_nfo'] = 'Management der angezeigten Menü Blöcke';
-$lang['g002_addlinks_nfo'] = 'Abschnitt für die Anzeige in dem Menü eine Liste mit Links zu anderen Websites';
-$lang['g002_randompict_nfo'] = 'Abschnitt für die Anzeige in dem Menü einer Aufnahme zufällig in der Galerie';
-$lang['g002_personnalblock_nfo'] = 'Anzeige Personalisierte in Blöcke Menü';
+$lang['g002_setmenu_nfo'] = 'Management der angezeigten MenÃ¼ BlÃ¶cke';
+$lang['g002_addlinks_nfo'] = 'Abschnitt fÃ¼r die Anzeige in dem MenÃ¼ eine Liste mit Links zu anderen Websites';
+$lang['g002_randompict_nfo'] = 'Abschnitt fÃ¼r die Anzeige in dem MenÃ¼ einer Aufnahme zufÃ¤llig in der Galerie';
+$lang['g002_personnalblock_nfo'] = 'Anzeige Personalisierte in BlÃ¶cke MenÃ¼';
 
 
 $lang['g002_mode_new_window'] = 'Neues Fenster';
 $lang['g002_mode_current_window'] = 'Aktuelle Fenster';
-$lang['g002_addlink'] = 'Link hinzufügen';
+$lang['g002_addlink'] = 'Link hinzufÃ¼gen';
 $lang['g002_label'] = 'Wortlaut';
 $lang['g002_url'] = 'URL';
 $lang['g002_mode'] = 'Mode';
-$lang['g002_icon'] = 'Repräsentatives Bild';
+$lang['g002_icon'] = 'ReprÃ¤sentatives Bild';
 $lang['g002_visible'] = 'Sichtbare';
 $lang['g002_linkslist'] = "Links Liste";
-$lang['g002_confirm_delete_link'] = "Link Löschen?";
+$lang['g002_confirm_delete_link'] = "Link LÃ¶schen?";
 $lang['g002_nolinks'] = 'Keine Links';
 $lang['g002_link'] = 'Link';
 $lang['g002_links'] = 'Links';
 
-$lang['g002_createoflink'] = 'Link hinzufügen';
+$lang['g002_createoflink'] = 'Link hinzufÃ¼gen';
 $lang['g002_editoflink'] = 'Link Bearbeiten';
-$lang['g002_createthelink'] = 'Link hinzufügen';
+$lang['g002_createthelink'] = 'Link hinzufÃ¼gen';
 $lang['g002_editthelink'] = 'Link Bearbeiten';
 
 $lang['g002_configlinks'] = 'Links Einstellungen';
 $lang['g002_setting_link_links']='Links';
-$lang['g002_setting_link_show_icon'] = 'Anzeige Links repräsentatives Bild';
+$lang['g002_setting_link_show_icon'] = 'Anzeige Links reprÃ¤sentatives Bild';
 
-$lang['g002_setting_block_menu'] = 'Menü Integration';
-$lang['g002_setting_block_active'] = 'Anzeigen der Abschnitt in dem Menü';
-$lang['g002_setting_block_title'] = 'Titel des Abschnitts in dem Menü';
+$lang['g002_setting_block_menu'] = 'MenÃ¼ Integration';
+$lang['g002_setting_block_active'] = 'Anzeigen der Abschnitt in dem MenÃ¼';
+$lang['g002_setting_block_title'] = 'Titel des Abschnitts in dem MenÃ¼';
 $lang['g002_apply'] = 'Anwenden';
 
-$lang['g002_sectionslist'] = 'Menü-Abschnitte';
+$lang['g002_sectionslist'] = 'MenÃ¼-Abschnitte';
 
 $lang['g002_yesno_y'] = 'Ja';
 $lang['g002_yesno_n'] = 'Nein';
 
-$lang['g002_owner'] = 'Eigentümer';
+$lang['g002_owner'] = 'EigentÃ¼mer';
 $lang['g002_sectionid'] = 'Benutzername';
 $lang['g002_name'] = 'Wortlaut';
 
-$lang['g002_translate'] = 'Übersetzen mit Google Translate';
+$lang['g002_translate'] = 'Ãœbersetzen mit Google Translate';
 
 $lang['g002_configrandompic'] = 'Konfiguration des Zufallsbild';
 $lang['g002_setting_randompic_aboutpicture'] = 'Angezeigt Daten';
 $lang['g002_setting_randompic_showname'] = 'Anzuzeigen des Bildes Namen';
 $lang['g002_setting_randompic_showcomment'] = 'Bild Kommentar ansehen';
 $lang['g002_show_n'] = 'Nein';
-$lang['g002_show_o'] = 'Anzeige über das Bild';
+$lang['g002_show_o'] = 'Anzeige Ã¼ber das Bild';
 $lang['g002_show_u'] = 'Anzeige unter das Bild';
 
 $lang['g002_personalisedlist'] = "Liste der personalisierten Abschnitte";
@@ -71,9 +71,9 @@ $lang['g002_title'] = 'Titel';
 $lang['g002_sections'] = 'Abschnitte';
 $lang['g002_section'] = 'Abschnitt';
 $lang['g002_nosections'] = 'Kein Abschnitte';
-$lang['g002_addsection'] = 'Hinzufügen neue Abschnitt';
+$lang['g002_addsection'] = 'HinzufÃ¼gen neue Abschnitt';
 $lang['g002_createofpersonalised'] = 'Erstellen eine neue personalisierte Abschnitt';
-$lang['g002_editofpersonalised'] = 'Ändern eine personalisierte Abschnitt';
+$lang['g002_editofpersonalised'] = 'Ã„ndern eine personalisierte Abschnitt';
 $lang['g002_setting_block_langchoice'] = 'Sprachwahl';
 $lang['g002_setting_personalised_content'] = 'Inhalt des Abschnitts';
 $lang['g002_setting_personalised_properties'] = 'Eigenschaften';
@@ -81,9 +81,9 @@ $lang['g002_setting_personalised_nfo'] = 'Beschreibung';
 
 $lang['g002_all_languages'] = "Alle Sprachen";
 
-$lang['g002_modmenu'] = '\'Menü\' Abschnitt';
+$lang['g002_modmenu'] = '\'MenÃ¼\' Abschnitt';
 $lang['g002_modspecial'] = '\'speciale\' Abschnitt';
-$lang['g002_labelmenu'] = 'Menü Wortlaut';
+$lang['g002_labelmenu'] = 'MenÃ¼ Wortlaut';
 
 
 ?>
--- plugins/LocalFilesEditor/language/de_DE/plugin.lang.php
+++ plugins/LocalFilesEditor/language/de_DE/plugin.lang.php
@@ -25,21 +25,21 @@ $lang['locfiledit_onglet_localconf'] = 'Lokale Konfiguration';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_tpl'] = 'Vorlagen';
 $lang['locfiledit_onglet_lang'] = 'Sprachen';
-$lang['locfiledit_onglet_plug'] = 'Persönliche Plugin';
-$lang['locfiledit_cant_save'] = 'Aktuelle Datei ist nicht änderbar. Prüfen Sie ob ein Verzeichnis "include/" ist änderbar (chmod)';
+$lang['locfiledit_onglet_plug'] = 'PersÃ¶nliche Plugin';
+$lang['locfiledit_cant_save'] = 'Aktuelle Datei ist nicht Ã¤nderbar. PrÃ¼fen Sie ob ein Verzeichnis "include/" ist Ã¤nderbar (chmod)';
 $lang['locfiledit_newfile'] = 'Die Datei ist nicht vorhanden und wird bei der Registrierung von localfile Editor';
 $lang['locfiledit_save_config'] = 'Datei geschrieben erfolgreich.';
 $lang['locfiledit_show_default'] = 'Anzeige Referenz-Datei: ';
 $lang['locfiledit_save_bak'] = 'Backup-Kopie wird auf Speichern.';
 $lang['locfiledit_saved_bak'] = 'Backup-Datei erstellt (%s)';
 $lang['locfiledit_save_file'] = 'Datei speichern';
-$lang['locfiledit_choose_file'] = 'Wählen die Datei auf der bearbeitet werden';
+$lang['locfiledit_choose_file'] = 'WÃ¤hlen die Datei auf der bearbeitet werden';
 $lang['locfiledit_edit'] = 'Bearbeiten';
 $lang['locfiledit_restore'] = 'Wiederherstellung der Backup-Datei';
-$lang['locfiledit_restore_confirm'] = 'Bitte bestätigen?
-Wiederherstellung wird nicht wirksam sein bis zum nächsten speichern.';
+$lang['locfiledit_restore_confirm'] = 'Bitte bestÃ¤tigen?
+Wiederherstellung wird nicht wirksam sein bis zum nÃ¤chsten speichern.';
 $lang['locfiledit_bak_loaded1'] = 'Backup-Datei geladen werden.';
-$lang['locfiledit_bak_loaded2'] = 'Sie müssen Datei speichern zur Wiederherstellung es.';
+$lang['locfiledit_bak_loaded2'] = 'Sie mÃ¼ssen Datei speichern zur Wiederherstellung es.';
 $lang['locfiledit_syntax_error'] = 'Syntax-Fehler! Datei kann nicht gespeichert werden.';
 $lang['locfiledit_enable_editarea'] = 'Aktivieren Bearbeiten Bereich';
 $lang['locfiledit_disable_editarea'] = 'Deaktivieren Bearbeiten Bereich';
@@ -50,6 +50,6 @@ $lang['locfiledit_empty_page'] = 'Leere Seite';
 $lang['locfiledit_parent_directory'] = 'Eltern-Verzeichnis';
 $lang['locfiledit_filename_error'] = 'Verboten Zeichen in Dateinamen.';
 $lang['locfiledit_file_already_exists'] = 'Datei ist bereits vorhanden.';
-$lang['locfiledit_model_error'] = 'Sie müssen ein Modell wählen.';
+$lang['locfiledit_model_error'] = 'Sie mÃ¼ssen ein Modell wÃ¤hlen.';
 
 ?>
