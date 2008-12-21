9330d8df50892938f4da44743588a958e83298c1
plegall <plg@piwigo.org>
2008-12-21 20:45:12 +0000

    modification: instructions are displayed by blocks that can be expanded individually. The icon doesn't fit with roma theme currently.
    git-svn-id: http://piwigo.org/svn/branches/2.0@3009 68402e56-0260-453c-a942-63ccdbb3a9ee

--- template-common/scripts.js
+++ template-common/scripts.js
@@ -90,6 +90,24 @@ function PwgWS(urlRoot)
 	};
 };
 
+function blockToggleDisplay(headerId, contentId)
+{
+  var revHeader = document.getElementById(headerId);
+  var revContent = document.getElementById(contentId);
+
+  if (revContent.style.display == 'none')
+  {
+    revContent.style.display = 'block';
+    revHeader.className = 'instructionBlockHeaderExpanded';
+  }
+  else
+  {
+    revContent.style.display = 'none';
+    revHeader.className = 'instructionBlockHeaderCollapsed';
+  }
+}
+
+
 PwgWS.prototype = {
 
 	callService : function(method, parameters, options)
