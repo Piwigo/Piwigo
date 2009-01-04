a4a103e9459dac2dd4baa5506149e140e61defb6
plegall <plg@piwigo.org>
2009-01-04 23:19:12 +0000

    bug fixed: convert some german language files from iso to utf-8.
    git-svn-id: http://piwigo.org/svn/branches/2.0@3047 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/install.lang.php
+++ language/de_DE/install.lang.php
@@ -32,21 +32,21 @@ $lang['reg_err_mail_address'] = 'Die E-Mail-Adresse muss in der Form xxx@yyy.eee
 $lang['install_webmaster'] = 'Administrator';
 $lang['install_webmaster_info'] = 'Benutzername des Administrators';
 
-$lang['step1_confirmation'] = 'Die Parameter sind korrekt ausgefüllt';
+$lang['step1_confirmation'] = 'Die Parameter sind korrekt ausgefÃ¼llt';
 $lang['step1_err_db'] = 'Die Verbindung zum Server ist OK, aber nicht die Verbindung zu dieser Datenbank';
 $lang['step1_err_server'] = 'Es konnte keine Verbindung zum Datenbankserver aufgebaut werden';
 
 $lang['step1_host'] = 'MySQL Host';
 $lang['step1_host_info'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
 $lang['step1_user'] = 'Benutzer';
-$lang['step1_user_info'] = 'Benutzernamen für die MySQL Datenbank';
+$lang['step1_user_info'] = 'Benutzernamen fÃ¼r die MySQL Datenbank';
 $lang['step1_pass'] = 'Passwort';
 $lang['step1_pass_info'] = 'das von Ihrem Hosting-Provider';
 $lang['step1_database'] = 'Name der Datenbank';
-$lang['step1_database_info'] = 'Passwort für die MySQL Datenbank';
+$lang['step1_database_info'] = 'Passwort fÃ¼r die MySQL Datenbank';
 $lang['step1_prefix'] = 'Vorwahl Tabellen';
 $lang['step1_prefix_info'] = 'die Namen der Tabellen mit diesem Pr&auml;fix (erm&ouml;glicht eine bessere Verwaltung der Datenbank)';
-$lang['step2_err_login1'] = 'gib bitte einen Benutzernamen für den Webmaster an';
+$lang['step2_err_login1'] = 'gib bitte einen Benutzernamen fÃ¼r den Webmaster an';
 $lang['step2_err_login3'] = 'der Benutzername des Webmasters darf nicht die Zeichen \' und " enthalten';
 $lang['step2_err_pass'] = 'Bitte w&auml;hlen Sie ein Passwort';
 $lang['install_end_title'] = 'Installation abgeschlossen';
@@ -54,11 +54,11 @@ $lang['step2_pwd'] = 'Passwort';
 $lang['step2_pwd_info'] = 'Administratorpasswort';
 $lang['step2_pwd_conf'] = 'Passwort [Best&auml;tigung]';
 $lang['step2_pwd_conf_info'] = 'Wiederholen Sie das eingegebene Passwort';
-$lang['step1_err_copy'] = 'Kopieren Sie den rosa Text ohne die Bindestriche und fügen Sie ihn in die Datei "include / mysql.inc.php" auf dem Webserver ein (Warnung: die Datei "mysql.inc.php" darf nur die rosa Zeichen enthalten, nicht mehr und nicht weniger)';
+$lang['step1_err_copy'] = 'Kopieren Sie den rosa Text ohne die Bindestriche und fÃ¼gen Sie ihn in die Datei "include / mysql.inc.php" auf dem Webserver ein (Warnung: die Datei "mysql.inc.php" darf nur die rosa Zeichen enthalten, nicht mehr und nicht weniger)';
 $lang['install_help'] = 'Brauchen Sie Hilfe? Stellen Sie Ihre Frage auf der <a href="%s"> Forum Piwigo </ a>.';
 $lang['install_end_message'] = 'Die Konfiguration der Piwigo abgeschlossen ist, hier ist der n&auml;chste Schritt<br /><br />
-* Gehen Sie zum Anmelden auf die Startseite: [ <a href="./identification.php">Identifizierung</a> ] und verwenden Sie die Login / Passwort für Webmaster<br />
+* Gehen Sie zum Anmelden auf die Startseite: [ <a href="./identification.php">Identifizierung</a> ] und verwenden Sie die Login / Passwort fÃ¼r Webmaster<br />
 * diesem Login erm&ouml;glicht Ihnen den Zugang zu den Verwaltungs-Panel und der Bilder- und Benutzerverwaltung.';
 $lang['conf_mail_webmaster'] = 'Webmaster Mail-Adresse';
-$lang['conf_mail_webmaster_info'] = 'Kontakt E-Mailadresse (nur für angemeldete Benutzer sichtbar)';
+$lang['conf_mail_webmaster_info'] = 'Kontakt E-Mailadresse (nur fÃ¼r angemeldete Benutzer sichtbar)';
 ?>
