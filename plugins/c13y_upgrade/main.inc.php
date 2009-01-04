aeaffa61bd895a00f131a5f9e4738d39a3b8a8ed
plegall <plg@piwigo.org>
2009-01-04 23:09:15 +0000

    Administration: happy new year 2009, all PHP headers updated.
    git-svn-id: http://piwigo.org/svn/branches/2.0@3046 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/c13y_upgrade/main.inc.php
+++ plugins/c13y_upgrade/main.inc.php
@@ -2,8 +2,8 @@
 // +-----------------------------------------------------------------------+
 // | Piwigo - a PHP based picture gallery                                  |
 // +-----------------------------------------------------------------------+
-// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
-// | Copyright(C) 2003-2008 Piwigo team    http://phpwebgallery.net |
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery team    http://phpwebgallery.net |
 // | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
 // +-----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or modify  |
