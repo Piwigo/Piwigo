1b711ad2365b47cb1f1969cad2195c180ad62609
plegall <plg@piwigo.org>
2009-01-04 23:28:36 +0000

    Administration: happy new year 2009, all PHP headers updated.
    git-svn-id: http://piwigo.org/svn/trunk@3049 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/c13y_upgrade/language/it_IT/plugin.lang.php
+++ plugins/c13y_upgrade/language/it_IT/plugin.lang.php
@@ -2,7 +2,7 @@
 // +-----------------------------------------------------------------------+
 // | Piwigo - a PHP based picture gallery                                  |
 // +-----------------------------------------------------------------------+
-// | Copyright(C) 2008      Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
 // | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
 // | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
 // +-----------------------------------------------------------------------+
