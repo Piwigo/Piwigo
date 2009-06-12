811923c0d48db15f1afd1998b157ab7fb708eedf
riopwg <riopwg@piwigo.org>
2009-06-12 11:40:54 +0000

    merge r3413 from branch 2.0 to trunk Update lang IT
    git-svn-id: http://piwigo.org/svn/trunk@3414 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/it_IT/plugin.lang.php
+++ plugins/LocalFilesEditor/language/it_IT/plugin.lang.php
@@ -51,6 +51,10 @@ $lang['locfiledit_parent_directory'] = 'Directory parente';
 $lang['locfiledit_filename_error'] = 'Caratteri vietati nel nome del file.';
 $lang['locfiledit_file_already_exists'] = 'Il file esiste già.';
 $lang['locfiledit_model_error'] = 'Selezionare un modello.';
+<<<<<<< .courant
 $lang['locfiledit_empty_filename'] = 'Indicate un nome di file.';
 
+=======
+$lang['locfiledit_empty_filename'] = 'Indicate un nome di file.';
+>>>>>>> .fusion-droit.r3413
 ?>
