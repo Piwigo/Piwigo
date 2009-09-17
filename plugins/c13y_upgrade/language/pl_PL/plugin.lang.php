af1237c9ca14c3540e408d7966923cbdbfdc0a5b
ddtddt <ddtddt@piwigo.org>
2009-09-17 22:17:03 +0000

    Add PL add_c13y_upgrade plugin to Branche 2 thanks to emcek
    git-svn-id: http://piwigo.org/svn/branches/2.0@3862 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/pl_PL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = 'Nie wykryto żadnych problemów po aktualizacji aplikacji, wtyczka "Sprawdź aktualizacje" została wyłączona.';
+$lang['c13y_dbl_email_user'] = 'Adresy e-mail użytkowników są identyczne';
+$lang['c13y_correction_dbl_email_user'] = 'Usuwanie powtórzonych nazw użytkowników';
+$lang['c13y_obsolete_plugin'] = 'Wtyczka jest nieaktualna';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" wtyczka została dołączona do tej wersji aplikacji i musisz ją odinstalować.';
+?>
