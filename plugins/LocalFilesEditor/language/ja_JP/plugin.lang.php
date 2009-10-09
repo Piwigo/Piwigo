74a8594c4252c15a7c6ba2f45f2fc54b06f53ed5
ddtddt <ddtddt@piwigo.org>
2009-10-09 07:35:01 +0000

    [branche2] Add ja_JP LocalFilesEditor language thanks to Mits
    git-svn-id: http://piwigo.org/svn/branches/2.0@4000 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/ja_JP/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'ローカル設定';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'テンプレート';
+$lang['locfiledit_onglet_lang'] = '言語';
+$lang['locfiledit_onglet_plug'] = 'パーソナルプラグイン';
+$lang['locfiledit_cant_save'] = '現在、ファイルを書き込むことができません。ディレクトリ「include/」に書込み権があるかどうか確認してください (chmod)。';
+$lang['locfiledit_newfile'] = 'ファイルが存在していないため、ローカルファイルエディタにより作成されます。';
+$lang['locfiledit_save_config'] = 'ファイルが正常に書き込まれました。';
+$lang['locfiledit_show_default'] = '参照ファイルの表示: ';
+$lang['locfiledit_save_bak'] = '保存するためのバックアップコピーが作成されました。';
+$lang['locfiledit_saved_bak'] = 'バックアップファイルが作成されました (%s)';
+$lang['locfiledit_save_file'] = 'ファイルを保存する';
+$lang['locfiledit_choose_file'] = '編集するファイルを選択してください。';
+$lang['locfiledit_edit'] = '編集';
+$lang['locfiledit_restore'] = 'バックアップファイルをリストアする';
+$lang['locfiledit_restore_confirm'] = '次の保存までリストアは有効になりません。';
+$lang['locfiledit_bak_loaded1'] = 'バックアップファイルが読み込まれました。';
+$lang['locfiledit_bak_loaded2'] = 'リストアするファイルを選択してください。';
+$lang['locfiledit_syntax_error'] = '構文エラー! ファイルを保存することができません。';
+$lang['locfiledit_enable_editarea'] = 'Editareaを有効にする';
+$lang['locfiledit_disable_editarea'] = 'Editareaを無効にする';
+$lang['locfiledit_new_tpl'] = '新しいファイルを作成する';
+$lang['locfiledit_new_filename'] = 'ファイル名';
+$lang['locfiledit_model'] = 'モデル';
+$lang['locfiledit_empty_page'] = '空のページ';
+$lang['locfiledit_parent_directory'] = '親ディレクトリ';
+$lang['locfiledit_filename_error'] = 'ファイル名に使用できない文字が使われています。';
+$lang['locfiledit_file_already_exists'] = 'ファイルがすでに存在しています。';
+$lang['locfiledit_model_error'] = 'モデルを選択してください。';
+$lang['locfiledit_empty_filename'] = 'ファイル名を入力してください。';
+
+?>
