f7788450b85f8be06dc4670a2410576db6408d6d
rub <rub@piwigo.org>
2009-12-21 15:11:35 +0000

    Feature 1255: modification in sql queries correction
    git-svn-id: http://piwigo.org/svn/trunk@4542 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/functions_notification.inc.php
+++ include/functions_notification.inc.php
@@ -463,7 +463,7 @@ SELECT DISTINCT id, path, name, tn_ext, file
   '.$where_sql.'
     AND date_available="'.$dates[$i]['date_available'].'"
     AND tn_ext IS NOT NULL
-  ORDER BY '.DB_RANDOM_FUNCTION.'())
+  ORDER BY '.DB_RANDOM_FUNCTION.'()
   LIMIT '.$max_elements.'
 ;';
       $dates[$i]['elements'] = array();
