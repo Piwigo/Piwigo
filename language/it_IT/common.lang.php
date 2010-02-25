c952f5fc71c4d4f424fd6da6db7eece8557767f1
riopwg <riopwg@piwigo.org>
2010-02-25 07:06:39 +0000

    Merge r4963 from branch 2.0 to trunk Update language IT
    git-svn-id: http://piwigo.org/svn/trunk@4964 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/common.lang.php
+++ language/it_IT/common.lang.php
@@ -368,4 +368,5 @@ $lang['cloud'] = 'nuvola';
 $lang['Reset_To_Default'] = 'Ripristinare le impostazioni predefinite';
 $lang['del_all_favorites_hint'] = 'supprimer toutes les images de vos favoris';
 $lang['Sent by'] = 'Envoyé par';
+$lang['Cookies are blocked or not supported by your browser. You must enable cookies to connect.'] = 'I cookies sono bloccati o non sopportati dal vostro browser. Dovete attivare i cookie per connettervi.';
 ?>
