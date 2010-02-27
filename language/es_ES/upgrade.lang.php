981b7c72c27d49f6c2209da27d138e96a02eef6f
jpr928 <jpr928@piwigo.org>
2010-02-27 09:59:49 +0000

    git-svn-id: http://piwigo.org/svn/branches/2.0@4976 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/upgrade.lang.php
+++ language/es_ES/upgrade.lang.php
@@ -21,24 +21,24 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 
-$lang['Upgrade'] = 'Puesta al día';
-$lang['introduction message'] = 'Esta página le propone poner al día la base de dato correspondiente a su antigua versión de piwigo hacia la nueva versión.
-El asistente de postura al día piensa en la versión actual es un <strong> versión %s </strong> (o equivalente). ';
-$lang['Upgrade from %s to %s'] = 'Puesta al día por la versión %s a %s';
+$lang['Upgrade'] = 'Actualización';
+$lang['introduction message'] = 'Esta página le propone actualizar la base de dato de su antigua versión de piwigo hacia la nueva versión.
+El asistente de actualización piensa que la versión actual es la <strong> versión %s </strong> (o equivalente). ';
+$lang['Upgrade from %s to %s'] = 'Actualización de la versión %s a %s';
 $lang['Statistics'] = 'Estadísticas';
-$lang['total upgrade time'] = 'tiempo total de la puesta al día';
+$lang['total upgrade time'] = 'tiempo total de la actualización';
 $lang['total SQL time'] = 'tiempo total de la demanda SQL';
 $lang['SQL queries'] = 'número de demanda SQL';
-$lang['Upgrade informations'] = 'Informaciones sobre la puesta al día';
+$lang['Upgrade informations'] = 'Informaciones sobre la actualización';
 $lang['perform a maintenance check'] = 'Por favor, efectúes un mantenimiento en [Administración>Especiales>Mantenimiento] si usted encuentra problemas.';
-$lang['deactivated plugins'] = 'Por precaución, el plugins siguiente han sido desactivados. Verifique si existen unas posturas al día antes de reactivarlas:';
-$lang['upgrade login message'] = 'Sólo un administrador puede lanzar la puesta al día: por favor, identifiqúese más abajo.';
-$lang['You do not have access rights to run upgrade'] = 'Usted no tiene los derechos necessaires para lanzar la puesta al día.';
+$lang['deactivated plugins'] = 'Por precaución, el plugins siguiente han sido desactivados. Verifique si existen unas actualización antes de reactivarlos:';
+$lang['upgrade login message'] = 'Sólo un administrador puede lanzar la actualización: por favor, identifiqúese más abajo.';
+$lang['You do not have access rights to run upgrade'] = 'Usted no tiene los derechos necessaires para lanzar la actualización.';
 
 // Upgrade informations from upgrade_1.3.1.php
 $lang['all sub-categories of private categories become private'] = 'Todas las subcategorías de categorías privadas se vuelven privadas';
-$lang['user permissions and group permissions have been erased'] = 'Los autorizaciones de los usuarios y de los grupos han sido borrados';
-$lang['only thumbnails prefix and webmaster mail saved'] = 'Sólo el prefijo de las miniaturas y el email electrónico del webmestre han sido salvaguardados por la configuración precedente';
+$lang['user permissions and group permissions have been erased'] = 'Las autorizaciones de los usuarios y de los grupos han sido borradas';
+$lang['only thumbnails prefix and webmaster mail saved'] = 'Sólo el prefijo de las miniaturas y el email  del webmestre han sido salvaguardados por la configuración precedente';
 
 $lang['in include/mysql.inc.php, before ?>, insert:'] = 'En el fichero <i>include/mysql.inc.php</i>, antes <b>?></b>,  inserte:';
 
