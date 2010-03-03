ef00827dbcfa4ee27ae4b85310ccea85cf9b98dd
nikrou <nikrou@piwigo.org>
2010-03-03 20:55:21 +0000

    Feature 1451 : confirm message when managing plugins change way to manage zero for plural forms in french
    git-svn-id: http://piwigo.org/svn/trunk@5039 68402e56-0260-453c-a942-63ccdbb3a9ee

--- admin/plugins_update.php
+++ admin/plugins_update.php
@@ -87,7 +87,7 @@ if (isset($_GET['upgradestatus']) and isset($_GET['plugin']))
     default:
       array_push($page['errors'],
         sprintf(l10n('An error occured during extraction (%s).'), $_GET['upgradestatus']),
-        l10n('Please check \"plugins\" folder and sub-folders permissions (CHMOD).'));
+        l10n('Please check "plugins" folder and sub-folders permissions (CHMOD).'));
   }  
 }
 
