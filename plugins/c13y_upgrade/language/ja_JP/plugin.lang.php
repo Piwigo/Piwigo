b31a5f78b3b72f8a36a2985c44f3554249e06bba
ddtddt <ddtddt@piwigo.org>
2010-03-25 12:50:09 +0000

    [trunk] add ja_JP for plugin - c13y_upgrade - thanks to irohaproject
    git-svn-id: http://piwigo.org/svn/trunk@5345 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/ja_JP/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['c13y_upgrade_no_anomaly'] = 'アプリケーション・アップグレード後、異常なし、「アップグレード確認」のプラグインは無効化された';
+$lang['c13y_dbl_email_user'] = '同じメールアドレスがあるユーザー';
+$lang['c13y_correction_dbl_email_user'] = '複数のユーザーを削除';
+$lang['c13y_obsolete_plugin'] = '旧プラグイン';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" のプラグインはこのアプリケーションのバージョンに含まれており、アンインストールする必要があります。';
+
+?>
