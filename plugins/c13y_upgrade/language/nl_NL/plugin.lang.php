69e45b415f5948bbeec29d1ea7cf8a4a157e26dd
ddtddt <ddtddt@piwigo.org>
2010-03-26 18:04:59 +0000

    [trunk] add nl_NL for plugins - c13y_upgrade - thanks to Kees Hessels
    git-svn-id: http://piwigo.org/svn/trunk@5389 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/nl_NL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['c13y_upgrade_no_anomaly'] = 'Geen afwijkingen geconstateerd na de applicatie-upgrade, de "Controleer upgrades" plugin is gedeactiveerd.';
+$lang['c13y_dbl_email_user'] = 'Gebruikers met hetzelfde email-adres';
+$lang['c13y_correction_dbl_email_user'] = 'Verwijder dubbele gebruikers';
+$lang['c13y_obsolete_plugin'] = 'Verouderde plugin';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" plugin is opgenomen in deze versie. U dient de plugin te verwijderen.';
+
+?>
