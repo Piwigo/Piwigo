e28ab6288837dbb2b868f47b4184f31c32cb68c2
nanticpwg <nanticpwg@piwigo.org>
2010-03-27 10:04:15 +0000

    more fixes
    git-svn-id: http://piwigo.org/svn/trunk@5401 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hr_HR/install.lang.php
+++ language/hr_HR/install.lang.php
@@ -30,37 +30,37 @@ $lang['Start Install'] = 'Pokreni ugradbu';
 $lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'e-mail adresa mora biti kao xxx@yyy.eee (example : jere@stranica.org)';
 
 $lang['Webmaster login'] = 'Webmaster-ova prijava';
-$lang['It will be shown to the visitors. It is necessary for website administration'] = 'Biti æe prikazano posjetiteljima. Potrebno je za upravljanje web mjestom';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'Biti će prikazano posjetiteljima. Potrebno je za upravljanje web mjestom';
 
 $lang['Parameters are correct'] = 'Postavke su ispravne';
-$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Povezivanje sa poslužiteljem uspješno, ali je nemoguæe povezivanje sa bazom podataka';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Povezivanje sa poslužiteljem uspješno, ali je nemoguće povezivanje sa bazom podataka';
 $lang['Can\'t connect to server'] = 'Ne mogu se povezati sa poslužiteljem';
-$lang['The next step of the installation is now possible'] = 'Slijedeæi korak ugradbe je omoguæen';
-$lang['next step'] = 'slijedeæi korak';
-$lang['Copy the text in pink between hyphens and paste it into the file "local/config/database.inc.php"(Warning : database.inc.php must only contain what is in pink, no line return or space character)'] = 'Kopirajte ružièasti tekst izmeðu crtica i prebacite ga u "include/mysql.inc.php"(Pozor : mysql.inc.php mora sadržavati samo ružièasti tekst, bez znakova novog reda ili razmaka)';
+$lang['The next step of the installation is now possible'] = 'Slijedeći korak ugradbe je omogućen';
+$lang['next step'] = 'slijedeći korak';
+$lang['Copy the text in pink between hyphens and paste it into the file "local/config/database.inc.php"(Warning : database.inc.php must only contain what is in pink, no line return or space character)'] = 'Kopirajte ružičasti tekst između crtica i prebacite ga u "include/mysql.inc.php"(Pozor : mysql.inc.php mora sadržavati samo ružičasti tekst, bez znakova novog reda ili razmaka)';
 
 $lang['Host'] = 'MySQL poslužitelj';
 $lang['localhost, sql.multimania.com, toto.freesurf.fr'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
 $lang['User'] = 'Korisnik';
-$lang['user login given by your host provider'] = 'korisnièko ime dobiveno od davatelja usluge smještaja';
+$lang['user login given by your host provider'] = 'korisničko ime dobiveno od davatelja usluge smještaja';
 $lang['Password'] = 'Zaporka';
-$lang['user password given by your host provider'] = 'korisnièka zaporka dobivena od davatelja usluge smještaja';
+$lang['user password given by your host provider'] = 'korisnička zaporka dobivena od davatelja usluge smještaja';
 $lang['Database name'] = 'Naziv baze podataka';
-$lang['also given by your host provider'] = 'takoðer dobivena od davatelja usluge smještaja';
+$lang['also given by your host provider'] = 'također dobivena od davatelja usluge smještaja';
 $lang['Database table prefix'] = 'Predznak tabela baze podataka';
-$lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'nazivi tabela baze podataka æe biti predznaèeni sa time (omoguæava vam bolje upravljanje tabelama)';
-$lang['enter a login for webmaster'] = 'upišite korisnièko ime za webmaster-a';
-$lang['webmaster login can\'t contain characters \' or "'] = 'webmaster-ovo korisnièko ime ne može sadržavati znakove \' ili "';
+$lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'nazivi tabela baze podataka će biti predznačeni sa time (omogućava vam bolje upravljanje tabelama)';
+$lang['enter a login for webmaster'] = 'upišite korisničko ime za webmaster-a';
+$lang['webmaster login can\'t contain characters \' or "'] = 'webmaster-ovo korisničko ime ne može sadržavati znakove \' ili "';
 $lang['please enter your password again'] = 'molimo upišite zaporku ponovo';
 $lang['Installation finished'] = 'Ugradba završena';
 $lang['Webmaster password'] = 'Webmaster-ova zaporka';
-$lang['Keep it confidential, it enables you to access administration panel'] = 'Èuvajte je na sigurnom mjestu, ona omoguæava pristup upravnoj ploèi';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'Čuvajte je na sigurnom mjestu, ona omogućava pristup upravnoj ploči';
 $lang['Password [confirm]'] = 'Zaporka [potvrdi]';
 $lang['verification'] = 'ovjera';
-$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'Trebate pomoæ? Pitajte na <a href="%s">Piwigo message board</a>.';
-$lang['install_end_message'] = 'Postava Piwigo-a je završena, slijedeæi korak je<br /><br />
-* idite na prijavnicu i koristite korisnièko ime/zaporku danu za webmaster-a<br />
-* ova prijava æe vam omoguæiti pristup upravnoj ploèi i uputama za pohranu slika u mape';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'Trebate pomoć? Pitajte na <a href="%s">Piwigo message board</a>.';
+$lang['install_end_message'] = 'Postava Piwigo-a je završena, slijedeći korak je<br /><br />
+* idite na prijavnicu i koristite korisničko ime/zaporku danu za webmaster-a<br />
+* ova prijava će vam omogućiti pristup upravnoj ploči i uputama za pohranu slika u mape';
 $lang['Webmaster mail address'] = 'Webmaster-ova e-mail adresa';
-$lang['Visitors will be able to contact site administrator with this mail'] = 'Posjetitelji æe moæi pisati upravitelju galerije preko ove adrese';
+$lang['Visitors will be able to contact site administrator with this mail'] = 'Posjetitelji će moći pisati upravitelju galerije preko ove adrese';
 ?>
