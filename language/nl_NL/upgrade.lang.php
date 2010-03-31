c8bbbb34055d57435ed44757bd69815aba1887c7
keeshessels <keeshessels@piwigo.org>
2010-03-31 20:09:54 +0000

    Added nl_NL admin.lang.php // missing translations
    git-svn-id: http://piwigo.org/svn/trunk@5508 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/upgrade.lang.php
+++ language/nl_NL/upgrade.lang.php
@@ -37,7 +37,7 @@ $lang['Only administrator can run upgrade: please sign in below.'] = 'Alleen de 
 $lang['You do not have access rights to run upgrade'] = 'U bent niet gerechtigd om een upgrade uit te voeren';
 $lang['All sub-categories of private categories become private'] = 'Alle sub-categorieën van private categorieën worden private';
 $lang['User permissions and group permissions have been erased'] = 'Gebruikers- en groepsrechten zijn verwijderd';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Alleen de mini-afbeelding-prefix en het email-adres van de webbeheerder zijn bewaard gebleven van de vorige configuratie';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Alleen het voorvoegsel (prefix) van de mini-afbeelding en het email-adres van de webbeheerder zijn bewaard gebleven van de vorige configuratie';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'In <i>%s</i>, voor <b>?></b>, voeg toe:';
 
 
