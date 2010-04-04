960affd0d4d7b44f6bd84f648f590eb84e6bf9ef
ddtddt <ddtddt@piwigo.org>
2010-04-04 06:36:49 +0000

    [trunk] update translation_analysis.php for translator
    git-svn-id: http://piwigo.org/svn/trunk@5622 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/translation_analysis.php
+++ tools/translation_analysis.php
@@ -23,7 +23,7 @@
 
 define('PHPWG_ROOT_PATH', '../');
 include_once( PHPWG_ROOT_PATH.'include/common.inc.php' );
-include_once( PHPWG_ROOT_PATH.'tools/translation_validated.inc.php' );
+include_once( PHPWG_ROOT_PATH.'tools/language/translation_validated.inc.php' );
 $languages = array_keys(get_languages());
 sort($languages);
 
