897b1bb33d5dd302466464b7ca67bc056a9583b4
plegall <plg@piwigo.org>
2010-04-29 22:57:47 +0000

    elease number 2.1.0RC3 hard coded
    git-svn-id: http://piwigo.org/svn/tags/2.1.0RC3@6017 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', 'Colibri');
+define('PHPWG_VERSION', '2.1.0RC3');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'Sylvia');
 
