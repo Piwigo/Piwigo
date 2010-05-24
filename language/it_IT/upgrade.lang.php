49a0af43a3011765b0bdbce0fbde210bf1895ef6
plegall <plg@piwigo.org>
2010-05-24 15:35:39 +0000

    merge r6268 from trunk to branch 2.1
    git-svn-id: http://piwigo.org/svn/branches/2.1@6331 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/upgrade.lang.php
+++ language/it_IT/upgrade.lang.php
@@ -24,20 +24,20 @@
 
 
 $lang['Upgrade'] = 'Aggiornamento';
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Questa pagina vi propone di aggiornare la vostra base dati corrispondente alla vecchia versione di Piwigo verso la nuova versione.
-L\'assistente all\'aggiornamento ha identificato il vostro prodotto attuale come essendo una <strong>versione %s</strong> (o equivalente).';
-$lang['Upgrade from version %s to %s'] = 'Aggiornamento dalla versione %s alla versione %s';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Questa pagina vi propone di aggiornare il vostro database corrispondente alla vecchia versione di Piwigo con la nuova versione.
+L\'assistente all\'aggiornamento ha identificato la vostra attuale <strong>versione %s</strong> (o equivalente).';
+$lang['Upgrade from version %s to %s'] = 'Aggiornamento dalla versione %s alla %s';
 $lang['Statistics'] = 'Statistiche';
-$lang['total upgrade time'] = 'tempo totale per l\'aggiornamento';
-$lang['total SQL time'] = 'tempo totale delle esecuzioni SQL';
-$lang['SQL queries'] = 'numero delle esecuzioni SQL';
-$lang['Upgrade informations'] = 'Informazioni sull\'aggiornamento';
+$lang['total upgrade time'] = 'tempo totale aggiornamento';
+$lang['total SQL time'] = 'tempo totale SQL';
+$lang['SQL queries'] = 'queries SQL';
+$lang['Upgrade informations'] = 'Informazioni aggiornamento';
 $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Se riscontrate dei problemi, effettuare una manutezione in [Amministrazione>Tools>Manutenzione].';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Per precauzione, i plugins sono stati disattivati. Verificate se non esistono degli aggiornamenti prima di riattivarli:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'Solo un\'amministratore può eseguire l\'aggiornamento: identificatevi.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Per precauzione, i plugins seguenti sono stati disattivati. E\' necessario verificare l\'aggiornamento dei plugins prima di riattivarli:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Solo l\'amministratore puÃ² eseguire l\'aggiornamento: fate login.';
 $lang['You do not have access rights to run upgrade'] = 'Non avete le autorizzazioni necessarie per effettuare l\'aggiornamento';
 $lang['All sub-categories of private categories become private'] = 'Tutte le sottocategorie delle categorie private diventono private';
 $lang['User permissions and group permissions have been erased'] = 'I permessi degli utenti e dei gruppi sono stati cancellati';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Solo il prefisso delle miniature e l\'indirizzo Email del webmaster sono stati recuperati dalla precedente configurazione';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Solo il prefisso delle miniature e l\'indirizzo email del webmaster sono stati recuperati dalla precedente configurazione';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'Nel file <i>%s</i>, prima di <b>?></b>, inserite:';
 ?>
