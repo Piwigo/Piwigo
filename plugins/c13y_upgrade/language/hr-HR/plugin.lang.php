d05af1589721e9f48d15d72cc3cc21ed8a5407fc
ddtddt <ddtddt@piwigo.org>
2010-08-07 08:17:04 +0000

    [branche2.1] /plugins/c13y_upgrade Add hr_HR thanks to LuxZg
    git-svn-id: http://piwigo.org/svn/branches/2.1@6744 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/hr-HR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['c13y_upgrade_no_anomaly'] = 'Nisu detektirane anomalije nakon nadogradnje aplikacije, plugin "Check upgrades" je deaktiviran.';
+$lang['c13y_dbl_email_user'] = 'Korisnici s istom e-mail adresom';
+$lang['c13y_correction_dbl_email_user'] = 'Obriši duplicirane korisnike';
+$lang['c13y_obsolete_plugin'] = 'Zastarjeli plugin';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" plugin je uključen u ovoj verziji aplikacije i morate ga deinstalirati.';
+
+?>
