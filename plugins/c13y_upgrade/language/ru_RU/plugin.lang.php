c1af0cdc16c839b1171a3521d540fb519c06be02
ddtddt <ddtddt@piwigo.org>
2010-09-21 06:43:34 +0000

    [branche2.1] [plugin] - admin_multi_view - c13y_upgrade - language_switch  add ka_GE and ru_RU thanks to geomagi
    git-svn-id: http://piwigo.org/svn/branches/2.1@6982 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/ru_RU/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = 'Аномалии не обнаружены после применения обновления. плагин "Проверить обновления" был отключен.';
+$lang['c13y_dbl_email_user'] = 'Пользователи с тем же адресом электронной почты';
+$lang['c13y_correction_dbl_email_user'] = 'Удаление дубликатов пользователей';
+$lang['c13y_obsolete_plugin'] = 'Устаревший плагин';
+$lang['c13y_correction_obsolete_plugin'] = 'Модуль "%s" отключена, потому что она не совместима с этой версией Piwigo . Пожалуйста, удалите его.';
+
+?>
