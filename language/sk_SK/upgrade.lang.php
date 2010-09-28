9450904d6da294f311af054266fcd3885501cb5f
ddtddt <ddtddt@piwigo.org>
2010-09-28 18:57:03 +0000

    [branche2.1] add sk_SK Slovak (Slovensky) thanks to dodo feature:1889
    git-svn-id: http://piwigo.org/svn/branches/2.1@7030 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/upgrade.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// | Czech language localization                                           |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009     Pavel Budka & Petr Jirsa    http://pbudka.co.cc |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Upgrade'] = 'Aktualizácia';
+$lang['Upgrade from version %s to %s'] = 'Aktualizácia z verzie %s na %s';
+$lang['Statistics'] = 'Štatistika';
+$lang['total upgrade time'] = 'celkový čas aktualizácie';
+$lang['total SQL time'] = 'celkový čas SQL';
+$lang['SQL queries'] = 'SQL dotazy';
+$lang['Upgrade informations'] = 'Informácie o aktualizácii';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Preveďte kontrolu údržby v [Administrácia>Nástroje>Údržba] pokiaľ príde k problému.';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Len administrátor môže spustiť aktualizáciu: prosím prihláste sa nížšie.';
+$lang['You do not have access rights to run upgrade'] = 'nemáte prístupové práva na spustenie upgrade';
+
+// Upgrade informations from upgrade_1.3.1.php
+$lang['All sub-categories of private categories become private'] = 'Všetky pod-ketegórie privátnych kategórií sa stanú privátnymi';
+$lang['User permissions and group permissions have been erased'] = 'Používateľské oprávnenia a oprávnenia skupín boli vymazané';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Z predchádzajúcej konfigurárie boli zachované len predpony náhľadov a email adresa správcu.';
+
+// missing translations 2.1.0
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Navrhujeme aktualizovať databázu Vašej galérie Piwigo zo staršej na novú verziu.
+Asistent aktualizácie sa domnieva, že aktuálne používate <strong>verziu %s</strong> (alebo odpovedajúcu).';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Ako predbežné opatrenie boli deaktivované nasledujúce doplnky. Prosím skontrolujte aktualizácie týchto doplnkov predtým než ich znovu aktivujete:';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'V <i>%s</i>, pred <b>?></b>, vložením:';
+
+?>
