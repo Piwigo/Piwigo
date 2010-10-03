95565ae119ce3f3117e3a976741b15bfd9494a7c
ddtddt <ddtddt@piwigo.org>
2010-10-03 18:48:00 +0000

    [trunk] - LocalFilesEditor - Add cs_CZ thanks to webprostor merge 7080
    git-svn-id: http://piwigo.org/svn/trunk@7081 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/cs_CZ/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+//  | plugin LocalFilesEditor ,czech lang by webprostor.eu |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'Lokální konfigurace';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'Šablony';
+$lang['locfiledit_onglet_lang'] = 'Jazyky';
+$lang['locfiledit_onglet_plug'] = 'Vlastní pluginy';
+$lang['locfiledit_cant_save'] = 'Vybraný soubor nelze pøepsat. Zkontrolujte jestli je složka "include/" zapisovatelná (chmod).';
+$lang['locfiledit_newfile'] = 'Soubor neexistuje a bude vytvoøen LocalFiles Editorem';
+$lang['locfiledit_save_config'] = 'Soubor byl zmìnìn.';
+$lang['locfiledit_show_default'] = 'Zobraz referenèní soubor: ';
+$lang['locfiledit_save_bak'] = 'Záloha bude vytvoøena pøi uložení.';
+$lang['locfiledit_saved_bak'] = 'Záloha vytvoøena (%s)';
+$lang['locfiledit_save_file'] = 'Uložit soubor';
+$lang['locfiledit_choose_file'] = 'Vyber soubor k editaci';
+$lang['locfiledit_edit'] = 'Editace';
+$lang['locfiledit_restore'] = 'Obnovit ze zálohy';
+$lang['locfiledit_restore_confirm'] = 'Prosím potvrïte?
+Obnova nebude mít efekt do dalšího uložení.';
+$lang['locfiledit_bak_loaded1'] = 'Záložní soubor nahrán.';
+$lang['locfiledit_bak_loaded2'] = 'Musíte nejprve soubor uložit aby mohl být obnoven.';
+$lang['locfiledit_syntax_error'] = 'Syntax error! Soubor nemùže být uložen.';
+$lang['locfiledit_enable_editarea'] = 'Povol editaci';
+$lang['locfiledit_disable_editarea'] = 'Zakázat editaci';
+$lang['locfiledit_new_tpl'] = 'Vytvoøit nový soubor';
+$lang['locfiledit_new_filename'] = 'Název souboru';
+$lang['locfiledit_model'] = 'Model';
+$lang['locfiledit_empty_page'] = 'Prázdná stránka';
+$lang['locfiledit_parent_directory'] = 'Nadøazená složka';
+$lang['locfiledit_filename_error'] = 'Nepovolené znaky v názvu souboru.';
+$lang['locfiledit_file_already_exists'] = 'Soubor už existuje.';
+$lang['locfiledit_model_error'] = 'Musíte zvolit model.';
+$lang['locfiledit_empty_filename'] = 'Musíte vyplnit název souboru.';
+$lang['locfiledit_webmaster_only'] = 'Pouze webmaster mùže vytváøet nebo mìnit lokální soubory.';
+
+?>
