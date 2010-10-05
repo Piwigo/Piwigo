da3bcf2a6a02496a9bc0d4f1ef413c38e167c0d8
ddtddt <ddtddt@piwigo.org>
2010-10-05 21:28:24 +0000

    [Branche2] - LocalFilesEditor - update cs_CZ thanks to webprostor / test encoding
    git-svn-id: http://piwigo.org/svn/branches/2.1@7113 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/cs_CZ/plugin.lang.php
+++ plugins/LocalFilesEditor/language/cs_CZ/plugin.lang.php
@@ -21,35 +21,37 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
+
 $lang['locfiledit_onglet_localconf'] = 'Lokální konfigurace';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_tpl'] = 'Šablony';
 $lang['locfiledit_onglet_lang'] = 'Jazyky';
 $lang['locfiledit_onglet_plug'] = 'Vlastní pluginy';
-$lang['locfiledit_cant_save'] = 'Vybraný soubor nelze pøepsat.Zkontrolujte jestli je složka "include/" zapisovatelná (chmod).';
-$lang['locfiledit_newfile'] = 'Soubor neexistuje a bude vytvoøen LocalFiles Editorem';
-$lang['locfiledit_save_config'] = 'Soubor byl zmìnìn.';
-$lang['locfiledit_show_default'] = 'Zobraz refereèní soubor: ';
-$lang['locfiledit_save_bak'] = 'Záloha bude vytvoøena pøi uložení.';
-$lang['locfiledit_saved_bak'] = 'Záloha vytvoøena (%s)';
+$lang['locfiledit_cant_save'] = 'Vybraný soubor nelze přepsat.Zkontrolujte jestli je složka "include/" zapisovatelná (chmod).';
+$lang['locfiledit_newfile'] = 'Soubor neexistuje a bude vytvořen LocalFiles Editorem';
+$lang['locfiledit_save_config'] = 'Soubor byl změněn.';
+$lang['locfiledit_show_default'] = 'Zobraz referenční soubor: ';
+$lang['locfiledit_save_bak'] = 'Záloha bude vytvořena při uložení.';
+$lang['locfiledit_saved_bak'] = 'Záloha vytvořena (%s)';
 $lang['locfiledit_save_file'] = 'Uložit soubor';
 $lang['locfiledit_choose_file'] = 'Vyber soubor k editaci';
 $lang['locfiledit_edit'] = 'Editace';
 $lang['locfiledit_restore'] = 'Obnovit ze zálohy';
-$lang['locfiledit_restore_confirm'] = 'Prosím potvrïte?Obnova nebude mít efekt do dalšího uložení.';
+$lang['locfiledit_restore_confirm'] = 'Prosím potvrďte?Obnova nebude mít efekt do dalšího uložení.';
 $lang['locfiledit_bak_loaded1'] = 'Záložní soubor nahrán.';
 $lang['locfiledit_bak_loaded2'] = 'Musíte nejprve soubor uložit aby mohl být obnoven.';
 $lang['locfiledit_syntax_error'] = 'Syntax error! Soubor nemůže být uložen.';
 $lang['locfiledit_enable_editarea'] = 'Povol editaci';
 $lang['locfiledit_disable_editarea'] = 'Zakázat editaci';
-$lang['locfiledit_new_tpl'] = 'Vytvoøit nový soubor';
+$lang['locfiledit_new_tpl'] = 'Vytvořit nový soubor';
 $lang['locfiledit_new_filename'] = 'Název souboru';
 $lang['locfiledit_model'] = 'Model';
 $lang['locfiledit_empty_page'] = 'Prázdná stránka';
-$lang['locfiledit_parent_directory'] = 'Nadøazená složka';
+$lang['locfiledit_parent_directory'] = 'Nadřazená složka';
 $lang['locfiledit_filename_error'] = 'Nepovolené znaky v názvu souboru.';
 $lang['locfiledit_file_already_exists'] = 'Soubor už existuje.';
 $lang['locfiledit_model_error'] = 'Musíte zvolit model.';
 $lang['locfiledit_empty_filename'] = 'Musíte vyplnit název souboru.';
-$lang['locfiledit_webmaster_only'] = 'Pouze webmaster mùže vytváøet nebo mìnit lokální soubory.';
+$lang['locfiledit_webmaster_only'] = 'Pouze webmaster může vytvářet nebo měnit lokální soubory.';
+
 ?>
