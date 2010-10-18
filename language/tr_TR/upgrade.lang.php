d2636f6e2fe887f2129e011f6b1ac229fdb4cd36
ddtddt <ddtddt@piwigo.org>
2010-10-18 20:24:28 +0000

    [Branche2] - add upgrade for tr_TR - thanks to Nakre
    git-svn-id: http://piwigo.org/svn/branches/2.1@7270 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/upgrade.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Upgrade'] = "Güncellestirme";
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "Bu sayfa yeni sürüme Piwigo eski sürümünden veritabani yükseltme öneriyor. Yükseltme yardimcisi Su anda bir <strong> sürüm% (veya esdegeri) </ strong> çalistiran düsünüyor.";
+$lang['Upgrade from version %s to %s'] = "%s sürümden %s sürümüne güncellestir";
+$lang['Statistics'] = "Istatistik";
+$lang['total upgrade time'] = "toplam güncelleme zamani";
+$lang['total SQL time'] = "tolam SQL zamani";
+$lang['SQL queries'] = "SQL sorgulari";
+$lang['Upgrade informations'] = "Güncelleme bilgileri";
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = "Herhangi bir sorunla karsilasirsaniz, [Yönetim> Araçlar> Bakim] dan bakim yapiniz.";
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = "Bir önlem olarak, asagidaki eklentiler devre disi olmustur.Onlari aktif etmeden önce eklentileri yükseltme için kontrol etmelisiniz";
+$lang['Only administrator can run upgrade: please sign in below.'] = "Sadece yönetici güncelleme çalistirabilir: Asagidan giris yapin.";
+$lang['You do not have access rights to run upgrade'] = "Güncelleme çalistirmak için yetkiniz yok";
+$lang['All sub-categories of private categories become private'] = "Özel kategorilerin tüm alt kategorileri de özel olur";
+$lang['User permissions and group permissions have been erased'] = "Kullanicilar ve gruplarin izinleri silinmistir";
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "Sadece küçük resim ön eki ve site yöneticisi posta adresiniz önceki yapilandirman kaydedili";
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i> içine, <b>?></b> önce, ekleyin:';
+
+
+?>
