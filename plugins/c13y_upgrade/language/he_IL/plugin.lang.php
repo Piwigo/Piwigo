536968f0b1a8ec674fb3119a8d87354cfa50ecb8
ddtddt <ddtddt@piwigo.org>
2010-10-20 04:55:27 +0000

    [Branche2] - Plugin's gore - add he_IL - Hebrew Thanks to ADIRBITON
    git-svn-id: http://piwigo.org/svn/branches/2.1@7288 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/he_IL/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = 'לא זוהו חריגות לאחר שידרוג התוכנה, התוסף "בדוק עבור עדכונים"  מנוטרל.';
+$lang['c13y_dbl_email_user'] = 'יש משתמש רשום כבר עם אותו דואר אלקטרוני';
+$lang['c13y_correction_dbl_email_user'] = 'מחק משתמשים כפולים';
+$lang['c13y_obsolete_plugin'] = 'תוסף ישן';
+$lang['c13y_correction_obsolete_plugin'] = 'התוסף "%s" כבר כלול בגירסה זאת עליך להסיר אותו.';
+
+?>
