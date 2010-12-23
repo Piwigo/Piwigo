7d8e12f60acae79d2574d029c647d5392ab63228
ddtddt <ddtddt@piwigo.org>
2010-12-23 20:29:38 +0000

    [trunk] - language - step 5 for 2.2
    git-svn-id: http://piwigo.org/svn/trunk@8261 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/vi_VN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = 'Không có sự bất thường nào được phát hiện sau khi nâng cấp ứng dụng, plugin "Dò tìm bản nâng cấp" đã được tắt.';
+$lang['c13y_dbl_email_user'] = 'Người dùng có cùng địa chỉ email';
+$lang['c13y_correction_dbl_email_user'] = 'Xóa các người dùng có trùng tên';
+$lang['c13y_obsolete_plugin'] = 'Plugin lỗi thời';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" plugin đã được kèm theo trong phiên bản của ứng dụng này và bạn bắt buộc phải gỡ bỏ nó ra.';
+
+?>
---
+++ plugins/c13y_upgrade/language/zh_CN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = '应用程序升级后没有发现异常，"检查更新" 插件已停用.';
+$lang['c13y_dbl_email_user'] = '用户使用相同的邮件地址';
+$lang['c13y_correction_dbl_email_user'] = '删除重复的用户';
+$lang['c13y_obsolete_plugin'] = '已无效的插件';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" 插件已包含此应用程序版本，您必须先卸载。';
+
+?>
