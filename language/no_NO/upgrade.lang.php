9339df2106924fbe0aabd8d4961c0992ac534bbe
ddtddt <ddtddt@piwigo.org>
2011-01-08 22:56:12 +0000

    [branches 2.1] - add update no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/branches/2.1@8542 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/upgrade.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Upgrade'] = 'Oppgrader';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denne siden tilbyr å oppgradere databasen fra din gamle Piwigo versjonen til den gjeldende versjonen. Oppgraderings assistenten mener at du for tiden kjører <strong>Versjon %s</strong> (eller lignende).';
+$lang['Upgrade from version %s to %s'] = 'Oppgrader fra versjon %s til %s';
+$lang['Statistics'] = 'Statistisk';
+$lang['total upgrade time'] = 'total oppgraderings tid';
+$lang['total SQL time'] = 'total SQL tid';
+$lang['SQL queries'] = 'SQL spørringer';
+$lang['Upgrade informations'] = 'oppgraderings informasjon';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Gjennomfør en vedlikeholds sjekk i [Administration>Tools>Maintenance] om du møter på problemer.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'For å være sikker, følgende tilleggsprogrammer har blitt deaktivert. Du må sjekke for oppgradering på dine tilleggsverktøy før du reaktiverer dem:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Only an administrator can run upgrade: please sign in below.';
+$lang['You do not have access rights to run upgrade'] = 'Du har ikke tilgangs rettigheter for å kunne oppgradere';
+$lang['All sub-categories of private categories become private'] = 'Alle grunn-album i privat album blir private';
+$lang['User permissions and group permissions have been erased'] = 'Bruker og gruppe rettigheter har blitt slettet';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Kun miniatyr bliders prefix and webmasters mail addresse har blitt lagret fra forrige konfigurasjon';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'I <i>%s</i>, før <b>?></b>, sett inn:';
+
+?>
