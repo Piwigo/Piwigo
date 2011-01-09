a862b910f8c0df57a2ed4eda64a6f6284588ee81
ddtddt <ddtddt@piwigo.org>
2011-01-09 17:50:42 +0000

    [branches 2.1] - LocalFilesEditor - add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/branches/2.1@8553 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/no_NO/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+$lang['locfiledit_onglet_localconf'] = 'Lokal konfigurasjon';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'Maler (templates)';
+$lang['locfiledit_onglet_lang'] = 'Språk';
+$lang['locfiledit_onglet_plug'] = 'Personlig tilleggsprogram';
+$lang['locfiledit_cant_save'] = 'Gjeldende fil kan ikke skrives til. sjekk om mappen "include/" har skriverettigheter (chmod)(775).';
+$lang['locfiledit_newfile'] = 'Filen eksisterer ikke og vil bli laget av LocalFiles Editor';
+$lang['locfiledit_save_config'] = 'Fil er skrevet.';
+$lang['locfiledit_show_default'] = 'Vis referanse fil: ';
+$lang['locfiledit_save_bak'] = 'Sikkerhets kopi vil bli laget ved lagring.';
+$lang['locfiledit_saved_bak'] = 'Sikkerhetskopi er laget (%s)';
+$lang['locfiledit_save_file'] = 'Lagre fil';
+$lang['locfiledit_choose_file'] = 'Velg fil du vil redigering';
+$lang['locfiledit_edit'] = 'Rediger';
+$lang['locfiledit_restore'] = 'Gjennopprett sikkerhets kopi';
+$lang['locfiledit_restore_confirm'] = 'Vennligst bekreft? Gjennoppretting vil ikke gjøres før neste lagring.';
+$lang['locfiledit_bak_loaded1'] = 'Sikkerhetskopi lastet.';
+$lang['locfiledit_bak_loaded2'] = 'Du må lagre filen for å gjennopprette den.';
+$lang['locfiledit_syntax_error'] = 'Syntax feil! Filen kan ikke bli lagret.';
+$lang['locfiledit_enable_editarea'] = 'Aktiver redigeringsområde ';
+$lang['locfiledit_disable_editarea'] = 'Deaktiver redigeringsområde';
+$lang['locfiledit_new_tpl'] = 'Lag ny fil';
+$lang['locfiledit_new_filename'] = 'Fil navn';
+$lang['locfiledit_model'] = 'Modusl';
+$lang['locfiledit_empty_page'] = 'Tøm side';
+$lang['locfiledit_parent_directory'] = 'Underliggende mappe';
+$lang['locfiledit_filename_error'] = 'Forbudte tegn i filnavnet.';
+$lang['locfiledit_file_already_exists'] = 'Filen eksisterer allerede.';
+$lang['locfiledit_model_error'] = 'Du må velge en modell.';
+$lang['locfiledit_empty_filename'] = 'Du må fylle inn fil navnet.';
+$lang['locfiledit_webmaster_only'] = 'Kun webmaster kan lage eller endre lokale filer.';
+
+?>
