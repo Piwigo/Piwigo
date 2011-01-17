91fc33dc8eea8bfd38f36f34c1abaa389b7e92e5
plegall <plg@piwigo.org>
2011-01-17 12:30:45 +0000

    feature 2102: a mapping from old language keys to new language keys, will be used to inject new values from the Photos plugin into core
    git-svn-id: http://piwigo.org/svn/trunk@8719 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ tools/photos.lang.php
@@ -0,0 +1,54 @@
+<?php
+// common
+$lang['%d new image'] = '%d new photo';
+$lang['%d new images'] = '%d new photos';
+$lang['add this image to your favorites'] = 'add this photo to your favorites';
+$lang['display best rated items'] = 'display best rated photos';
+$lang['display each day with pictures, month per month'] = 'display each day with photos, month per month';
+$lang['delete this image from your favorites'] = 'delete this photo from your favorites';
+$lang['display my favorites pictures'] = 'display your favorites photos';
+$lang['shows images at the root of this album'] = 'display this album';
+$lang['See last users comments'] = 'display last user comments';
+$lang['display most visited pictures'] = 'display most visited photos';
+$lang['Rate this picture'] = 'Rate this photo';
+$lang['Random pictures'] = 'Random photos';
+$lang['display a set of random pictures'] = 'display a set of random photos';
+$lang['Recent pictures'] = 'Recent photos';
+$lang['display most recent pictures'] = 'display most recent photos';
+$lang['display only recently posted images'] = 'display only recently posted photos';
+$lang['return to the display of all images'] = 'return to the display of all photos';
+$lang['%d image'] = '%d photo';
+$lang['%d images'] = '%d photos';
+$lang['%d image is also linked to current tags'] = '%d photo is also linked to current tags';
+$lang['%d images are also linked to current tags'] = '%d photos are also linked to current tags';
+$lang['See images linked to this tag only'] = 'display photos linked to this tag';
+$lang['images posted during the last %d days'] = 'photos posted during the last %d days';
+$lang['delete all images from your favorites'] = 'delete all photos from your favorites';
+$lang['Complete RSS feed (images, comments)'] = 'Complete RSS feed (photos, comments)';
+$lang['Image only RSS feed'] = 'Photos only RSS feed';
+$lang['The RSS notification feed provides notification on news from this website : new pictures, updated albums, new comments. Use a RSS feed reader.'] = 'The RSS notification feed provides notification on news from this website : new photos, updated albums, new comments. Use a RSS feed reader.';
+$lang['Maximum height of the pictures'] = 'Maximum photo height';
+$lang['Maximum width of the pictures'] = 'Maximum photo width';
+$lang['display all elements in all sub-albums'] = 'display all photos in all sub-albums';
+$lang['The number of images per row must be a not null scalar'] = 'The number of photos per row must be a not null scalar';
+$lang['Number of images per row'] = 'Number of photos per row';
+$lang['Elements posted within the last %d day.'] = 'Photos posted within the last %d day.';
+$lang['Elements posted within the last %d days.'] = 'Photos posted within the last %d days.';
+$lang['picture'] = 'photo';
+$lang['Click on the picture to see it in high definition'] = 'Click on the photo to see it in high definition';
+
+// admin
+$lang['Include display of recent pictures group by dates'] = 'Include display of recent photos grouped by dates';
+$lang['Manage image ranks'] = 'Manage photo ranks';
+$lang['add new elements to caddie'] = 'add new photos to caddie';
+$lang['display maximum informations (added albums and elements, deleted albums and elements)'] = 'display maximum informations (added albums and photos, deleted albums and photos)';
+$lang['elements added in the database'] = 'photos added in the database';
+$lang['elements deleted in the database'] = 'photos deleted from the database';
+$lang['elements per page'] = 'photos per page';
+$lang['elements updated in the database'] = 'photos updated in the database';
+$lang['even already synchronized elements'] = 'even already synchronized photos';
+$lang['first element added on %s'] = 'first photo added on %s';
+$lang['jump to image'] = 'jump to photo';
+$lang['manage image ranks'] = 'manage photo ranks';
+$lang['Select at least one picture'] = 'Select at least one photo';
+?>
