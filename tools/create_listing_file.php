8659a16b3ef48f1105be70e5d5116a8c47d05c77
plegall <plg@piwigo.org>
2011-01-20 20:48:42 +0000

    release number 2.2.0RC1 hard coded + turn off show_gt and check_uprgade_feed
    git-svn-id: http://piwigo.org/svn/tags/2.2.0RC1@8816 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = 'Colibri';
+$conf['version'] = '2.2.0RC1';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
