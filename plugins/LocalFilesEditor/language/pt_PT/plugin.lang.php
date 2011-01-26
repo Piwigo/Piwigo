082662136b3fa73cfb29d93182ce3f71eab09263
ddtddt <ddtddt@piwigo.org>
2011-01-26 17:11:51 +0000

    [branches 2.1] - LocalFilesEditor - add pt_PT Thanks to PsyKick_RuhYn
    git-svn-id: http://piwigo.org/svn/branches/2.1@8920 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/pt_PT/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'Configuração Local';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = 'Modelos';
+$lang['locfiledit_onglet_lang'] = 'Linguagens';
+$lang['locfiledit_onglet_plug'] = 'Plugin Pessoal';
+$lang['locfiledit_cant_save'] = 'O ficheiro actual não tem permissões de escrita. Confirme que a pasta "local/" tem permissões de escrita (chmod).';
+$lang['locfiledit_newfile'] = 'O ficheiro não existe e irá ser criado pelo Editor LocalFiles';
+$lang['locfiledit_save_config'] = 'Ficheiro escrito com sucesso.';
+$lang['locfiledit_show_default'] = 'Mostrar ficheiro de referência: ';
+$lang['locfiledit_save_bak'] = 'Cópia de segurança será guardada quando clicar em Salvar Ficheiro.';
+$lang['locfiledit_saved_bak'] = 'Cópia de segurança criada (%s)';
+$lang['locfiledit_save_file'] = 'Salvar Ficheiro';
+$lang['locfiledit_choose_file'] = 'Escolha o ficheiro a ser editado';
+$lang['locfiledit_edit'] = 'Editar';
+$lang['locfiledit_restore'] = 'Restaure uma cópia de segurança';
+$lang['locfiledit_restore_confirm'] = 'Por favor confirme?
+O Restauro não será efectuado até Salvar a próxima vez.';
+$lang['locfiledit_bak_loaded1'] = 'Ficheiro de cópia de segurança carregado.';
+$lang['locfiledit_bak_loaded2'] = 'Deverá salvar o ficheiro para o restaurar.';
+$lang['locfiledit_syntax_error'] = 'Erro de sintaxe! Impossível salvar ficheiro.';
+$lang['locfiledit_enable_editarea'] = 'Activar Área de Edição';
+$lang['locfiledit_disable_editarea'] = 'Desactivar Área de Edição';
+$lang['locfiledit_new_tpl'] = 'Criar novo ficheiro';
+$lang['locfiledit_new_filename'] = 'Nome do ficheiro';
+$lang['locfiledit_model'] = 'Modelo';
+$lang['locfiledit_empty_page'] = 'Página Vazia';
+$lang['locfiledit_parent_directory'] = 'Pasta Mãe';
+$lang['locfiledit_filename_error'] = 'Caracteres proibidos no nome do ficheiro.';
+$lang['locfiledit_file_already_exists'] = 'O ficheiro já existe.';
+$lang['locfiledit_model_error'] = 'Tem que escolher um Modelo.';
+$lang['locfiledit_empty_filename'] = 'Tem que preencher o Nome do ficheiro.';
+$lang['locfiledit_webmaster_only'] = 'Apenas webmasters podem criar ou modificar ficheiros locais.';
+
+?>
