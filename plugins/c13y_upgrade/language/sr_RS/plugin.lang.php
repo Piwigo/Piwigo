38bb7842020b37d7bdae1e7aebcad97b06ff5bd5
ddtddt <ddtddt@piwigo.org>
2011-02-28 16:40:07 +0000

    [trunk] - plugins - c13y_upgrade - add sr_RS Thanks to Ћирилица1
    git-svn-id: http://piwigo.org/svn/trunk@9428 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/sr_RS/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+// --------- Starting below: New or revised $lang ---- from Butterfly (1.8)
+$lang['c13y_upgrade_no_anomaly'] = 'Недостаци нису откривени после надоградње, додатак "Провера надоградње" није активиран.';
+$lang['c13y_dbl_email_user'] = 'Корисници са истом адресом електронске поште';
+$lang['c13y_correction_dbl_email_user'] = 'Обришите дупле кориснике';
+$lang['c13y_obsolete_plugin'] = 'Застарео додатак';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" додатак је укључен у ову верзију апликације и морате да га избаците.';
+
+?>
