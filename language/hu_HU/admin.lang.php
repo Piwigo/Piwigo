0552013a5765ffa46aabf93068eeb2109c2cb9e4
ddtddt <ddtddt@piwigo.org>
2011-03-09 12:53:39 +0000

    [trunk] - update HU
    git-svn-id: http://piwigo.org/svn/trunk@9582 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/admin.lang.php
+++ language/hu_HU/admin.lang.php
@@ -771,7 +771,6 @@ $lang['Week starts on'] = 'A hét első napja';
 $lang['You have %d orphan tags: %s.'] = '%d árva címke található: %s.';
 $lang['Remove from caddie'] = 'Eltávolítás a kosárból';
 $lang['[Simulation]'] = '[Szimuláció]';
-// missing translations
 $lang['Themes which need upgrade'] = 'Frissítést igénylő témák';
 $lang['Languages which need upgrade'] = 'Frissítést igénylő nyelvek';
 $lang['All themes are up to date.'] = 'Minden téma naprakész.';
