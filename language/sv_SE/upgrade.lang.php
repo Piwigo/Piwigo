11eed62758c47d0f8ec9470464f78d6e56b443ec
ddtddt <ddtddt@piwigo.org>
2011-03-10 05:54:07 +0000

    [trunk] - language - add upgrade part sv_SE thanks to jonas
    git-svn-id: http://piwigo.org/svn/trunk@9592 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/upgrade.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+
+$lang['Upgrade'] = 'Uppgradera';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denna sida föreslår att du ska uppgradera databasen från din gamla versionen till den nya versionen. Uppgraderingen assistenten tycker att du för närvarande kör en <strong>versionen %s </strong> (eller motsvarande).';
+$lang['Upgrade from version %s to %s'] = 'Uppgradera från version %s till %s';
+$lang['Statistics'] = 'Statistik';
+$lang['total upgrade time'] = 'total uppgraderings tid';
+$lang['total SQL time'] = 'Total SQL tid';
+$lang['SQL queries'] = 'SQL-frågor';
+$lang['Upgrade informations'] = 'Uppgraderings information';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Gör en underhållskontroll i mynyn [Administration> Verktyg> Underhåll] om du stöter på några problem. .';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Som en försiktighetsåtgärd, har följande insticksmoduler avaktiverats, Du måste kontrollera alla plugins uppgraderingar före återaktivering av dem:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Endast en administratör kan köra uppgraderingen: Logga in nedan.';
+$lang['You do not have access rights to run upgrade'] = 'Du har inte behörighet att köra uppgradering';
+$lang['All sub-albums of private albums become private'] = 'Alla underliggande album av "privata album" blir "privata';
+$lang['User permissions and group permissions have been erased'] = 'Användare och grupper behörigheter har raderats';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'miniatyrbilder Endast prefix och webmaster postadress har sparats från tidigare konfiguration';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'I <i>%s</i>, innan <b>?></b>, infoga:';
+
+?>
