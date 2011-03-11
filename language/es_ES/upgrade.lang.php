13b4bf3c76515fdc1eeb5e619dc76be20f0a2d14
ddtddt <ddtddt@piwigo.org>
2011-03-11 18:24:37 +0000

    [trunk] - language - update es_ES thanks to jpr928
    git-svn-id: http://piwigo.org/svn/trunk@9614 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/upgrade.lang.php
+++ language/es_ES/upgrade.lang.php
@@ -32,14 +32,11 @@ $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you 
 $lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Por precaución, el plugins siguiente han sido desactivados. Verifique si existen unas actualización antes de reactivarlos:';
 $lang['Only administrator can run upgrade: please sign in below.'] = 'Sólo un administrador puede lanzar la actualización: por favor, identifiqúese más abajo.';
 $lang['You do not have access rights to run upgrade'] = 'Usted no tiene los derechos necessaires para lanzar la actualización.';
-
-// Upgrade informations from upgrade_1.3.1.php
 $lang['All sub-albums of private albums become private'] = 'Todos los sub-àlbumes de los àlbumes privados son privados';
 $lang['User permissions and group permissions have been erased'] = 'Las autorizaciones de los usuarios y de los grupos han sido borradas';
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Sólo el prefijo de las miniaturas y el email  del webmestre han sido salvaguardados por la configuración precedente';
-
-
 $lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Esta página le propone poner al día la base de datos de su antigua versión de piwigo hacia la nueva versión.
 El asistente de postura al día piensa que usted dispone actualmente del <strong>versión %s</strong> (o equivalente).';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'En <i>%s</i>, antes <b>?></b>, insertar:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Como medida de precaución, los siguientes temas han sido desactivados. Compruebe si existe actualizaciones  antes de reactivar:';
 ?>
