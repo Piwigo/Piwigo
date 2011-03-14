a57f08560097b7063a2559816646bb337cbcbfd0
ddtddt <ddtddt@piwigo.org>
2011-03-14 06:28:22 +0000

    [trunk] - language - update sv_SE thanks to jonas
    git-svn-id: http://piwigo.org/svn/trunk@9658 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/upgrade.lang.php
+++ language/sv_SE/upgrade.lang.php
@@ -24,21 +24,21 @@
 
 
 $lang['Upgrade'] = 'Uppgradera';
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denna sida föreslår att du ska uppgradera databasen från din gamla versionen till den nya versionen. Uppgraderingen assistenten tycker att du för närvarande kör en <strong>versionen %s </strong> (eller motsvarande).';
-$lang['Upgrade from version %s to %s'] = 'Uppgradera från version %s till %s';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denna sida fÃ¶reslÃ¥r att du ska uppgradera databasen frÃ¥n din gamla versionen till den nya versionen. Uppgraderingen assistenten tycker att du fÃ¶r nÃ¤rvarande kÃ¶r en <strong>versionen %s </strong> (eller motsvarande).';
+$lang['Upgrade from version %s to %s'] = 'Uppgradera frÃ¥n version %s till %s';
 $lang['Statistics'] = 'Statistik';
 $lang['total upgrade time'] = 'total uppgraderings tid';
 $lang['total SQL time'] = 'Total SQL tid';
-$lang['SQL queries'] = 'SQL-frågor';
+$lang['SQL queries'] = 'SQL-frÃ¥gor';
 $lang['Upgrade informations'] = 'Uppgraderings information';
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Gör en underhållskontroll i mynyn [Administration> Verktyg> Underhåll] om du stöter på några problem. .';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Som en försiktighetsåtgärd, har följande insticksmoduler avaktiverats, Du måste kontrollera alla plugins uppgraderingar före återaktivering av dem:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'Endast en administratör kan köra uppgraderingen: Logga in nedan.';
-$lang['You do not have access rights to run upgrade'] = 'Du har inte behörighet att köra uppgradering';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'GÃ¶r en underhÃ¥llskontroll i mynyn [Administration> Verktyg> UnderhÃ¥ll] om du stÃ¶ter pÃ¥ nÃ¥gra problem. .';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Som en fÃ¶rsiktighetsÃ¥tgÃ¤rd, har fÃ¶ljande insticksmoduler avaktiverats, Du mÃ¥ste kontrollera alla plugins uppgraderingar fÃ¶re Ã¥teraktivering av dem:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Endast en administratÃ¶r kan kÃ¶ra uppgraderingen: Logga in nedan.';
+$lang['You do not have access rights to run upgrade'] = 'Du har inte behÃ¶righet att kÃ¶ra uppgradering';
 $lang['All sub-albums of private albums become private'] = 'Alla underliggande album av "privata album" blir "privata';
-$lang['User permissions and group permissions have been erased'] = 'Användare och grupper behörigheter har raderats';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'miniatyrbilder Endast prefix och webmaster postadress har sparats från tidigare konfiguration';
+$lang['User permissions and group permissions have been erased'] = 'AnvÃ¤ndare och grupper behÃ¶righeter har raderats';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'miniatyrbilder Endast prefix och webmaster postadress har sparats frÃ¥n tidigare konfiguration';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'I <i>%s</i>, innan <b>?></b>, infoga:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Som en försiktighetsåtgärd, har följande teman har avaktiverats. Du måste kollatema uppgraderingar innan du aktivera dem:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Som en fÃ¶rsiktighetsÃ¥tgÃ¤rd, har fÃ¶ljande teman har avaktiverats. Du mÃ¥ste kolla tema uppgraderingar innan du aktivera dem:';
 
 ?>
