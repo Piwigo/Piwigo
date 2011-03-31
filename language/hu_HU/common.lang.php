a74bc256e7735b6fcf9845ff2947be8a084a2057
samli <samli@piwigo.org>
2011-03-31 18:40:12 +0000

    [trunk] [common.lang.php] modified HU language
    git-svn-id: http://piwigo.org/svn/trunk@9955 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/common.lang.php
+++ language/hu_HU/common.lang.php
@@ -360,5 +360,5 @@ $lang['Manage this user comment: %s'] = 'Itt kezelheti a felhasználó hozzász�
 $lang['This author modified following comment:'] = 'A szerző a következő hozzászólást módosította:';
 $lang['This author removed the comment with id %d'] = 'A szerző a következő hozzászólást törölte id %d';
 $lang['validate this comment'] = 'hozzászólás engedélyezése';
-
+$lang['html tags are not allowed in login'] = 'Belépéskor a html tag-ek nem engedélyezettek';
 ?>
