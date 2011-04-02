d9a8ff3936d506154dc8352dcd166b9edd184614
plegall <plg@piwigo.org>
2011-04-02 19:48:08 +0000

    change version to 2.3 on trunk
    git-svn-id: http://piwigo.org/svn/trunk@10000 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = 'Colibri';
+$conf['version'] = '2.3';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
