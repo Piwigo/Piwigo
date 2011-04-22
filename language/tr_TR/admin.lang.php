03c9195f7c846522c21f99f35d01d4054900f343
ddtddt <ddtddt@piwigo.org>
2011-04-22 06:28:56 +0000

    [trunk] - language - update TR thanks nakre report Websize Regeneration
    git-svn-id: http://piwigo.org/svn/trunk@10559 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/admin.lang.php
+++ language/tr_TR/admin.lang.php
@@ -785,4 +785,9 @@ $lang['Regenerate Thumbnails'] = 'Küçük Resimleri Tekrar Oluştur';
 $lang['Thumbnails generation in progress...'] = 'Küçük resimler oluşturuluyor...';
 $lang['%s thumbnails have been regenerated'] = '%s küçük resimler oluşturuldu';
 $lang['%s thumbnails can not be regenerated'] = '%s küçük resimler oluşturulamadı';
+$lang['Regenerate Websize Photos'] = 'Yeniden web boyutu oluştur';
+$lang['Photos generation in progress...'] = 'Resimler oluşturulmaya devam ediyor...';
+$lang['%s photos have been regenerated'] = '%s resimler oluşturuldu';
+$lang['%s photos can not be regenerated'] = '%s resimler oluşturulamadı';
+$lang['Only photos with HD can be regenerated!'] = 'Sadece HD  olan resimler oluşturulabilir!';
 ?>
