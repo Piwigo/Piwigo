4f614c9fef6767207dd5a236d0c2b979141bbf3e
plegall <plg@piwigo.org>
2011-06-01 10:49:16 +0000

    change version to next release: 2.3.0beta2
    git-svn-id: http://piwigo.org/svn/trunk@11188 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = '2.3.0beta1';
+$conf['version'] = '2.3.0beta2';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
