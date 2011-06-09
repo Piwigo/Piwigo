ab8700e7a742bdc6918486238cf7bd6d30ba396d
plegall <plg@piwigo.org>
2011-06-09 20:56:07 +0000

    change version number to 2.2.3
    git-svn-id: http://piwigo.org/svn/branches/2.2@11286 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.2.2');
+define('PHPWG_VERSION', '2.2.3');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'Sylvia');
 
