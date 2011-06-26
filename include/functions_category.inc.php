f14d7f54b15e72a80ee3be5329c3a8ad78d9277c
mistic100 <mistic@piwigo.org>
2011-06-26 10:03:48 +0000

    merge r11512 form trunk bug:2361 fixed : remove html tags in categories drop-down lists
    git-svn-id: http://piwigo.org/svn/branches/2.2@11513 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/functions_category.inc.php
+++ include/functions_category.inc.php
@@ -317,9 +317,13 @@ function display_select_categories($categories,
     }
     if ($fullname)
     {
-      $option = get_cat_display_name_cache($category['uppercats'],
-                                           null,
-                                           false);
+      $option = strip_tags(
+        get_cat_display_name_cache(
+          $category['uppercats'],
+          null,
+          false
+          )
+        );
     }
     else
     {
