ea8bd93420af754a05f06203587961ae0365947c
ddtddt <ddtddt@piwigo.org>
2011-07-05 19:37:01 +0000

    [trunk] - language - SL (Slovenia - Slovenšcina) thanks to klemen  - feature:2320
    git-svn-id: http://piwigo.org/svn/trunk@11640 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SL/admin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+$lang['%d album including'] = '%d album vsebuje';
+$lang['%d album moved'] = '%d album je bil premaknjen';
+$lang['%d albums including'] = '%d albumov vsebuje';
+$lang['%d albums moved'] = '%d albumov je bilo premaknjenih';
+$lang['%d anomalies have been detected corrected.'] = '%d anomalij je bilo zaznanih in odpravljenih.';
+$lang['%d anomalies have been detected.'] = '%d anomalij je bilo zaznanih.';
+$lang['%d anomalies have been ignored.'] = '%d anomalij je bilo ignoriranih.';
+$lang['%d anomalies have not been corrected.'] = '%d anomalij ni bilo odpravljenih.';
+$lang['%d anomaly has been corrected.'] = '%d anomalija je bila odpravljena.';
+$lang['%d anomaly has been detected.'] = '%d anomalija je bila zaznana.';
+$lang['%d anomaly has been ignored.'] = '%d anomalija je bila ignorirana.';
+$lang['%d anomaly has not been corrected.'] = '%d anomalija ni bila odpravljena.';
+$lang['%d association'] = '%d asocijacija';
+$lang['%d associations'] = '%d asocijacij';
+$lang['%d group'] = '%d skupina';
+$lang['%d groups'] = '%d skupine';
+$lang['%d guest'] = '%d gost';
+$lang['%d guests'] = '%d gostov';
+$lang['%d line filtered'] = '%d vrstica filtrirana';
+$lang['%d lines filtered'] = '%d vrstic filtriranih';
+
+?>
