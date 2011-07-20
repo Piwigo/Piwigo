6a24f5ed79ebe65f03bac859a166c4d3a7a0281e
ddtddt <ddtddt@piwigo.org>
2011-07-20 06:11:31 +0000

    [trunk] -  language - add th_TH - Thaï - ภาษาไทย Thanks to Songchai and Jomyuthack (Piwigo Thailand FanClub)
    git-svn-id: http://piwigo.org/svn/trunk@11779 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/common.lang.php
@@ -0,0 +1,235 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: ภาษาไทย [TH]
+Version: 2.2.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations 
+$lang_info['language_name'] = "ภาษาไทย";
+$lang_info['country'] = "ประเทศไทย";
+$lang_info['direction'] = "ltr";
+$lang_info['code'] = "th";
+$lang_info['zero_plural'] = "1";
+
+$lang['%d album updated'] = '%d อัลบัมได้ปรับแต่งแล้ว';
+$lang['%d albums updated'] = '%d อัลบัมเหล่านั้นได้ปรับแต่งแล้ว';
+$lang['%d comment to validate'] = '%d ความเห็นรอการตรวจสอบ';
+$lang['%d comments to validate'] = '%d กลุ่มความเห็นรอการตรวจสอบ';
+$lang['%d new comment'] = '%d เพิ่มความคิดเห็น';
+$lang['%d new comments'] = '%d new เพิ่มหลายความคิดเห็น';
+$lang['%d comment'] = '%d ความคิดเห็น';
+$lang['%d comments'] = '%d ความคิดเห็น(จำนวนหนึ่ง)';
+$lang['%d hit'] = '%d นิยม';
+$lang['%d hits'] = '%d นิยม(มากกว่าหนึ่ง)';
+$lang['%d new photo'] = '%d ภาพใหม่';
+$lang['%d new photos'] = '%d ภาพใหม่(มากกว่าหนึ่ง)';
+$lang['%d new user'] = '%d ผู้ใช้ใหม่';
+$lang['%d new users'] = '%d ผู้ใช้ใหม่(มากกว่าหนึ่ง)';
+$lang['About'] = 'ข้อมูลที่เกี่ยวข้อง';
+$lang['All tags'] = 'แทคทั้งหมด';
+$lang['Any tag'] = 'แท็คใดๆ';
+$lang['At least one listed rule must be satisfied.'] = 'ต้องมีกฏของลำดับอย่างน้อย 1 กฏเพื่อยอมรับ';
+$lang['At least one tag must match'] = 'ต้องมีอย่างน้อย 1 แท็คที่เข้ากันได้';
+$lang['Author'] = 'เจ้าของ';
+$lang['Average rate'] = 'ระดับเฉลี่ย';
+$lang['Albums'] = 'อัลบัม(มากกว่าหนึ่ง)';
+$lang['Album'] = 'อัลบัม';
+$lang['Close this window'] = 'ปิดหน้าต่างนี้';
+$lang['Complete RSS feed (photos, comments)'] = 'RSS feed เสร็จสมบูรณ์ (ภาพ, ความคิดเห็น)';
+$lang['Confirm Password'] = 'ยืนยันรหัสผ่าน';
+$lang['Connection settings'] = 'ตั้งค่าการเชื่อมต่อ';
+$lang['Login'] = 'เข้าสู่ระบบ';
+$lang['Contact webmaster'] = 'ติดต่อผู้ดูแลเว็บ';
+$lang['Create a new account'] = 'สร้างบัญชีใหม่';
+$lang['Created on'] = 'สร้างเมื่อ';
+$lang['Creation date'] = 'วันที่สร้าง';
+$lang['Current password is wrong'] = 'รหัสผ่านเดิมผิด';
+$lang['Dimensions'] = 'มิติ';
+$lang['Display'] = 'Display';
+$lang['Each listed rule must be satisfied.'] = 'กฏของลำดับแต่ละกฏต้องได้รับการยอมรับ';
+$lang['Email address'] = 'อี-เมล์';
+$lang['Enter your personnal informations'] = 'กรอกข้อมูลส่วนตัว';
+$lang['Error sending email'] = 'เกิดข้อผิดพลาดขณะส่งเมล์';
+$lang['File name'] = 'ชื่อไฟล์';
+$lang['File'] = 'ไฟล์';
+$lang['Filesize'] = 'ขนาดไฟล์';
+$lang['Filter and display'] = 'ตัวกรอง และ การแสดงผล';
+$lang['Filter'] = 'ตัวกรอง';
+$lang['Forgot your password?'] = 'ลืมรหัสผ่าน';
+$lang['Go through the gallery as a visitor'] = 'เข้าชมแกเลอรีเหมือนผู้เยี่ชมทั่วไป';
+$lang['Help'] = 'ช่วยเหลือ';
+$lang['Identification'] = 'เอกลักษณ์';
+$lang['Photos only RSS feed'] = 'RSS feed สำหรับภาพเท่านั้น';
+$lang['Keyword'] = 'คำค้น';
+$lang['New password confirmation does not correspond'] = 'รหัสผ่านและยืนยันรหัสผ่านต้องเหมือนกัน';
+$lang['New password sent by email'] = 'รหัสผ่านใหม่ส่งไปที่เมล์ของท่านแล้ว';
+$lang['No email address'] = 'ไม่มีเมล์';
+$lang['No classic user matches this email address'] = 'ไม่มีเมล์นี้ในระบบ';
+$lang['Notification'] = 'การแจ้งเตือน';
+$lang['Number of items'] = 'จำนวนหน่วย';
+$lang['Original dimensions'] = 'มิติดั้งเดิม';
+$lang['Password forgotten'] = 'ลืมรหัสผ่าน';
+$lang['Password'] = 'รหัสผ่าน';
+$lang['Post date'] = 'วันที่โพสต์';
+$lang['Posted on'] = 'โพสต์ไว้เมื่อ';
+$lang['Profile'] = 'ข้อมูลส่วนตัว';
+$lang['Quick connect'] = 'เชื่อมต่ออย่างเร็ว';
+$lang['RSS feed'] = 'RSS feed';
+$lang['Rate'] = 'ระดับ';
+$lang['Register'] = 'สมัครสมาชิก';
+$lang['Registration'] = 'การสมัครสมาชิก';
+$lang['Related tags'] = 'แท็คใกล้เคียง';
+$lang['Reset'] = 'เริ่มใหม่';
+$lang['Retrieve password'] = 'ค้นหารหัสผ่าน';
+$lang['Search rules'] = 'ขอบเขตการค้นหา';
+$lang['Search tags'] = 'ค้นหาแท็ค';
+$lang['Search'] = 'ค้นหา';
+$lang['display available tags'] = 'แสดงแท็คที่ใช้ได้';
+$lang['Send new password'] = 'ส่งรหัสผ่านใหม่';
+$lang['Since'] = 'ตั้งแต';
+$lang['Sort by'] = 'จัดเรียงโดย';
+$lang['Sort order'] = 'ลำดับการจัดเรียง';
+$lang['Tag'] = 'แท็ค';
+$lang['Tags'] = 'แท็ค';
+$lang['The RSS notification feed provides notification on news from this website : new photos, updated albums, new comments. Use a RSS feed reader.'] = 'RSS feed แสดงความเคลื่อนไหวต่างๆจากเว็บ: ภาพใหม่, เปลี่ยนแปลงอัลบัม, ความเห็นใหม่ เพื่อใช้สำหรับ RSS feed reader';
+$lang['Unknown feed identifier'] = 'feed ที่ไม่รู้จัก';
+$lang['User comments'] = 'ความคิดเห็น';
+$lang['Username'] = 'ชื่อผู้ใช้';
+$lang['Visits'] = 'เยี่ยมชมแล้ว(ครั้ง)';
+$lang['Webmaster'] = 'ผู้ดูแลเว็บ';
+$lang['Week %d'] = 'สัปดาห์ %d';
+$lang['About Piwigo'] = 'ข้อมูล Piwigo';
+$lang['You are not authorized to access the requested page'] = 'ท่านไม่มีสิทธิเข้าใช้งานหน้านี้';
+$lang['add to caddie'] = 'เพิ่มไปที่แคดดี้';
+$lang['add this photo to your favorites'] = 'ตั้งภาพนี้เป็นภาพโปรด';
+$lang['Administration'] = 'เข้าจัดการระบบ';
+$lang['all'] = 'ทั้งหมด';
+$lang['ascending'] = 'น้อยไปหามาก';
+$lang['author(s) : %s'] = 'เจ้าของ : %s';
+$lang['Expand all albums'] = 'ขยายทุกอัลบั้ม';
+$lang['posted after %s (%s)'] = 'โพสต์ไว้หลังจาก %s (%s)';
+$lang['posted before %s (%s)'] = 'โพสต์ไว้ก่อน %s (%s)';
+$lang['posted between %s (%s) and %s (%s)'] = 'โพสต์ระหว่าง %s (%s) กับ %s (%s)';
+$lang['posted on %s'] = 'โพสต์เมื่อ %s';
+$lang['Best rated'] = 'ระดับดีที่สุด';
+$lang['display best rated photos'] = 'แสดงภาพระดับดีที่สุด';
+$lang['caddie'] = 'แคดดี้';
+$lang['Calendar'] = 'ปฏิทิน';
+$lang['All'] = 'ทั้งหมด';
+$lang['display each day with photos, month per month'] = 'แสดงภาพในแต่ละวัน, เดือนต่อเดือน';
+$lang['View'] = 'ชม';
+$lang['chronology_monthly_calendar'] = 'ปฏิทินแสดงเป็นเดือน';
+$lang['chronology_monthly_list'] = 'แสดงรายเดือน';
+$lang['chronology_weekly_list'] = 'แสดงรายสัปดาห์';
+$lang['day'][0] = "วันอาทิตย์";
+$lang['day'][1] = "วันจันทร์";
+$lang['day'][2] = "วันอังคาร";
+$lang['day'][3] = "วันพุธ";
+$lang['day'][4] = "วันพฤหัสบดี";
+$lang['day'][5] = "วันศุกร์";
+$lang['day'][6] = "วันเสาร์";
+$lang['month'][10] = "ตุลาคม";
+$lang['month'][11] = "พฤษจิกายน";
+$lang['month'][12] = "ธันวาคม";
+$lang['month'][1] = "มกราคม";
+$lang['month'][2] = "กุมภาพันธ์";
+$lang['month'][3] = "มีนาคม";
+$lang['month'][4] = "เมษายน";
+$lang['month'][5] = "พฤษภาคม";
+$lang['month'][6] = "มิถุยายน";
+$lang['month'][7] = "กรกฎาคม";
+$lang['month'][8] = "สิงหาคม";
+$lang['month'][9] = "กันยายน";
+$lang['New on %s'] = 'มาใหม่เมื่อ %s';
+$lang['Click here if your browser does not automatically forward you'] = 'คลิกที่นี่หากไม่มีการเปลี่ยนหน้าอัตโนมัติ';
+$lang['comment date'] = 'วันที่ลงความเห็น';
+$lang['Comment'] = 'ความเห็น';
+$lang['Your comment has been registered'] = 'ความเห็นของท่านได้รับการยอมรับแล้ว';
+$lang['Anti-flood system : please wait for a moment before trying to post another comment'] = 'ระบบป้องกันการโพสต์ซ้ำซาก : กรุณารอสักครู่ก่อนที่ท่านจะเพิ่มความเห็นอีกครั้ง';
+$lang['Your comment has NOT been registered because it did not pass the validation rules'] = 'Your ความเห็นของท่านไม่ผ่านการอณุมัติ';
+$lang['An administrator must authorize your comment before it is visible.'] = 'จะต้องให้ผู้ดูแลระบบตรวจสอบความเห็นของท่านก่อน';
+$lang['This login is already used by another user'] = 'ชื่อผู้ใช้นี้ซ้ำ';
+$lang['Comments'] = 'ความเห็น';
+$lang['Add a comment'] = 'เพิ่มความเห็น';
+$lang['created after %s (%s)'] = 'สร้างขึ้นหลังจาก %s (%s)';
+$lang['created before %s (%s)'] = 'สร้างขึ้นก่อน %s (%s)';
+$lang['created between %s (%s) and %s (%s)'] = 'สร้างขึ้นระหว่าง %s (%s) และ %s (%s)';
+$lang['created on %s'] = 'สร้างขึ้นเมื่อ %s';
+$lang['Customize'] = 'ปรับแต่ง';
+$lang['Your Gallery Customization'] = 'การปรับแต่งแกลอรี่ของท่าน';
+$lang['Default'] = 'Default';
+$lang['delete this photo from your favorites'] = 'บภาพนี้ออกจากรายการโปรด';
+$lang['Delete'] = 'ลบ';
+$lang['descending'] = 'มาก-น้อย';
+$lang['download'] = 'ดาวน์โหลด';
+$lang['download this file'] = 'ดาวน์โหลดแฟ้มนี้';
+$lang['edit'] = 'แก้ไข';
+$lang['wrong date'] = 'วันที่ผิด';
+$lang['excluded'] = 'ไม่รวม';
+$lang['Your favorites'] = 'รายการโปรดของท่าน';
+$lang['display your favorites photos'] = 'แสดงภาพสุดโปรด';
+$lang['Favorites'] = 'รายการโปรด';
+$lang['First'] = 'ลำดับแรก';
+$lang['The gallery is locked for maintenance. Please, come back later.'] = 'แกลอรี่ปิดปรับปรุง';
+$lang['Page generated in'] = 'เวลาในการสร้างหน้านี้';
+$lang['guest'] = 'ผู้มาเยือน';
+$lang['Hello'] = 'สวัสดี';
+$lang['available for administrators only'] = 'สำหรับผู้ดูแลระบบเท่านั้น';
+$lang['display this album'] = 'แสดงอัลบั้ม';
+$lang['display last user comments'] = 'แสดงความเห็นล่าสุด';
+$lang['customize the appareance of the gallery'] = 'ปรับแต่งมุมมองของแกลอรี่';
+$lang['search'] = 'ค้นหา';
+$lang['Home'] = 'หน้าหลัก';
+$lang['in this album'] = 'ในอัลบั้มนี้';
+$lang['in %d sub-album'] = 'ใน %d อัลบั้มย่อย';
+$lang['in %d sub-albums'] = 'ใน %d อัลบั้มย่อยหลายอัน';
+$lang['Language'] = 'ภาษา';
+$lang['last %d days'] = 'ล่าสุด%d วัน';
+$lang['Last'] = 'ล่าสุด';
+$lang['Logout'] = 'ออกจากระบบ';
+$lang['E-mail address'] = 'ที่อยู่อีเมล์';
+$lang['obligatory'] = 'จำเป็น';
+$lang['Maximum photo height'] = 'จำกัดความสูงรูปภาพ';
+$lang['Maximum height must be a number superior to 50'] = 'การจำกัดความสูงของรูปภาพจะต้องไม่น้อยกว่า 50';
+$lang['Maximum photo width'] = 'จำกัดความกว้างรูปภาพ';
+$lang['Maximum width must be a number superior to 50'] = 'การจำกัดความกว้างของรูปภาพจะต้องไม่น้อยกว่า 50';
+$lang['display a calendar by creation date'] = 'แสดงปฏิทินตามวันที่สร้างอัลบั้ม';
+$lang['display all photos in all sub-albums'] = 'แสดงรูปภาพทั้งหมดในอัลบั้มย่อย';
+$lang['return to normal view mode'] = 'กลับไปยังโหมดการดูปกติ';
+$lang['display a calendar by posted date'] = 'แสดงปฏิทินตามวันที่โพส';
+$lang['Most visited'] = 'เข้าชมสูงสุด';
+$lang['display most visited photos'] = 'แสดงรูปภาพที่เข้าชมสูงสุด';
+$lang['The number of photos per page must be a not null scalar'] = 'จำนวนของการแสดงภาพถ่ายต่อหนึ่งหน้า จะต้องเป็นเลขจำนวนเต็ม';
+$lang['Number of photos per page'] = 'จำนวนรูปภาพต่อหนึ่งหน้า';
+$lang['Unknown identifier'] = 'ไม่ได้ระบุตัวตน';
+$lang['New password'] = 'รหัสผ่านใหม่';
+$lang['Rate this photo'] = 'ความนิยมรูปภาพ';
+$lang['Next'] = 'ต่อไป';
+
+
+
+?>
