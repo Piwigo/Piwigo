742c7866583e396c44e8f0321569162eca3f83a2
ddtddt <ddtddt@piwigo.org>
2011-07-21 11:20:41 +0000

    [trunk] - language - beginning of ko_KR - 한국어 – Korean Thanks to Aisika  - feature:2320
    git-svn-id: http://piwigo.org/svn/trunk@11804 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ko_KR/common.lang.php
@@ -0,0 +1,308 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: 한국어 [KR]
+Version: 2.2.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=509
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = "한국어";
+$lang_info['country'] = "대한민국";
+$lang_info['direction'] = "ltr";
+$lang_info['code'] = "en";
+$lang_info['zero_plural'] = "1";
+
+$lang['%d album updated'] = '%d 개의 앨범이 업데이트 되었습니다';
+$lang['%d albums updated'] = '%d 개의 앨범이 업데이트 되었습니다';
+$lang['%d comment to validate'] = '%d 개의 승인 대기중인 코멘트가 있습니다';
+$lang['%d comments to validate'] = '%d 개의 승인 대기중인 코멘트가 있습니다';
+$lang['%d new comment'] = '%d 개의 새 댓글이 있습니다';
+$lang['%d new comments'] = '%d 개의 새 댓글이 있습니다';
+$lang['%d comment'] = '댓글 %d 개';
+$lang['%d comments'] = '댓글 %d 개';
+$lang['%d hit'] = '조회수 %d';
+$lang['%d hits'] = '조회수 %d';
+$lang['%d new photo'] = '%d 개의 새 사진';
+$lang['%d new photos'] = '%d 개의 새 사진';
+$lang['%d new user'] = '%d 명의 신규 유저';
+$lang['%d new users'] = '%d 명의 신규 유저';
+$lang['About'] = 'About';
+$lang['All tags'] = '모든 태그';
+$lang['Any tag'] = '아무 태그';
+$lang['At least one listed rule must be satisfied.'] = '적어도 하나의 기준을 만족해야 합니다.';
+$lang['At least one tag must match'] = '적어도 하나의 태그가 일치해야 합니다';
+$lang['Author'] = '저자';
+$lang['Average rate'] = '평균';
+$lang['Albums'] = '앨범';
+$lang['Album'] = '앨범';
+$lang['Close this window'] = '창 닫기';
+$lang['Complete RSS feed (photos, comments)'] = 'RSS피드 (사진과 댓글)';
+$lang['Confirm Password'] = '비밀번호 확인';
+$lang['Connection settings'] = '연결 설정';
+$lang['Login'] = '로그인';
+$lang['Contact webmaster'] = '웹마스터에게 연락하기';
+$lang['Create a new account'] = '새 계정 만들기';
+$lang['Created on'] = '생성일';
+$lang['Creation date'] = '만들어진 날짜';
+$lang['Current password is wrong'] = '현재 비밀번호가 잘못되었습니다';
+$lang['Dimensions'] = '해상도';
+$lang['Display'] = '표시';
+$lang['Each listed rule must be satisfied.'] = '각각의 기준을 만족해야 합니다.';
+$lang['Email address'] = '메일 주소';
+$lang['Enter your personnal informations'] = '개인 정보를 입력해 주세요';
+$lang['Error sending email'] = '메일을 보내는 중 오류가 발생하였습니다';
+$lang['File name'] = '파일 이름';
+$lang['File'] = '파일';
+$lang['Filesize'] = '파일 크기';
+$lang['Filter and display'] = '필터 후 표시';
+$lang['Filter'] = '필터';
+$lang['Forgot your password?'] = '비밀번호를 잊으셨나요?';
+$lang['Go through the gallery as a visitor'] = '손님으로 앨범 구경';
+$lang['Help'] = '도움말';
+$lang['Identification'] = '확인';
+$lang['Photos only RSS feed'] = 'RSS피드 사진들';
+$lang['Keyword'] = '키워드';
+$lang['New password confirmation does not correspond'] = '새로운 비밀번호 확인이 일치하지 않습니다.';
+$lang['New password sent by email'] = '새 비밀번호가 이메일로 전송되었습니다';
+$lang['No email address'] = '이메일 주소가 없습니다';
+$lang['No classic user matches this email address'] = '이 이메일 주소와 일치하는 사용자가 없습니다';
+$lang['Notification'] = '알림';
+$lang['Number of items'] = '항목 수';
+$lang['Original dimensions'] = '원본 해상도';
+$lang['Password forgotten'] = '비밀번호를 잃어버렸습니다';
+$lang['Password'] = '비밀번호';
+$lang['Post date'] = '등록일';
+$lang['Posted on'] = '등록일';
+$lang['Profile'] = '프로필';
+$lang['Quick connect'] = '빠른 연결';
+$lang['RSS feed'] = 'RSS 피드';
+$lang['Rate'] = '등급';
+$lang['Register'] = '신규 가입';
+$lang['Registration'] = '새 사용자 등록';
+$lang['Related tags'] = '관련 태그';
+$lang['Reset'] = '초기화';
+$lang['Retrieve password'] = '비밀번호 찾기';
+$lang['Search rules'] = '검색 조건';
+$lang['Search tags'] = '태그 검색';
+$lang['Search'] = '검색';
+$lang['display available tags'] = '사용 가능한 태그 보이기';
+$lang['Send new password'] = '새 비밀번호를 보냅니다';
+$lang['Since'] = '기간';
+$lang['Sort by'] = '정렬 기준';
+$lang['Sort order'] = '정렬 순서';
+$lang['Tag'] = '태그';
+$lang['Tags'] = '태그';
+$lang['The RSS notification feed provides notification on news from this website : new photos, updated albums, new comments. Use a RSS feed reader.'] = 'RSS 피드 알림은 이 사이트의 새 사진, 댓글, 업데이트된 앨범에 대해 알려줍니다. RSS 리더와 함께 사용하세요.';
+$lang['Unknown feed identifier'] = '알 수 없는 피드 식별자입니다';
+$lang['User comments'] = '사용자 댓글';
+$lang['Username'] = '사용자 이름';
+$lang['Visits'] = '방문자';
+$lang['Webmaster'] = '웹마스터';
+$lang['Week %d'] = '이번주 %d';
+$lang['About Piwigo'] = 'Piwigo에 대해';
+$lang['You are not authorized to access the requested page'] = '이 페이지에 접근할 권한이 없습니다';
+$lang['add to caddie'] = '캐디에 추가';
+$lang['add this photo to your favorites'] = '이 사진을 즐겨찾기에 추가';
+$lang['Administration'] = '관리';
+$lang['all'] = '모두';
+$lang['ascending'] = '오름차순';
+$lang['author(s) : %s'] = '작가 : %s';
+$lang['Expand all albums'] = '모든 앨범 열기';
+$lang['posted after %s (%s)'] = '%s (%s) 이후의 포스트';
+$lang['posted before %s (%s)'] = '%s (%s) 이전의 포스트';
+$lang['posted between %s (%s) and %s (%s)'] = '%s (%s) 부터 %s (%s) 까지의 포스트';
+$lang['posted on %s'] = '%s 의 포스트';
+$lang['Best rated'] = '최고 등급';
+$lang['display best rated photos'] = '최고 등급의 사진들 보기';
+$lang['caddie'] = '캐디';
+$lang['Calendar'] = '캘린더';
+$lang['All'] = '모두';
+$lang['display each day with photos, month per month'] = '매일을 사진으로 표시하여 한달을 표시합니다';
+$lang['View'] = '보기';
+$lang['chronology_monthly_calendar'] = '월별 캘린더';
+$lang['chronology_monthly_list'] = '월별 리스트';
+$lang['chronology_weekly_list'] = '주간 리스트';
+$lang['New on %s'] = '새로 등록됨(%s)';
+$lang['Click here if your browser does not automatically forward you'] = '자동으로 이동되지 않을 땐 이곳을 클릭하세요';
+$lang['comment date'] = '의견 등록일';
+$lang['Comment'] = '의견';
+$lang['Your comment has been registered'] = '댓글이 등록되었습니다';
+$lang['Anti-flood system : please wait for a moment before trying to post another comment'] = '도배 방지 시스템: 다른 의견을 달기 전까지 잠시만 기다려 주세요.';
+$lang['Your comment has NOT been registered because it did not pass the validation rules'] = '유효 조건에 어긋난 의견이기 때문에 등록되지 않았습니다';
+$lang['An administrator must authorize your comment before it is visible.'] = '관리자의 승인이 있기 전까지 다른 사용자들이 이 의견을 볼 수 없습니다.';
+$lang['This login is already used by another user'] = '다른 사용자가 이미 이 계정을 사용중입니다';
+$lang['Comments'] = '의견';
+$lang['Add a comment'] = '의견 남기기';
+$lang['created after %s (%s)'] = '%s 이후 생성됨 (%s)';
+$lang['created before %s (%s)'] = '%s 이전 생성됨(%s)';
+$lang['created between %s (%s) and %s (%s)'] = '%s (%s) 에서 %s (%s) 사이에 생성됨';
+$lang['created on %s'] = '%s 에 생성됨';
+$lang['Customize'] = '개인화';
+$lang['Your Gallery Customization'] = '갤러리 개인화';
+$lang['day'] = '정렬';
+$lang['Default'] = '기본값';
+$lang['delete this photo from your favorites'] = '이 사진을 즐겨찾기에서 삭제';
+$lang['Delete'] = '삭제';
+$lang['descending'] = '내림차순';
+$lang['download'] = '내려받기';
+$lang['download this file'] = '이 파일을 내려받기';
+$lang['edit'] = '편집';
+$lang['wrong date'] = '잘못된 날짜';
+$lang['excluded'] = '제외됨';
+$lang['Your favorites'] = '나의 즐겨찾기';
+$lang['display your favorites photos'] = '내 즐겨찾는 사진들을 표시';
+$lang['Favorites'] = '즐겨찾기';
+$lang['First'] = '처음';
+$lang['The gallery is locked for maintenance. Please, come back later.'] = '이 갤러리는 유지/보수를 위해 잠겨 있습니다. 나중에 다시 방문해 주세요.';
+$lang['Page generated in'] = '페이지 생성됨';
+$lang['guest'] = '손님';
+$lang['Hello'] = '안녕하세요!';
+$lang['available for administrators only'] = '관리자만 사용 가능합니다';
+$lang['display this album'] = '이 앨범을 표시';
+$lang['display last user comments'] = '마지막 사용자 의견을 표시';
+$lang['customize the appareance of the gallery'] = '갤러리 외형을 개인화합니다';
+$lang['search'] = '검색';
+$lang['Home'] = '홈';
+$lang['day'][0] = "일요일";
+$lang['day'][1] = "월요일";
+$lang['day'][2] = "화요일";
+$lang['day'][3] = "수요일";
+$lang['day'][4] = "목요일";
+$lang['day'][5] = "금요일";
+$lang['day'][6] = "토요일";
+$lang['month'][10] = "10월";
+$lang['month'][11] = "11월";
+$lang['month'][12] = "12월";
+$lang['month'][1] = "1월";
+$lang['month'][2] = "2월";
+$lang['month'][3] = "3월";
+$lang['month'][4] = "4월";
+$lang['month'][5] = "5월";
+$lang['month'][6] = "6월";
+$lang['month'][7] = "7월";
+$lang['month'][8] = "8월";
+$lang['month'][9] = "9월";
+$lang['Links'] = '링크';
+$lang['in this album'] = '이 앨범에서';
+$lang['in %d sub-album'] = '%d 하위 앨범에서';
+$lang['in %d sub-albums'] = '%d 하위 앨범에서';
+$lang['included'] = '포함됨';
+$lang['Invalid password!'] = '잘못된 비밀번호!';
+$lang['Language'] = '언어';
+$lang['last %d days'] = '최근 %d 일';
+$lang['Last'] = '마지막';
+$lang['Logout'] = '로그아웃';
+$lang['E-mail address'] = 'E-mail 주소';
+$lang['obligatory'] = '필수';
+$lang['Maximum photo height'] = '최대 높이';
+$lang['Maximum height must be a number superior to 50'] = '높이가 적어도 50이상은 되어야 합니다';
+$lang['Maximum photo width'] = '최대 넓이';
+$lang['Maximum width must be a number superior to 50'] = '넓이가 적어도 50이상은 되어야 합니다';
+$lang['display a calendar by creation date'] = '만들어진 날짜별로 달력 표시';
+$lang['display all photos in all sub-albums'] = '하위 앨범의 모든 사진 표시';
+$lang['return to normal view mode'] = '일반 모드로 돌아감';
+$lang['display a calendar by posted date'] = '등록된 날짜별로 달력 표시';
+$lang['Most visited'] = '최다 방문';
+$lang['display most visited photos'] = '가장 많이 방문한 사진 표시';
+$lang['The number of photos per row must be a not null scalar'] = '한 행에 표시할 사진 수는 0이 아닌 정수여야 합니다';
+$lang['Number of photos per row'] = '한 행에 표시할 사진 수';
+$lang['The number of rows per page must be a not null scalar'] = '한 페이지에 표시할 행 수는 0이 아닌 정수여야 합니다';
+$lang['Number of rows per page'] = '한 페이지에 표시할 행 수';
+$lang['Unknown identifier'] = '알 수 없는 식별자';
+$lang['New password'] = '새 비밀번호';
+$lang['Rate this photo'] = '이 사진 평가';
+$lang['Next'] = '다음';
+$lang['no rate'] = '등급 없음';
+$lang['Photos posted within the last %d day.'] = '%d 일 이내에 등록된 사진.';
+$lang['Photos posted within the last %d days.'] = '%d 일 이내에 등록된 사진.';
+$lang['password updated'] = '비밀번호가 변경되었습니다';
+$lang['Recent period must be a positive integer value'] = '최근 기간은 양의 정수여야 합니다';
+$lang['photo'] = '사진';
+$lang['Click on the photo to see it in high definition'] = '고해상도로 보시려면 사진을 클릭하세요';
+$lang['Show file metadata'] = 'Metadata 보기';
+$lang['Powered by'] = 'Powered by';
+$lang['Preferences'] = '환경설정';
+$lang['Previous'] = '이전';
+$lang['Random photos'] = '무작위 사진';
+$lang['display a set of random photos'] = '무작위로 사진들을 보여줍니다';
+$lang['Recent albums'] = '최근 앨범';
+$lang['display recently updated albums'] = '최근에 업데이트된 앨범을 보여줍니다';
+$lang['Recent period'] = '최근 기간';
+$lang['Recent photos'] = '최근 사진';
+$lang['display most recent photos'] = '가장 최근의 사진을 보여줍니다';
+$lang['Redirection...'] = '연결중...';
+$lang['Please, enter a login'] = '로그인 정보를 입력하세요';
+$lang['login mustn\'t end with a space character'] = '공백문자로 끝낼 수 없습니다';
+$lang['login mustn\'t start with a space character'] = '공백문자로 시작할 수 없습니다';
+$lang['this login is already used'] = '이 정보는 이미 사용중입니다';
+$lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = '메일 주소는 xxx@yyy.eee 처럼 되야 합니다. (예 : jack@altern.org)';
+$lang['please enter your password again'] = '비밀번호를 다시 입력 해 주세요';
+$lang['Auto login'] = '자동 로그인';
+$lang['remove this tag from the list'] = '목록에서 이 태그를 삭제';
+$lang['representative'] = '대표사진';
+$lang['Search for Author'] = '작가로 검색';
+$lang['Search in albums'] = '앨범에서 검색';
+$lang['Search by Date'] = '날짜로 검색';
+$lang['Date'] = '날짜';
+$lang['End-Date'] = '끝나는 날짜';
+$lang['Kind of date'] = '날짜의 종류';
+$lang['Submit'] = '등록';
+$lang['Yes'] = '예';
+$lang['No'] = '아니오';
+$lang['%d photo'] = '%d 개 사진';
+$lang['%d photos'] = '%d 개 사진';
+$lang['%d photo is also linked to current tags'] = '현재 태그에 %d 개의 사진이 연결되어 있습니다';
+$lang['%d photos are also linked to current tags'] = '현재 태그에 %d 개의 사진들이 연결되어 있습니다';
+$lang['display photos linked to this tag'] = '이 태그에 연결된 사진들 보이기';
+$lang['photos posted during the last %d days'] = '최근 %d 일간 등록된 사진들';
+$lang['Piwigo Help'] = 'Piwigo 도움말';
+$lang['Rank'] = '등급';
+$lang['group by letters'] = '문자로 그룹화';
+$lang['letters'] = '문자';
+$lang['show tag cloud'] = '태그 구름 보이기';
+$lang['cloud'] = '구름';
+$lang['Reset to default values'] = '기본값으로 설정';
+$lang['delete all photos from your favorites'] = '즐겨찾기의 모든 사진을 지웁니다';
+$lang['Sent by'] = '보낸이';
+$lang['Cookies are blocked or not supported by your browser. You must enable cookies to connect.'] = '쿠키가 차단되었거나 지원되지 않는 브라우저입니다. 로그인하기 위해서는 쿠키를 사용해야 합니다.';
+$lang['edit this comment'] = '이 댓글 편집';
+$lang['Edit a comment'] = '댓글 편집';
+$lang['Are you sure?'] = '확실합니까?';
+$lang['(!) This comment requires validation'] = '(!) 이 댓글은 확인이 필요합니다.';
+$lang['Welcome'] = '환영합니다';
+$lang['Welcome to your Piwigo photo gallery!'] = 'Piwigo 포토 갤러리에 오신것을 환영합니다!';
+$lang['... or browse your empty gallery'] = '... 혹은 빈 갤러리를 탐색합니다';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... 혹은 이 메시지를 비활성화합니다. 스스로 무언가를 해 봅니다.';
+$lang['Hello %s, your Piwigo photo gallery is empty!'] = '안녕하세요 %s, 현재 갤러리가 비었습니다!';
+$lang['I want to add photos'] = '사진을 등록하고 싶습니다';
+$lang['Manage this user comment: %s'] = '이 사용자 댓글 관리: %s';
+$lang['Email address is missing. Please specify an email address.'] = '메일 주소란이 비었습니다. 메일 주소를 확인 해 주세요.';
+$lang['This author modified following comment:'] = '이 작가가 다음 댓글을 수정하였습니다:';
+$lang['This author removed the comment with id %d'] = '이 작가가 다음 사용자의 댓글을 삭제하였습니다: %d';
+$lang['delete this comment'] = '이 댓글을 삭제';
+$lang['validate this comment'] = '이 댓글을 확인';
+
+
+?>
