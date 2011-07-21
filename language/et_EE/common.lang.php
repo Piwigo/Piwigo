16570f32d203c369845c5109eeddaad1e13b0224
ddtddt <ddtddt@piwigo.org>
2011-07-21 10:40:54 +0000

    [trunk] - language - beginning of et_EE Thanks to jolly  - feature:2320
    git-svn-id: http://piwigo.org/svn/trunk@11803 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/common.lang.php
@@ -0,0 +1,140 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Estonian [EE]
+Version: 2.3.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=569
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = "Estonian";
+$lang_info['country'] = "Estonia";
+$lang_info['direction'] = "ltr";
+$lang_info['code'] = "et";
+$lang_info['zero_plural'] = "1";
+
+$lang['%d album updated'] = '%d album uuendatud';
+$lang['%d albums updated'] = '%d albumit uuendatud';
+$lang['%d comment to validate'] = '%d kommentaar kinnitada';
+$lang['%d comments to validate'] = '%d kommentaari kinnitada';
+$lang['%d new comment'] = '%d uus kommentaar';
+$lang['%d new comments'] = '%d uut kommentaari';
+$lang['%d comment'] = '%d kommentaar';
+$lang['%d comments'] = '%d kommentaari';
+$lang['%d hit'] = '%d tabamus';
+$lang['%d hits'] = '%d tabamust';
+$lang['%d new photo'] = '%d uus foto';
+$lang['%d new photos'] = '%d uut fotot';
+$lang['%d new user'] = '%d uus kasutaja';
+$lang['%d new users'] = '%d uut kasutajat';
+$lang['About'] = 'About';
+$lang['All tags'] = 'Kõik sildid';
+$lang['Any tag'] = 'Mistahes silt';
+$lang['At least one listed rule must be satisfied.'] = 'Vähemalt üks loetletud reeglitest peab olema rahuldatud.';
+$lang['At least one tag must match'] = 'Vähemalt üks silt peab sobima';
+$lang['Author'] = 'Autor';
+$lang['Average rate'] = 'Keskmine hinne';
+$lang['Albums'] = 'Albumid';
+$lang['Album'] = 'Album';
+$lang['Close this window'] = 'Sule see aken';
+$lang['Complete RSS feed (photos, comments)'] = 'Täielik RSS voog (pildid, kommentaarid)';
+$lang['Confirm Password'] = 'Kinnita parool';
+$lang['Connection settings'] = 'Ühenduse seaded';
+$lang['Login'] = 'Kasutajanimi';
+$lang['Contact webmaster'] = 'Kontakteeru webmasteriga';
+$lang['Create a new account'] = 'Loo uus konto';
+$lang['Created on'] = 'Loodud';
+$lang['Creation date'] = 'Koostamise kuupäev';
+$lang['Current password is wrong'] = 'Praegune parool on vale';
+$lang['Dimensions'] = 'Dimensioonid';
+$lang['Display'] = 'Näita';
+$lang['Each listed rule must be satisfied.'] = 'Kõik loetletud reeglid peavad olema rahuldatud.';
+$lang['Email address'] = 'E-posti aadress';
+$lang['Enter your personnal informations'] = 'Sisesta isiklikud andmed';
+$lang['Error sending email'] = 'Viga e-posti saatmisel';
+$lang['File name'] = 'Faili nimi';
+$lang['File'] = 'Fail';
+$lang['Filesize'] = 'Failisuurus';
+$lang['Filter and display'] = 'Filtreeri ja kuva';
+$lang['Filter'] = 'Filtreeri';
+$lang['Forgot your password?'] = 'Unustasid parooli?';
+$lang['Go through the gallery as a visitor'] = 'Vaata galerii läbi külastajana';
+$lang['Help'] = 'Abi';
+$lang['Identification'] = 'Identiditseerimine';
+$lang['Photos only RSS feed'] = 'Ainult piltide RSS voog';
+$lang['Keyword'] = 'Märksõna';
+$lang['day'][0] = 'Pühapäev';
+$lang['day'][1] = 'Esmaspäev';
+$lang['day'][2] = 'Teisipäev';
+$lang['day'][3] = 'Kolmapäev';
+$lang['day'][4] = 'Neljapäev';
+$lang['day'][5] = 'Reede';
+$lang['day'][6] = 'Laupäev';
+$lang['month'][10] = 'Oktoober';
+$lang['month'][11] = 'November';
+$lang['month'][12] = 'Detsember';
+$lang['month'][1] = 'Jaanuar';
+$lang['month'][2] = 'Veebruar';
+$lang['month'][3] = 'Märts';
+$lang['month'][4] = 'Aprill';
+$lang['month'][5] = 'Mai';
+$lang['month'][6] = 'Juuni';
+$lang['month'][7] = 'Juuli';
+$lang['month'][8] = 'August';
+$lang['month'][9] = 'September';
+$lang['New password confirmation does not correspond'] = 'Uus paroolikinnitus ei sobi';
+$lang['New password sent by email'] = 'Uus parool saadetud e-postiga';
+$lang['No email address'] = 'E-posti aadress puudu';
+$lang['No classic user matches this email address'] = 'Mitte ükski klassikaline kasutaja ei sobitu selle e-postiga';
+$lang['Notification'] = 'Teadaanne';
+$lang['Number of items'] = 'Ühikute arv';
+$lang['Original dimensions'] = 'Originaal mõõtmed';
+$lang['Password forgotten'] = 'Unustatud parool';
+$lang['Password'] = 'Parool';
+$lang['Post date'] = 'Postituse kuupäev';
+$lang['Posted on'] = 'Postitatud';
+$lang['Profile'] = 'Profiil';
+$lang['Quick connect'] = 'Kiirühendus';
+$lang['RSS feed'] = 'RSS voog';
+$lang['Rate'] = 'Hinda';
+$lang['Register'] = 'Registreeru';
+$lang['Registration'] = 'Registreerimine';
+$lang['Related tags'] = 'Seotud märksõnad';
+$lang['Reset'] = 'Algseadista';
+$lang['Retrieve password'] = 'Tagasta parool';
+$lang['Search rules'] = 'Otsingu eeskirjad';
+$lang['Search tags'] = 'Otsingu märksõnad';
+$lang['Search'] = 'Otsi';
+$lang['display available tags'] = 'kuva võimalikud märksõnad';
+$lang['Send new password'] = 'Saada uus parool';
+$lang['Since'] = 'Alates';
+$lang['Sort by'] = 'Sorteeri';
+$lang['Sort order'] = 'Sorteerimisjärjestus';
+$lang['Tag'] = 'Märksõna';
+$lang['Tags'] = 'Märksõnad';
+
+
+
+
+?>
