81e9fcc116cd0db6ed95e0131ff995566eb97940
ddtddt <ddtddt@piwigo.org>
2011-07-22 06:54:59 +0000

    [trunk] - language - update
    git-svn-id: http://piwigo.org/svn/trunk@11810 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/admin.lang.php
@@ -0,0 +1,86 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+// +                                                                       +
+// + Revised by AM on 18/04/2011                                           +
+// +                                                                       +
+// +-----------------------------------------------------------------------+
+
+$lang[' and %d virtual'] = " και %d εικονικό";
+$lang[' and %d virtual'] = " και %d εικονικά";
+$lang['%d album including'] = "%d λεύκωμα περιλαμβάνει";
+$lang['%d album moved'] = "%d λεύκωμα μετακινήθηκε";
+$lang['%d albums including'] = "%d λευκώματα περιλαμβάνουν";
+$lang['%d albums moved'] = "%d λευκώματα μετακινήθηκαν";
+$lang['%d anomalies have been detected corrected.'] = "%d ασυνέπειες εντοπίστηκαν και διορθώθηκαν.";
+$lang['%d anomalies have been detected.'] = "%d ασυνέπειες εντοπίστηκαν.";
+$lang['%d anomalies have been ignored.'] = "%d ασυνέπειες αγνοήθηκαν.";
+$lang['%d anomalies have not been corrected.'] = "%d ασυνέπειες δεν διορθώθηκαν.";
+$lang['%d anomaly has been corrected.'] = "%d ασυνέπεια εντοπίστηκε και διορθώθηκε.";
+$lang['%d anomaly has been detected.'] = "%d ασυνέπεια εντοπίστηκε.";
+$lang['%d anomaly has been ignored.'] = "%d ασυνέπεια αγνοήθηκε.";
+$lang['%d anomaly has not been corrected.'] = "%d ασυνέπεια δεν διορθώθηκε.";
+$lang['%d association'] = "%d συσχέτιση";
+$lang['%d associations'] = "%d συσχετίσεις";
+$lang['%d group'] = "%d ομάδα";
+$lang['%d groups'] = "%d ομάδες";
+$lang['%d guest'] = "%d επισκέπτης";
+$lang['%d guests'] = "%d επισκέπτης";
+$lang['%d line filtered'] = "%d γραμμή παραλείφθηκε";
+$lang['%d lines filtered'] = "%d γραμμές παραλείφθηκαν";
+$lang['%d mail was not sent.'] = "%d μήνυμα δεν εστάλη.";
+$lang['%d mail was sent.'] = "%d μήνυμα εστάλη.";
+$lang['%d mails were not sent.'] = "%d μηνύματα δεν εστάλησαν.";
+$lang['%d mails were sent.'] = "%d μηνύματα εστάλησαν.";
+$lang['%d member'] = "%d μέλος";
+$lang['%d members'] = "%d μέλη";
+$lang['%d of %d photos selected'] = '%d από %d φωτογραφίες επιλεγμένες';
+$lang['%d parameter was updated.'] = "%d παράμετρος ενημερώθηκε.";
+$lang['%d parameters were updated.'] = "%d παράμετροι ενημερώθηκαν.";
+$lang['%d photo was deleted'] = "%d φωτογραφία διαγράφηκε";
+$lang['%d photos uploaded'] = '%d φωτογραφίες ανέβηκαν';
+$lang['%d photos were deleted'] = "%d φωτογραφίες διαγράφηκαν";
+$lang['%d physical'] = " %d φυσικό";
+$lang['%d physical'] = " %d φυσικά";
+$lang['%d tag'] = "%d ετικέτα";
+$lang['%d tags'] = "%d ετικέτες";
+$lang['%d user comment rejected'] = "%d σχόλιο απορρίφθηκε";
+$lang['%d user comment validated'] = "%d σχόλιο επικυρώθηκε";
+$lang['%d user comments rejected'] = "%d σχόλια απορρίφθηκαν";
+$lang['%d user comments validated'] = "%d σχόλια επικυρώθηκαν";
+$lang['%d user deleted'] = "%d χρήστης διαγράφηκε";
+$lang['%d user was not updated.'] = "%d χρήστης δεν ενημερώθηκε.";
+$lang['%d user was updated.'] = "%d χρήστης ενημερώθηκε.";
+$lang['%d user'] = "%d χρήστης";
+$lang['%d users deleted'] = "%d χρήστες διαγράφηκαν";
+$lang['%d users were not updated.'] = "%d χρήστες δεν ενημερώθηκαν.";
+$lang['%d users were updated.'] = "%d χρήστες ενημερώθηκαν.";
+$lang['%d users'] = "%d χρήστες";
+$lang['%d waiting for validation'] = "%d περιμένουν επικύρωση";
+$lang['%s has been successfully upgraded.'] = "%s αναβαθμίστηκε επιτυχώς.";
+$lang['%s must be to set to false in your local/config/config.inc.php file'] = "%s must be set to false in your local/config/config.inc.php file";
+$lang['%s value is not correct file because exif are not supported'] = "%s πεδίο είναι λάθος γιατί δεν υποστηρίζεται η πληροφορία EXIF";
+$lang['+ Add an upload box'] = '+ Προσθήκη και άλλου αρχείου';
+$lang[', click on'] = ", κάντε κλικ";
+
+
+?>
