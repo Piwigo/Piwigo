6a4096f8b941d84606adeaa442824d38a547fba5
plegall <plg@piwigo.org>
2011-09-08 09:30:16 +0000

    change version number to 2.3.0RC2
    git-svn-id: http://piwigo.org/svn/trunk@12101 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.3.0RC1');
+define('PHPWG_VERSION', '2.3.0RC2');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'Sylvia');
 
