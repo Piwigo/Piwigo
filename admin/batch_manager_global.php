33c38abc00c24051f22f4ba79063b2446e52c4a5
plegall <plg@piwigo.org>
2011-10-01 22:53:29 +0000

    coding guideline: replace tabs by spaces
    git-svn-id: http://piwigo.org/svn/trunk@12295 68402e56-0260-453c-a942-63ccdbb3a9ee

--- admin/batch_manager_global.php
+++ admin/batch_manager_global.php
@@ -540,11 +540,14 @@ $template->assign(
 
 if (!empty($_SESSION['bulk_manager_filter']['tags']))
 {
-	$query = '
-SELECT id, name
-	FROM '.TAGS_TABLE.'
-	WHERE id IN ('.implode(',', $_SESSION['bulk_manager_filter']['tags']).')';
-	$template->assign('filter_tags', get_taglist($query));
+  $query = '
+SELECT
+    id,
+    name
+  FROM '.TAGS_TABLE.'
+  WHERE id IN ('.implode(',', $_SESSION['bulk_manager_filter']['tags']).')
+;';
+  $template->assign('filter_tags', get_taglist($query));
 }
 
 // Virtualy associate a picture to a category
