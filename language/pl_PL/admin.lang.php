b513fc1cd61a7b69a691b2523e957b2a97218524
ddtddt <ddtddt@piwigo.org>
2011-10-02 16:12:32 +0000

    [trunk] - language - update pl_PL - Polski - Polish Recovery in plugin  - feature:2320
    git-svn-id: http://piwigo.org/svn/trunk@12311 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/admin.lang.php
+++ language/pl_PL/admin.lang.php
@@ -861,4 +861,7 @@ $lang['Select at least two tags for merging'] = 'Wybierz co najmniej dwa tagi do
 $lang['Select an album'] = 'Wybierz album';
 $lang['By default, Piwigo will create a new websize from the HD (high definition) version of your photo.'] = 'Piwigo stworzy domyślnie nową stronę z twoich zdjęć w jakości HD.';
 $lang['If no HD is available and if the current websize is bigger than resize dimensions, Piwigo will move it as HD and create a downsized websize photo from it.'] = 'Jeśli wersja HD jest niedostępna a bieżący rozmiar jest większy niż rozmiar zmiany, Piwigo przesunie zdjęcia jako HD i stworzy z nich pomniejszone wersje.'; 
+$lang['An error has occured during upgrade.'] = 'Wystąpił błąd podczas aktualizacji.';
+$lang['An error has occured during extract. Please check files permissions of your piwigo installation.<br><a href="%s">Click here to show log error</a>.'] = 'Wystąpił błąd podczas rozpokowywania. Sprawdź uprawnienia do plików w katalogu gdize jest zainstalowane piwigo.<br><a href="%s">Kliknij aby zobaczyć błędy</a>.';
+$lang['Piwigo cannot retrieve upgrade file from server'] = 'Piwigo nie może ściągnąć pliku aktualizacji z serwera';
 ?>
