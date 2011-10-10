b4558f32f1662d959eb42a52dedabbe5cb22bfdc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2011-10-10 15:10:04 +0000

    [trunk (Admin)] Update af_ZA, thanks to : maeztro
    git-svn-id: http://piwigo.org/svn/trunk@12398 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/af_ZA/install.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Admin configuration'] = 'Admin konfigurasie';
+$lang['Basic configuration'] = 'Basiese konfigurasie';
+$lang['Congratulations, Piwigo installation is completed'] = 'Baie geluk, Piwigo installasie is voltooi';
+$lang['Database configuration'] = 'Databasis konfigurasie';
+$lang['Download the config file'] = 'Laai die konfigurasie lêer af';
+$lang['Installation'] = 'Installasie';
+$lang['Just another Piwigo gallery'] = 'Net nog \'n Piwigo gallery';
+$lang['Password [confirm]'] = 'Wagwoord [bevestig]';
+$lang['PHP 5 is required'] = 'PHP 5 word vereis';
+$lang['Sorry!'] = 'Jammer!';
+$lang['Start Install'] = 'Begin installasie';
+$lang['User'] = 'Gebruiker';
+$lang['verification'] = 'verifikasie';
+$lang['Webmaster login'] = 'Webmeester login';
+$lang['Webmaster mail address'] = 'Webmeester epos adres';
+$lang['Welcome to my photo gallery'] = 'Welkom by my fotogallery';
+$lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Jy kan die konfigurasie lêer aflaai en oplaai na die local/config gids van jou installasie';
+?>
