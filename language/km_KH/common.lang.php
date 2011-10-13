cc2e3d775405bed1825d04a0e079120cada7dd3b
ddtddt <ddtddt@piwigo.org>
2011-10-13 17:29:42 +0000

    [trunk] - language - add km_KH – ភាសា- khmer (Cambogia) Thanks to Vanndy - Step 1 - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@12425 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/km_KH/common.lang.php
@@ -0,0 +1,90 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: ភាសា [KH]
+Version: 2.3.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=579
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = "ភាសា​";
+$lang_info['country'] = "ប្រទេស";
+$lang_info['direction'] = "ltr";
+$lang_info['code'] = "en";
+$lang_info['zero_plural'] = "1";
+
+$lang['%d album updated'] = '%d សៀវ​ភៅ​រូប​ថត​បាន​អាប់​ដែត​';
+$lang['%d albums updated'] = '%d សៀវ​ភៅ​រូប​ថត​ជា​ច្រើន​បាន​អាប់​ដែត​';
+$lang['%d comment to validate'] = '%d បញ្ចេញ​មតិដើម្បី​បញ្ជាក់​';
+$lang['%d comment'] = '%d បញ្ចេញមតិ';
+$lang['%d comments to validate'] = '%d បញ្ចេញ​មតិ​ផ្សេងៗ​ដើម្បី​បញ្ជាក់​';
+$lang['%d comments'] = '%d មតិ​ផ្សេងៗ';
+$lang['%d hit'] = '%d ដង';
+$lang['%d hit'] = '%d ចុច';
+$lang['%d hits'] = '%d ដង';
+$lang['%d Kb'] = '%d​ Kb';
+$lang['%d new comment'] = '%d មតិ​ថ្មី​';
+$lang['%d new comments'] = '%d មតិ​ថ្មីៗ​';
+$lang['%d new photo'] = '%d រូបថត​ថ្មី​';
+$lang['%d new photos'] = '%d រូប​ថត​ថ្មីៗ';
+$lang['%d new user'] = '%d អ្នក​ប្រើ​ប្រាស់​ថ្មី​';
+$lang['%d new users'] = '%d ​អ្នក​ប្រើ​ប្រាស់​ថ្មី​ច្រើន​នាក់​';
+$lang['%d rates'] = '%d ការ​វាយ​តម្លៃ​ទាំង​ឡាយ​';
+$lang['About'] = 'អំពី';
+$lang['All tags'] = 'ស្លាក​ទាំងអស់';
+$lang['Any tag'] = 'ស្លាក​ផ្សេង​ទៀត​';
+$lang['At least one listed rule must be satisfied.'] = 'យ៉ាងហោច​ណាស់​​បញ្ជីអត្ថបទ​មួយ​ត្រូវ​​ធ្វើ​ឲ្យ​បាន​ត្រឹម​ត្រូវ​សិន';
+$lang['At least one tag must match'] = 'យ៉ាង​ហោច​ណាស់​មាន​ ស្លាក​មួយ​ដាក់ត្រូវ​គ្នា';
+$lang['Author'] = 'អ្នក​និ​ពន្ធ​';
+$lang['Albums'] = 'សៀវ​ភៅ​រូប​ថត​ច្រើន​';
+$lang['Album'] = 'សៀវ​ភៅ​រូប​ថត​';
+$lang['Close this window'] = 'បិទ​ផ្ទាំង​មួយ​នេះ';
+$lang['Confirm Password'] = 'បញ្ជាក់​អក្សរ​សម្ងាត់​';
+$lang['Connection settings'] = 'ការ​កំណត់​បណ្តាញ​ភ្ជាប់​';
+$lang['Login'] = 'ចូល';
+$lang['Contact webmaster'] = 'ទំនាក់ទំនង អ្នកជំនាញ​ខាងវិបសាយ​';
+$lang['Create a new account'] = 'បង្កើត​គណនី​ថ្មី';
+$lang['Created on'] = 'បាន​បង្កើត​នៅ​​';
+$lang['Creation date'] = 'ថ្ងៃ​ដែល​បាន​​បង្កើត​';
+$lang['Current password is wrong'] = 'អក្សរ​សម្ងាត់​​នេះ​ខុស​';
+$lang['Dimensions'] = 'ទំហំ';
+$lang['Display'] = 'បង្ហាញ';
+$lang['Each listed rule must be satisfied.'] = 'គ្រប់​បញ្ជីអត្ថបទ​ទាំង​អស់​ត្រូវ​តែធ្វើ​ឲ្យ​បាន​ត្រឹម​ត្រូវ​សិន';
+$lang['Email address'] = 'អាស័យដ្ឋាន​អេឡិច​ត្រូ​និច​';
+$lang['Enter your personnal informations'] = 'បញ្ចូល​ពត៌​មាន​ផ្ទាល់​ខ្លួន​របស់​អ្នក​';
+$lang['Error sending email'] = 'មាន​បញ្ហា​កើត​ឡើង​ពេល​កំពុង​ផ្ញើ​សារ​អេ​ឡិច​ត្រូនិច';
+$lang['File name'] = 'ឈ្មោះ​ឯកសារ';
+$lang['File'] = 'ឯកសារ​';
+$lang['Filesize'] = 'ទំហំ​ឯកសារ';
+$lang['Filter and display'] = 'ច្រោះយក​ និង​ បង្ហាញ';
+$lang['Filter'] = 'ច្រោះ​យក​';
+$lang['Forgot your password?'] = 'ភ្លេច​អក្សរ​សម្ងាត់​របស់​អ្នក?';
+$lang['Go through the gallery as a visitor'] = 'ចូលក្នុង​វិចិត្រ​សាល​ក្នុង​នាម​ជា​ភ្ញៀវ';
+$lang['Help'] = 'ជំនួយ';
+$lang['Identification'] = 'អត្ត​សញ្ញាណ​';
+$lang['Photos only RSS feed'] = 'រូប​ភាព​សំរាប់​តែ RSS feed';
+$lang['Keyword'] = 'ពាក្យ​គន្លឹះ';
+$lang['Links'] = 'តំណភ្ជាប់​ជាច្រើន';
+
+?>
