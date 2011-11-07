96e78f4654650d1e10113d3d4b073bc796af3b77
plegall <plg@piwigo.org>
2011-11-07 10:18:18 +0000

    merge r12565 from trunk to branch 2.3
    [trunk (admin)] Update de_DE, thanks to : Polly
    
    
    git-svn-id: http://piwigo.org/svn/branches/2.3@12567 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/install.lang.php
+++ language/de_DE/install.lang.php
@@ -72,4 +72,6 @@ $lang['An alternate solution is to copy the text in the box above and paste it i
 $lang['Creation of config file local/config/database.inc.php failed.'] = 'Die Erstellung der Datei local/config/database.inc.php ist fehlgeschlagen.';
 $lang['Download the config file'] = 'Lade die Konfigurationsdatei herunter';
 $lang['You can download the config file and upload it to local/config directory of your installation.'] = 'SIe können die Konfigurationsdatei herunterladen und in den Ordner local/config ihrer Installation hochladen.';
+$lang['Just another Piwigo gallery'] = 'Nur eine neue Piwigo Bildgallerie';
+$lang['Welcome to my photo gallery'] = 'Willkommen bei meiner Bildgallerie';
 ?>
