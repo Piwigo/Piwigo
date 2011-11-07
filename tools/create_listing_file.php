ff687492bbc9182362301de07f448ee4057fceee
plegall <plg@piwigo.org>
2011-11-07 11:02:27 +0000

    next version is 2.3.1
    git-svn-id: http://piwigo.org/svn/branches/2.3@12577 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = '2.3.0';
+$conf['version'] = '2.3.1';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
