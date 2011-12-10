e84762613e82bffe58b1ab8fcc2676ad26928af2
ddtddt <ddtddt@piwigo.org>
2011-12-10 07:45:05 +0000

    [trunk] - language - Add - fi_FI  – Finnish - Thanks to Jussi Sunnari - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@12717 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/common.lang.php
@@ -0,0 +1,88 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Finnish [FI]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = "Finnish";
+$lang_info['country'] = "Suomi";
+$lang_info['direction'] = "ltr";
+$lang_info['code'] = "fi";
+$lang_info['zero_plural'] = "1";
+
+$lang['%d album updated'] = '%d albumi päivitetty';
+$lang['%d albums updated'] = '%d albumit päivitetty';
+$lang['%d comment to validate'] = '%d kommentin vahvistus';
+$lang['%d comments to validate'] = '%d kommentien vahvistus';
+$lang['%d new comment'] = '%d uusi kommentti';
+$lang['%d new comments'] = '%d uudet kommentit';
+$lang['%d comment'] = '%d kommentti';
+$lang['%d comments'] = '%d kommentit';
+$lang['%d hit'] = '%d osuma';
+$lang['%d hits'] = '%d osumat';
+$lang['%d new photo'] = '%d uusi kuva';
+$lang['%d new photos'] = '%d uudet kuvat';
+$lang['%d new user'] = '%d uusi käyttäjä';
+$lang['%d new users'] = '%d uudet käyttäjät';
+$lang['About'] = 'About';
+$lang['All tags'] = 'Kaikki merkinnät';
+$lang['Any tag'] = 'Merkintä';
+$lang['At least one listed rule must be satisfied.'] = 'At least one listed criterion must be satisfied.';
+$lang['At least one tag must match'] = 'At least one tag must match';
+$lang['Author'] = 'Tekijä';
+$lang['Average rate'] = 'Keskiarvo pisteet';
+$lang['Albums'] = 'Albumit';
+$lang['Album'] = 'Albumi';
+$lang['Close this window'] = 'Sulje tämä ikkuna';
+$lang['Complete RSS feed (photos, comments)'] = 'Täydellinen RSS-syöte (kuvat, kommentit)';
+$lang['Confirm Password'] = 'Vahvista salasana';
+$lang['Connection settings'] = 'Yhteysasetukset';
+$lang['Login'] = 'Kirjaannu';
+$lang['Contact webmaster'] = 'Ota yhteys webmasteriin';
+$lang['Create a new account'] = 'Luo uusi tili';
+$lang['Created on'] = 'Luotu';
+$lang['Creation date'] = 'Luontipäivämäärä';
+$lang['Current password is wrong'] = 'Salasana on väärä';
+$lang['Dimensions'] = 'Mitat';
+$lang['Display'] = 'Näytä';
+$lang['Each listed rule must be satisfied.'] = 'Kaikkien kohtien on oltava oikein.';
+$lang['Email address'] = 'Sähköpostiosoite';
+$lang['Enter your personnal informations'] = 'Henkilökohtaiset tietosi';
+$lang['Error sending email'] = 'Virhe sähköpostin lähettämisessä';
+$lang['File name'] = 'Tiedostonimi';
+$lang['File'] = 'Tiedosto';
+$lang['Filesize'] = 'Tiedoston koko';
+$lang['Filter and display'] = 'Suodatin ja näyttö';
+$lang['Filter'] = 'Suodatin';
+$lang['Forgot your password?'] = 'Unohditko salasanasi?';
+$lang['Go through the gallery as a visitor'] = 'Selaa galleriaa vieraana';
+$lang['Help'] = 'Apua';
+$lang['Identification'] = 'Tunnistus';
+$lang['Photos only RSS feed'] = 'Kuvat ainoastaan RSS listauksena';
+$lang['Keyword'] = 'Avainsana';
+
+?>
