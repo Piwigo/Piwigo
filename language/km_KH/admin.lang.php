c9f58763dcb89bb8e36765d20903b11f1a7e1bff
ddtddt <ddtddt@piwigo.org>
2011-12-30 16:13:16 +0000

    [trunk] - language - add km_KH – ​ភាសា​ខ្មែរ​- khmer (Cambodia) Thanks to Vanndy - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@12808 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/km_KH/admin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+
+$lang['Add to caddie'] = 'ដាក់​ចូល​ទៅ​ក្នុង​កូន​ប្រអប់';
+$lang['Notification'] = "ការ​ផ្តល់​ដំណឹង​";
+$lang['Upload Photos'] = 'បញ្ចូល​រូប​ថត​';
+$lang['Select an album'] = 'ជ្រើស​រើស​សៀវ​ភៅ​រូប​ថត​';
+$lang['... or '] = '... ឬ ';
+$lang['create a new album'] = 'បង្កើត​សៀវ​ភៅ​រូប​ថត​ថ្មី​មួយ';
+$lang['Select files'] = 'ជ្រើស​រើស​រូប​ថត​';
+$lang['Start Upload'] = 'ចាប់​ផ្តើម​បញ្ចូល​';
+$lang['Learn more'] = 'ស្វែ​ងយល់​បន្ថែម​';
+$lang['Maximum file size: %sB.'] = 'ទំហំ​រូប​អតិ​បរមា​: %sB.';
+$lang['Allowed file types: %s.'] = 'អនុញ្ញាត​ឯកសារ​ប្រភេទ​: %s.';
+$lang['+ Add an upload box'] = '+ បន្ថែម​ប្រអប់​មួយ​ទៀត​';
+$lang['Add another set of photos'] = 'បន្ថែម​សំនុំ​រូប​មួយ​ផ្សេង​ទៀត​';
+$lang['Photo %s of %s'] = 'រូប​ថត​ទី %s នៃ %s';
+$lang['%d photos uploaded'] = '%d រូប​ថត​បាន​បញ្ចូល​';
+$lang['All %d photos are selected'] = 'រូប​ថត​ទាំង​អស់​ %d ត្រូវ​បាន​ជ្រើស​រើស​';
+$lang['You are using the Browser uploader. Try the <a href="%s">Flash uploader</a> instead.'] = 'អ្នក​កំពុង​ប្រើ​ប្រាស់​ Browser uploader. សាក​ល្បង​ប្រើ​ <a href="%s">Flash uploader</a> ជំនួស​វិញ';
+$lang['You are using the Flash uploader. Problems? Try the <a href="%s">Browser uploader</a> instead.'] = 'អ្នក​កំពុង​ប្រើ​ប្រាស់​ Flash uploader. មាន​បញ្ហា​? សាក​ល្បង​ប្រើ​ <a href="%s">Browser uploader</a> ជំនួស​វិញ​';
+$lang['Exif extension not available, admin should disable exif use'] = 'Exif extension not available, admin should disable exif use ផ្នែក​បន្ថែម​ Exif មិន​អាច​ដំណើរ​ការ​បាន​។ អ្នក​គ្រប់​គ្រង​គួរ​តែ​ទៅ​បិទ exif use';
+$lang['Hide'] = "លាក់​";
+$lang['ACCESS_0'] = "ចូល​ដំណើរ​ការ​សេរី";
+$lang['ACCESS_1'] = "ដំណើរ​ការសំរាប់​​ទាំង​អស់​";
+$lang['ACCESS_2'] = "ដំណើរ​ការ​សំរាប់​អ្នក​ចុះឈ្មោះ​ប្រើ​ប្រាស់​";
+$lang['ACCESS_3'] = "ដំណើរ​ការ​សំរាប់​អ្នក​គ្រប់​គ្រង​";
+$lang['ACCESS_4'] = "ដំណើរ​ការ​សំរាប់​ប្រធាន​បណ្តាញ​ធំ​";
+$lang['ACCESS_5'] = "មិន​អាច​ដំណើរ​ការ​បាន​";
+$lang['Level 0'] = "---";
+$lang['Level 1'] = "ទំនាក់​ទំនង​";
+$lang['Level 2'] = "មិត្ត​ភក្តិ​";
+$lang['Level 4'] = "គ្រួ​សារ​";
+$lang['Level 8'] = "អ្នក​គ្រប់​គ្រង​";
+
+?>
