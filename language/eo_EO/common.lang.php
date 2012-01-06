929c66497d04ca81917cd3c2424895bcf4b01d72
ddtddt <ddtddt@piwigo.org>
2012-01-06 19:23:06 +0000

    [trunk] - language - add eo_EO -Esperanto Thanks to Axel Rousseau - step 1- feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@12860 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/common.lang.php
@@ -0,0 +1,340 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Esperanto [EO]
+Version: 2.3.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=596
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Esperanto';
+$lang_info['country'] = 'Esperantio';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'eo';
+$lang_info['zero_plural'] = false;
+
+$lang['%d Kb'] = '%d Ko';
+$lang['%d album updated'] = '%d fotaro ĝisdatigita';
+$lang['%d albums updated'] = '%d fotaroj ĝisdatigitaj';
+$lang['%d comment to validate'] = '%d validigenda komento';
+$lang['%d comments to validate'] = '%d validigendaj komentoj';
+$lang['%d new comment'] = '%d nova komento';
+$lang['%d new comments'] = '%d novaj komentoj';
+$lang['%d comment'] = '%d komento';
+$lang['%d comments'] = '%d komentoj';
+$lang['%d hit'] = 'vidi %d foje';
+$lang['%d hits'] = 'vidi %d foje';
+$lang['%d new photo'] = '%d nova foto';
+$lang['%d new photos'] = '%d novaj fotoj';
+$lang['%d new user'] = '%d nova uzanto';
+$lang['%d new users'] = '%d novaj fotoj';
+$lang['About'] = 'Pri';
+$lang['All tags'] = 'Ĉiuj etikedoj';
+$lang['Any tag'] = 'Iu ajn etikedo';
+$lang['At least one listed rule must be satisfied.'] = 'Almenaŭ unu regulo plenumiĝas.';
+$lang['At least one tag must match'] = 'Almenaŭ unu etikedo kongruas';
+$lang['Albums'] = 'Fotaroj';
+$lang['Album'] = 'Fotaro';
+$lang['Close this window'] = 'Fermi la fenestron';
+$lang['Complete RSS feed (photos, comments)'] = 'Plena RSS Fadeno (fotoj kaj komentoj)';
+$lang['Confirm Password'] = 'Konfirmi pasvorton';
+$lang['Connection settings'] = 'Konekta agordo';
+$lang['Login'] = 'Konekto';
+$lang['Contact webmaster'] = 'Kontakti la respondeculon';
+$lang['Create a new account'] = 'Krei novan konton';
+$lang['Created on'] = 'Kreita je la ';
+$lang['Creation date'] = 'Kreita dato';
+$lang['Current password is wrong'] = 'Neĝusta pasvorto';
+$lang['Dimensions'] = 'Grandeco';
+$lang['Display'] = 'Ekrano';
+$lang['Each listed rule must be satisfied.'] = 'Ĉiuj reguloj devas kongrui';
+$lang['Email address'] = 'Retadreso';
+$lang['Enter your personnal informations'] = 'Entajpi viajn personajn informojn';
+$lang['Error sending email'] = 'Eraro sendanta la retmesaĝon';
+$lang['File name'] = 'Dosiernomo';
+$lang['File'] = 'Dosiero';
+$lang['Filesize'] = 'Dosierpezo';
+$lang['Filter and display'] = 'Filtri kaj surekranigi';
+$lang['Filter'] = 'Filtrilo';
+$lang['Forgot your password?'] = 'Ĉu vi forgesis vian pasvorton ?';
+$lang['Go through the gallery as a visitor'] = 'Trairi la galerion kiel vizitanto';
+$lang['Help'] = 'Helpo';
+$lang['Identification'] = 'Identigado';
+$lang['Photos only RSS feed'] = 'Nur fota RSS Fadeno';
+$lang['Keyword'] = 'Ŝlosilvorto';
+$lang['Links'] = 'Ligiloj';
+$lang['N/A'] = 'ne disponeblas';
+$lang['New on %s'] = 'Nova je la %s';
+$lang['New password confirmation does not correspond'] = 'Konfirmado de nova pasvorto ne kongruas';
+$lang['Notification'] = 'Sciigo';
+$lang['Number of items'] = 'Nombro de ero';
+$lang['Original dimensions'] = 'Originala grandeco';
+$lang['Password forgotten'] = 'Forgesita pasvorto';
+$lang['Password'] = 'Pasvorto';
+$lang['Post date'] = 'Aldonita dato';
+$lang['Posted on'] = 'Aldonita je la';
+$lang['Profile'] = 'Profilo';
+$lang['Quick connect'] = 'Rapida konekto';
+$lang['RSS feed'] = 'RSS fadeno';
+$lang['Register'] = 'Registri';
+$lang['Registration'] = 'Registrado';
+$lang['Related tags'] = 'Ligitaj etikedoj';
+$lang['Reset'] = 'Rekomenci';
+$lang['Retrieve password'] = 'Ricevi pasvorton';
+$lang['Search rules'] = 'Serĉadaj reguloj';
+$lang['Search tags'] = 'Serĉi etikedojn';
+$lang['Search'] = 'Serĉi';
+$lang['display available tags'] = 'Montri la disponeblajn etikedojn';
+$lang['Since'] = 'De';
+$lang['Sort by'] = 'Ordigi laŭ';
+$lang['Sort order'] = 'Ordigo';
+$lang['Tag'] = 'Etikedo';
+$lang['Tags'] = 'Etikedoj';
+$lang['The RSS notification feed provides notification on news from this website : new photos, updated albums, new comments. Use a RSS feed reader.'] = 'La RSS fadeno sciigas pri eventoj de la galerio : novaj fotoj, ĝisdatigitaj fotaroj, novaj komentoj de uzantoj. Tio uziĝas per RSS legilo.';
+$lang['Unknown feed identifier'] = 'Nekonata fadena identigilo';
+$lang['User comments'] = 'Uzantaj komentoj';
+$lang['Username'] = 'Uzantnomo';
+$lang['Visits'] = 'Vizitoj';
+$lang['Webmaster'] = 'Retestro';
+$lang['Week %d'] = 'Semajno %d';
+$lang['About Piwigo'] = 'Pri Piwigo';
+$lang['You are not authorized to access the requested page'] = 'Vi ne rajtas legi tiun paĝon. Forfikiĝu !';
+$lang['add to caddie'] = 'aldoni al la fot-stoko';
+$lang['add this photo to your favorites'] = 'Aldoni tiun foton al via legosigno';
+$lang['Administration'] = 'Administrado';
+$lang['all'] = 'ĉio';
+$lang['ascending'] = 'kreskanta';
+$lang['author(s) : %s'] = 'aŭtoro(j) : %s';
+$lang['Expand all albums'] = 'Etendi ĉiujn fotarojn';
+$lang['posted after %s (%s)'] = 'aldonita post la %s (%s)';
+$lang['posted before %s (%s)'] = 'aldonita antaŭ la %s (%s)';
+$lang['posted between %s (%s) and %s (%s)'] = 'aldonita inter la %s (%s) kaj la %s (%s)';
+$lang['posted on %s'] = 'aldonita je la %s';
+$lang['Best rated'] = 'Plej bonaj';
+$lang['display best rated photos'] = 'Montri la plej bonajn fotojn';
+$lang['caddie'] = 'Fot-stoko';
+$lang['Calendar'] = 'Kalendaro';
+$lang['All'] = 'Ĉiuj';
+$lang['display each day with photos, month per month'] = 'Montri ĉiujn tagojn, laŭ monatoj';
+$lang['View'] = 'Vido';
+$lang['chronology_monthly_calendar'] = 'Monata kalendaro';
+$lang['chronology_monthly_list'] = 'Monata listo';
+$lang['chronology_weekly_list'] = 'Semajna listo';
+$lang['Click here if your browser does not automatically forward you'] = 'Musklaku tien se via foliumilo ne mem plusendas vin.';
+$lang['comment date'] = 'dato de la komento';
+$lang['Comment'] = 'Komento';
+$lang['Your comment has been registered'] = 'Via komento estis registrita';
+$lang['Anti-flood system : please wait for a moment before trying to post another comment'] = 'Kontraŭ-fekula sistemo : bonvolu pacienciĝi antaŭ aldoni novan komenton';
+$lang['Your comment has NOT been registered because it did not pass the validation rules'] = 'Via komento ne validiĝis ĉar ĝi ne kongruas al nia striktaj reguloj';
+$lang['An administrator must authorize your comment before it is visible.'] = 'Administranto devas validigi vian komenton antaŭ ke ĝi iĝu videbla.';
+$lang['This login is already used by another user'] = 'Tiu uzanta nomo jam ekzistas';
+$lang['Comments'] = 'Komentoj';
+$lang['Add a comment'] = 'Aldoni komenton';
+$lang['created after %s (%s)'] = 'kreita post la %s (%s)';
+$lang['created before %s (%s)'] = 'kreita antaŭ la %s (%s)';
+$lang['created between %s (%s) and %s (%s)'] = 'kreita antaŭ la %s (%s) kaj la %s (%s)';
+$lang['created on %s'] = 'kreita la %s';
+$lang['Customize'] = 'Personigi';
+$lang['Your Gallery Customization'] = 'Personigado de via galerio ';
+$lang['day'][0] = 'Dimanĉo';
+$lang['day'][1] = 'Lundo';
+$lang['day'][2] = 'Mardo';
+$lang['day'][3] = 'Merkredo';
+$lang['day'][4] = 'Ĵaŭdo';
+$lang['day'][5] = 'Vendredo';
+$lang['day'][6] = 'Sabato';
+$lang['Default'] = 'Apriora';
+$lang['delete this photo from your favorites'] = 'Forigi tiun foton el viaj legosignoj';
+$lang['Delete'] = 'Forigi';
+$lang['descending'] = 'malkreskante';
+$lang['download'] = 'elŝuti';
+$lang['download this file'] = 'elŝuti la dosieron';
+$lang['edit'] = 'redakti';
+$lang['wrong date'] = 'maltaŭga dato';
+$lang['excluded'] = 'eksigita';
+$lang['Your favorites'] = 'Miaj legosignoj';
+$lang['display your favorites photos'] = 'montri fotojn de miaj legosignoj';
+$lang['Favorites'] = 'Legosignoj';
+$lang['First'] = 'Unua';
+$lang['The gallery is locked for maintenance. Please, come back later.'] = 'Ni ŝlosis la galerion por plibonigi ĝin, bonvolu reveni poste.';
+$lang['Page generated in'] = 'Paĝo kreita je la';
+$lang['guest'] = 'Vizitanto';
+$lang['Hello'] = 'Saluton';
+$lang['available for administrators only'] = 'disponeblaj nur por la administrantoj';
+$lang['display this album'] = 'montri tiun fotaron';
+$lang['display last user comments'] = 'Montri la lasatjn komentoj';
+$lang['customize the appareance of the gallery'] = 'Personigi aspekton de la galerio';
+$lang['search'] = 'serĉi';
+$lang['Home'] = 'Akceptejo';
+$lang['in this album'] = 'en tiu fotaro';
+$lang['in %d sub-album'] = 'en %d sub-fotaro';
+$lang['in %d sub-albums'] = 'en %d sub-fotaroj';
+$lang['included'] = 'inkluzivita';
+$lang['Invalid password!'] = 'Nevalidat pasvorto !';
+$lang['Language'] = 'Lingvo';
+$lang['last %d days'] = '%d lastaj tagoj';
+$lang['Last'] = 'Lasta';
+$lang['Logout'] = 'Elsaluti';
+$lang['E-mail address'] = 'Retadreso';
+$lang['obligatory'] = 'devigata';
+$lang['Maximum photo height'] = 'Maksimuma alteco de la fotoj';
+$lang['Maximum height must be a number superior to 50'] = 'Maksimuma alteco devas esti pli ol 50';
+$lang['Maximum photo width'] = 'Maksimuma larĝeco de la fotoj';
+$lang['Maximum width must be a number superior to 50'] = 'Maksimuma larĝeco devas esti pli ol 50';
+$lang['display a calendar by creation date'] = 'montri kalendaron laŭ kreigita dato';
+$lang['display all photos in all sub-albums'] = 'montri ĉiujn fotojn en ĉiuj sub-fotaroj';
+$lang['return to normal view mode'] = 'reiri al la normala vido';
+$lang['display a calendar by posted date'] = 'montri kalendaron laŭ aldonita dato';
+$lang['month'][10] = 'Oktobro';
+$lang['month'][11] = 'Novembro';
+$lang['month'][12] = 'Decembro';
+$lang['month'][1] = 'Januaro';
+$lang['month'][2] = 'Februaro';
+$lang['month'][3] = 'Marto';
+$lang['month'][4] = 'Aprilo';
+$lang['month'][5] = 'Majo';
+$lang['month'][6] = 'Junio';
+$lang['month'][7] = 'Julio';
+$lang['month'][8] = 'Aŭgusto';
+$lang['month'][9] = 'Septembro';
+$lang['Most visited'] = 'Plej ofte vizitita';
+$lang['display most visited photos'] = 'montri la plej ofte vizititajn fotojn';
+$lang['The number of photos per page must be a not null scalar'] = 'Nombro de fotoj en unu paĝo devas esti ne-nula nombro';
+$lang['Number of photos per page'] = 'Nombro de miniaturo';
+$lang['Unknown identifier'] = 'Nekonata identigilo';
+$lang['New password'] = 'Nova pasvorto';
+$lang['Rate this photo'] = 'Taksu tiun foton';
+$lang['Next'] = 'Venonta';
+$lang['no rate'] = 'neniu taksado';
+$lang['Photos posted within the last %d day.'] = 'Montri nur fotojn aldonitaj dum la lasta %d tago.';
+$lang['Photos posted within the last %d days.'] = 'Montri nur fotojn aldonitaj dum la lastaj %d tagoj.';
+$lang['password updated'] = 'ĝisdatigita pasvorto';
+$lang['Recent period must be a positive integer value'] = 'Tiu periodo devas esti pozitiva nombro';
+$lang['photo'] = 'foto';
+$lang['Click on the photo to see it in high definition'] = 'Musklaku la foton por grandigi ĝin (aŭ proksimiĝu la ekranon)';
+$lang['Show file metadata'] = 'Montri dosiermetadatumojn';
+$lang['Powered by'] = 'potencigita per';
+$lang['Preferences'] = 'Agordo';
+$lang['Previous'] = 'Lasta';
+$lang['Random photos'] = 'Hazarda foto';
+$lang['display a set of random photos'] = 'montri hazarde plurajn fotojn';
+$lang['Recent albums'] = 'Lastaj fotaroj';
+$lang['display recently updated albums'] = 'montri la plej freŝajn fotarojn';
+$lang['Recent period'] = 'Lasta periodo';
+$lang['Recent photos'] = 'Lastaj fotoj';
+$lang['display most recent photos'] = 'montri la plej lastajn fotojn';
+$lang['Redirection...'] = 'Alidirektado...';
+$lang['Please, enter a login'] = 'Bonvolu entajpi uzantnomon';
+$lang['login mustn\'t end with a space character'] = 'Uzantnomo ne finiĝu per spaceto';
+$lang['login mustn\'t start with a space character'] = 'Uzantnomo ne komenciĝu per spaceto';
+$lang['this login is already used'] = 'Tiu uzantnomo jam uziĝas';
+$lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'La retadreso devas simili al xxx@yyy.eee (ekzemple : zamenhof@bialistoko.pl)';
+$lang['please enter your password again'] = 'Bonvolu tajpi vian pasvorton denove (aŭ kopi-gluu ĝin)';
+$lang['Auto login'] = 'Aŭtomata konekto';
+$lang['remove this tag from the list'] = 'forigi tiun etikedon de la listo';
+$lang['representative'] = 'delegito';
+$lang['Search for Author'] = 'Serĉi aŭtoron';
+$lang['Search in albums'] = 'Serĉi en la fotaroj';
+$lang['Search by Date'] = 'Serĉi laŭ dato';
+$lang['Date'] = 'Dato';
+$lang['End-Date'] = 'Fin-dato';
+$lang['Kind of date'] = 'Type de date';
+$lang['Search for words'] = 'Serĉi vorton';
+$lang['Search for all terms'] = 'Serĉi ĉiujn vortojn';
+$lang['Search for any terms'] = 'Serĉi iun vorton';
+$lang['Empty query. No criteria has been entered.'] = 'Malplena demando, neniu kritero entajpigita.';
+$lang['Search Options'] = 'Serĉaj opcioj';
+$lang['Search results'] = 'Serĉa rezulto';
+$lang['Search in sub-albums'] = 'Serĉi en la sub-fotaroj';
+$lang['searched words : %s'] = 'serĉitaj vortoj : %s';
+$lang['Contact'] = 'Kontakto';
+$lang['set as album representative'] = 'Elekti kiel delegito de tiu fotaro';
+$lang['Show number of comments'] = 'Montri la nombron de komentoj';
+$lang['Show number of hits'] = 'Montri la nombron de vidoj';
+$lang['slideshow'] = 'bildoserio';
+$lang['stop the slideshow'] = 'halti la bildoserion';
+$lang['Specials'] = 'Apartaj';
+$lang['SQL queries in'] = 'SQL peto en';
+$lang['display only recently posted photos'] = 'n\'montri nur la lastaj fotoj';
+$lang['return to the display of all photos'] = 'reiri al ĉiuj fotoj';
+$lang['the beginning'] = 'la komenco';
+$lang['Interface theme'] = 'Etoso';
+$lang['Thumbnails'] = 'Miniaturo';
+$lang['Menu'] = 'Menuo';
+$lang['A comment on your site'] = 'Komento sur via retejo';
+$lang['today'] = 'Hodiaŭ';
+$lang['Update your rating'] = 'Ĝisdatigu vian taksadon';
+$lang['wrong filename'] = 'maltaŭga dosiernomo';
+$lang['the username must be given'] = 'vi devas indiki la uzantnomon';
+$lang['useful when password forgotten'] = 'helpinda se vi forgesas vian pasvorton';
+$lang['Quick search'] = 'Rapida serĉo';
+$lang['Connected user: %s'] = 'Konektita uzanto : %s';
+$lang['IP: %s'] = 'IP : %s';
+$lang['Browser: %s'] = 'Foliumilo: %s';
+$lang['Author: %s'] = 'Aŭtoro: %s';
+$lang['Comment: %s'] = 'Komento: %s';
+$lang['Comment by %s'] = 'Komentis %s';
+$lang['User: %s'] = 'Uzanto: %s';
+$lang['Email: %s'] = 'Retadreso: %s';
+$lang['Admin: %s'] = 'Administrado: %s';
+$lang['Registration of %s'] = 'Registris %s';
+$lang['Album: %s'] = 'Fotaro: %s';
+$lang['Bad status for user "guest", using default status. Please notify the webmaster.'] = 'Malbona stato por la uzanto "Vizitanto", uzo de la apriora stato. Bonvolu averti la retestro.';
+$lang['this email address is already in use'] = 'tiu retadreso jam uziĝas';
+$lang['Album results for'] = 'Rezulto de fotaro por';
+$lang['Tag results for'] = 'Rezulto de la etikedoj por';
+$lang['from %s to %s'] = 'de %s al %s';
+$lang['Play of slideshow'] = 'Legi la bildoserion';
+$lang['Pause of slideshow'] = 'Paŭzi la bildserion';
+$lang['Repeat the slideshow'] = 'Ripeti la bildserion';
+$lang['Not repeat the slideshow'] = 'Ne ripeti la bildoserion';
+$lang['Reduce diaporama speed'] = 'Malrapidigi la bildoserion';
+$lang['Accelerate diaporama speed'] = 'Rapidigi la bildoserion';
+$lang['Submit'] = 'Validigi';
+$lang['Yes'] = 'Jes';
+$lang['No'] = 'Ne';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotoj';
+$lang['%d photo is also linked to current tags'] = '%d foto ankaŭ ligiĝas al la kurantaj etikedoj';
+$lang['%d photos are also linked to current tags'] = '%d fotoj ankaŭ ligiĝas al la kurantaj etikedoj';
+$lang['display photos linked to this tag'] = 'montri fotojn ligita al tiu etikedo';
+$lang['photos posted during the last %d days'] = 'fotoj aldonitaj tiuj lastaj %d tagoj';
+$lang['Piwigo Help'] = 'Helpo de Piwigo';
+$lang['Rank'] = 'Rango';
+$lang['group by letters'] = 'grupigi per literoj';
+$lang['letters'] = 'literoj';
+$lang['show tag cloud'] = 'montri etikedoj nube';
+$lang['cloud'] = 'nubo';
+$lang['Reset to default values'] = 'reŝarĝi la apriorajn valuojn';
+$lang['delete all photos from your favorites'] = 'Forigi ĉiujn fotojn el viaj legosignoj';
+$lang['Sent by'] = 'Senditaj de';
+$lang['Cookies are blocked or not supported by your browser. You must enable cookies to connect.'] = 'La kuketoj ne disponeblas sur mia komputilo. Aktivu ilin por konektiĝi.';
+$lang['edit this comment'] = "redakti tiun komenton";
+$lang['Edit a comment'] = "redakti komenton";
+$lang['Are you sure?'] = "Ĉu vi certas ?";
+$lang['(!) This comment requires validation'] = '(!) Tiu komento validigendas';
+
+
+?>
