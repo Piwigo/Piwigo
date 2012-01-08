f9e12b51437b7305ea63627960b8b6d5254d424c
ddtddt <ddtddt@piwigo.org>
2012-01-08 07:35:53 +0000

    [trunk] - language - add plugin core th_TH - Thaï – ภาษาไทย  Thanks to Pradit Saenkaew - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@12862 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/th_TH/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = 'ไฟล์การตั้งค่าพื้นฐาน';
+$lang['locfiledit_onglet_css'] = 'ไฟล์ CSS';
+$lang['locfiledit_onglet_tpl'] = 'ไฟล์ แม่แบบ';
+$lang['locfiledit_onglet_lang'] = 'ไฟล์ ภาษา';
+$lang['locfiledit_onglet_plug'] = 'ไฟล์ ปลั๊กอิน';
+$lang['locfiledit_cant_save'] = 'ไฟล์ปัจจุบันไม่สามารถเขียนได้. โปรดตรวจสอบสิทธิ์การเข้าถึงไดเรกทอรี่ "local/" ว่ามันสามารถเขียนได้ (โดยการปรับค่า chmod เป็นค่า 777).';
+$lang['locfiledit_newfile'] = 'ไฟล์ไม่มีอยู่ และมันจะถูกสร้างโดย LocalFiles Editor';
+$lang['locfiledit_save_config'] = 'การเขียนไฟล์เสร็จสมบูรณ์.';
+$lang['locfiledit_show_default'] = 'แสดงไฟล์อ้างอิง: ';
+$lang['locfiledit_save_bak'] = 'คัดลอก สำรองไฟล์ที่จะสร้างเมื่อบันทึก.';
+$lang['locfiledit_saved_bak'] = 'สำรองไฟล์และสร้าง (%s)';
+$lang['locfiledit_save_file'] = 'บันทึกไฟล์';
+$lang['locfiledit_choose_file'] = 'เลือกไฟล์เพื่อทำการแก้ไข';
+$lang['locfiledit_edit'] = 'แก้ไข';
+$lang['locfiledit_restore'] = 'คืนค่าการสำรองไฟล์';
+$lang['locfiledit_restore_confirm'] = 'กรุณายืนยัน?
+การคืนค่าจะยังไม่มีผล จนกว่าจะบันทึกเสร็จเรียบร้อย.';
+$lang['locfiledit_bak_loaded1'] = 'ไฟล์สำรองได้ย้ายแล้ว.';
+$lang['locfiledit_bak_loaded2'] = 'คุณจะต้องบันทึกไฟล์เพื่อทำการคืนค่า.';
+$lang['locfiledit_syntax_error'] = 'ประโยคผิดพลาด! ไฟล์ไม่สามารถบันทึกได้.';
+$lang['locfiledit_new_tpl'] = 'สร้างไฟล์ใหม่';
+$lang['locfiledit_new_filename'] = 'ชื่อไฟล์';
+$lang['locfiledit_model'] = 'แบบ';
+$lang['locfiledit_empty_page'] = 'หน้าเปล่า';
+$lang['locfiledit_parent_directory'] = 'ไดเรกทอรี่พื้นฐาน';
+$lang['locfiledit_filename_error'] = 'ตัวอักขระถูกห้ามในการตั้งเป็นชื่อไฟล์.';
+$lang['locfiledit_file_already_exists'] = 'ไฟล์นี้มีอยู่แล้ว.';
+$lang['locfiledit_model_error'] = 'คุณจะต้องเลือกแบบ.';
+$lang['locfiledit_empty_filename'] = 'คุณจะต้องกรอกชื่อไฟล์.';
+$lang['locfiledit_webmaster_only'] = 'สำหรับเว็บมาสเตอร์เท่านั้นที่สามารถสร้างหรือแก้ไขปรับแต่งไฟล์การตั้งค่าพื้นฐานได้.';
+?>
