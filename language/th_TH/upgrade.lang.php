b832cc558a845cd898f64e47a51d02581c2594dd
ddtddt <ddtddt@piwigo.org>
2012-01-29 18:14:37 +0000

    [trunk] - language - update th_TH - Thaï – ภาษาไทย
    git-svn-id: http://piwigo.org/svn/trunk@12979 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/upgrade.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+
+$lang['Upgrade'] = "อัพเกรด";
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "หน้านี้คือ หน้าการอัพเกรดฐานข้อมูลจาก Piwigo เวอร์ชั่นเก่าของคุณ ไปยังเวอร์ชั่นใหม่. ซึ่งการอัพเกรดจะทำให้ ระบบ Piwigo แกลลอรี่ ของคุณเป็นปัจจบัน หรือเป็นการใช้งานบนเวอร์ชั่นที่ถูก <strong>ปล่อย %s</strong> (หรือ เทียบเท่า).";
+$lang['Upgrade from version %s to %s'] = "อัพเกรดจากเวอร์ชั่น %s ไปยัง %s";
+$lang['Statistics'] = "สถิติ";
+$lang['total upgrade time'] = "ระยะเวลาการอัพเกรดทั้งหมด";
+$lang['total SQL time'] = "เวลา SQL ทั้งหมด";
+$lang['SQL queries'] = "SQL queries";
+$lang['Upgrade informations'] = "ข้อมูลการอัพเกรด";
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = "การปรับปรุงระบบโปรดตรวจสอบใน [การจัดการระบบ>เครื่องมือ>การปรับปรุง] หากคุณพบปัญหาใดๆ ในการอัพเกรด.";
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = "โปรดระมัดระวัง, ปลั๊กอินที่คุณใช้งานอยู่ได้ถูกยกเลิกการใช้งานแล้ว. คุณจะต้องตรวจสอบก่อนการอัพเกรดปลั๊กอิน:";
+$lang['Only administrator can run upgrade: please sign in below.'] = "สำหรับผู้ดูแลระบบเท่านั้นที่สามารภทำการอัพเกรดระบบได้: โปรดเข้าสู่ระบบด้านล่าง.";
+$lang['You do not have access rights to run upgrade'] = "คุณไม่ได้เข้าถึงการอัพเกรดอย่างถูกต้อง";
+$lang['All sub-albums of private albums become private'] = "อัลบั้มย่อยทั้งหมดของอัลบั้มส่วนตัว จะกลายเป็นอัลบั้มส่วนตัว";
+$lang['User permissions and group permissions have been erased'] = "การอนุญาตให้สิทธฺ์ของผู้ใช้งานและกลุ่มจะถูกลบ";
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "รูปภาพขนาดย่อ คำนำหน้าของรูปภาพขนาดย่อ และที่อยู่เมลของเว็บมาสเตอร์เท่านั้นที่จะได้ถูกบันทึกจากการตั้งค่าครั้งก่อน";
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'ใน <i>%s</i>, ก่อน <b>?></b>, ใส่:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'โปรดระมัดระวัง, ธีมที่คุณใช้งานอยู่ได้ถูกยกเลิกการใช้งานแล้ว. คุณจะต้องตรวจสอบก่อนการอัพเกรดธีม:';
+?>
