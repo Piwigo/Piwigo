1d7ce5134a7842e4f4f69073694b58c8498412cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-04 07:30:48 +0000

    [trunk (install)] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/trunk@13026 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/install.lang.php
+++ language/hu_HU/install.lang.php
@@ -70,4 +70,6 @@ $lang['Download the config file'] = 'Konfigurációs fájl letöltése';
 $lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Letöltheti a konfigurációs fájlt és töltse fel a telepítésének local/config könyvtárába.';
 $lang['Just another Piwigo gallery'] = 'Csak egy újabb Piwigo galéria';
 $lang['Welcome to my photo gallery'] = 'Üdvözöljük a képgalériában';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Kérdése van? Keresse fel fórumunkat : %s';
+$lang['Welcome to your new installation of Piwigo!'] = 'Köszönt a frissen telepített Piwigo galéria!';
 ?>
