34d10109e4d3204a22b15fb27a5b1261c2520633
ddtddt <ddtddt@piwigo.org>
2012-02-13 20:18:03 +0000

    [Branche 2.3] - language - feature:2573 - recovery trunk - km_KH – ​ភាសា​ខ្មែរ​- khmer (Cambodia)
    git-svn-id: http://piwigo.org/svn/branches/2.3@13159 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/km_KH/admin.lang.php
@@ -0,0 +1,85 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+
+$lang['Notification'] = "ការ​ផ្តល់​ដំណឹង​";
+$lang['Upload Photos'] = 'បញ្ចូល​រូប​ថត​';
+$lang['Select an album'] = 'ជ្រើស​រើស​សៀវ​ភៅ​រូប​ថត​';
+$lang['... or '] = '... ឬ ';
+$lang['create a new album'] = 'បង្កើត​សៀវ​ភៅ​រូប​ថត​ថ្មី​មួយ';
+$lang['Select files'] = 'ជ្រើស​រើស​រូប​ថត​';
+$lang['Start Upload'] = 'ចាប់​ផ្តើម​បញ្ចូល​';
+$lang['Learn more'] = 'ស្វែ​ងយល់​បន្ថែម​';
+$lang['Maximum file size: %sB.'] = 'ទំហំ​រូប​អតិ​បរមា​: %sB.';
+$lang['Allowed file types: %s.'] = 'អនុញ្ញាត​ឯកសារ​ប្រភេទ​: %s.';
+$lang['+ Add an upload box'] = '+ បន្ថែម​ប្រអប់​មួយ​ទៀត​';
+$lang['Add another set of photos'] = 'បន្ថែម​សំនុំ​រូប​មួយ​ផ្សេង​ទៀត​';
+$lang['Photo %s of %s'] = 'រូប​ថត​ទី %s នៃ %s';
+$lang['%d photos uploaded'] = '%d រូប​ថត​បាន​បញ្ចូល​';
+$lang['All %d photos are selected'] = 'រូប​ថត​ទាំង​អស់​ %d ត្រូវ​បាន​ជ្រើស​រើស​';
+$lang['You are using the Browser uploader. Try the <a href="%s">Flash uploader</a> instead.'] = 'អ្នក​កំពុង​ប្រើ​ប្រាស់​ Browser uploader. សាក​ល្បង​ប្រើ​ <a href="%s">Flash uploader</a> ជំនួស​វិញ';
+$lang['You are using the Flash uploader. Problems? Try the <a href="%s">Browser uploader</a> instead.'] = 'អ្នក​កំពុង​ប្រើ​ប្រាស់​ Flash uploader. មាន​បញ្ហា​? សាក​ល្បង​ប្រើ​ <a href="%s">Browser uploader</a> ជំនួស​វិញ​';
+$lang['Exif extension not available, admin should disable exif use'] = 'Exif extension not available, admin should disable exif use ផ្នែក​បន្ថែម​ Exif មិន​អាច​ដំណើរ​ការ​បាន​។ អ្នក​គ្រប់​គ្រង​គួរ​តែ​ទៅ​បិទ exif use';
+$lang['Hide'] = "លាក់​";
+$lang['ACCESS_0'] = "ចូល​ដំណើរ​ការ​សេរី";
+$lang['ACCESS_1'] = "ដំណើរ​ការសំរាប់​​ទាំង​អស់​";
+$lang['ACCESS_2'] = "ដំណើរ​ការ​សំរាប់​អ្នក​ចុះឈ្មោះ​ប្រើ​ប្រាស់​";
+$lang['ACCESS_3'] = "ដំណើរ​ការ​សំរាប់​អ្នក​គ្រប់​គ្រង​";
+$lang['ACCESS_4'] = "ដំណើរ​ការ​សំរាប់​ប្រធាន​បណ្តាញ​ធំ​";
+$lang['ACCESS_5'] = "មិន​អាច​ដំណើរ​ការ​បាន​";
+$lang['Level 0'] = "---";
+$lang['Level 1'] = "ទំនាក់​ទំនង​";
+$lang['Level 2'] = "មិត្ត​ភក្តិ​";
+$lang['Level 4'] = "គ្រួ​សារ​";
+$lang['Level 8'] = "អ្នក​គ្រប់​គ្រង​";
+$lang['%d album including'] = 'សៀវ​ភៅ​រូប​ថត %d កំពុង​ដាក់​បញ្ចូល';
+$lang['%d album moved'] = 'សៀវ​ភៅ​រូប​ថត %d បានផ្លាស់​ប្តូរ​ទី​តាំង​​';
+$lang['%d albums including'] = 'សៀវ​ភៅ​រូប​ថត %d កំពុង​បញ្ចូល';
+$lang['%d albums moved'] = 'សៀវ​ភៅ​រូប​ថត %d បាន​ផ្លាស់​ប្តូរ​ទី​តាំង​';
+$lang['%d anomalies have been detected corrected.'] = 'ភាព​ខុស​ប្រក្រតីចំនួន​ %d ត្រូវ​បាន​រក​ឃើញ​ និង​បាន​កែ​ប្រែ';
+$lang['%d anomalies have been detected.'] = 'ភាព​ខុស​ប្រក្រ​តី​ចំនួន​ %d ត្រូវ​បាន​រក​ឃើញ​';
+$lang['%d anomalies have been ignored.'] = 'ភាព​ខុស​ប្រក្រ​តី​ចំនួន​ %d មិន​បាន​ទទួល​យក';
+$lang['%d anomalies have not been corrected.'] = 'ភាព​មិន​ប្រ​ក្រ​តី​ចំនួន​ %d មិ​នទាន់​​បាន​កែ​ប្រែ​';
+$lang['%d anomaly has been corrected.'] = 'ភាព​មិន​ប្រ​ក្រ​តី​ចំនួន​ %d ត្រូវ​បាន​កែ​ប្រែ​';
+$lang['%d anomaly has been detected.'] = 'ភាព​មិន​ប្រ​ក្រ​តី​ចំនួន​ %d ត្រូវ​បាន​រក​ឃើញ​';
+$lang['%d anomaly has been ignored.'] = 'ភាព​មិន​ប្រ​ក្រ​តី​ចំនួន​ %d មិន​បាន​ទទួល​យក';
+$lang['%d anomaly has not been corrected.'] = 'ភាព​មិន​ប្រ​ក្រ​តី​ចំនួន %d មិន​ទាន់​បាន​កែ​ប្រែ';
+$lang['%d group'] = '%d ក្រុម​';
+$lang['%d groups'] = '%d ក្រុម​';
+$lang['%d guest'] = 'ភ្ញៀវ​ %d នាក់​​';
+$lang['%d guests'] = 'ភ្ញៀវ​ %d នាក់​';
+$lang['%d line filtered'] = '​ច្រោះ​យក​ %d បន្ទាត់';
+$lang['%d lines filtered'] = 'ច្រោះ​យក​ %d បន្ទាត់​';
+$lang['%d mail was not sent.'] = 'អ៊ីម៉ែល​ចំនួន %d មិន​បាន​ផ្ញើ​ចេញ​ទេ';
+$lang['%d mail was sent.'] = 'អ៊ី​ម៉ែល​ចំនួន​ %d បាន​ផ្ញើ​ចេញ​';
+$lang['%d mails were not sent.'] = 'អ៊ី​ម៉ែល​ចំនួន​ %d មិន​បាន​ផ្ញើ​ចេញ​ទេ​';
+$lang['%d mails were sent.'] = 'អ៊ី​ម៉ែល​ចំនួន​ %d បាន​ផ្ញើ​ចេញ';
+$lang['%d member'] = 'សមាជិក​ %d នាក់​';
+$lang['%d members'] = 'សមា​ជិក​ %d នាក់​';
+$lang['%d of %d photos selected'] = '%d នៃ​ %d រូប​ថត​បាន​ជ្រើស​រើស​';
+$lang['%d photo was deleted'] = 'រូប​ថត​ %d បា​ន​លុប';
+$lang['%d photos were deleted'] = 'រូប​ថត​ %d បាន​លុប';
+$lang['%d physical'] = '%d រូប​សណ្ឋាន​';
+
+?>
