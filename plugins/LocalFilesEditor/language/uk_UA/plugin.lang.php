9175a64472cecf9e4ecb7d399f0c57000f496ea2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-16 12:10:56 +0000

    [LocalFilesEditor] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/trunk@13189 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/uk_UA/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Резервна копія файлу завантажена.';
+$lang['locfiledit_bak_loaded2'] = 'Ви повинні зберегти файл, щоб відновити його.';
+$lang['locfiledit_cant_save'] = 'Поточний файл не доступний для запису. Переконайтеся, що каталог  "local/" доступний для запису (CHMOD).';
+$lang['locfiledit_choose_file'] = 'Виберіть файл для редагування';
+$lang['locfiledit_edit'] = 'Редагувати';
+$lang['locfiledit_empty_filename'] = 'Ви повинні заповнити ім\'я файлу.';
+$lang['locfiledit_empty_page'] = 'Порожня сторінка';
+$lang['locfiledit_filename_error'] = 'Заборонені символи в імені файлу.';
+$lang['locfiledit_file_already_exists'] = 'Файл вже існує.';
+$lang['locfiledit_model'] = 'Модель';
+$lang['locfiledit_model_error'] = 'Ви повинні вибрати модель.';
+$lang['locfiledit_newfile'] = 'Файл не існують і будуть створено через LocalFiles Editor';
+$lang['locfiledit_new_filename'] = 'Назва файлу';
+$lang['locfiledit_new_tpl'] = 'Створити новий файл';
+$lang['locfiledit_onglet_css'] = 'Стилі';
+$lang['locfiledit_onglet_lang'] = 'Мови';
+$lang['locfiledit_onglet_localconf'] = 'Локальна конфігурація';
+$lang['locfiledit_onglet_plug'] = 'Персональні плаґіни';
+$lang['locfiledit_onglet_tpl'] = 'Шаблони';
+$lang['locfiledit_parent_directory'] = 'Батьківська тека';
+$lang['locfiledit_restore'] = 'Відновлення файлу резервної копії';
+$lang['locfiledit_restore_confirm'] = 'Будь ласка, підтвердіть?
+Відновлення не буде ефективним до наступного збереження.';
+$lang['locfiledit_saved_bak'] = 'Резервне копіювання створеного файлу (%s)';
+$lang['locfiledit_save_bak'] = 'Резервна копія створюється при збереженні.';
+$lang['locfiledit_save_config'] = 'Файл успішно записаний.';
+$lang['locfiledit_save_file'] = 'Зберегти файл';
+$lang['locfiledit_show_default'] = 'Показати посилання на файл:';
+$lang['locfiledit_syntax_error'] = 'Синтаксична помилка! Файл не може бути збережений.';
+$lang['locfiledit_webmaster_only'] = 'Тільки веб-майстри можуть створювати або змінювати локальні файли.';
+?>
