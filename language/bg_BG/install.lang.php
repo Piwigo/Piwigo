8eb05890bee96d4fcc8c035a60bd8e91c28bb46f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-23 00:51:18 +0000

    [trunk (install)] Update bg_BG, thanks to : hamsia
    git-svn-id: http://piwigo.org/svn/trunk@13312 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/install.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['also given by your host provider'] = 'също предоставена от доставчика на услугата';
+$lang['Basic configuration'] = 'Основно конфигуриране';
+$lang['Database configuration'] = 'Конфигуриране на базата данни';
+$lang['Database name'] = 'Име на базата данни';
+$lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'имената на таблиците в базата данни, ще имат тази представка ( това Ви дава възможност да управлявате таблиците по-добре)';
+$lang['Default gallery language'] = 'Език по подразбиране за галерията';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Не се колебайте да се консултира в нашите форуми за всяка помощ:% S';
+$lang['Download the config file'] = 'Изтегляне на конфигурационния файл';
+$lang['enter a login for webmaster'] = 'въведете потребителско име за уебмастер';
+$lang['Installation'] = 'Инсталиране';
+$lang['Just another Piwigo gallery'] = 'Още една  Piwigo фотогалерия';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'Пазете я в тайна, тя Ви дава достъп до административния панел';
+$lang['Password'] = 'Парола';
+$lang['Password [confirm]'] = 'Парола [потвърждаване]';
+$lang['please enter your password again'] = 'моля, въведете паролата още веднъж';
+$lang['User'] = 'Потребител';
+$lang['verification'] = 'проверка';
+$lang['webmaster login can\'t contain characters \' or "'] = 'потребителското име за уебмастера, не може да съдържа символите\' или "';
+$lang['Webmaster password'] = 'Парола на уебмастера';
+$lang['Welcome to my photo gallery'] = 'Здравейте и добре дошли в моята фотогалерия';
+$lang['Welcome to your new installation of Piwigo!'] = 'Здравейте и добре дошли във вашата нова Piwigo инсталация! ';
+$lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Вие можете да изтеглите конфигурационния файл и да го качите в директорията  local/config на вашата инсталация';
+?>
