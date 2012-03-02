9ae55cd3e8699681da3d634c74fd2b1597d93f61
plegall <plg@piwigo.org>
2012-03-02 20:30:27 +0000

    next version is 2.4.0RC1
    git-svn-id: http://piwigo.org/svn/trunk@13463 68402e56-0260-453c-a942-63ccdbb3a9ee

--- tools/create_listing_file.php
+++ tools/create_listing_file.php
@@ -27,7 +27,7 @@
 
 // ****** Gallery configuration ****** //
 // Script version
-$conf['version'] = '2.4.0beta2';
+$conf['version'] = '2.4.0RC1';
 
 // URL of main gallery
 // Example : http://www.my.domain/my/directory
