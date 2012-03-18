8e5a58c953e6f8f782ba78b6b1243ead6f325640
ddtddt <ddtddt@piwigo.org>
2012-03-18 18:04:08 +0000

    [trunk] - language - update sv_SE - Svenska - Swedish Thanks to styxsailor - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@13623 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/install.lang.php
+++ language/sv_SE/install.lang.php
@@ -70,6 +70,8 @@ $lang['Download the config file'] = 'Ladda ner konfigurations filen';
 $lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Du kan ladda ner konfigurationsfilen och ladda upp den till local/config .';
 $lang['Just another Piwigo gallery'] = 'Ännu ett Piwigo galleri';
 $lang['Welcome to my photo gallery'] = 'Välkommen till mitt fotogalleri';
+$lang['Welcome to your new installation of Piwigo!'] = 'Välkomment till din nya installation av Piwigo!';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Tveka inte att kontakta våra forum för hjälp: %s';
 
 
 ?>
