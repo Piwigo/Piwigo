338f5be02919a844368b386f46ed787c82afcdb8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-19 11:11:00 +0000

    [trunk (upgrade)] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/trunk@13648 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/bg_BG/upgrade.lang.php
+++ language/bg_BG/upgrade.lang.php
@@ -30,4 +30,11 @@ $lang['Upgrade from version %s to %s'] = 'Обновяване от версия
 $lang['Upgrade informations'] = 'Информация за обновяването';
 $lang['You do not have access rights to run upgrade'] = 'Вие нямате права, за да стартирате обновяване.';
 $lang['All sub-albums of private albums become private'] = 'Всички под-албуми на лични албуми, стават също лични.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Като предпазна мярка, следните приставки са били деактивирани. Трябва да проверите за ъпгрейд на приставките преди да ги активирате отново:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Като предпазна мярка, следните теми бяха деактивирани. Трябва да проверите за ъпгрейд на темите преди да ги активирате отново:';
+$lang['In <i>%s</i>, before <b></b>, insert:'] = 'Във <i>%s</i>, преди <b></b>, добави:';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Само миниатюрата на префикса и webmaster имейл адреса бяха запазени от предишната конфигурация';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Направете проверка за подръжка в  [Administration>Tools>Maintenance] ако имате някакви проблеми.';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Тази страница предлага да актуализира базата данни от вашия Piwigo стара версия към поновата версия. Ъпгрейд асистента мисли, че в момента използвате <strong> релииз %s</ strong> (или подобна).';
+$lang['User permissions and group permissions have been erased'] = 'Разрешенията на потребителите и групите бяха изтрити';
 ?>
