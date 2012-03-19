bd0cf9cab448d1c3084e1a0d8d370ed9adb8145e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-19 20:56:37 +0000

    [LocalFilesEditor] Add sh_RS, thanks to : vladamilanovic
    git-svn-id: http://piwigo.org/svn/trunk@13650 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/sh_RS/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Rezervni fajl učitan.';
+$lang['locfiledit_bak_loaded2'] = 'Morate sačuvati fajl da bi ste ga vratili.';
+$lang['locfiledit_cant_save'] = 'Trenutni fajl nije upisiv. Proverite da li je moguće pisanje u direktorijumu "local/" (chmod).';
+$lang['locfiledit_choose_file'] = 'Odaberite fajl koji će biti uređivan';
+$lang['locfiledit_edit'] = 'Uredi';
+$lang['locfiledit_empty_filename'] = 'Morate popuniti naziv fajla.';
+$lang['locfiledit_empty_page'] = 'Prazna strana';
+$lang['locfiledit_filename_error'] = 'Zabranjeni karakter u nazivu fajla.';
+$lang['locfiledit_file_already_exists'] = 'Fajl već postoji.';
+$lang['locfiledit_model'] = 'Model';
+$lang['locfiledit_model_error'] = 'Morate odabrati model.';
+$lang['locfiledit_newfile'] = 'Fajl ne postoji i biće kreiran od strane LocalFiles editora';
+$lang['locfiledit_new_filename'] = 'Naziv fajla';
+$lang['locfiledit_new_tpl'] = 'Kreiraj novi fajl';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Jezici';
+$lang['locfiledit_onglet_localconf'] = 'Lokalno podešavanje';
+$lang['locfiledit_onglet_plug'] = 'Lični dodatak';
+$lang['locfiledit_onglet_tpl'] = 'Šabloni';
+$lang['locfiledit_parent_directory'] = 'Nadređeni direktorijum';
+$lang['locfiledit_restore'] = 'Vrati rezervni fajl';
+$lang['locfiledit_restore_confirm'] = 'Molim potvrdite? Vraćanje neće imati efekta do sledećeg snimanja.';
+$lang['locfiledit_saved_bak'] = 'Rezervan kopija fajla kreirana (%s)';
+$lang['locfiledit_save_bak'] = 'Rezervna kopija biće kreirana prilikom snimanja.';
+$lang['locfiledit_save_config'] = 'Fajl upisan uspešno.';
+$lang['locfiledit_save_file'] = 'Snimi fajl';
+$lang['locfiledit_show_default'] = 'Prikaži referentni fajl:';
+$lang['locfiledit_syntax_error'] = 'Sintaksna greška! Fajl ne može biti sačuvan.';
+$lang['locfiledit_webmaster_only'] = 'Samo webmasteri mogu kreirati ili modifikovati lokalne fajlove.';
+?>
