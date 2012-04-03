9896a93e73d04dcf12c99dd939293424f117e879
plegall <plg@piwigo.org>
2012-04-03 22:15:41 +0000

    next release is 2.4.0RC3
    git-svn-id: http://piwigo.org/svn/trunk@13885 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.4.0RC2');
+define('PHPWG_VERSION', '2.4.0RC3');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'elegant');
 
