6b507f7b3fc3673629e8f1e6be4a23350d9a0793
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-04 11:54:12 +0000

    [LocalFilesEditor] Update no_NO, thanks to : abbamust
    git-svn-id: http://piwigo.org/svn/trunk@13891 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/no_NO/plugin.lang.php
+++ plugins/LocalFilesEditor/language/no_NO/plugin.lang.php
@@ -19,7 +19,7 @@
 // | along with this program; if not, write to the Free Software           |
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
-// +-----------------------------------------------------------------------+$lang['locfiledit_onglet_localconf'] = 'Lokal konfigurasjon';
+$lang['locfiledit_onglet_localconf'] = 'Lokal konfigurasjon';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_tpl'] = 'Maler (templates)';
 $lang['locfiledit_onglet_lang'] = 'Språk';
