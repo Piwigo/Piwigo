74a8eb6b41ec603abbccfd0d8a06fd6f903b6a85
ddtddt <ddtddt@piwigo.org>
2012-04-10 17:03:28 +0000

    [trunk] - language - update eo_EO -Esperanto thanks to Axel Rousseau  - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@14081 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/eo_EO/admin.lang.php
+++ language/eo_EO/admin.lang.php
@@ -24,4 +24,43 @@
 
 $lang['Add to caddie'] = 'Aldoni al la fot-stoko';
 $lang['Caddie'] = 'Fot-stoko';
+$lang['Notification'] = "Sciigo";
+$lang['Upload Photos'] = 'Alsuti fotojn';
+$lang['Select an album'] = 'Elekti fotaron';
+$lang['... or '] = '... au ';
+$lang['create a new album'] = 'krei novan fotaron';
+$lang['Select files'] = 'Elekti dosierojn';
+$lang['Start Upload'] = 'Ek-alsuti';
+$lang['Learn more'] = 'Scii pli';
+$lang['Maximum file size: %sB.'] = 'Maksimuma grando de la fotoj: %sB.';
+$lang['Allowed file types: %s.'] = 'dosiertipo permesita: %s.';
+$lang['+ Add an upload box'] = '+ Aldoni alsutan fenestron';
+$lang['Add another set of photos'] = 'Aldoni plian aron de fotoj';
+$lang['Photo %s of %s'] = 'Foto %s de %s';
+$lang['%d photos uploaded'] = '%d fotoj alsutitaj';
+$lang['All %d photos are selected'] = 'Ciuj %d fotoj estas elektitaj';
+$lang['You are using the Browser uploader. Try the <a href="%s">Flash uploader</a> instead.'] = 'Vi uzas la foliumilan alsutilon, kaze de problemo, provu la <a href="%s">Flash alsutilon</a> anstatau.';
+$lang['You are using the Flash uploader. Problems? Try the <a href="%s">Browser uploader</a> instead.'] = 'Vi uzas la Flash-alsutilon, kaze de problemo, provu la <a href="%s">Foliumilan alsutilon</a> anstatau.';
+$lang['Exif extension not available, admin should disable exif use'] = 'Exif funkcioj ne disponeblas, administranto malvalidigu EXIF-uzon';
+$lang['Hide'] = "Kasita";
+$lang['Photo Properties'] = 'Fotaj ecoj';
+$lang['Photo name'] = 'Foto-nomo';
+$lang['Name'] = 'Nomo';
+$lang['Description'] = 'Priskribo';
+$lang['Parent album'] = 'Patra fotaro';
+$lang['Album name'] = 'Fotara nomo';
+$lang['Create'] = 'Krei';
+$lang['ACCESS_0'] = "Libera atingo";
+$lang['ACCESS_1'] = "Atingo por ciuj";
+$lang['ACCESS_2'] = "Atingo por registritaj uzantoj";
+$lang['ACCESS_3'] = "Atingo por administrantoj";
+$lang['ACCESS_4'] = "Atingo por retestroj";
+$lang['ACCESS_5'] = "Neniu atingo";
+$lang['Level 0'] = "---";
+$lang['Level 1'] = "Kontakto";
+$lang['Level 2'] = "Amikoj";
+$lang['Level 4'] = "Familio";
+$lang['Level 8'] = "Administranto";
+
+
 ?>
