f6ed8a00896d44761d4a6eb1e32264780c97494e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-15 12:40:46 +0000

    [trunk (install)] Update fi_FI, thanks to : streen
    git-svn-id: http://piwigo.org/svn/trunk@14163 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/install.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Admin configuration'] = 'Pääkäyttäjän konfiguroiminen';
+$lang['Basic configuration'] = 'Perusasetukset';
+$lang['Can\'t connect to server'] = 'Ei pysty yhdistämään palvelimeen';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Yhteys palvelimeen onnistui, mutta yhteys tietokantaan on mahdotonta.';
+$lang['Database configuration'] = 'Tietokannan konfiguroiminen';
+$lang['Database name'] = 'Tietokannan nimi';
+$lang['Database table prefix'] = 'Tietokannan taulun etuliite';
+$lang['Default gallery language'] = 'Gallerian oletuskieli';
+$lang['Host'] = 'Host';
+$lang['Installation'] = 'Asennus';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'Tullaan näyttämään vierailijoille. Olennainen sivuston hallintaa ajatellen';
+$lang['Password'] = 'Salasana';
+$lang['Start Install'] = 'Aloita asennus';
+$lang['User'] = 'Käyttäjä';
+$lang['Webmaster login'] = 'Webmaster -kirjatuminen';
+$lang['also given by your host provider'] = 'myöskin palveluntarjoajan määrittelemä';
+$lang['localhost, sql.multimania.com, toto.freesurf.fr'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
+$lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'sähköpostin tulee olla muodossa xxx@yyy.eee (esim. jack@altern.org)';
+$lang['user login given by your host provider'] = 'käyttäjänimi, jonka palveluntarjoaja on määrittänyt';
+$lang['user password given by your host provider'] = 'käyttäjän salasana, jonka palveluntarjoaja on määrittänyt';
+?>
