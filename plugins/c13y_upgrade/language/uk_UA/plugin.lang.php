9f31663068f9080c05b7faef28dea486e517ce03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-21 23:29:27 +0000

    [c13y_upgrade] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/trunk@14261 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/uk_UA/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['c13y_correction_dbl_email_user'] = 'Видалення дублікатів користувачів';
+$lang['c13y_correction_obsolete_plugin'] = '"%s" плаґін був включений в цій версії програми, і ви повинні видалити його.';
+$lang['c13y_dbl_email_user'] = 'Користувачі, що мають одну і ту ж адресу електронної пошти';
+$lang['c13y_obsolete_plugin'] = 'Застарілий плаґін';
+$lang['c13y_upgrade_no_anomaly'] = 'Немає аномалії, виявленої після застосування оновлення, плаґін "Check upgrades" був відключений.';
+?>
