07ec4d9a7feca3190072dc43620cce7c91d63966
ddtddt <ddtddt@piwigo.org>
2012-04-23 11:02:31 +0000

    [trunk] - language - update ca_ES - Catalan - Thanks to Albert Parera - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@14282 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ca_ES/upgrade.lang.php
+++ language/ca_ES/upgrade.lang.php
@@ -24,20 +24,22 @@
 
 
 $lang['Upgrade'] = "Actualització";
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "Aquesta pàgina proposa actualitzar la base de dades de la teva galeria Piwigo antiga a la nova versió. L'assistent d'actualització creu que esteu executant la <strong>versió %s</strong> (o equivalent).";
-$lang['Upgrade from version %s to %s'] = "Actualitza de la versió %s a la %s";
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "Aquesta pàgina actualitza la base de dades de la teva antiga galeria Piwigo a la nova versió. L'assistent d'actualització detecta que esteu executant la <strong>versió %s</strong> (o equivalent).";
+$lang['Upgrade from version %s to %s'] = "Actualitzar la versió %s a la %s";
 $lang['Statistics'] = "Estadístiques";
 $lang['total upgrade time'] = "temps total d'actualització";
 $lang['total SQL time'] = "temps total SQL ";
 $lang['SQL queries'] = "consultes SQL";
 $lang['Upgrade informations'] = "Informació de l'actualització";
+
 $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = "Si trobeu algun problema realitzeu una revisió de manteniment [Administració>Eines>Manteniment].";
+
 $lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = "Com a precaució, els següents connectors s'han desactivat. Haureu de revisar les actualitzacions dels connectors abans de reactivar-los:";
 $lang['Only administrator can run upgrade: please sign in below.'] = "Únicament un usuari administrador pot executar l'actualització: si us plau, inicieu la sessió a sota.";
 $lang['You do not have access rights to run upgrade'] = "No teniu drets d'accés per executar l'actualització";
 $lang['All sub-albums of private albums become private'] = "Tots els sub-àlbums dels àlbums privats esdeveneixen privats";
 $lang['User permissions and group permissions have been erased'] = "S'han eliminat els permisos d'usuaris i de grups";
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "Només s'ha guardat de l'anterior configuració els prefixos de les miniatures i l'adreça de correu electrònic de l'administrador de la web";
-$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'In <i>%s</i>, before <b>?></b>, insert:';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'En <i>%s</i>, abans <b>?></b>, insertar:';
 $lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Com a precaució, els següents tempes s\'han desactivat. Haureu de revisar les actualitzacions dels temes abans de reactivar-los:';
 ?>
