5383432a247264088deba184003b4c9b63f5a8d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-24 18:08:07 +0000

    [c13y_upgrade] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/trunk@14356 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/ca_ES/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['c13y_correction_dbl_email_user'] = 'Eliminar usuaris duplicats';
+$lang['c13y_correction_obsolete_plugin'] = 'El complement (plug-in) "%s" ja s\'ha inclòs de sèrie en aquesta versió de l\'aplicació. Desinstal·la\'l.';
+$lang['c13y_dbl_email_user'] = 'Usuaris amb la mateixa adreça de correu electrònic';
+$lang['c13y_obsolete_plugin'] = 'Complement (plug-in) obsolet.';
+$lang['c13y_upgrade_no_anomaly'] = 'No s\'ha detectat cap anomalia després de l\'actualitzar-se l\'aplicació. El complement (plug-in) "Comprovar actualitzacions" s\'ha desactivat.';
+?>
