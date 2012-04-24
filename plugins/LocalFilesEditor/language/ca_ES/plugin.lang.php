31babd14928b3922161b0384e51d8b28290e97c3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-24 18:08:53 +0000

    [localfileseditor] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/trunk@14362 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/ca_ES/plugin.lang.php
+++ plugins/LocalFilesEditor/language/ca_ES/plugin.lang.php
@@ -31,4 +31,23 @@ $lang['locfiledit_onglet_lang'] = 'Idiomes';
 $lang['locfiledit_onglet_localconf'] = 'Configuració local';
 $lang['locfiledit_onglet_plug'] = 'Complement personal';
 $lang['locfiledit_onglet_tpl'] = 'Plantilles';
+$lang['locfiledit_bak_loaded1'] = 'Arxiu de còpia de seguretat carregat.';
+$lang['locfiledit_cant_save'] = 'L\'arxiu actual no té permís d\'escriptura. Comprova si el directori "local/" disposa d\'aquest permís (chmod).';
+$lang['locfiledit_choose_file'] = 'Tria l\'arxiu a editar';
+$lang['locfiledit_empty_filename'] = 'Has d\'escriure el nom del fitxer.';
+$lang['locfiledit_empty_page'] = 'Pàgina en blanc';
+$lang['locfiledit_filename_error'] = 'Caracters prohibits en el nom del fitxer.';
+$lang['locfiledit_model_error'] = 'Has de triar un model.';
+$lang['locfiledit_newfile'] = 'El fitxer no existeix i serà creat per LocalFiles Editor';
+$lang['locfiledit_parent_directory'] = 'Directori pare';
+$lang['locfiledit_restore_confirm'] = 'Si us plau, confirma-ho?
+La restauració no serà efectiva fins que sigui desat.';
+$lang['locfiledit_restore'] = 'Restaura l\'arxiu de còpia de seguretat';
+$lang['locfiledit_save_bak'] = 'Es crearà una còpia de seguretat en el moment en què sigui desat.';
+$lang['locfiledit_save_config'] = 'L\'arxiu ha estat escrit correctament.';
+$lang['locfiledit_save_file'] = 'Desa el fitxer';
+$lang['locfiledit_saved_bak'] = 'Arxiu de còpia de seguretat creat (%s)';
+$lang['locfiledit_show_default'] = 'Mostra el fitxer de referència:';
+$lang['locfiledit_syntax_error'] = 'Error de sintaxi! L\'arxiu no es pot desar.';
+$lang['locfiledit_webmaster_only'] = 'Només els administradors web poden crear o modificar arxius locals.';
 ?>
