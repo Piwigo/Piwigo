2addd8dcb72851811431186e407d843ba70e9e69
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-29 13:08:18 +0000

    [trunk_upgrade] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/trunk@14516 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fa_IR/upgrade.lang.php
+++ language/fa_IR/upgrade.lang.php
@@ -23,20 +23,22 @@
 
 
 
-$lang['Upgrade'] = "آپگريد";
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "پیشنهاد می‌شود که با استفاده از این صفحه بانک‌اطلاعاتی نسخه قدیمی Piwigo خود را به نسخه جدید آن ارتقا دهید. این آپگرید می‌تواند شما را در ارتقا به نسخه‌ی جدید راهنمایی می‌کند و نسخه Piwigo شما را به <strong>نسخه %s </strong> راهنمایی می‌کند.";
-$lang['Upgrade from version %s to %s'] = "آپگريد از نسخه %s به نسخه %s";
+$lang['Upgrade'] = 'آپگرید';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'پیشنهاد می‌شود که با استفاده از این صفحه بانک اطلاعاتی نگارش قدیمی Piwigo خود را به نگارش تازه ی آن ارتقا دهید. سیستم آپگرید نگارش کنونی گالری شما را <strong>نگارش %s </strong> تشخیص داده است.';
+$lang['Upgrade from version %s to %s'] = 'آپگرید از نگارش %s به نگارش %s';
 $lang['Statistics'] = "آمار";
-$lang['total upgrade time'] = "تمام زمان آپگريد";
-$lang['total SQL time'] = "تمام زمان SQL";
-$lang['SQL queries'] ="کوري‌هاي SQL";
-$lang['Upgrade informations'] = "اطلاعات آپگريد";
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = "اگر به مشکلی برخوردید به [مدیریت>ابزارها>نگهداری] مراجعه نمایید.";
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = "هشدار ، پلاگین‌های زیر در حال حاضر از کار افتاده‌اند ، قبل از فعال کردن مجدد آنان 'چک آپدیت' را کلیک کنید:";
-$lang['Only administrator can run upgrade: please sign in below.'] = "فقط مدیران می‌توانند سیستم را آپگرید کنند";
+$lang['total upgrade time'] = 'زمان کل آپگرید';
+$lang['total SQL time'] = 'زمان کل SQL';
+$lang['SQL queries'] = 'کوئری(Query) های SQL';
+$lang['Upgrade informations'] = 'اطلاعات آپگرید';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'اگر به مشکلی برخوردید به [مدیریت > ابزار ها > نگهداری] مراجعه نمایید.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'هشدار، پلاگین های زیر در حال حاضر از کار افتاده اند. برای امنیت بیشتر، قبل از فعال کردن دوباره ی آن ها باید آپگرید های تازه آنها را بررسی کنید:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'فقط مدیران می توانند سیستم را آپگرید کنند.';
 $lang['You do not have access rights to run upgrade'] = "شما دسترسی لازم برای آپگرید را ندارید";
-$lang['All sub-albums of private albums become private'] = "تمام زیر دسته‌های مربوط به دسته‌های شخصی ، شخصی شدند";
-$lang['User permissions and group permissions have been erased'] = "مجوزهای کاربران و گروه‌ها حذف شد";
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "فقط پیشوند عکس‌ریزه‌ها(thumbnails) و آدرس ایمیل وب‌مستر از نسخه قدیمی باقی خواهد ماند";
+$lang['All sub-albums of private albums become private'] = 'تمام زیرآلبوم های مربوط به آلبوم های شخصی، شخصی شدند';
+$lang['User permissions and group permissions have been erased'] = 'مجوز های کاربران و گروه ها پاک شد';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'فقط پیشوند عکس های بندانگشتی و آدرس ایمیل مدیرکل از تنظیمات قدیمی باقی خواهد ماند';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'در <i>%s</i>, قبل از <b>?></b>, اضافه کنید:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'هشدار، قالب های زیر در حال حاضر از کار افتاده اند. برای امنیت بیشتر، قبل از فعال کردن دوباره ی آن ها باید آپگرید های تازه آنها را بررسی کنید:';
+$lang['In <i>%s</i>, before <b></b>, insert:'] = 'در <i>%s</i>، قبل از <b></b>، اضافه کنید:';
 ?>
