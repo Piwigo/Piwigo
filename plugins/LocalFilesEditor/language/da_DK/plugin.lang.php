fe70409d81e680ce370fb5405c3abf36b81569ae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-30 17:40:36 +0000

    [localfileseditor] Add da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/trunk@14609 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/da_DK/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Backup fil indlæst.';
+$lang['locfiledit_bak_loaded2'] = 'Du skal gemme filen for at gendanne det.';
+$lang['locfiledit_cant_save'] = 'Aktuelle fil er ikke skrivbar. Kontroller, om en mappe "lokal/" er skrivbar (chmod).';
+$lang['locfiledit_choose_file'] = 'Vælg den fil der skal redigeres';
+$lang['locfiledit_edit'] = 'Rediger';
+$lang['locfiledit_new_tpl'] = 'Opret ny fil';
+$lang['locfiledit_newfile'] = 'Filen er ikke eksisterende, og vil blive skabt af LocalFiles Editor';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Sprog';
+$lang['locfiledit_onglet_localconf'] = 'Lokal konfiguration';
+$lang['locfiledit_onglet_plug'] = 'Personlig Plugin';
+$lang['locfiledit_onglet_tpl'] = 'Skabeloner';
+$lang['locfiledit_restore_confirm'] = 'venligst bekræft?
+Gendan vil ikke være effektiv før næste gem.';
+$lang['locfiledit_restore'] = 'Genopret backup fil';
+$lang['locfiledit_save_bak'] = 'Backup kopi vil blive oprettet når der Gemmes.';
+$lang['locfiledit_save_config'] = 'Fil skrevet med succes.';
+$lang['locfiledit_save_file'] = 'Gem fil';
+$lang['locfiledit_saved_bak'] = 'Backup fil oprettet (%s)';
+$lang['locfiledit_show_default'] = 'Vis referencefil:';
+$lang['locfiledit_syntax_error'] = 'Syntaks fejl! Filen kan ikke gemmes.';
+?>
