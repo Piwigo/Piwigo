818ee631f0c4a245bcc5d1365f52f279d29b2d8d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-02 20:17:27 +0000

    [localfileseditor] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/trunk@14677 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/el_GR/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Το αρχείο αντιγράφου ασφαλείας έχει φορτωθεί.';
+$lang['locfiledit_bak_loaded2'] = 'Πρέπει να αποθηκεύσετε το αρχείο για να το επαναφέρετε.';
+$lang['locfiledit_cant_save'] = 'Το τρέχων αρχείο δεν είναι εγγράψιμο. Ελέγξτε αν ο κατάλογος  "local/" είναι εγγράψιμος (chmod).';
+$lang['locfiledit_choose_file'] = 'Επιλέξτε το αρχείο που θέλετε να επεξεργαστείτε';
+$lang['locfiledit_edit'] = 'Επεξεργασία';
+$lang['locfiledit_empty_filename'] = 'Θα πρέπει να συμπληρώσετε το όνομα του αρχείου.';
+$lang['locfiledit_empty_page'] = 'Κενή σελίδα';
+$lang['locfiledit_file_already_exists'] = 'Το αρχείο υπάρχει ήδη.';
+$lang['locfiledit_filename_error'] = 'Απαγορευμένοι χαρακτήρες στο όνομα αρχείου.';
+$lang['locfiledit_model_error'] = 'Πρέπει να επιλέξετε ένα υπόδειγμα.';
+$lang['locfiledit_model'] = 'Υπόδειγμα';
+$lang['locfiledit_new_filename'] = 'Όνομα Αρχείου';
+$lang['locfiledit_new_tpl'] = 'Δημιουργία νέου αρχείου';
+$lang['locfiledit_newfile'] = 'Το αρχείο δεν υπάρχει και θα δημιουργηθεί από τον  LocalFiles Editor';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Γλώσσες';
+$lang['locfiledit_onglet_localconf'] = 'Τοπικές ρυθμίσεις';
+$lang['locfiledit_onglet_plug'] = 'Προσωπικά Πρόσθετα';
+$lang['locfiledit_onglet_tpl'] = 'Πρότυπα';
+$lang['locfiledit_parent_directory'] = 'Γονικός κατάλογος';
+$lang['locfiledit_restore_confirm'] = 'Παρακαλούμε να επιβεβαιώσετε;
+Η Επαναφορά αρχείου αντιγράφου ασφαλείας δεν θα γίνει μέχρι την επόμενη αποθήκευση.';
+$lang['locfiledit_restore'] = 'Επαναφορά αρχείου αντιγράφου ασφαλείας';
+$lang['locfiledit_save_bak'] = 'Αντίγραφο ασφαλείας θα δημιουργηθεί κατά την αποθήκευση.';
+$lang['locfiledit_save_config'] = 'Το αρχείο εγγράφθικε με επιτυχία.';
+$lang['locfiledit_save_file'] = 'Αποθήκευση Αρχείου';
+$lang['locfiledit_saved_bak'] = 'Δημιουργία αρχείου ασφαλείας(%s)';
+$lang['locfiledit_show_default'] = 'Εμφάνιση αρχείου αναφοράς:';
+$lang['locfiledit_syntax_error'] = 'Συντακτικό σφάλμα! Το αρχείο δεν μπορεί να αποθηκευτεί.';
+$lang['locfiledit_webmaster_only'] = 'Μόνο webmasters μπορεί να δημιουργεί ή να τροποποιεί τοπικά αρχεία.';
+?>
