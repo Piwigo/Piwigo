ea80a821e5c091cca898fa666d3b7f54377edff5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-05 12:59:44 +0000

    [trunk_install] Update mk_MK, thanks to : is4neno2
    git-svn-id: http://piwigo.org/svn/trunk@14773 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mk_MK/install.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Admin configuration'] = 'Администраторски поднесувања';
+$lang['Basic configuration'] = 'Основни поднесувања';
+$lang['Congratulations, Piwigo installation is completed'] = 'Браво, Piwago инсталацијата е завршена';
+$lang['Database configuration'] = 'Конфигурација на датабаза';
+$lang['Database name'] = 'Име на датабазата';
+$lang['Default gallery language'] = 'Основен јазик на галеријата';
+$lang['Hope to see you back soon.'] = 'Се надеваме дека ќе се видиме наскоро.';
+$lang['Installation'] = 'Инсталација';
+$lang['Password'] = 'Лозинка';
+$lang['Sorry!'] = 'Извини !';
+$lang['Start Install'] = 'Стартувај ја инсталацијата';
+$lang['Try to configure PHP 5'] = 'Пробај да го сетираш PHP 5';
+$lang['User'] = 'Корисник';
+?>
