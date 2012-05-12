86380e116ff9f14f29d5e81964ac69eb4f093a10
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-12 08:13:01 +0000

    [trunk_install] Update fi_FI, thanks to : Tome-
    git-svn-id: http://piwigo.org/svn/trunk@14914 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fi_FI/install.lang.php
+++ language/fi_FI/install.lang.php
@@ -40,4 +40,19 @@ $lang['localhost, sql.multimania.com, toto.freesurf.fr'] = 'paikallinen isäntä
 $lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'sähköpostin tulee olla muodossa xxx@yyy.eee (esim. jack@altern.org)';
 $lang['user login given by your host provider'] = 'käyttäjänimi, jonka palveluntarjoaja on määrittänyt';
 $lang['user password given by your host provider'] = 'käyttäjän salasana, jonka palveluntarjoaja on määrittänyt';
+$lang['Congratulations, Piwigo installation is completed'] = 'Onnittelut, Piwigon asennus on onnistunut';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Älä epäröi ottaa yhteyttä, kysy apua foorumiltamme: %s';
+$lang['It appears your webhost is currently running PHP %s.'] = 'Näyttää että palveluntarjoajasi käyttää PHP %s.';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'Tarvitsetko apua? Kysy asiasta <a href="%s">Piwigon keskustelupalstalla</a>';
+$lang['Note you can change your configuration by yourself and restart Piwigo after that.'] = 'Voit muokata asetuksia itse ja käynnistää Piwigon uudelleen tämän jälkeen.';
+$lang['PHP 5 is required'] = 'PHP 5 vaaditaan';
+$lang['Password ']['confirm'] = 'Salasana [hyväksy]';
+$lang['Try to configure PHP 5'] = 'Yritä määrittää PHP 5';
+$lang['Visitors will be able to contact site administrator with this mail'] = 'Vierailijat voivat ottaa yhteyttä sivuston ylläpitäjään tätä sähköpostiosoitetta käyttäen';
+$lang['Webmaster mail address'] = 'Ylläpitäjän sähköpostiosoite';
+$lang['Webmaster password'] = 'Ylläpitäjän salasana';
+$lang['Welcome to my photo gallery'] = 'Tervetuloa kuvagalleriaani';
+$lang['enter a login for webmaster'] = 'Lisää käyttäjätunnus ylläpitäjälle';
+$lang['please enter your password again'] = 'Salasana uudelleen';
+$lang['webmaster login can\'t contain characters \' or "'] = 'Ylläpitäjän käyttäjätunnus ei voi sisältää \' tai " merkkejä';
 ?>
