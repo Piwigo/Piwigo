8499e67e5e8c25061c5b7de325b88f493a96ca1f
ddtddt <ddtddt@piwigo.org>
2012-06-03 17:56:00 +0000

    [trunk] - language - add plugin core fr_CA - Quebec - Québécois - Thanks to TiOger - feature:2457
    git-svn-id: http://piwigo.org/svn/trunk@15507 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/fr_CA/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['c13y_upgrade_no_anomaly'] = 'Pas d\'anomalie détectée après la mise à jour de l\'application, le greffon "Check upgrades" a été désactivé.';
+$lang['c13y_dbl_email_user'] = 'Utilisateurs avec la même adresse de courriel';
+$lang['c13y_correction_dbl_email_user'] = 'Supprimez les utilisateurs en double';
+$lang['c13y_obsolete_plugin'] = 'Greffon obsolète';
+$lang['c13y_correction_obsolete_plugin'] = 'Le greffon "%s" est inclus dans cette version de l\'application, mais vous devez le désinstaller.'; 
+?>
