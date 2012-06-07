d6a72de977e1e4f83aa7c99186705b0e8c3d921a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-07 17:02:25 +0000

    [localfileseditor] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/trunk@15592 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/fa_IR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'فایل پشتیبان بارگذاری شد.';
+$lang['locfiledit_bak_loaded2'] = 'برای بازیابی فایل باید آن را ذخیره کنید.';
+$lang['locfiledit_cant_save'] = 'این فایل قابل ویرایش نمی باشد. باید دسترسی (chmod) آن را از داخل هاست تغییر دهید.';
+$lang['locfiledit_choose_file'] = 'یک فایل را برای ویرایش انتخاب کنید';
+$lang['locfiledit_edit'] = 'ویرایش';
+$lang['locfiledit_empty_filename'] = 'باید یک نام برای فایل خود انتخاب کنید.';
+$lang['locfiledit_empty_page'] = 'برگه ی خالی';
+$lang['locfiledit_file_already_exists'] = 'فایلی با این نام موجود می باشد.';
+$lang['locfiledit_filename_error'] = 'نویسه های غیر مجاز در نام فایل استفاده شده است.';
+$lang['locfiledit_model_error'] = 'باید یک الگو را انتخاب کنید.';
+$lang['locfiledit_model'] = 'الگو';
+$lang['locfiledit_new_filename'] = 'نام فایل';
+$lang['locfiledit_new_tpl'] = 'ایجاد فایل تازه';
+$lang['locfiledit_newfile'] = 'این فایل وجود ندارد و بوسیله ی LocalFiles Editor ایجاد خواهد شد';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'زبان ها';
+$lang['locfiledit_onglet_localconf'] = 'پیکربندی محلی (Local Config)';
+$lang['locfiledit_onglet_plug'] = 'افزونه های شخصی';
+$lang['locfiledit_onglet_tpl'] = 'قالب ها';
+$lang['locfiledit_parent_directory'] = 'پوشه ی مادر';
+$lang['locfiledit_restore_confirm'] = 'آیا مطمئن می باشید؟
+<br>
+بدون ذخیره ی فایل بازیابی آن ممکن نخواهد بود.';
+$lang['locfiledit_restore'] = 'بازیابی فایل پشتیبان';
+$lang['locfiledit_save_bak'] = 'فایل پشتیبان پس از نخستین ذخیره ایجاد خواهد شد.';
+$lang['locfiledit_save_config'] = 'فایل با موفقیت ویرایش شد.';
+$lang['locfiledit_save_file'] = 'ذخیره فایل';
+$lang['locfiledit_saved_bak'] = 'فایل پشتیبان ایجاد شد (%s)';
+$lang['locfiledit_show_default'] = 'نمایش فایل مرجع';
+$lang['locfiledit_syntax_error'] = 'خطای دستوری (Syntax)! امکان ذخیره ی فایل وجود ندارد.';
+$lang['locfiledit_webmaster_only'] = 'فقط مدیرکل می تواند یک فایل محلی را ایجاد یا ویرایش کند.';
+?>
