5214b49e95c422252b3c45d635211c1a051a5196
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-15 12:35:27 +0000

    [trunk_upgrade] Update en_UK, thanks to : Tadjio
    git-svn-id: http://piwigo.org/svn/trunk@15723 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/upgrade.lang.php
+++ language/en_UK/upgrade.lang.php
@@ -24,7 +24,7 @@
 
 
 $lang['Upgrade'] = "Upgrade";
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "This page proposes to upgrade the database from your Piwigo old version to the new version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).";
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'This page proposes to upgrade the database from your old Piwigo version to the new version. The upgrade assistant thinks you are currently running <strong>release %s</strong> (or equivalent).';
 $lang['Upgrade from version %s to %s'] = "Upgrade from version %s to %s";
 $lang['Statistics'] = "Statistics";
 $lang['total upgrade time'] = "total upgrade time";
@@ -39,5 +39,5 @@ $lang['All sub-albums of private albums become private'] = "All sub-albums of pr
 $lang['User permissions and group permissions have been erased'] = "Users and groups permissions have been erased";
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "Only thumbnails prefix and webmaster mail address have been saved from previous configuration";
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'In <i>%s</i>, before <b>?></b>, insert:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'As a precaution, the following themes have been deactivated. You must check for themes upgrade before reactiving them:';
 ?>
