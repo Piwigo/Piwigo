3a8ca0edaf417227ac7df8e6e78d86acb86aeb66
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-18 18:49:02 +0000

    [trunk_upgrade] Update en_UK, thanks to : Tadjio
    git-svn-id: http://piwigo.org/svn/trunk@15819 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/upgrade.lang.php
+++ language/en_UK/upgrade.lang.php
@@ -39,5 +39,5 @@ $lang['All sub-albums of private albums become private'] = "All sub-albums of pr
 $lang['User permissions and group permissions have been erased'] = "Users and groups permissions have been erased";
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "Only thumbnails prefix and webmaster mail address have been saved from previous configuration";
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'In <i>%s</i>, before <b>?></b>, insert:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'As a precaution, the following themes have been deactivated. You must check for themes upgrade before reactiving them:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'As a precaution, the following themes have been deactivated. You must check for themes upgrades before reactivating them:';
 ?>
