0252fed78e0d8ac20c3db17240d86e19fb92cb9c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-22 13:43:27 +0000

    [localfileseditor] Update af_ZA, thanks to : maeztro
    git-svn-id: http://piwigo.org/svn/trunk@15947 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/af_ZA/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Rugsteun lêer gelaai.';
+$lang['locfiledit_bak_loaded2'] = 'Jy moet \'n lêer stoor om dit te herstel.';
+$lang['locfiledit_cant_save'] = 'Huidige lêer kan nie geskryf word nie. Gaan na of die subgids "local/" skryfbaar is (chmod).';
+$lang['locfiledit_choose_file'] = 'Kies die lêer wat geredigeer moet word';
+$lang['locfiledit_edit'] = 'Redigeer';
+$lang['locfiledit_empty_filename'] = 'Jy moet \'n lêernaam intik.';
+$lang['locfiledit_empty_page'] = 'Leë blad';
+$lang['locfiledit_file_already_exists'] = 'Lêer bestaan alreeds';
+$lang['locfiledit_filename_error'] = 'Verbode karakters in die lêernaam.';
+$lang['locfiledit_model_error'] = 'Jy moet \'n model kies.';
+$lang['locfiledit_model'] = 'Model';
+$lang['locfiledit_new_filename'] = 'Lêernaam';
+$lang['locfiledit_new_tpl'] = 'Skep \'n nuwe lêer';
+$lang['locfiledit_newfile'] = 'Lêer bestaan nie en sal geskep word met die LocalFiles Editor ';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Tale';
+$lang['locfiledit_onglet_localconf'] = 'Local config';
+$lang['locfiledit_onglet_plug'] = 'Persoonlike Invoegsel';
+$lang['locfiledit_onglet_tpl'] = 'Sjablone';
+$lang['locfiledit_parent_directory'] = 'Bron gids';
+$lang['locfiledit_restore_confirm'] = 'Bevestig asseblief? Herstel sal nie effektief wees voor volgende stoor nie.';
+$lang['locfiledit_restore'] = 'Herstel die rugsteun lêer';
+$lang['locfiledit_save_bak'] = 'Rugsteun kopie sal geskep word met stoor.';
+$lang['locfiledit_save_config'] = 'Lêer suksesvol geskryf.';
+$lang['locfiledit_save_file'] = 'Stoor lêer';
+$lang['locfiledit_saved_bak'] = 'Rugsteun lêer geskep (%s)';
+$lang['locfiledit_show_default'] = 'Vertoon verwysings lêer:';
+$lang['locfiledit_syntax_error'] = 'Sintaks fout! Lêer kan nie gestoor word nie.';
+$lang['locfiledit_webmaster_only'] = 'Slegs webmeesters kan lokale lêers skep of verander.';
+?>
