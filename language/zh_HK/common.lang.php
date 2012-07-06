43abcc35b3f4fd1d7d20492a9302acd5530a7e0c
ddtddt <ddtddt@piwigo.org>
2012-07-06 14:35:39 +0000

    [trunk] - language - add zh_HK - 中文 (香港) - Chinese (Hong Kong S.A.R.) Thanks to fatman - feature:2676
    git-svn-id: http://piwigo.org/svn/trunk@16394 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/common.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: 中文 (香港) [HK]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = '中文 (香港)';
+$lang_info['country'] = '香港特別行政區';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'zh_HK';
+$lang_info['zero_plural'] = true;
+
+
+$lang['%d album updated'] = '%d 個相簿已更新';
+$lang['%d albums updated'] = '%d 個相簿已更新';
+$lang['%d comment to validate'] = '%d 個留言須要審核';
+$lang['%d comment'] = '%d 個留言';
+$lang['%d comments to validate'] = '%d 個留言須要審核';
+$lang['%d comments'] = '%d 個留言';
+$lang['%d hit'] = '%d 個點擊';
+$lang['%d hits'] = '%d 個點擊';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d 個新留言';
+$lang['%d new comments'] = '%d 個新留言';
+$lang['%d new photo'] = '%d 張新相片';
+$lang['%d new photos'] = '%d 張新相片';
+$lang['%d new user'] = '%d 個用戶';
+$lang['%d new users'] = '%d 個新用戶';
+$lang['%d photo is also linked to current tags'] = '當前的標籤連結到 %d 張相片';
+$lang['%d photo'] = '%d 張相片';
+$lang['%d photos are also linked to current tags'] = '當前的標籤連結到 %d 張相片';
+$lang['%d photos'] = '%d 張相片';
+$lang['%d rates'] = '%d 個評級';
+$lang['(!) This comment requires validation'] = '(!) 這個留言須要審核';
+$lang['... or browse your empty gallery'] = '... 或瀏覽你的未有相片的相簿';
+$lang['... or please deactivate this message, I will find my way by myself'] = '...  或取消此信息，我會自己找到方法';
+$lang['A comment on your site'] = '你的網站有一個留言';
+$lang['About Piwigo'] = '關於 Piwigo';
+$lang['About'] = '關於';
+?>
