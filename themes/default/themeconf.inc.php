97f795f89fa449abc8b35436f2cea7104adeea56
flop25 <flop25@piwigo.org>
2012-07-08 15:47:48 +0000

    adding a conf img_dir and a loader : ajax-loader-big.gif 128x128 bug:2684
    git-svn-id: http://piwigo.org/svn/trunk@16502 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/default/themeconf.inc.php
+++ themes/default/themeconf.inc.php
@@ -2,6 +2,7 @@
 $themeconf = array(
   'name'         => 'default',
   'icon_dir'      => 'themes/default/icon',
+  'img_dir'      => 'themes/default/images',
   'mime_icon_dir' => 'themes/default/icon/mimetypes/',
   'local_head'    => 'local_head.tpl',
 );
