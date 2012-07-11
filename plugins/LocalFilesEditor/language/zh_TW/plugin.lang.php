a1a035a4ebb13a38b050fc0d0c2eccee3a094e9a
ddtddt <ddtddt@piwigo.org>
2012-07-11 07:52:09 +0000

    [2.4] - language - LocalFilesEditor - add zh_TW - 中文 (繁體) - Chinese traditional Thanks to chouyh
    git-svn-id: http://piwigo.org/svn/branches/2.4@16607 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/zh_TW/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['locfiledit_onglet_localconf'] = '本地配置';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_tpl'] = '樣本';
+$lang['locfiledit_onglet_lang'] = '語言';
+$lang['locfiledit_onglet_plug'] = '個人插件';
+$lang['locfiledit_cant_save'] = '當前的文件不能寫入。請確認 "local/" 目錄是可寫入的 (chmod)。';
+$lang['locfiledit_newfile'] = '檔案不存在，將由本地文件編輯器重新創建。';
+$lang['locfiledit_save_config'] = '檔案已成功寫入';
+$lang['locfiledit_show_default'] = '顯示參考文件: ';
+$lang['locfiledit_save_bak'] = '備份副本將被建立並儲存';
+$lang['locfiledit_saved_bak'] = '備份文件已建立 (%s) ';
+$lang['locfiledit_save_file'] = '存檔';
+$lang['locfiledit_choose_file'] = '選擇需編輯的檔案';
+$lang['locfiledit_edit'] = '編輯';
+$lang['locfiledit_restore'] = '還原備份檔案';
+$lang['locfiledit_restore_confirm'] = '請確認？下次儲存時，還原才會生效。';
+$lang['locfiledit_bak_loaded1'] = '備份文件已載入';
+$lang['locfiledit_bak_loaded2'] = '你必須儲存文件才能把它還原';
+$lang['locfiledit_syntax_error'] = '語法錯誤！檔案無法儲存';
+$lang['locfiledit_new_tpl'] = '創建新的檔案';
+$lang['locfiledit_new_filename'] = '檔案名稱';
+$lang['locfiledit_model'] = '模式';
+$lang['locfiledit_empty_page'] = '空白頁';
+$lang['locfiledit_parent_directory'] = '父目錄';
+$lang['locfiledit_filename_error'] = '檔案名稱中有禁用字符';
+$lang['locfiledit_file_already_exists'] = '檔案已存在';
+$lang['locfiledit_model_error'] = '你必須選擇模式';
+$lang['locfiledit_empty_filename'] = '你必須輸入檔名';
+$lang['locfiledit_webmaster_only'] = '只有網頁管理員才能建立或修改本地文件';
+?>
