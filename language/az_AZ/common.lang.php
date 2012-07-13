723af15d760fedb0bc2028ca6e10531936cf643a
ddtddt <ddtddt@piwigo.org>
2012-07-13 21:48:27 +0000

    [trunk] - language - add az_AZ – Azeri - Azərbaycanca Thanks to Nightly - feature:2676
    git-svn-id: http://piwigo.org/svn/trunk@16699 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/az_AZ/common.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Azərbaycanca [AZ]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=618
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'Azərbaycanca';
+$lang_info['country'] = 'Azərbaycan';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'az';
+$lang_info['zero_plural'] = true;
+
+
+$lang['%d album updated'] = '%d albom yeniləndi';
+$lang['%d albums updated'] = '%d albomlar yeniləndi';
+$lang['%d comment to validate'] = '%d təsdiqlənməli şərh';
+$lang['%d comment'] = '%d şərh';
+$lang['%d comments to validate'] = '%d təsdiqlənməli şərhlər';
+$lang['%d comments'] = '%d şərhlər';
+$lang['%d hit'] = '%d keçid';
+$lang['%d hits'] = '%d keçidlər';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d yeni şərh';
+$lang['%d new comments'] = '%d yeni şərhlər';
+$lang['%d new photo'] = '%d yeni foto';
+$lang['%d new photos'] = '%d yeni fotolar';
+$lang['%d new user'] = '%d yeni istifadəçi';
+$lang['%d new users'] = '%d yeni istifadəçilər';
+$lang['%d photo is also linked to current tags'] = '%d foto həmçinin bu etiketlərlə əlaqələndirilmişdir';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos are also linked to current tags'] = '%d fotolar həmçinin bu etiketlərlə əlaqələndirilmişdir';
+$lang['%d photos'] = '%d foto';
+$lang['%d rates'] = '%d qiymətlər';
+$lang['(!) This comment requires validation'] = '(!) Şərh təsdiq olunmalıdır';
+$lang['... or browse your empty gallery'] = '... ya da boş qalereyanızı gözdən keçirdin';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... ya da zəhmət olmasa bu mesajı deaktiv edin, mən özüm yolu tapacam';
+$lang['A comment on your site'] = 'Saytınızdakı şərh';
+$lang['About Piwigo'] = 'Piwigo haqqında';
+$lang['About'] = 'Haqqında';
+?>
