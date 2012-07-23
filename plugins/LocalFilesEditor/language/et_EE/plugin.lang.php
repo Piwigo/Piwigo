33c271855f325effb5256b38555e3e265e53a34e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-23 21:03:56 +0000

    [localfileseditor] Update et_EE, thanks to : kristian
    git-svn-id: http://piwigo.org/svn/trunk@16956 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/et_EE/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Varukoopia avatud.';
+$lang['locfiledit_bak_loaded2'] = 'Taastamise jaoks pead sa faili salvestama.';
+$lang['locfiledit_cant_save'] = 'Aktiivne faili pole kirjutatav. Vaata kas kaust "local/" on kirjutusõigustega (chmod).';
+$lang['locfiledit_choose_file'] = 'Vali fail mida redigeerida';
+$lang['locfiledit_edit'] = 'Redigeeri';
+$lang['locfiledit_empty_filename'] = 'Sa pead sisestama faili nimi.';
+$lang['locfiledit_empty_page'] = 'Tühi leht';
+$lang['locfiledit_file_already_exists'] = 'Fail on juba olemas.';
+$lang['locfiledit_filename_error'] = 'Lubamatuid tähte faili nimis.';
+$lang['locfiledit_model_error'] = 'Sa pead valima mudeli.';
+$lang['locfiledit_model'] = 'Mudel';
+$lang['locfiledit_new_filename'] = 'Faili nimi';
+$lang['locfiledit_new_tpl'] = 'Loo uus fail';
+$lang['locfiledit_newfile'] = 'Faili pole olemas, LocalFiles Editor tekitab selle';
+$lang['locfiledit_onglet_css'] = 'CSS laaditabel';
+$lang['locfiledit_onglet_lang'] = 'Keeled';
+$lang['locfiledit_onglet_localconf'] = 'Kohalik seadistamine';
+$lang['locfiledit_onglet_plug'] = 'Isiklik plugin';
+$lang['locfiledit_onglet_tpl'] = 'Mallid';
+$lang['locfiledit_parent_directory'] = 'Ülemine kaust';
+$lang['locfiledit_restore_confirm'] = 'Palun kinnita?
+Taastatakse alles salvestamise korras.';
+$lang['locfiledit_restore'] = 'Taasta varukoopia';
+$lang['locfiledit_save_bak'] = 'Varukoopia tehakse salvestamisel.';
+$lang['locfiledit_save_config'] = 'Fail edukalt loodud.';
+$lang['locfiledit_save_file'] = 'Salvesta fail';
+$lang['locfiledit_saved_bak'] = 'Varukoopia loomine (%s)';
+$lang['locfiledit_show_default'] = 'Kuva etalon-faili:';
+$lang['locfiledit_syntax_error'] = 'Süntaksiviga! Faili ei salvestatud.';
+$lang['locfiledit_webmaster_only'] = 'Ainult veebimeistrid võivad luua või muuta kohalike faile.';
+?>
