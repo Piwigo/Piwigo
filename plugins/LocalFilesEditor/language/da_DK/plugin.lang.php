32db9324929ac9ba1b6eda62c1a797fed3a9e81b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-18 18:58:01 +0000

    [localfileseditor] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/trunk@18003 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/da_DK/plugin.lang.php
+++ plugins/LocalFilesEditor/language/da_DK/plugin.lang.php
@@ -20,34 +20,34 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['locfiledit_bak_loaded1'] = 'Backup fil indlæst.';
-$lang['locfiledit_bak_loaded2'] = 'Du skal gemme filen for at gendanne det.';
-$lang['locfiledit_cant_save'] = 'Den aktuelle fil er ikke skrivbar. Kontroller, om mappen "lokal/" er skrivbar (chmod).';
+$lang['locfiledit_bak_loaded1'] = 'Fil med sikkerhedskopi indlæst.';
+$lang['locfiledit_bak_loaded2'] = 'Du skal gemme filen for at gendanne den.';
+$lang['locfiledit_cant_save'] = 'Den aktuelle fil er ikke skrivbar. Undersøg om mappen "local/" er skrivbar (chmod).';
 $lang['locfiledit_choose_file'] = 'Vælg den fil, der skal redigeres';
 $lang['locfiledit_edit'] = 'Rediger';
 $lang['locfiledit_new_tpl'] = 'Opret ny fil';
-$lang['locfiledit_newfile'] = 'Filen er ikke eksisterende, og vil blive skabt af LocalFiles Editor';
+$lang['locfiledit_newfile'] = 'Filen findes ikke og vil blive oprettet af LocalFiles Editor';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_lang'] = 'Sprog';
-$lang['locfiledit_onglet_localconf'] = 'Lokal konfiguration';
+$lang['locfiledit_onglet_localconf'] = 'Lokal opsætning';
 $lang['locfiledit_onglet_plug'] = 'Personlig Plugin';
 $lang['locfiledit_onglet_tpl'] = 'Skabeloner';
 $lang['locfiledit_restore_confirm'] = 'Bekræft venligst?
-Gendan vil ikke være effektiv før næste gem.';
-$lang['locfiledit_restore'] = 'Genopret backup fil';
-$lang['locfiledit_save_bak'] = 'Backup kopi vil blive oprettet når der gemmes.';
+Gendan vil ikke træde i kraft før næste gem.';
+$lang['locfiledit_restore'] = 'Genopret fil med sikkerhedskopi';
+$lang['locfiledit_save_bak'] = 'Sikkerhedskopi vil blive oprettet når der gemmes.';
 $lang['locfiledit_save_config'] = 'Fil skrevet med succes.';
 $lang['locfiledit_save_file'] = 'Gem fil';
-$lang['locfiledit_saved_bak'] = 'Backup fil oprettet (%s)';
+$lang['locfiledit_saved_bak'] = 'Fil med sikkerhedskopi oprettet (%s)';
 $lang['locfiledit_show_default'] = 'Vis referencefil:';
-$lang['locfiledit_syntax_error'] = 'Syntaks fejl! Filen kan ikke gemmes.';
-$lang['locfiledit_empty_filename'] = 'Du skal udfylde filnavnet.';
+$lang['locfiledit_syntax_error'] = 'Syntaksfejl! Filen kan ikke gemmes.';
+$lang['locfiledit_empty_filename'] = 'Du skal skrive et filnavn.';
 $lang['locfiledit_empty_page'] = 'Tom side';
-$lang['locfiledit_file_already_exists'] = 'Fil eksisterer allerede.';
-$lang['locfiledit_filename_error'] = 'Forbudt tegn i filnavnet.';
+$lang['locfiledit_file_already_exists'] = 'Filen findes allerede.';
+$lang['locfiledit_filename_error'] = 'Forbudt tegn i filnavn.';
 $lang['locfiledit_model_error'] = 'Du skal vælge en model.';
 $lang['locfiledit_model'] = 'Model';
-$lang['locfiledit_new_filename'] = 'Fil navn';
-$lang['locfiledit_parent_directory'] = 'Hoved mappe';
+$lang['locfiledit_new_filename'] = 'Filnavn';
+$lang['locfiledit_parent_directory'] = 'Hovedmappe';
 $lang['locfiledit_webmaster_only'] = 'Kun webmastere kan oprette eller ændre lokale filer.';
 ?>
