3d3aad65f705158251778817d8d4660c0e13fa33
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-19 04:32:40 +0000

    [trunk_upgrade] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/trunk@18042 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/upgrade.lang.php
+++ language/da_DK/upgrade.lang.php
@@ -22,20 +22,20 @@
 // +-----------------------------------------------------------------------+
 
 $lang['Upgrade'] = 'Opgrader';
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denne side foreslår at opgradere databasen fra din gamle version af Piwigo til den nye version. Opgraderingsassistenten tror du burger en<strong>release %s</strong> (or equivalent).';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Denne side foreslår at opgradere databasen fra din gamle version af Piwigo til den nye version. Opgraderingsassistenten mener, at du pt. benytter en <strong>release %s</strong> (eller tilsvarende).';
 $lang['Upgrade from version %s to %s'] = 'Opgrader fra version %s til %s';
 $lang['Statistics'] = 'Statistik';
-$lang['total upgrade time'] = 'total opgraderings tid';
-$lang['total SQL time'] = 'total SQL tid';
-$lang['SQL queries'] = 'SQL queries';
-$lang['Upgrade informations'] = 'Opgraderings information';
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Udfør et vedligeholdelsescheck I [Administration>Tools>Maintenance] hvis du oplever problemer.';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'For en sikkerheds skyld er følgende plugins blevet deaktiveret. Check dine plugins opgraderinger før du reaktiverer dem:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'Kun en administrator kan lave en opgradering: Login in herunder.';
-$lang['You do not have access rights to run upgrade'] = 'Du har ikke adgangsrettighed til at køre en opgradering';
-$lang['All sub-albums of private albums become private'] = 'Alle under-kategorier af private kategorier bliver private';
-$lang['User permissions and group permissions have been erased'] = 'Bruger og gruppe tilladelser er blevet slettet';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Kun  thumbnails prefix og webmaster mail addresse er blevet gemt fra tidligere konfiguration. ';
+$lang['total upgrade time'] = 'total opgraderingstid';
+$lang['total SQL time'] = 'total SQL-tid';
+$lang['SQL queries'] = 'SQL-forespørgsler';
+$lang['Upgrade informations'] = 'Opgraderingsinformation';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Udfør et vedligeholdelsescheck i [Administration>Tools>Maintenance] hvis du oplever problemer.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Som en forholdsregel er følgende plugins blevet deaktiveret. Du skal se efter plugin-opdateringer før du genaktiverer dem:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Kun en administrator kan foretage en opgradering: Log ind herunder.';
+$lang['You do not have access rights to run upgrade'] = 'Du har ikke adgangsrettigheder til at foretage en opgradering';
+$lang['All sub-albums of private albums become private'] = 'Alle underalbummer hørende til private albummer bliver private';
+$lang['User permissions and group permissions have been erased'] = 'Brugeres og gruppers rettigheder er blevet slettet';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Kun miniaturebilledepræfix og webmasters mail-adresse er gemt fra tidligere opsætning. ';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'I <i>%s</i>, før <b>?></b>, indsæt:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Som en forholdsregel er følgende temaer blevet deaktiveret. Check for tema opgraderinger før du reaktiverer dem:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Som en forholdsregel er følgende temaer blevet deaktiveret. Du skal se efter tema-opdateringer før du genaktiverer dem:';
 ?>
