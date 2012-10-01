a95f11331368a31834d862055c8983f02059022d
ddtddt <ddtddt@piwigo.org>
2012-10-01 17:17:37 +0000

    [trunk] - language addnew language/ta_IN Tamil தமிழ்  Thanks to sivastar
    git-svn-id: http://piwigo.org/svn/trunk@18398 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/common.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: தமிழ் [IN]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=638
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'தமிழ்';
+$lang_info['country'] = 'இந்தியா';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'ta';
+$lang_info['zero_plural'] = true;
+
+
+$lang['%d album updated'] = '%d ஆல்பம் மேம்படுத்தப்பட்டது';
+$lang['%d albums updated'] = '%d ஆல்பங்கள் மேம்படுத்தப்பட்டது';
+$lang['%d comment to validate'] = '%d கருத்து பரிசீலிக்கப்படுகிறது';
+$lang['%d comment'] = '%d கருத்து';
+$lang['%d comments to validate'] = '%d கருத்து பரிசீலிக்கப்படுகிறது';
+$lang['%d comments'] = '%d கருத்து';
+$lang['%d hit'] = '%d ஹிட்';
+$lang['%d hits'] = '%d ஹிட்ஸ்';
+$lang['%d Kb'] = '%d கேபி';
+$lang['%d new comment'] = '%d புதிய கருத்து';
+$lang['%d new comments'] = '%d புதிய கருத்துக்கள்';
+$lang['%d new photo'] = '%d புதிய புகைப்படம்';
+$lang['%d new photos'] = '%d புதிய புகைப்படங்கள்';
+$lang['%d new user'] = '%d புதிய உறுப்பினர்';
+$lang['%d new users'] = '%d புதிய உறுப்பினர்கள்';
+$lang['%d photo is also linked to current tags'] = '%d புகைப்படங்களுக்கும் தற்போதைய குறிச்சொற்கள் இணைக்கப்பட்டது';
+$lang['%d photo'] = '%d புகைப்படம்';
+$lang['%d photos are also linked to current tags'] = '%d புகைப்படங்களுக்கும் தற்போதைய குறிச்சொற்கள் இணைக்கப்பட்டது';
+$lang['%d photos'] = '%d புகைப்படங்கள்';
+$lang['%d rates'] = '%d மதிப்பீடுகள்';
+$lang['(!) This comment requires validation'] = '(!)ிந்தக் கருத்துக்கு பரிசீலனை தேவைப்படுகிறது';
+$lang['... or browse your empty gallery'] = '... அல்லது உங்களுடைய காலியான ஆல்பத்தில் உலவு';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... அல்லது தயவு செய்து இந்த செய்தியை செயலிழக்க செய்யவும், நான் என் வழியில் கண்டுகொள்கிறேன்';
+$lang['A comment on your site'] = 'உங்கள் தளத்திற்கு ஒரு கருத்து வந்துள்ளது';
+$lang['About Piwigo'] = 'Piwigo பற்றி';
+$lang['About'] = 'பற்றி';
+
+
+?>
