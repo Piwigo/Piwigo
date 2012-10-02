88676a7b5bf24b8e03d21450175b0773412ecd67
ddtddt <ddtddt@piwigo.org>
2012-10-02 20:14:10 +0000

    [trunk] - language addnew language/ms_MY Malay  - Bahasa Malaysia  Thanks to sivastar
    git-svn-id: http://piwigo.org/svn/trunk@18446 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ms_MY/common.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Malay [MY]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=645
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'Malay';
+$lang_info['country'] = 'Malaysia';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'ms';
+$lang_info['zero_plural'] = true;
+
+
+$lang['%d album updated'] = '%d album dikemaskini';
+$lang['%d albums updated'] = '%d album-album dikemaskini';
+$lang['%d comment to validate'] = '%d komen untuk mengesahkan';
+$lang['%d comment'] = '%d komen';
+$lang['%d comments to validate'] = '%d komen untuk mengesahkan';
+$lang['%d comments'] = '%d komen-komen';
+$lang['%d hit'] = '%d hit';
+$lang['%d hits'] = '%d hits';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d komen baru';
+$lang['%d new comments'] = '%d komen-komen baru';
+$lang['%d new photo'] = '%d baru foto';
+$lang['%d new photos'] = '%d baru foto-foto';
+$lang['%d new user'] = '%d pengguna baru';
+$lang['%d new users'] = '%d pengguna baru';
+$lang['%d photo is also linked to current tags'] = '%d foto juga dikaitkan dengan tags semasa';
+$lang['%d photo'] = '%d photo';
+$lang['%d photos are also linked to current tags'] = '%d gambar juga dikaitkan dengan tags semasa';
+$lang['%d photos'] = '%d foto-foto';
+$lang['%d rates'] = '%d kadar-kadar';
+$lang['(!) This comment requires validation'] = '(!) Komen ini memerlukan pengesahan';
+$lang['... or browse your empty gallery'] = '... atau melayari galeri kosong anda';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... atau sila menyahaktifkan mesej ini, saya akan mencari jalan saya oleh diri saya sendiri';
+$lang['A comment on your site'] = 'Satu komen di laman anda';
+$lang['About Piwigo'] = 'Mengenai Piwigo';
+$lang['About'] = 'Mengenai';
+
+?>
