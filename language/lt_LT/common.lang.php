81c89fe79f221cd0cbe3d2fcc4bf2d1e157a7644
ddtddt <ddtddt@piwigo.org>
2012-10-31 18:59:40 +0000

    [trunk] - language addnew language/lt_LT Lithuanian - Lietuviu  Thanks to Giedrelis
    git-svn-id: http://piwigo.org/svn/trunk@18827 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/common.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Lietuviu [LT]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=648
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'Lietuviu';
+$lang_info['country'] = 'Lietuva';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'lt';
+$lang_info['zero_plural'] = true;
+
+
+$lang['%d album updated'] = '%d albumas atnaujintas';
+$lang['%d albums updated'] = '%d albumai atnaujinti';
+$lang['%d comment to validate'] = '%d patvirtinti komentara ';
+$lang['%d comment'] = '%d komentuoti';
+$lang['%d comments to validate'] = '%d patvirtinti komentarus';
+$lang['%d comments'] = '%d komentarai';
+$lang['%d hit'] = '%d paspaudimas';
+$lang['%d hits'] = '%d paspaudimai';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d naujas komentaras';
+$lang['%d new comments'] = '%d nauji komentarai';
+$lang['%d new photo'] = '%d nauja nuotrauka';
+$lang['%d new photos'] = '%d naujos nuotraukos';
+$lang['%d new user'] = '%d naujas narys';
+$lang['%d new users'] = '%d nauji nariai';
+$lang['%d photo is also linked to current tags'] = '%d nuotrauka taip pat yra susijusi su dabartinemis žymomis';
+$lang['%d photo'] = '%d nuotrauka';
+$lang['%d photos are also linked to current tags'] = '%d nuotraukos taip pat siejamos su dabartinemis žymomis';
+$lang['%d photos'] = '%d nuotraukos';
+$lang['%d rates'] = '%d reitingai';
+$lang['(!) This comment requires validation'] = '(!) Reikalingas komentro patvirtinimas';
+$lang['... or browse your empty gallery'] = '... arba ieškokite tušcios jusu galerijos';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... arba išjungti ši pranešima, aš rasiu pats ';
+$lang['A comment on your site'] = 'Komentaras tavo internetineje svetaineje';
+$lang['About Piwigo'] = 'Apie Piwigo';
+$lang['About'] = 'Apie';
+?>
