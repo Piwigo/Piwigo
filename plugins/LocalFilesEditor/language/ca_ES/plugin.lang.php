69ec920d2dc5459b08bee2eef0a7dcb94e011db0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-15 19:14:03 +0000

    [localfileseditor] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/trunk@19034 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/ca_ES/plugin.lang.php
+++ plugins/LocalFilesEditor/language/ca_ES/plugin.lang.php
@@ -20,34 +20,34 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['locfiledit_bak_loaded2'] = 'Cal que desis l\'arxiu per tal de poder-lo restaurar.';
-$lang['locfiledit_edit'] = 'Editar';
+$lang['locfiledit_bak_loaded2'] = 'Cal que deseu l\'arxiu per tal de poder-lo restaurar.';
+$lang['locfiledit_edit'] = 'Edita';
 $lang['locfiledit_file_already_exists'] = 'Aquest arxiu ja existeix.';
 $lang['locfiledit_model'] = 'Model';
-$lang['locfiledit_new_filename'] = 'Nom d\'arxiu';
-$lang['locfiledit_new_tpl'] = 'Crear un nou arxiu';
+$lang['locfiledit_new_filename'] = 'Nom de l\'arxiu';
+$lang['locfiledit_new_tpl'] = 'Crea un nou arxiu';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_lang'] = 'Idiomes';
 $lang['locfiledit_onglet_localconf'] = 'Configuració local';
-$lang['locfiledit_onglet_plug'] = 'Complement personal';
+$lang['locfiledit_onglet_plug'] = 'Connector personal';
 $lang['locfiledit_onglet_tpl'] = 'Plantilles';
 $lang['locfiledit_bak_loaded1'] = 'Arxiu de còpia de seguretat carregat.';
-$lang['locfiledit_cant_save'] = 'L\'arxiu actual no té permís d\'escriptura. Comprova si el directori "local/" disposa d\'aquest permís (chmod).';
-$lang['locfiledit_choose_file'] = 'Tria l\'arxiu a editar';
-$lang['locfiledit_empty_filename'] = 'Has d\'escriure el nom del fitxer.';
+$lang['locfiledit_cant_save'] = 'L\'arxiu actual no té permís d\'escriptura. Comproveu si el directori "local/" disposa d\'aquest permís (chmod).';
+$lang['locfiledit_choose_file'] = 'Esculliu l\'arxiu a editar';
+$lang['locfiledit_empty_filename'] = 'Heu d\'escriure el nom del fitxer.';
 $lang['locfiledit_empty_page'] = 'Pàgina en blanc';
-$lang['locfiledit_filename_error'] = 'Caracters prohibits en el nom del fitxer.';
-$lang['locfiledit_model_error'] = 'Has de triar un model.';
+$lang['locfiledit_filename_error'] = 'Caràcters prohibits en el nom del fitxer.';
+$lang['locfiledit_model_error'] = 'Heu de triar un model.';
 $lang['locfiledit_newfile'] = 'El fitxer no existeix i serà creat per LocalFiles Editor';
 $lang['locfiledit_parent_directory'] = 'Directori pare';
-$lang['locfiledit_restore_confirm'] = 'Si us plau, confirma-ho?
-La restauració no serà efectiva fins que sigui desat.';
+$lang['locfiledit_restore_confirm'] = 'Esteu segurs?
+La restauració no serà efectiva fins a finalitzar.';
 $lang['locfiledit_restore'] = 'Restaura l\'arxiu de còpia de seguretat';
-$lang['locfiledit_save_bak'] = 'Es crearà una còpia de seguretat en el moment en què sigui desat.';
-$lang['locfiledit_save_config'] = 'L\'arxiu ha estat escrit correctament.';
+$lang['locfiledit_save_bak'] = 'La còpia de seguretat es crearà en desar.';
+$lang['locfiledit_save_config'] = 'L\'arxiu s\'ha escrit correctament';
 $lang['locfiledit_save_file'] = 'Desa el fitxer';
-$lang['locfiledit_saved_bak'] = 'Arxiu de còpia de seguretat creat (%s)';
-$lang['locfiledit_show_default'] = 'Mostra el fitxer de referència:';
-$lang['locfiledit_syntax_error'] = 'Error de sintaxi! L\'arxiu no es pot desar.';
+$lang['locfiledit_saved_bak'] = 'Arxiu de còpia de seguretat creat el (%s)';
+$lang['locfiledit_show_default'] = 'Visualitza el fitxer de referència:';
+$lang['locfiledit_syntax_error'] = 'Error de sintaxi! L\'arxiu s\'ha pogut desar.';
 $lang['locfiledit_webmaster_only'] = 'Només els administradors web poden crear o modificar arxius locals.';
 ?>
