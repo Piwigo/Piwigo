4eeba4f4d9472b2d4bad75f02c6656984aef62d8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-26 05:46:57 +0000

    [trunk_admin] Update fi_FI, thanks to : Tome-
    git-svn-id: http://piwigo.org/svn/trunk@19172 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/admin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s ago'] = '%s sitten';
+$lang['... or '] = '... tai';
+$lang['%d years'] = '%d vuodet';
+$lang['%d year'] = '%d vuosi';
+$lang['%d week'] = '%d viikko';
+$lang['%d weeks'] = '%d viikot';
+$lang['%d seconds'] = '%d sekunnit';
+$lang['%d users deleted'] = '%d käyttäjät poistettu';
+$lang['%d users'] = '%d käyttäjät';
+$lang['%d user'] = '%d käyttäjä';
+$lang['%d second'] = '%d sekunti';
+$lang['%d photo was deleted'] = '%d kuva poistettu';
+$lang['%d photos uploaded'] = '%d kuvat ladattu';
+$lang['%d photos were deleted'] = '%d kuvat poistettu';
+$lang['%d month'] = '%d kuukausi';
+$lang['%d months'] = '%d kuukausia';
+$lang['%d minute'] = '%d minuutti';
+$lang['%d minutes'] = '%d minuutteja';
+$lang['%d groups'] = '%d ryhmät';
+$lang['%d days'] = '%d päivät';
+$lang['%d group'] = '%d ryhmä';
+$lang['%d day'] = '%d päivä';
+?>
