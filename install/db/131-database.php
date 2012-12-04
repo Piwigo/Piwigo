94f17094cbdfb76e71236f949b9dc32d8606c7e9
plegall <plg@piwigo.org>
2012-12-04 12:47:45 +0000

    feature 2614: on a upgraded gallery, nb_categories_page is set to 50 instead of the default value 12 for a new installation.
    git-svn-id: http://piwigo.org/svn/trunk@19281 68402e56-0260-453c-a942-63ccdbb3a9ee

--- install/db/131-database.php
+++ install/db/131-database.php
@@ -28,7 +28,7 @@ if (!defined('PHPWG_ROOT_PATH'))
 
 $upgrade_description = 'add "nb_categories_page" parameter';
 
-conf_update_param('nb_categories_page', '12');
+conf_update_param('nb_categories_page', '50');
 
 echo "\n".$upgrade_description."\n";
 
