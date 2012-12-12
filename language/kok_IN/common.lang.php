261bfd9fafc31cf218aee9566184ef2b409375b7
ddtddt <ddtddt@piwigo.org>
2012-12-12 05:54:24 +0000

    [trunk] - new language Konkani  - कोंकणी  - kok_IN - Thanks to Seby Fernandes - the first lines
    git-svn-id: http://piwigo.org/svn/trunk@19399 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/kok_IN/common.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: कोंकणी [IN]
+Version: 2.4.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=660
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'कोंकणी';
+$lang_info['country'] = 'भारत';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'kok';
+$lang_info['zero_plural'] = true;
+
+$lang['%d album updated'] = '%d आल्बम अद्यावत केला';
+$lang['%d albums updated'] = '%d आल्बम अद्यावत केल्यात';
+$lang['%d comment to validate'] = '%d वैध थारावूंक टिप्पणी दिवची';
+$lang['%d comment'] = '%d टिप्पणी';
+$lang['%d comments to validate'] = '%d वैध थारावूंक टिप्पण्यो दिवच्यो';
+$lang['%d comments'] = '%d टिप्पण्यो';
+$lang['%d hit'] = '%d हिट';
+$lang['%d hits'] = '%d हिट्स';
+$lang['%d Kb'] = '%d केबी';
+$lang['%d new comment'] = '%d नवी टिप्पणी';
+$lang['%d new comments'] = '%d नव्यो टिप्पण्यो';
+$lang['%d new photo'] = '%d नवो फोटो';
+$lang['%d new photos'] = '%d नवे फोटे';
+$lang['%d new user'] = '%d नवो वापरपी';
+$lang['%d new users'] = '%d नवे वापरपी';
+$lang['%d photo is also linked to current tags'] = '%d फोटो सध्याच्या टॅग्सांक लिंक्ड केल्ले आसात';
+$lang['%d photo'] = '%d फोटो';
+$lang['%d photos are also linked to current tags'] = '%d फोटे सध्याच्या टॅग्सांक लिंक्ड केल्ले आसात';
+$lang['%d photos'] = '%d फोटे';
+$lang['%d rates'] = '%d दर';
+$lang['(!) This comment requires validation'] = '(!) हे टिप्पणीक सत्यत्वस्थापनाची आवश्यकता आसा';
+$lang['... or browse your empty gallery'] = '... वा तुमची रिकामी गॅलरी ब्रावज करची';
+$lang['... or please deactivate this message, I will find my way by myself'] = '... वा उपकार करून हो संदेश असक्रीय करचो, म्हजी वाट हांव स्वता सोदून काडटलों';
+$lang['A comment on your site'] = 'तुमचे सायटीटचेर टिप्पणी';
+$lang['About Piwigo'] = 'Piwigo विशीं';
+$lang['About'] = 'विशीं';
+
+?>
