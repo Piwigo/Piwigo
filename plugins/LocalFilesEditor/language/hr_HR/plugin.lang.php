2505a1b30f193327a168ccc138cb8880f38afcec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-03 21:42:38 +0000

    [localfileseditor] Update hr_HR, thanks to : alen
    git-svn-id: http://piwigo.org/svn/trunk@19768 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/hr_HR/plugin.lang.php
+++ plugins/LocalFilesEditor/language/hr_HR/plugin.lang.php
@@ -19,7 +19,7 @@
 // | along with this program; if not, write to the Free Software           |
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
-// +-----------------------------------------------------------------------+$lang['locfiledit_onglet_localconf'] = 'Lokalno podešavanje';
+$lang['locfiledit_onglet_localconf'] = 'Lokalne postavke';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_tpl'] = 'Predlošci';
 $lang['locfiledit_onglet_lang'] = 'Jezici';
