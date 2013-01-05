285145db1e57a136cde76e34fe71858fdabf30dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 21:10:44 +0000

    [trunk_install] Update ja_JP, thanks to : ryoon
    git-svn-id: http://piwigo.org/svn/trunk@19874 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ja_JP/install.lang.php
+++ language/ja_JP/install.lang.php
@@ -21,52 +21,56 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 
-$lang['Installation'] = 'インストレーション';
+$lang['Installation'] = 'インストール';
 $lang['Basic configuration'] = '基本設定';
-$lang['Default gallery language'] = 'ギャラリーのデフォルト言語';
+$lang['Default gallery language'] = 'ギャラリーのデフォルトの言語';
 $lang['Database configuration'] = 'データベース設定';
 $lang['Admin configuration'] = '管理設定';
 $lang['Start Install'] = 'インストールを開始する';
 $lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'メールアドレスは、 xxx@yyy.eee のような形式にしてください (例: jack@altern.org)。';
 
 $lang['Webmaster login'] = 'ウェブマスタログイン';
-$lang['It will be shown to the visitors. It is necessary for website administration'] = 'ウェブマスタは、ビジターに表示されます。ウェブサイト管理に必要です。';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = '訪問者一覧に表示されます。ウェブサイト管理に必要です。';
 
 $lang['Connection to server succeed, but it was impossible to connect to database'] = 'サーバへ接続することができましたが、データベースに接続できません。';
 $lang['Can\'t connect to server'] = 'サーバに接続できません。';
 
-$lang['Host'] = 'MySQLホスト';
+$lang['Host'] = 'ホスト';
 $lang['localhost, sql.multimania.com, toto.freesurf.fr'] = '例) localhost、sql.multimania.com、toto.freesurf.fr';
-$lang['User'] = 'ユーザ';
-$lang['user login given by your host provider'] = 'あなたのホストプロバイダから提供されたデータベースユーザ名です。';
+$lang['User'] = 'ユーザー';
+$lang['user login given by your host provider'] = 'あなたのホスティング業者から提供されたデータベースユーザ名です。';
 $lang['Password'] = 'パスワード';
-$lang['user password given by your host provider'] = 'あなたのホストプロバイダから提供されたデータベースパスワードです。';
+$lang['user password given by your host provider'] = 'あなたのホスティング業者から提供されたデータベースパスワードです。';
 $lang['Database name'] = 'データベース名';
-$lang['also given by your host provider'] = 'こちらも、あなたのホストプロバイダから提供されたデータベース名です。';
+$lang['also given by your host provider'] = 'こちらも、あなたのホスティング業者から提供されたデータベース名です。';
 $lang['Database table prefix'] = 'データベーステーブル接頭辞';
 $lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'データベーステーブルに接頭辞として付けられます (あなたのテーブルを管理しやすくします)。';
 $lang['enter a login for webmaster'] = 'ウェブマスタのユーザIDを入力してください。';
 $lang['webmaster login can\'t contain characters \' or "'] = 'ウェブマスタのユーザIDには、「\'」または「"」を含まないでください。';
 $lang['please enter your password again'] = 'もう一度あなたのパスワードを入力してください。';
 $lang['Webmaster password'] = 'ウェブマスタパスワード';
-$lang['Keep it confidential, it enables you to access administration panel'] = 'ウェブマスタパスワードは、内密にしてください。ウェブマスタパスワードを使用して、あなたは管理パネルにアクセスすることができます。';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'ウェブマスタパスワードは、秘密にしてください。ウェブマスタパスワードを使用して、管理パネルにアクセスできます。';
 $lang['Password [confirm]'] = 'パスワード [もう一度]';
 $lang['verification'] = '確認';
 $lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'ヘルプが必要ですか? <a href="%s">Piwigoメッセージボード</a>にて、あなたの質問を投稿してください。';
 $lang['Webmaster mail address'] = 'ウェブマスタメールアドレス';
-$lang['Visitors will be able to contact site administrator with this mail'] = 'ビジターは、このメールアドレスでサイト管理者に連絡することができます。';
-$lang['PHP 5 is required'] = 'PHP 5は必要';
-$lang['It appears your webhost is currently running PHP %s.'] = 'あなたのウェブホストは現在PHP %sを使っているらしいです。';
-$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigoは.htaccess ファイルを作成するか、変更しようとしてPHP 5に設定してみます。';
+$lang['Visitors will be able to contact site administrator with this mail'] = '訪問者は、このメールアドレスでサイト管理者に連絡することができます。';
+$lang['PHP 5 is required'] = 'PHP 5が必要です';
+$lang['It appears your webhost is currently running PHP %s.'] = 'あなたのウェブホストは現在PHP %sを使っています。';
+$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigoは.htaccess ファイルを作成するか変更して、PHP 5を設定してみます。';
 $lang['Note you can change your configuration by yourself and restart Piwigo after that.'] = '注：自分で設定を変更し、その後Piwigoを再起動もできます。';
 $lang['Try to configure PHP 5'] = 'PHP 5を設定してみます。';
 $lang['Sorry!'] = '申し訳ありません!';
-$lang['Piwigo was not able to configure PHP 5.'] = 'PiwigoはPHP 5に設定できませんでした。';
-$lang['You may referer to your hosting provider\'s support and see how you could switch to PHP 5 by yourself.'] = 'あなたのホームページ・プロバイダーのサポートに参照し、自分でPHP 5に変更方法を見られます。';
+$lang['Piwigo was not able to configure PHP 5.'] = 'PiwigoはPHP 5を設定できませんでした。';
+$lang['You may referer to your hosting provider\'s support and see how you could switch to PHP 5 by yourself.'] = 'あなたのホスティング業者のサポートに連絡し、どうやったらPHP 5を設定できるか確認すべきです。';
 $lang['Hope to see you back soon.'] = 'またお越し下さい';
-$lang['Congratulations, Piwigo installation is completed'] = 'おめでとうございました。Piwigo のインストール完了しました。';
+$lang['Congratulations, Piwigo installation is completed'] = 'おめでとうございました。Piwigo のインストールが完了しました。';
 $lang['An alternate solution is to copy the text in the box above and paste it into the file "local/config/database.inc.php" (Warning : database.inc.php must only contain what is in the textarea, no line return or space character)'] = 'その他の解決は、上のボックスにあるテクストをコピーし、"local/config/database.inc.php"に貼り付けます。（注意：database.inc.php は上のテクスト内容以外のこと（エンターキーやスペースなど）を含まないでください。)';
-$lang['Creation of config file local/config/database.inc.php failed.'] = 'local/config/database.inc.php のconfig ファイル作成失敗しました。';
-$lang['Download the config file'] = 'config ファイルをダウンロードします。';
-$lang['You can download the config file and upload it to local/config directory of your installation.'] = 'config ファイルをダウンロードし、インストールされた local/config ディレクトリーにアップロードすることができます。';
+$lang['Creation of config file local/config/database.inc.php failed.'] = 'local/config/database.inc.php の設定ファイル作成に失敗しました。';
+$lang['Download the config file'] = '設定ファイルをダウンロードします。';
+$lang['You can download the config file and upload it to local/config directory of your installation.'] = '設定ファイルをダウンロードし、インストールされた local/config ディレクトリーにアップロードすることができます。';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'どうぞ、フォーラムで相談するのをためらわないでください: %s';
+$lang['Just another Piwigo gallery'] = '他のPiwigoギャラリー';
+$lang['Welcome to your new installation of Piwigo!'] = '新しくPiwigoをインストールしていただき、ありがとうざいます!';
+$lang['Welcome to my photo gallery'] = '私のフォトギャラリーへようこそ';
 ?>
