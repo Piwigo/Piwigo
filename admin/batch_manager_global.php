3edee072527b6ac58ee0a7734900758e6606d06e
flop25 <flop25@piwigo.org>
2013-01-30 19:04:35 +0000

    language changed to match the one defined in lang files
    git-svn-id: http://piwigo.org/svn/trunk@20489 68402e56-0260-453c-a942-63ccdbb3a9ee

--- admin/batch_manager_global.php
+++ admin/batch_manager_global.php
@@ -413,10 +413,10 @@ DELETE
   if ('generate_derivatives' == $action)
   {
     if ($_POST['regenerateSuccess'] != '0')
-      array_push($page['infos'], sprintf(l10n('%s photos were generated'), $_POST['regenerateSuccess']));
+      array_push($page['infos'], sprintf(l10n('%s photos have been regenerated'), $_POST['regenerateSuccess']));
 
     if ($_POST['regenerateError'] != '0')
-      array_push($page['warnings'], sprintf(l10n('%s photos were not generated'), $_POST['regenerateError']));
+      array_push($page['warnings'], sprintf(l10n('%s photos can not be regenerated'), $_POST['regenerateError']));
 
   }
 
