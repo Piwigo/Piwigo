9c0863b08b54e04d152cc7d79254ef3a415c64cc
ddtddt <ddtddt@piwigo.org>
2013-02-19 19:44:40 +0000

    [2.4] - language update for 2.4.7
    git-svn-id: http://piwigo.org/svn/branches/2.4@20857 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fi_FI/admin.lang.php
+++ language/fi_FI/admin.lang.php
@@ -2,7 +2,7 @@
 // +-----------------------------------------------------------------------+
 // | Piwigo - a PHP based photo gallery                                    |
 // +-----------------------------------------------------------------------+
-// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
 // | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
 // | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
 // +-----------------------------------------------------------------------+
@@ -42,4 +42,91 @@ $lang['%d groups'] = '%d ryhmät';
 $lang['%d days'] = '%d päivät';
 $lang['%d group'] = '%d ryhmä';
 $lang['%d day'] = '%d päivä';
+$lang['%d albums moved'] = '%d albumia siirretty';
+$lang['%d guests'] = '%d vierasta';
+$lang['%d guest'] = '%d vieras';
+$lang['Description'] = 'Kuvaus';
+$lang['Database'] = 'Tietokanta';
+$lang['Check for updates'] = 'Tarkista päivitykset';
+$lang['Can\'t create temporary file.'] = 'Väliaikaistiedostoa ei voida luoda.';
+$lang['Week starts on'] = 'Viikon ensimmäinen päivä on';
+$lang['Watermark'] = 'Vesileima';
+$lang['Version'] = 'Versio';
+$lang['Tools'] = 'Työkalut';
+$lang['Summary'] = 'Yhteenveto';
+$lang['Send'] = 'Lähetä';
+$lang['See you soon.'] = 'Näkemiin.';
+$lang['Path'] = 'Polku';
+$lang['Panorama'] = 'Panorama';
+$lang['Operating system'] = 'Käyttöjärjestelmä';
+$lang['Opacity'] = 'Läpinäkyvyys';
+$lang['No results'] = 'Ei tuloksia';
+$lang['Move'] = 'Siirrä';
+$lang['File'] = 'Tiedosto';
+$lang['ERROR'] = 'VIRHE';
+$lang['Drop into album'] = 'Valitse albumi';
+$lang['Documentation'] = 'Dokumentaatio';
+$lang['Directory'] = 'Hakemisto';
+$lang['Crop'] = 'Rajaa';
+$lang['Cancel'] = 'Peruuta';
+$lang['Allow rating'] = 'Salli arvosanat';
+$lang['Album name'] = 'Albumin nimi';
+$lang['Add a user'] = 'Lisää käyttäjä';
+$lang['Add'] = 'Lisää';
+$lang['A new version of Piwigo is available.'] = 'Uusi versio Piwigosta on saatavilla.';
+$lang['ACCESS_0'] = 'Vapaa pääsy';
+$lang['%d user deleted'] = '%d käyttäjä poistettu';
+$lang['%d hour'] = '%d tunti';
+$lang['%d hours'] = '%d tuntia';
+$lang['%d member'] = '%d jäsen';
+$lang['%d members'] = '%d jäsentä';
+$lang['Allow user customization'] = 'Salli kustomointi käyttäjille';
+$lang['Allow users to edit their own comments'] = 'Salli käyttäjien muokata kommenttejaan';
+$lang['Allow users to delete their own comments'] = 'Salli käyttäjien poistaa kommenttejaan';
+$lang['Allow user registration'] = 'Salli käyttäjien rekisteröityminen';
+$lang['All themes are up to date.'] = 'Kaikki teemat ovat ajan tasalla.';
+$lang['%d parameters were updated.'] = '%d parametriä päivitetty.';
+$lang['%d parameter was updated.'] = '%d parametri päivitetty.';
+$lang['%d mails were sent.'] = '%d sähköpostiviestiä lähetetty.';
+$lang['%d mail was sent.'] = '%d sähköpostiviesti lähetetty.';
+$lang['%d mail was not sent.'] = '%d sähköpostiviesti lähettämättä.';
+$lang['%d mails were not sent.'] = '%d sähköpostiviestiä lähettämättä.';
+$lang['%d albums including'] = '%d albumia sisältäen';
+$lang['%d album including'] = '%d albumi sisältäen';
+$lang['Choose an action'] = 'Valitse toiminto';
+$lang['%d user was updated.'] = '%d käyttäjä päivitetty.';
+$lang['%d users were not updated.'] = '%d käyttäjää ei päivitetty.';
+$lang['%d users were updated.'] = '%d käyttäjää päivitetty.';
+$lang['%d album moved'] = '%d albumi siirretty';
+$lang['Level 0'] = '---';
+$lang['Level 4'] = 'Perhe';
+$lang['Level 2'] = 'Kaverit';
+$lang['Level 1'] = 'Kontaktit';
+$lang['Main Page'] = 'Pääsivu';
+$lang['IP'] = 'IP';
+$lang['I decide to update anyway'] = 'Haluan päivittää silti';
+$lang['Form'] = 'Lomake';
+$lang['Groups'] = 'Ryhmät';
+$lang['Groups and users'] = 'Ryhmät ja käyttäjät';
+$lang['Guest Settings'] = 'Vieraiden asetukset';
+$lang['Guest cannot be deleted'] = 'Vierasta ei voi poistaa';
+$lang['Guests'] = 'Vieraat';
+$lang['Height'] = 'Korkeus';
+$lang['Hello'] = 'Hei';
+$lang['Hello,'] = 'Hei,';
+$lang['Group name'] = 'Ryhmän nimi';
+$lang['Group management'] = 'Ryhmien hallinta';
+$lang['Group'] = 'Ryhmä';
+$lang['Go to'] = 'Siirry';
+$lang['General statistics'] = 'Yleiset tilastot';
+$lang['Deleted on'] = 'Poistettu';
+$lang['Correction applied with success'] = 'Korjaus onnistuneesti lisätty';
+$lang['Correction'] = 'Korjaus';
+$lang['Check integrity'] = 'Eheyden tarkistus';
+$lang['Check all'] = 'Valitse kaikki';
+$lang['%s has been successfully updated.'] = '%s päivitettiin onnistuneesti.';
+$lang['%d waiting for validation'] = '%d odottaa hyväksyntää';
+$lang['%d user was not updated.'] = '%d käyttäjää ei päivitetty';
+$lang[' and %d virtuals'] = 'ja %d virtuaalista';
+$lang[' and %d virtual'] = 'ja %d virtuaalinen';
 ?>
