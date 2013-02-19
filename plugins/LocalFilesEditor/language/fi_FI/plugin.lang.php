2e90ef9d6d835566aa26809dd785bbc43ee2fda0
ddtddt <ddtddt@piwigo.org>
2013-02-19 18:28:54 +0000

    [2.4] - language update for 2.4.7 / plugins languages
    git-svn-id: http://piwigo.org/svn/branches/2.4@20856 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/fi_FI/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_bak_loaded1'] = 'Varmuuskopiotiedosto ladattu.';
+$lang['locfiledit_bak_loaded2'] = 'Sinun on tallennettava tiedosto palauttaaksesi sen.';
+$lang['locfiledit_cant_save'] = 'Nykyiseen tiedostoon ei voida kirjoittaa. Tarkista, onko hakemistoon "local/" kirjoitusoikeudet (chmod).';
+$lang['locfiledit_choose_file'] = 'Valitse muokattava tiedosto';
+$lang['locfiledit_edit'] = 'Muokkaa';
+$lang['locfiledit_empty_filename'] = 'Sinun on annettava tiedostonimi.';
+$lang['locfiledit_empty_page'] = 'Tyhjä sivu';
+$lang['locfiledit_file_already_exists'] = 'Tiedosto on jo olemassa.';
+$lang['locfiledit_filename_error'] = 'Tiedostonimi sisältää kiellettyjä merkkejä.';
+$lang['locfiledit_model'] = 'Malli';
+$lang['locfiledit_model_error'] = 'Sinun on valittava malli.';
+$lang['locfiledit_new_filename'] = 'Tiedostonimi';
+$lang['locfiledit_new_tpl'] = 'Luo uusi tiedosto';
+$lang['locfiledit_newfile'] = 'Tiedostoa ei ole, joten se luodaan LocalFiles Editorilla';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Kielet';
+$lang['locfiledit_onglet_localconf'] = 'Paikallinen konfiguraatio';
+$lang['locfiledit_onglet_plug'] = 'Henkilökohtainen lisäosa';
+$lang['locfiledit_onglet_tpl'] = 'Mallineet';
+$lang['locfiledit_parent_directory'] = 'Ylähakemisto';
+$lang['locfiledit_restore'] = 'Palauta varmuuskopio';
+$lang['locfiledit_restore_confirm'] = 'Vahvistetaanko?
+Palautus astuu voimaan vasta seuraavan tallennuksen yhteydessä.';
+$lang['locfiledit_save_bak'] = 'Varmuuskopio luodaan seuraavan tallennuksen yhteydessä.';
+$lang['locfiledit_save_config'] = 'Tiedosto kirjoitettu onnistuneesti.';
+$lang['locfiledit_save_file'] = 'Tallenna tiedosto';
+$lang['locfiledit_saved_bak'] = 'Varmuuskopio luotu (%s)';
+$lang['locfiledit_show_default'] = 'Näytä referenssitiedosto:';
+$lang['locfiledit_syntax_error'] = 'Syntaksivirhe! Tiedostoa ei voida tallentaa.';
+$lang['locfiledit_webmaster_only'] = 'Vain webmasterit voivat luoda ja muuttaa paikallisia tiedostoja.';
+?>
