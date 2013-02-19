6e3d680c01764da00976e24c2923b6df7742852a
ddtddt <ddtddt@piwigo.org>
2013-02-19 09:05:49 +0000

    [trunk] - language - create Galician  - Galego  thanks to damufo
    git-svn-id: http://piwigo.org/svn/trunk@20837 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/common.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2010 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Galiza [ES]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=681
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+$lang_info['language_name'] = 'Galego';
+$lang_info['country'] = 'Galiza';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'gl';
+$lang_info['zero_plural'] = false;
+
+$lang['%d comments'] = '%d comments';
+$lang['%d hit'] = '%d hit';
+$lang['%d hits'] = '%d hits';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d new comment';
+$lang['%d new comments'] = '%d new comments';
+$lang['%d new photo'] = '%d new photo';
+$lang['%d new photos'] = '%d new photos';
+$lang['%d new user'] = '%d new user';
+$lang['%d new users'] = '%d novos usuarios';
+$lang['Date'] = 'Data';
+$lang['day'][0] = 'Domingo';
+$lang['day'][1] = 'Luns';
+$lang['day'][2] = 'Martes';
+$lang['day'][3] = 'Mércores';
+$lang['day'][4] = 'Xoves';
+$lang['day'][5] = 'Venres';
+$lang['day'][6] = 'Sábado';
+$lang['month'][10] = 'Outubro';
+$lang['month'][11] = 'Novembro';
+$lang['month'][12] = 'Decembro';
+$lang['month'][1] = 'Xaneiro';
+$lang['month'][2] = 'Febreiro';
+$lang['month'][3] = 'Marzo';
+$lang['month'][4] = 'Abril';
+$lang['month'][5] = 'Maio';
+$lang['month'][6] = 'Xuño';
+$lang['month'][7] = 'Xullo';
+$lang['month'][8] = 'Agosto';
+$lang['month'][9] = 'Setembro';
+
+?>
