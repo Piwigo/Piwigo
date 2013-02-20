cacab7730bc159b32a1afd13848ab60f8251c389
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-20 23:30:26 +0000

    [trunk_upgrade] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/trunk@20878 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/upgrade.lang.php
+++ language/pl_PL/upgrade.lang.php
@@ -28,15 +28,15 @@ $lang['total upgrade time'] = 'sumaryczny czas aktualizacji';
 $lang['total SQL time'] = 'sumaryczny czas SQL';
 $lang['SQL queries'] = 'zapytań SQL';
 $lang['Upgrade informations'] = 'Informacje o aktualizacji';
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Jeżeli napotkasz jakiś problem wykonaj weryfikację przez [Administracja>Narzędzia>Maintenance].';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Jeżeli napotkasz jakiś problem wykonaj weryfikację przez [Administracja>Narzędzia>Konserwacja].';
 $lang['Only administrator can run upgrade: please sign in below.'] = 'Tylko administrator może wykonać aktualizację: zaloguj się poniżej.';
 $lang['You do not have access rights to run upgrade'] = 'Nie masz uprawnień do wykonania aktualizacji';
 $lang['All sub-albums of private albums become private'] = 'Wszystkie pod-albumy prywatnych albumów będą prywatne';
 $lang['User permissions and group permissions have been erased'] = 'Uprawnienia użytkowników oraz grup zostały usunięte';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Z poprzedniej konfiguracji zostały zapisane tylko prefiksy miniatur oraz adres email administratora.';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Z poprzedniej konfiguracji zostały zapisane tylko prefiks miniatur oraz adres e-mail administratora.';
 $lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Ta strona proponuje aktualizację Twojej bazy danych Piwigo do nowej wersji. Asystent aktualizacji myśli, że aktualnie używasz <strong>wersji %s</strong> (lub równoważnej).';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Jako zabezpieczenie, następujące wtyczki zostały zdezaktywowane. Przed ich aktywacją musisz sprawdzić czy nie ma nowszych wersji:';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Na wszelki wypadek następujące wtyczki zostały zdezaktywowane. Przed ich aktywacją, należy sprawdzić, czy nie ma nowszych wersji';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'W <i>%s</i>, przed <b>?></b>, wstaw:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Zapobiegawczo, zostały dezayktywowane następujące tematy. Przed ich ponownym aktywowaniem, należy sprawdzić ich aktualizacje:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Na wszelki wypadek następujące motywy zostały dezaktywowane. Przed ich aktywacją, należy sprawdzić, czy nie ma nowszych wersji';
 
 ?>
