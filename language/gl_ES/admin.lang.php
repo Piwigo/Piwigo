c200de4bfafd58a51508e2b8ff59fd5af7dd4612
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-20 23:35:13 +0000

    [trunk_admin] Update gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/trunk@20895 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/admin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums including'] = '%d álbums incluídos';
+$lang['%d albums moved'] = '%d álbums movidos';
+$lang['%d anomalies have been detected corrected.'] = '%d anomalías foron detectadas e corrixidas.';
+$lang['%d anomalies have been detected.'] = '%d anomalías foron detectadas.';
+$lang['%d anomalies have been ignored.'] = '%d anomalías foron ignoradas.';
+$lang['%d anomalies have not been corrected.'] = '%d anomalías non foron corrixidas.';
+$lang['%d anomaly has been corrected.'] = '%d anomalía foi corrixida.';
+$lang['%d anomaly has been detected.'] = '%d anomalía foi detectada.';
+$lang['%d anomaly has been ignored.'] = '%d anomalía foi ignorada.';
+$lang['%d anomaly has not been corrected.'] = '%d anomalía non foi corrixida.';
+$lang['%d association'] = '%d asociación';
+$lang['%d associations'] = '%d asociacións';
+$lang['%d day'] = '%d día';
+$lang['%d days'] = '%d días';
+$lang['%d group'] = '%d grupo';
+$lang['%d groups'] = '%d grupos';
+$lang[' and %d virtual'] = 'e %d virtual';
+$lang[' and %d virtuals'] = 'e %d virtuais';
+$lang['%d album including'] = '%d álbum incluído';
+$lang['%d album moved'] = '%d álbum movido';
+?>
