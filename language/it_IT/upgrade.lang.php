86f3a2d888e367ce7a115a433112b7056135109a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-21 19:50:59 +0000

    [trunk_upgrade] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/trunk@20917 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/upgrade.lang.php
+++ language/it_IT/upgrade.lang.php
@@ -24,8 +24,7 @@
 
 
 $lang['Upgrade'] = 'Aggiornamento';
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Questa pagina vi propone di aggiornare il vostro database corrispondente alla vecchia versione di Piwigo con la nuova versione.
-L\'assistente all\'aggiornamento ha identificato la vostra attuale <strong>versione %s</strong> (o equivalente).';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Questa pagina vi propone di aggiornare il vostro database corrispondente alla vecchia versione di Piwigo con la nuova versione. L\'assistente all\'aggiornamento ha identificato la vostra attuale <strong>versione %s</strong> (o equivalente).';
 $lang['Upgrade from version %s to %s'] = 'Aggiornamento dalla versione %s alla %s';
 $lang['Statistics'] = 'Statistiche';
 $lang['total upgrade time'] = 'tempo totale aggiornamento';
@@ -33,12 +32,12 @@ $lang['total SQL time'] = 'tempo totale SQL';
 $lang['SQL queries'] = 'queries SQL';
 $lang['Upgrade informations'] = 'Informazioni aggiornamento';
 $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Se riscontrate dei problemi, effettuate una manutenzione in [Amministrazione>Tools>Manutenzione].';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Per precauzione, i seguenti plugins sono stati disattivati. È necessario verificare se esistono degli aggiornamenti prima di riattivarli:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'Solo l\'amministratore può eseguire l\'aggiornamento: fate login.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Per precauzione, i seguenti plugins sono stati disattivati. E\' necessario verificare se esistono degli aggiornamenti prima di riattivarli:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Solo l\'amministratore può eseguire l\'aggiornamento: fare login.';
 $lang['You do not have access rights to run upgrade'] = 'Non avete le autorizzazioni necessarie per effettuare l\'aggiornamento';
 $lang['All sub-albums of private albums become private'] = 'Tutti i sub-album degli album privati diventano privati';
 $lang['User permissions and group permissions have been erased'] = 'I permessi degli utenti e dei gruppi sono stati cancellati';
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Solo il prefisso delle miniature e l\'indirizzo email del webmaster sono stati recuperati dalla precedente configurazione';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'Nel file <i>%s</i>, prima di <b>?></b>, inserite:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Per precauzione, i seguenti temi sono stati disattivati. È necessario verificare se esistono degli aggiornamenti prima di riattivarli:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Per precauzione, i seguenti temi sono stati disattivati. E\' necessario verificare se esistono degli aggiornamenti prima di riattivarli:';
 ?>
