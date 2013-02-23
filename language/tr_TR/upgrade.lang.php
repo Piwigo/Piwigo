d78c943bb9d74f02145aceb653f7a475a09c8b6b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-23 07:01:44 +0000

    [trunk_upgrade] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/trunk@20973 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/upgrade.lang.php
+++ language/tr_TR/upgrade.lang.php
@@ -21,22 +21,22 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 
-$lang['Upgrade'] = "G�ncellestirme";
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = "Bu sayfa yeni s�r�me Piwigo eski s�r�m�nden veritabani y�kseltme �neriyor. Y�kseltme yardimcisi Su anda bir <strong> s�r�m %s (veya esdegeri) </strong> �alistiran d�s�n�yor.";
-$lang['Upgrade from version %s to %s'] = "%s s�r�mden %s s�r�m�ne g�ncellestir";
-$lang['Statistics'] = "Istatistik";
-$lang['total upgrade time'] = "toplam g�ncelleme zamani";
-$lang['total SQL time'] = 'toplam SQL zamani';
-$lang['SQL queries'] = "SQL sorgulari";
-$lang['Upgrade informations'] = "G�ncelleme bilgileri";
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = "Herhangi bir sorunla karsilasirsaniz, [Y�netim> Ara�lar> Bakim] dan bakim yapiniz.";
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = "Bir �nlem olarak, asagidaki eklentiler devre disi olmustur.Onlari aktif etmeden �nce eklentileri y�kseltme i�in kontrol etmelisiniz";
-$lang['Only administrator can run upgrade: please sign in below.'] = "Sadece y�netici g�ncelleme �alistirabilir: Asagidan giris yapin.";
-$lang['You do not have access rights to run upgrade'] = "G�ncelleme �alistirmak i�in yetkiniz yok";
-$lang['All sub-albums of private albums become private'] = 'Özel albümlerin alt albümleri de özel olur';
-$lang['User permissions and group permissions have been erased'] = "Kullanicilar ve gruplarin izinleri silinmistir";
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = "Sadece k���k resim �n eki ve site y�neticisi posta adresiniz �nceki yapilandirman kaydedili";
-$lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i> i�ine, <b>?></b> �nce, ekleyin:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = '�nlem olarak, asagidaki temalar devre disi olmustur. Tekrar etkinlestirmeden �nce g�ncelleme yapmalisiniz:';
+$lang['Upgrade'] = 'Yükseltme';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Bu sayfa, eski Piwigo sürümüne ait veritabanını yeni sürüme yükseltmeyi öneriyor. Yükseltme yardımcısı şu anda <strong>sürüm %s</strong> (veya eşdeğeri) çalıştırıldığınızı düşünüyor.';
+$lang['Upgrade from version %s to %s'] = '%s sürümünden %s sürümüne yükselt';
+$lang['Statistics'] = 'İstatistik';
+$lang['total upgrade time'] = 'toplam güncelleme zamanı';
+$lang['total SQL time'] = 'toplam SQL zamanı';
+$lang['SQL queries'] = 'SQL sorguları';
+$lang['Upgrade informations'] = 'Yükseltme bilgileri';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Herhangi bir sorunla karşılaşırsanız, [Yönetim>Araçlar>Bakım] ile bakım yapınız.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Bir önlem olarak, belirtilen eklentiler devre dışı bırakıldı. Tekrar etkinleştirmeden önce yükseltme seçeneklerini kontrol etmelisiniz.';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Sadece yönetici yüksletmeyi çalıştırabilir: Lütfen oturum açın.';
+$lang['You do not have access rights to run upgrade'] = 'Yüksletmeyi çalıştırmak için erişim hakkınız bulunmuyor';
+$lang['All sub-albums of private albums become private'] = 'Özel albümlerin tüm alt albümleri de özel olur';
+$lang['User permissions and group permissions have been erased'] = 'Kullanicılar ve grupların izinleri silinmiştir';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Önceki yapılandırmadan sadece pul resimleri ön takıları ve site yöneticisi e-posta adresi kaydedildi.';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i> içine, <b>?></b> önce, ekleyin:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Bir önlem olarak, belirtilen temalar devre dışı bırakıldı. Tekrar etkinleştirmeden önce yükseltme seçeneklerini kontrol etmelisiniz.';
 
 ?>
