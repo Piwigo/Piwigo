a49cc27620584969bd322100b37e284d1f033e4e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-25 17:51:04 +0000

    [localfileseditor] Update gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/trunk@21035 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/gl_ES/plugin.lang.php
+++ plugins/LocalFilesEditor/language/gl_ES/plugin.lang.php
@@ -22,4 +22,32 @@
 // +-----------------------------------------------------------------------+
 $lang['locfiledit_bak_loaded2'] = 'Terás que gardar o ficheiro para poder restauralo.';
 $lang['locfiledit_bak_loaded1'] = 'Cargar o ficheiro da copia de seguranza';
+$lang['locfiledit_restore_confirm'] = 'Confirmas que desexas cargar a copia de seguranza?
+Posteriormente deberás premer e Gardar para restaurala.';
+$lang['locfiledit_restore'] = 'Restaurar o ficheiro de copia de seguranza';
+$lang['locfiledit_save_bak'] = 'A copia de seguranza será creada ó gardar.';
+$lang['locfiledit_save_config'] = 'O ficheiro escribiuse con éxito.';
+$lang['locfiledit_save_file'] = 'Gardar ficheiro';
+$lang['locfiledit_saved_bak'] = 'Ficheiro de copia de seguranza creada (%)';
+$lang['locfiledit_show_default'] = 'Amosar o ficheiro referencia:';
+$lang['locfiledit_syntax_error'] = 'Erro de sintaxe! o ficheiro non pode ser gravado';
+$lang['locfiledit_webmaster_only'] = 'Só os administradores web poden crear ou modificar ficheiros locais.';
+$lang['locfiledit_cant_save'] = 'Non se pode escribir o actual ficheiro. Comproba se o cartafol "local/" ten permiso para escribir (chmod).';
+$lang['locfiledit_choose_file'] = 'Escolle o ficheiro a ser modificado';
+$lang['locfiledit_edit'] = 'Modificar';
+$lang['locfiledit_empty_filename'] = 'Debes inserir o nome do ficheiro.';
+$lang['locfiledit_empty_page'] = 'Páxina baleira';
+$lang['locfiledit_file_already_exists'] = 'O ficheiro xa existe.';
+$lang['locfiledit_filename_error'] = 'Hai caracteres prohibidos no nome do ficheiro.';
+$lang['locfiledit_model'] = 'Modelo';
+$lang['locfiledit_model_error'] = 'Debes escoller un modelo.';
+$lang['locfiledit_new_filename'] = 'Nome do ficheiro';
+$lang['locfiledit_new_tpl'] = 'Crear un novo ficheiro';
+$lang['locfiledit_newfile'] = 'O ficheiro non existe mentres non se insire dentro algunha información.';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Idiomas';
+$lang['locfiledit_onglet_localconf'] = 'Configuración local';
+$lang['locfiledit_onglet_plug'] = 'Engadido persoal';
+$lang['locfiledit_onglet_tpl'] = 'Modelos';
+$lang['locfiledit_parent_directory'] = 'Cartafol pai';
 ?>
