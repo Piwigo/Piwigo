577c7957c41ce1cbdfe701c1cdfe8cb71e1c7cd2
ddtddt <ddtddt@piwigo.org>
2013-03-02 14:00:11 +0000

    [trunk] - Language - update Révision to 2.5.0
    git-svn-id: http://piwigo.org/svn/trunk@21097 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/gl_ES/common.lang.php
+++ language/gl_ES/common.lang.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: Galego [ES]
-Version: 2.4.1
+Version: 2.5.0
 Language URI: http://piwigo.org/ext/extension_view.php?eid=681
 Author: Piwigo team
 Author URI: http://piwigo.org
