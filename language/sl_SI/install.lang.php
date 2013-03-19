5b98180385cd38a52ea80f58758569b9d00715e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-19 11:26:25 +0000

    [trunk_install] Update sl_SI, thanks to : obucek
    git-svn-id: http://piwigo.org/svn/trunk@21717 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/install.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PHP 5 is required'] = 'Zahtevan PHP 5';
+$lang['Password'] = 'Geslo';
+$lang['Password ']['confirm'] = 'Geslo [potrdi]';
+$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigo bo poskušal preklopiti vaše nastavitve na PHP 5 z ustvarjanjem ali urejanjem .htacceess datoteke';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo ni mogel nastaviti PHP 5.';
+$lang['Sorry!'] = 'Oprostite!';
+$lang['Start Install'] = 'Začetek namestitve';
+$lang['Try to configure PHP 5'] = 'Poskušam nastaviti PHP 5';
+$lang['User'] = 'Uporabnik';
+$lang['Visitors will be able to contact site administrator with this mail'] = 'Uporabniki bodo te elektronski naslov uporabljali za kontakt s skrbnikom spletne strani ';
+$lang['Webmaster login'] = 'Prijava za spletne skrbnike';
+$lang['Webmaster mail address'] = 'Elektronski naslov skrbnika';
+$lang['Webmaster password'] = 'Skrbniško geslo';
+$lang['Welcome to my photo gallery'] = 'Dobrodošli v mojo galerijo';
+$lang['Welcome to your new installation of Piwigo!'] = 'Dobrodošli na vaši novi namestitvi Piwiga!';
+$lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Lahko prenesete namestitveno datoteko ter jo naložite v local/config imenik vaše namestitve.';
+$lang['Admin configuration'] = 'Admin nastavitve';
+$lang['Basic configuration'] = 'Osnovne nastavitve';
+$lang['Can\'t connect to server'] = 'Ne morem se povezati s strežnikom';
+$lang['Congratulations, Piwigo installation is completed'] = 'Čestitamo! Instalacija je uspešno dokončana!';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Uspešno ste se povezali na strežnik vendar je povezava s podatkovno bazo spodletela.';
+$lang['Creation of config file local/config/database.inc.php failed.'] = 'Ustvarjanje nastavitvene datoteke local/config/database.inc.php je spodletelo.';
+$lang['Database configuration'] = 'Nastavitve podatkovne baze';
+$lang['Database name'] = 'Ime podatkovne baze';
+$lang['Database table prefix'] = 'Predpona podatkovnih tabel';
+$lang['Default gallery language'] = 'Privzeti jezik galerije';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Ne oklevajte in povprašajte za pomoč na našem forumu: %s';
+$lang['Download the config file'] = 'Prenesite config datoteko';
+$lang['Hope to see you back soon.'] = 'Upam, da se kmalu ponovno vidimo.';
+$lang['Host'] = 'Gostitelj';
+$lang['Installation'] = 'Instalacija';
+$lang['It appears your webhost is currently running PHP %s.'] = 'Zdi se, da vaš ponudnik ima nameščen PHP %s';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'Pokazano bo obiskovalcem. To je potrebno za upravljanje spletne strani';
+$lang['Just another Piwigo gallery'] = 'Samo še ena Piwigo galerija';
+?>
