6ef5f85eae3a6b04fc34171e344a0840f1e6d278
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-26 22:41:57 +0000

    [trunk_admin] Update nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/trunk@21861 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/admin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['... or '] = '... eller';
+$lang['ACCESS_0'] = 'Fri tilgang';
+$lang['ACCESS_5'] = 'Ingen tilgang';
+$lang['%d users were updated.'] = '%d brukarar oppdatera';
+$lang['%d user comments rejected'] = '%d brukarkommentarar avvist';
+$lang['%d user comments validated'] = '%d brukarkommentarar godkjend';
+$lang['%d user deleted'] = '%d brukar sletta';
+$lang['%d user was not updated.'] = '%d brukar er ikkje oppdatera';
+$lang['%d user was updated.'] = '%d brukar oppdatera';
+$lang['%d users'] = '%d brukarar';
+$lang['%d users deleted'] = '%d brukarar sletta';
+$lang['%d users were not updated.'] = '%d brukarar er ikkje oppdatera';
+$lang['%d photos were deleted'] = '%d bilete sletta';
+$lang['%d second'] = '%d sekund';
+$lang['%d seconds'] = '%d sekundar';
+$lang['%d tag'] = '%d stikkord';
+$lang['%d tags'] = '%d stikkord';
+$lang['%d user'] = '%d brukar';
+$lang['%d user comment rejected'] = '%d brukarkommentar avvist';
+$lang['%d user comment validated'] = '%d brukarkommentarar godkjend';
+$lang['%d guest'] = '%d gjest';
+$lang['%d guests'] = '%d gjester';
+$lang['%d hour'] = '%d time';
+$lang['%d hours'] = '%d timar';
+$lang['%d member'] = '%d medlem';
+$lang['%d members'] = '%d medlemer';
+$lang['%d minute'] = '%d minutt';
+$lang['%d minutes'] = '%d minuttar';
+$lang['%d month'] = '%d månad';
+$lang['%d months'] = '%d månader';
+$lang['%d photo was deleted'] = '%d bilete sletta';
+$lang['wrong filename'] = 'feil filnamn';
+?>
