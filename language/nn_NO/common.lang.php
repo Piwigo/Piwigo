6130c4c0bd991caed27223152bb0b4ff26e7649a
ddtddt <ddtddt@piwigo.org>
2013-03-26 18:55:33 +0000

    [trunk] - Language - add nn_NO - Norwegian nynorsk Thanks to kiho
    git-svn-id: http://piwigo.org/svn/trunk@21856 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/common.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Norwegian nynorsk [NO]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=689
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'Norwegian nynorsk';
+$lang_info['country'] = 'Norway';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'nn';
+$lang_info['zero_plural'] = true;
+
+$lang['%d comments'] = '%d kommentarar';
+$lang['%d hit'] = '%d treff';
+$lang['%d hits'] = '%d treff';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d ny kommentar';
+$lang['%d new comments'] = '%d nye kommentarar';
+$lang['%d new photo'] = '%d nytt bilete';
+$lang['%d new photos'] = '%d nye bilete';
+$lang['%d new user'] = '%d ny brukar';
+$lang['%d new users'] = '%d nye brukarar';
+$lang['Date'] = 'Dato';
+$lang['day'][0] = 'Sundag';
+$lang['day'][1] = 'Måndag';
+$lang['day'][2] = 'Tysdag';
+$lang['day'][3] = 'Onsdag';
+$lang['day'][4] = 'Torsdag';
+$lang['day'][5] = 'Fredag';
+$lang['day'][6] = 'Laurdag';
+$lang['month'][10] = 'Oktober';
+$lang['month'][11] = 'November';
+$lang['month'][12] = 'Desember';
+$lang['month'][1] = 'Januar';
+$lang['month'][2] = 'Februar';
+$lang['month'][3] = 'Mars';
+$lang['month'][4] = 'April';
+$lang['month'][5] = 'Mai';
+$lang['month'][6] = 'June';
+$lang['month'][7] = 'Juli';
+$lang['month'][8] = 'August';
+$lang['month'][9] = 'September';
+?>
