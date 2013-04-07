e183c8dc03c1d783a5e2b19ad6468fd80fe0a82a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-07 09:07:12 +0000

    [trunk_upgrade] Update lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/trunk@22052 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/upgrade.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Saugumo sumetimais toliau išvardinti priedai buvo išjungti. Prieš įjungiant priedus vėl, patikrinkite, ar nėra jų atnaujinimų:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Saugumo sumetimais toliau išvardintos temos buvo išjungtos. Prieš įjungiant temas vėl, patikrinkite, ar nėra jų atnaujinimų:';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Šis puslapis siųlo atnaujinti duomenų bazę iš senosios Piwigo versijos į naująją. Atnaujinimo asistentas mano, kad Jūs dabar naudojate <strong>leidimą %s</strong> (arba analogišką).';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Profilaktiškai peržiūrėkite [Administration>Tools>Maintenance], jei atsirado kokių nors netikėtumų.';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Iš buvusios konfigūracijos išsaugoti tik miniatiūrų prefiksai ir webmasterio el-pašto adresas';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Tik administratorius gali pradėti atnaujinimą - prašome prisijungti žemiau.';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i> viduje, prieš <b>?></b>, įterpkite:';
+$lang['All sub-albums of private albums become private'] = 'Visi privačių albumų subalbumai tampa privatūs';
+$lang['total upgrade time'] = 'bendras atnaujinimo laikas';
+$lang['total SQL time'] = 'bendras SQL laikas';
+$lang['You do not have access rights to run upgrade'] = 'Jūs neturite leidimo pradėti atnaujinimo';
+$lang['User permissions and group permissions have been erased'] = 'Vartotojų ir grupių leidimai buvo ištrinti';
+$lang['Upgrade informations'] = 'Atnaujinimo informacija';
+$lang['Upgrade from version %s to %s'] = 'Atnaujinti iš %s versijos į %s';
+$lang['Upgrade'] = 'Atnaujinimas';
+$lang['Statistics'] = 'Statistika';
+$lang['SQL queries'] = 'SQL Užklausos';
+?>
