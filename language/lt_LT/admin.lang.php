a85a4f4f3e90f198bc33eb825ee9c3aaae246c6d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-07 17:54:43 +0000

    [trunk_admin] Update lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/trunk@22059 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/admin.lang.php
@@ -0,0 +1,91 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d users were not updated.'] = '%d vartotojų neatnaujinta.';
+$lang['%d users were updated.'] = '%d vartotojų atnaujinta.';
+$lang['%d waiting for validation'] = '%d laukia patvirtinimo';
+$lang['%d week'] = '%d savaitę';
+$lang['%d weeks'] = '%d savaites';
+$lang['%d year'] = '%d metus';
+$lang['%d years'] = '%d metų';
+$lang['%s ago'] = 'prieš %s';
+$lang['%s has been successfully updated.'] = '%s sėkmingai atnaujinta.';
+$lang['%s in the future'] = '%s ateityje';
+$lang['%d parameters were updated.'] = '%d parametrai pakeisti.';
+$lang['%d photo was deleted'] = '%d nuotrauka ištrinta';
+$lang['%d photos uploaded'] = 'įkelta %d nuotraukų';
+$lang['%d photos were deleted'] = 'ištrinta %d nuotrauka';
+$lang['%d physical'] = ' %d fizinių';
+$lang['%d physicals'] = ' %d fizinių';
+$lang['%d second'] = '%d sekundė';
+$lang['%d seconds'] = '%d sekundžių';
+$lang['%d tag'] = '%d žymė';
+$lang['%d tags'] = '%d žymės';
+$lang['%d user'] = '%d vartotojas';
+$lang['%d user comment rejected'] = '%d vartotojų komentaras atmestas';
+$lang['%d user comment validated'] = '%d vartotojų komentaras patvirtintas';
+$lang['%d user comments rejected'] = '%d vartotojų komentarų atmesta';
+$lang['%d user comments validated'] = '%d vartotojų komentarų patvirtinta';
+$lang['%d user deleted'] = '%d vartotojas pašalintas';
+$lang['%d user was not updated.'] = '%d vartotojų neatnaujinta';
+$lang['%d user was updated.'] = '%d vartotojas atnaujintas';
+$lang['%d users'] = '%d vartotojų';
+$lang['%d users deleted'] = '%d vartotojų pašalinta';
+$lang['%d guest'] = '%d svečias';
+$lang['%d guests'] = '%d svečiai';
+$lang['%d hour'] = '%d valanda';
+$lang['%d hours'] = '%d valandų';
+$lang['%d line filtered'] = '%d eilutė nufiltruota';
+$lang['%d lines filtered'] = '%d eilutės nufiltruotos';
+$lang['%d mail was not sent.'] = '%d pranešimas nebuvo išsiųstas.';
+$lang['%d mail was sent.'] = '%d pranešimas išsiųstas.';
+$lang['%d mails were not sent.'] = '%d pranešimai nebuvo išsiųsta.';
+$lang['%d mails were sent.'] = '%d pranešimai išsiųsta.';
+$lang['%d member'] = '%d narys';
+$lang['%d members'] = '%d nariai';
+$lang['%d minute'] = '%d minutė';
+$lang['%d minutes'] = '%d minučių';
+$lang['%d month'] = '%d mėnesį';
+$lang['%d months'] = '%d mėnesius';
+$lang['%d of %d photos selected'] = 'pasirinkta %d iš %d nuotraukų';
+$lang['%d parameter was updated.'] = '%d parametras atnaujintas';
+$lang[' and %d virtuals'] = 'ir %d virtualių';
+$lang[' and %d virtual'] = 'ir %d virtualių';
+$lang['%d album including'] = 'įskaitant %d albumą';
+$lang['%d album moved'] = '%d albumas perkeltas';
+$lang['%d albums including'] = 'įskaitant %d albumus';
+$lang['%d albums moved'] = '%d albumų perkelta';
+$lang['%d anomalies have been detected corrected.'] = 'aptikta ir sutvarkyta %d anomalijų(-os).';
+$lang['%d anomalies have been detected.'] = 'aptikta %d anomalijų(-os).';
+$lang['%d anomalies have been ignored.'] = 'ignoruota %d anomalijų(-os).';
+$lang['%d anomalies have not been corrected.'] = '%d anomalijų(-os) nebuvo sutvarkyta.';
+$lang['%d anomaly has been corrected.'] = '%d anomalijų(-os) buvo sutvarkyta.';
+$lang['%d anomaly has been detected.'] = 'aptikta %d anomalija.';
+$lang['%d anomaly has been ignored.'] = 'ignoruota %d anomalija.';
+$lang['%d anomaly has not been corrected.'] = '%d anomalija nebuvo sutvarkyta.';
+$lang['%d association'] = '%d asociacija';
+$lang['%d associations'] = '%d asociacijų';
+$lang['%d day'] = '%d diena';
+$lang['%d days'] = '%d dienos';
+$lang['%d group'] = '%d grupė';
+$lang['%d groups'] = '%d grupės';
+?>
