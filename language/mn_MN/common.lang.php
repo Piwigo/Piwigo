6251b390a21158e17bff4ce797a504b81f6b1079
ddtddt <ddtddt@piwigo.org>
2013-04-17 06:50:43 +0000

    [trunk] - Language_- new language Mongolian (Cyrillic, Mongolia) - Монгол хэл (Монгол улс) language (mn_MN) Thanks to Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@22249 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mn_MN/common.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Монгол хэл [MN]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=696
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+$lang_info['language_name'] = 'Монгол хэл';
+$lang_info['country'] = 'Монгол улс';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'mn';
+$lang_info['zero_plural'] = true;
+
+$lang['%d comments'] = '%d сэтгэгдэл';
+$lang['%d hit'] = '%d удаа үзсэн';
+$lang['%d hits'] = '%d удаа үзсэн';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d сэтгэгдэл';
+$lang['%d new comments'] = '%d сэтгэгдэл';
+$lang['%d new photo'] = '%d шинэ зураг';
+$lang['%d new photos'] = '%d шинэ зурагнууд';
+$lang['%d new user'] = '%d шинэ хэрэглэгч';
+$lang['%d new users'] = '%d шинэ хэрэглэгч';
+$lang['Date'] = 'Date';
+$lang['day'][0] = 'Бүтэн сайн';
+$lang['day'][1] = 'Даваа';
+$lang['day'][2] = 'Мягмар';
+$lang['day'][3] = 'Лхагва';
+$lang['day'][4] = 'Пүрэв';
+$lang['day'][5] = 'Баасан';
+$lang['day'][6] = 'Хагас сайн';
+$lang['month'][10] = '10 сар';
+$lang['month'][11] = '11 сар';
+$lang['month'][12] = '12 сар';
+$lang['month'][1] = '1 сар';
+$lang['month'][2] = '2 сар';
+$lang['month'][3] = '3 сар';
+$lang['month'][4] = '4 сар';
+$lang['month'][5] = '5 сар';
+$lang['month'][6] = '6 сар';
+$lang['month'][7] = '7 сар';
+$lang['month'][8] = '8 сар';
+$lang['month'][9] = '9 сар';
+?>
