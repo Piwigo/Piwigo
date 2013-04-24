6bc5c9c4580cd1d12c9798baf6cc40577859da65
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-24 05:31:42 +0000

    [trunk_install] Update kn_IN, thanks to : rudramurthy
    git-svn-id: http://piwigo.org/svn/trunk@22349 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/kn_IN/install.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['localhost, sql.multimania.com, toto.freesurf.fr'] = 'localhost, sql.multimania.com, toto.freesurf.fr';
+$lang['Start Install'] = 'ಸ್ಥಾಪನಾ ಕಾರ್ಯವನ್ನು ಆರಂಭಿಸಿ';
+$lang['verification'] = 'ಪರಿಶೀಲನೆ';
+$lang['user password given by your host provider'] = 'ಹೋಸ್ಟ್ ಪ್ರೊವೈಡರ್ ಒದಗಿಸಿರುವ ನಿಮ್ಮ ಬಳಕೆದಾರರ ಗುಪ್ತಪದ ';
+$lang['user login given by your host provider'] = 'ಹೋಸ್ಟ್ ಪ್ರೊವೈಡರ್ ಒದಗಿಸಿರುವ ನಿಮ್ಮ ಬಳಕೆದಾರರ ಲಾಗಿನ್';
+$lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'ಮಿಂಚೆ ವಿಳಾಸ ಈ ರೀತಿಯಲ್ಲಿರಲೇಬೇಕು xxx@yyy.eee (example: jack@altern.org)';
+$lang['please enter your password again'] = 'ನಿಮ್ಮ ಗುಪ್ತಪದವನ್ನು ಮತ್ತೊಮ್ಮೆ ನಮೂದಿಸಿ';
+$lang['enter a login for webmaster'] = 'ವೆಬ್ ಮಾಸ್ಟರ್ ನ ಲಾಗಿನ್ ಅನ್ನು ನಮೂದಿಸಿ';
+$lang['Welcome to your new installation of Piwigo!'] = 'ನಿಮ್ಮ ಹೊಸ Piwigo ಸ್ಥಾಪನಾ ಕೆಲಸಕ್ಕೆ ಸುಸ್ವಾಗತ';
+$lang['Welcome to my photo gallery'] = 'ನನ್ನ ಚಿತ್ರಶಾಲೆ (ಗ್ಯಾಲರಿ) ಗೆ ಸುಸ್ವಾಗತ.';
+$lang['Webmaster password'] = 'ವೆಬ್ ಮಾಸ್ಟರ್ ಗುಪ್ತಪದ';
+$lang['Webmaster mail address'] = 'ವೆಬ್ ಮಾಸ್ಟರ್ ಮಿಂಚೆ ವಿಳಾಸ';
+$lang['Webmaster login'] = 'ವೆಬ್ ಮಾಸ್ಟರ್ ಲಾಗಿನ್';
+$lang['User'] = 'ಬಳಕೆದಾರ';
+$lang['Sorry!'] = 'ಕ್ಷಮಿಸಿ!';
+$lang['Default gallery language'] = 'ಚಿತ್ರಶಾಲೆ (ಗ್ಯಾಲರಿ)ಯ  ಪೂರ್ವನಿಯೋಜಿತ ಭಾಷೆ';
+$lang['Host'] = 'ಹೋಸ್ಟ್';
+$lang['Installation'] = 'ಸ್ಥಾಪನೆ';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'ಸಹಾಯ ಬೇಕೆ? ನಿಮ್ಮ ಪ್ರಶ್ನೆಗಳನ್ನು  <a href="%s">Piwigo message board</a> ನಲ್ಲಿ ಕೇಳಿ.';
+$lang['Just another Piwigo gallery'] = 'ಇದು ನಿಮ್ಮದೇ  Piwigo ಚಿತ್ರಶಾಲೆ (ಗ್ಯಾಲರಿ)';
+$lang['Database name'] = 'ಡೇಟಾಬೇಸಿನ ಹೆಸರು';
+$lang['Can\'t connect to server'] = 'ಸರ್ವರ್ ನೊಂದಿಗೆ ಸಂಪರ್ಕಿಸಲು ಸಾಧ್ಯವಾಗುತ್ತಿಲ್ಲ.';
+?>
