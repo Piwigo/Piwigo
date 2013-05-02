838b929783a1295ae2cc17091961458728d63d94
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-02 11:07:52 +0000

    [trunk_admin] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@22464 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mn_MN/admin.lang.php
@@ -0,0 +1,158 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Zoom'] = 'Томсгох';
+$lang['add a new watermark'] = 'шинээр усан хээ нэмэх';
+$lang['average time'] = 'дундаж хугацаа';
+$lang['bottom left corner'] = 'зүүн доод булан';
+$lang['bottom right corner'] = 'баруун доод булан';
+$lang['clean'] = 'цэвэрлэх';
+$lang['create a new album'] = 'шинэ цомог үүсгэх';
+$lang['create a new site'] = 'шинэ сайт үүсгэх';
+$lang['default values'] = 'үндсэн утга';
+$lang['delete'] = 'устгах';
+$lang['delete album'] = 'цомогыг устгах';
+$lang['delete photo'] = 'зургыг устгах';
+$lang['deleted'] = 'устсан';
+$lang['email'] = 'Имэйл';
+$lang['jump to album'] = 'цомог уруу очих';
+$lang['jump to photo'] = 'зураг уруу очих';
+$lang['guest'] = 'зочин';
+$lang['new'] = 'шинэ';
+$lang['number of miniaturized photos'] = 'үүсгэсэн дүрс зургын тоо';
+$lang['other'] = 'бусад';
+$lang['photos per page'] = 'нэг хуудсанд харагдах зураг';
+$lang['ranks'] = 'түвшин';
+$lang['registered users'] = 'бүртгүүлсэн хэрэглэгч';
+$lang['registration date'] = 'бүртгүүлсэн огноо';
+$lang['remove tags'] = 'түлхүүр үгийг хасах';
+$lang['remove this filter'] = 'энэ шүүлтүүрийг хасах';
+$lang['test'] = 'шалгах';
+$lang['status'] = 'байдал';
+$lang['sub-albums'] = 'дэд цомог';
+$lang['top left corner'] = 'зүүн дээд булан';
+$lang['top right corner'] = 'баруун дээд булан';
+$lang['total time'] = 'нийт хугацаа';
+$lang['user_status_admin'] = 'Админ';
+$lang['user_status_guest'] = 'Зочин';
+$lang['user_status_normal'] = 'Хэрэглэгч';
+$lang['user_status_webmaster'] = 'Вэбмастер';
+$lang['wrong filename'] = 'файлын нэр буруу байна';
+$lang['User'] = 'Хэрэглэгч';
+$lang['Thumbnail'] = 'Дүрс зураг';
+$lang['Thumbnails generation in progress...'] = 'Дүрс зургыг үүсгэж байна...';
+$lang['The whole page'] = 'Хуудсыг бүхэлд нь';
+$lang['This album contains no photo.'] = 'Энэ цомог хоосон байна';
+$lang['The name of an album must not be empty'] = 'Цомогын нэр хоосон байх ёсгүй';
+$lang['The gallery URL is not valid.'] = 'Цомогны URL холбоос буруу байна';
+$lang['The following tag was deleted'] = 'Дараахь түлхүүр үг устлаа';
+$lang['Settings'] = 'Тохиргоо';
+$lang['Tag "%s" was added'] = '"%s" гэсэн түлхүүр үгийг амжилттай нэмлээ';
+$lang['Tag "%s" already exists'] = '"%s" гэсэн түлхүүр үг бүртгэгдсэн байна';
+$lang['Send mail on HTML format'] = 'Мэйлийг HTML хэлбэрээр илгээх';
+$lang['Representant'] = 'Цомогны дүрс зураг';
+$lang['Representation of albums'] = 'Цомогны дүрс зураг';
+$lang['Representative'] = 'Цомогны дүрс зураг';
+$lang['Save Settings'] = 'Тохиргоог хадаглах';
+$lang['Searching...'] = 'Хайж байна...';
+$lang['Select a file'] = 'Файлаа сонгоно уу';
+$lang['Select an album'] = 'Цомогоо сонгоно уу';
+$lang['Reject'] = 'Цуцлах';
+$lang['Refresh'] = 'Сэргээх';
+$lang['Rating'] = 'Үнэлгээ';
+$lang['Photo name'] = 'Зургын нэр';
+$lang['Path'] = 'Байршил';
+$lang['Photos'] = 'Зураг';
+$lang['No photo in this album'] = 'Энэ цомог хоосон байна';
+$lang['Number of downloads'] = 'Татсан тоо';
+$lang['Operating system'] = 'Үйлдлийн систем';
+$lang['New Version'] = 'Шинэ хувилбар';
+$lang['New name'] = 'Шинэ нэр';
+$lang['Members'] = 'Гишүүд';
+$lang['Modify information'] = 'Мэдээллийг засах';
+$lang['Month'] = 'Сар';
+$lang['Move'] = 'Зөөх';
+$lang['Move albums'] = 'Цомогыг зөөх';
+$lang['Move to album'] = 'Цомог уруу зөөх';
+$lang['Informations'] = 'Мэдээлэл';
+$lang['Install'] = 'Суулгах';
+$lang['Installed Languages'] = 'Идэвхтэй байгаа хэл';
+$lang['Languages'] = 'Хэл';
+$lang['Languages which need upgrade'] = 'Шинэчлэх шаардлагатай хэл';
+$lang['Level 2'] = 'Найз нөхөд';
+$lang['Level 4'] = 'Гэр бүлийнхэн';
+$lang['Level 8'] = 'Админууд';
+$lang['List'] = 'Жагсаалт';
+$lang['Image Quality'] = 'Зургын чанар';
+$lang['Forbidden'] = 'Хориотой';
+$lang['GD version'] = 'GD хувилбар';
+$lang['Group'] = 'Бүлэг';
+$lang['Group name'] = 'Бүлэгийн нэр';
+$lang['Groups'] = 'Бүлэг';
+$lang['Guests'] = 'Зочин';
+$lang['Height'] = 'Өндөр';
+$lang['Hide'] = 'Нуух';
+$lang['High definition'] = 'Өндөр нарийвчлалтай';
+$lang['Hour'] = 'Цаг';
+$lang['File'] = 'Файл';
+$lang['Edit photo'] = 'Зургыг засах';
+$lang['Error list'] = 'Алдааны жагсаалт';
+$lang['Everybody'] = 'Бүх хүн';
+$lang['Edit photo information'] = 'Зурагны мэдээллийг засах';
+$lang['Edit album'] = 'Цомогыг засах';
+$lang['ERROR'] = 'АЛДАА';
+$lang['Correction'] = 'Засвар';
+$lang['Create'] = 'Үүсгэх';
+$lang['Delete this language'] = 'Энэ хэлийг устгах';
+$lang['Delete selected photos'] = 'Сонгосон зургуудыг устгах';
+$lang['Delete Representant'] = 'Цомогны дүрс зургыг устгах';
+$lang['Day'] = 'Өдөр';
+$lang['Date'] = 'Огноо';
+$lang['Configuration'] = 'Тохиргоо';
+$lang['Check all'] = 'Бүгдийг сонгох';
+$lang['Cancel'] = 'Цуцлах';
+$lang['Average rate'] = 'Дундаж үнэлгээ';
+$lang['Album name'] = 'Цомогын нэр';
+$lang['Active Plugins'] = 'Идэвхтэй байгаа өргөтгөл';
+$lang['Add'] = 'Нэмэх';
+$lang['Add New Language'] = 'Шинэ хэл нэмэх';
+$lang['Add Photos'] = 'Зураг нэмэх';
+$lang['Add a tag'] = 'Түлхүүр үг нэмэх';
+$lang['Add a user'] = 'Хэрэглэгч нэмэх';
+$lang['Add tags'] = 'Түлхүүр үгс нэмэх';
+$lang['Active Languages'] = 'Идэвхтэй байгаа хэл';
+$lang['%d week'] = '%d долоо хоног';
+$lang['%d weeks'] = '%d долоо хоног';
+$lang['%d year'] = '%d жил';
+$lang['%d years'] = '%d жил';
+$lang['%s ago'] = '%d өмнө';
+$lang['A new version of Piwigo is available.'] = 'Piwigo-н шинэ хувилбар гарсан байна.';
+$lang['%d second'] = '%d секунд';
+$lang['%d seconds'] = '%d секунд';
+$lang['%d day'] = '%d өдөр';
+$lang['%d days'] = '%d хоног';
+$lang['%d hour'] = '%d цаг';
+$lang['%d hours'] = '%d цаг';
+$lang['%d photo was deleted'] = '%d зургыг устаглаа';
+$lang['%d albums moved'] = '%d цомогуудыг амжилттай зөөлөө';
+$lang['%d album moved'] = '%d цомогыг амжилттай зөөлөө';
+?>
