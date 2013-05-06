c9ed2f16e79614d8f1c7887e5a546034ae7da669
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-06 18:26:42 +0000

    [trunk_upgrade] Update es_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/trunk@22510 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/upgrade.lang.php
+++ language/es_ES/upgrade.lang.php
@@ -22,21 +22,20 @@
 // +-----------------------------------------------------------------------+
 
 $lang['Upgrade'] = 'Actualización';
-$lang['Upgrade from version %s to %s'] = 'Actualización de la versión %s a %s';
+$lang['Upgrade from version %s to %s'] = 'Actualización de la versión %s a la %s';
 $lang['Statistics'] = 'Estadísticas';
 $lang['total upgrade time'] = 'tiempo total de la actualización';
-$lang['total SQL time'] = 'tiempo total de la demanda SQL';
-$lang['SQL queries'] = 'número de demanda SQL';
-$lang['Upgrade informations'] = 'Informaciones sobre la actualización';
-$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Por favor, efectúes un mantenimiento en [Administración>herramientas>Mantenimiento] si usted encuentra problemas.';
-$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Por precaución, el plugins siguiente han sido desactivados. Verifique si existen unas actualización antes de reactivarlos:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'Sólo un administrador puede lanzar la actualización: por favor, identifiqúese más abajo.';
-$lang['You do not have access rights to run upgrade'] = 'Usted no tiene los derechos necessaires para lanzar la actualización.';
-$lang['All sub-albums of private albums become private'] = 'Todos los sub-àlbumes de los àlbumes privados son privados';
-$lang['User permissions and group permissions have been erased'] = 'Las autorizaciones de los usuarios y de los grupos han sido borradas';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Sólo el prefijo de las miniaturas y el email  del webmestre han sido salvaguardados por la configuración precedente';
-$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Esta página le propone poner al día la base de datos de su antigua versión de piwigo hacia la nueva versión.
-El asistente de postura al día piensa que usted dispone actualmente del <strong>versión %s</strong> (o equivalente).';
+$lang['total SQL time'] = 'tiempo total de la consulta SQL';
+$lang['SQL queries'] = 'consultas SQL';
+$lang['Upgrade informations'] = 'Información acerca de la actualización';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Por favor, realice una comprobación de mantenimiento en [Administración>Herramientas>Mantenimiento] en caso de detectar algún problema.';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Por precaución, se han desactivado los siguientes plugins. Verifique si existen actualizaciones de los mismos antes de reactivarlos:';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Sólo un administrador puede lanzar la actualización: por favor, identifíquese a continuación.';
+$lang['You do not have access rights to run upgrade'] = 'No dispone de los permisos suficientes para realizar la actualización.';
+$lang['All sub-albums of private albums become private'] = 'Todos aquellos sub-álbumes pertenecientes a álbumes privados se convertirán en privados';
+$lang['User permissions and group permissions have been erased'] = 'Los permisos de usuarios y grupos han sido eliminados';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Sólo el prefijo de las miniaturas y el email del webmaster han sido guardados por la configuración anterior';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Esta página propone actualizar la base de datos de su antigua versión de Piwigo a la nueva versión.  El asistente de actualización detecta que actualmente está ejecutando la <strong>versión %s</strong> (o equivalente).';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'En <i>%s</i>, antes <b>?></b>, insertar:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Como medida de precaución, los siguientes temas han sido desactivados. Compruebe si existe actualizaciones  antes de reactivar:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Como medida de precaución, los siguientes temas han sido desactivados. Compruebe si existe actualizaciones antes de reactivarlos:';
 ?>
