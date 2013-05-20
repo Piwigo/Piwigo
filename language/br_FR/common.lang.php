93e51094cda2cc42599301134aa2143fc912b9cf
ddtddt <ddtddt@piwigo.org>
2013-05-20 10:12:45 +0000

    [trunk] - Language_- Add br_FR Breton - Brezhoneg - Thanks to yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@22748 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/br_FR/common.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Brezhoneg [FR]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=698
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Brezhoneg';
+$lang_info['country'] = 'Bro-C\'hall';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'br';
+$lang_info['zero_plural'] = true;
+
+$lang['%d comments'] = '%d evezhiadenn';
+$lang['%d hit'] = '%d klik';
+$lang['%d hits'] = '%d klik';
+$lang['%d Kb'] = '%d Ko';
+$lang['%d new comment'] = '%d evezhiadenn nevez';
+$lang['%d new comments'] = '%d evezhiadenn nevez';
+$lang['%d new photo'] = '%d luc\'hskeudenn nevez';
+$lang['%d new photos'] = '%d luc\'hskeudenn nevez';
+$lang['%d new user'] = '%d implijer nevez';
+$lang['%d new users'] = '%d implijer nevez';
+$lang['Date'] = 'Deiziad';
+$lang['day'][0] = 'Sul';
+$lang['day'][1] = 'Lun';
+$lang['day'][2] = 'Meurzh';
+$lang['day'][3] = 'Merc\'her';
+$lang['day'][4] = 'Yaou';
+$lang['day'][5] = 'Gwener';
+$lang['day'][6] = 'Sadorn';
+$lang['month'][10] = 'Here';
+$lang['month'][11] = 'Du';
+$lang['month'][12] = 'Kerzu';
+$lang['month'][1] = 'Genver';
+$lang['month'][2] = 'C\'hwevrer';
+$lang['month'][3] = 'Meurzh';
+$lang['month'][4] = 'Ebrel';
+$lang['month'][5] = 'Mae';
+$lang['month'][6] = 'Mezheven';
+$lang['month'][7] = 'Gouere';
+$lang['month'][8] = 'Eost';
+$lang['month'][9] = 'Gwengolo'; 
+?>
