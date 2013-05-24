d3ebc321b7ac7f2ba0d6a45a1a7928dadc63ebe3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-24 21:29:15 +0000

    [trunk_install] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@22860 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mn_MN/install.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['It appears your webhost is currently running PHP %s.'] = 'Таны вэбсервер PHP %s хувилбарыг ашиглаж байна';
+$lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'] = 'имэйл хаяг xxx@yyy.eee гэсэн хэлбэртэй байх ёстой (жишээ нь: jack@altern.org)';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo PHP 5-г тохиргоог өөрчилж чадсангүй';
+$lang['Congratulations, Piwigo installation is completed'] = 'Танд баяр хүргэе, Piwigo амжилттай сууж дууслаа.';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Сервертэй амжилттай холбогдсон боловч мэдээллийн сантай холбогдох боломж алга байна.';
+$lang['Try to configure PHP 5'] = 'PHP 5 тохиргоогоо өөрчилж үзнэ үү';
+$lang['User'] = 'Хэрэглэгч';
+$lang['Just another Piwigo gallery'] = 'Бас нэг Piwigo-н галарей ';
+$lang['Database configuration'] = 'Мэдээллийн сангийн тохиргоо';
+$lang['Webmaster mail address'] = 'Вэбмастерийн имэйл хаяг';
+$lang['Webmaster password'] = 'Вэбмастерийн нууц үг';
+$lang['Welcome to my photo gallery'] = 'Миний зургийн цомогт тавтай морилно уу';
+$lang['please enter your password again'] = 'нууц үгээ дахин оруулна уу';
+$lang['Start Install'] = 'Суулгаж эхлэх';
+$lang['Sorry!'] = 'Уучлаарай';
+$lang['Password'] = 'Нууц үг';
+$lang['Password ']['confirm'] = 'Нууц үгээ давтах';
+$lang['Can\'t connect to server'] = 'Сервертэй холбогдож чадахгүй байна';
+$lang['Database name'] = 'Мэдээллийн сангын нэр';
+$lang['Default gallery language'] = 'Үндсэн хэл';
+$lang['PHP 5 is required'] = 'PHP 5 шаардлагатай';
+$lang['Download the config file'] = 'Тохиргооны файлыг татах';
+$lang['Admin configuration'] = 'Админий тохиргоо';
+$lang['Basic configuration'] = 'Үндсэн тохиргоо';
+?>
