c3574ce9d50b2833953ba1ef38fdcf3bbcf0e3d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-15 13:09:17 +0000

    [trunk_install] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/trunk@23227 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ca_ES/install.lang.php
+++ language/ca_ES/install.lang.php
@@ -64,4 +64,5 @@ $lang['Don\'t hesitate to consult our forums for any help : %s'] = 'No dubtis en
 $lang['Just another Piwigo gallery'] = 'Just another Piwigo gallery';
 $lang['Welcome to my photo gallery'] = 'Benvingut a la meva galeria fotogràfica';
 $lang['Welcome to your new installation of Piwigo!'] = 'Benvingut a la nova instal·lació de Piwigo!';
+$lang['localhost or other, supplied by your host provider'] = 'localhost o un altre, facilitat pel teu proveïdor d\'allotjament';
 ?>
