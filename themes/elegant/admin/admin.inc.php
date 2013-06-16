e5eb631031be9fad145924c3298d68dc2a64b330
mistic100 <mistic@piwigo.org>
2013-06-16 21:14:19 +0000

    remove the ugly load_conf_from_db();
    git-svn-id: http://piwigo.org/svn/trunk@23277 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/admin/admin.inc.php
+++ themes/elegant/admin/admin.inc.php
@@ -13,11 +13,11 @@ if(isset($_POST['submit_elegant']))
 	$config_send['p_main_menu']=(isset($_POST['p_main_menu']) and !empty($_POST['p_main_menu'])) ? $_POST['p_main_menu'] : 'on';
 	$config_send['p_pict_descr']=(isset($_POST['p_pict_descr']) and !empty($_POST['p_pict_descr'])) ? $_POST['p_pict_descr'] : 'on';
 	$config_send['p_pict_comment']=(isset($_POST['p_pict_comment']) and !empty($_POST['p_pict_comment'])) ? $_POST['p_pict_comment'] : 'off';
-	conf_update_param('elegant', pwg_db_real_escape_string(serialize($config_send)));
+  
+	$conf['elegant'] = serialize($config_send);
+	conf_update_param('elegant', pwg_db_real_escape_string($conf['elegant']));
 
   array_push($page['infos'], l10n('Information data registered in database'));
-
-  load_conf_from_db();
 }
 
 $template->set_filenames(array(
