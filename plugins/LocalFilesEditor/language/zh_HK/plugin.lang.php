23329cc2ff9883e22389587d0eeadd8c80f297dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-19 15:02:54 +0000

    [localfileseditor] Add zh_HK, thanks to : hkdigit
    git-svn-id: http://piwigo.org/svn/trunk@23368 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/zh_HK/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_restore'] = '將備份檔案還原';
+$lang['locfiledit_restore_confirm'] = '請確定？
+還原將不會有效，直到有下一個儲存。';
+$lang['locfiledit_save_bak'] = '儲存時會建立備份副本。';
+$lang['locfiledit_save_config'] = '檔案成功地寫入。';
+$lang['locfiledit_save_file'] = '儲存檔案';
+$lang['locfiledit_saved_bak'] = '建立的備份檔案(%s)';
+$lang['locfiledit_show_default'] = '顯示參照檔案：';
+$lang['locfiledit_syntax_error'] = '語法錯誤！無法儲存檔案。';
+$lang['locfiledit_webmaster_only'] = '只有網站管理員可以建立或修改本地檔案。';
+$lang['locfiledit_bak_loaded1'] = '載入備份文件。';
+$lang['locfiledit_bak_loaded2'] = '您必須儲存檔案來還原它。';
+$lang['locfiledit_cant_save'] = '檔案是無法寫入。檢查目錄"local/"有寫入權限(chmod)。';
+$lang['locfiledit_choose_file'] = '選擇要編輯的檔案';
+$lang['locfiledit_edit'] = '編輯';
+$lang['locfiledit_empty_filename'] = '您必須輸入檔案名稱。';
+$lang['locfiledit_empty_page'] = '空頁';
+$lang['locfiledit_file_already_exists'] = '檔案已存在。';
+$lang['locfiledit_filename_error'] = '檔案名有禁止的字符。';
+$lang['locfiledit_model'] = '模式';
+$lang['locfiledit_model_error'] = '你必須選擇一種模式。';
+$lang['locfiledit_new_filename'] = '檔案名稱';
+$lang['locfiledit_new_tpl'] = '建立新的檔案';
+$lang['locfiledit_newfile'] = '該檔案不存在，直到輸入一些信息。信息一旦輸入並保存，該文件將被創建。';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = '語言';
+$lang['locfiledit_onglet_localconf'] = '本地設定';
+$lang['locfiledit_onglet_plug'] = '個人插件';
+$lang['locfiledit_onglet_tpl'] = '範本';
+$lang['locfiledit_parent_directory'] = '父目錄';
+?>
