231e718ccfd94fae119e5b5f7b7ccaa44278a6bb
flop25 <flop25@piwigo.org>
2013-06-21 14:09:41 +0000

    indent correction
    git-svn-id: http://piwigo.org/svn/trunk@23429 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/admin/admin.inc.php
+++ themes/elegant/admin/admin.inc.php
@@ -10,12 +10,12 @@ $config_send= array();
 
 if(isset($_POST['submit_elegant']))
 {
-	$config_send['p_main_menu']=(isset($_POST['p_main_menu']) and !empty($_POST['p_main_menu'])) ? $_POST['p_main_menu'] : 'on';
-	$config_send['p_pict_descr']=(isset($_POST['p_pict_descr']) and !empty($_POST['p_pict_descr'])) ? $_POST['p_pict_descr'] : 'on';
-	$config_send['p_pict_comment']=(isset($_POST['p_pict_comment']) and !empty($_POST['p_pict_comment'])) ? $_POST['p_pict_comment'] : 'off';
+  $config_send['p_main_menu']=(isset($_POST['p_main_menu']) and !empty($_POST['p_main_menu'])) ? $_POST['p_main_menu'] : 'on';
+  $config_send['p_pict_descr']=(isset($_POST['p_pict_descr']) and !empty($_POST['p_pict_descr'])) ? $_POST['p_pict_descr'] : 'on';
+  $config_send['p_pict_comment']=(isset($_POST['p_pict_comment']) and !empty($_POST['p_pict_comment'])) ? $_POST['p_pict_comment'] : 'off';
   
-	$conf['elegant'] = serialize($config_send);
-	conf_update_param('elegant', pwg_db_real_escape_string($conf['elegant']));
+  $conf['elegant'] = serialize($config_send);
+  conf_update_param('elegant', pwg_db_real_escape_string($conf['elegant']));
 
   array_push($page['infos'], l10n('Information data registered in database'));
 }
