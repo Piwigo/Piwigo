02f3c45e549d3faa47cb01d0353f28859cc1a656
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-23 19:24:41 +0000

    [elegant] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/trunk@23480 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/pt_BR/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Escolha qual deve ser o estado padrão para cada painel, ou desativar a animação:';
+$lang['Comments Panel'] = 'Painel de Comentários';
+$lang['Disable the animation'] = 'Desabilitar a animação';
+$lang['Displayed'] = 'Exibido';
+$lang['Elegant, Configuration Page'] = 'Elegante, Página de Configuração';
+$lang['Hidden'] = 'Oculto';
+$lang['Main Menu Panel'] = 'Painel do Menu Principal';
+$lang['Panels options'] = 'Opções de painéis';
+$lang['Photo Description Panel'] = 'Painel de Descrição de Foto';
+?>
