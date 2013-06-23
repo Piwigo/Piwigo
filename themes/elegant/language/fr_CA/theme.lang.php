81e30f462b44ab1a2b75fb033bf7147940219f44
ddtddt <ddtddt@piwigo.org>
2013-06-23 16:45:11 +0000

    [trunk] - elegant - add fr_CA - Quebec - Québécois - Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/trunk@23469 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/fr_CA/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Elegant, Configuration Page'] = 'Élégant, Page de Configuration';
+$lang['Panels options'] = 'Options des panneaux';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Choisissez le comportement par défaut des panneaux rétractables, ou désactivez leur animation';
+$lang['Main Menu Panel'] = 'Panneau du menu principal';
+$lang['Displayed'] = 'Affiché';
+$lang['Hidden'] = 'Caché';
+$lang['Disable the animation'] = 'Désactiver l\'animation';
+$lang['Photo Description Panel'] = 'Panneau de description de la photo';
+$lang['Comments Panel'] = 'Panneau des commentaires'; 
+?>
