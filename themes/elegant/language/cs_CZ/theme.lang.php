67d736e3fd0399399e554c53503a2b24f50e5a70
ddtddt <ddtddt@piwigo.org>
2013-06-23 18:35:13 +0000

    [trunk] - elegant - Add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/trunk@23475 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/cs_CZ/theme.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Elegant, Configuration Page'] = 'Elegant, stránka s konfigurací';
+$lang['Panels options'] = 'Nastavení panelů';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Nastavte výchozí stav pro každý panel nebo deaktivujte animaci:';
+$lang['Main Menu Panel'] = 'Hlavní menu panel';
+$lang['Displayed'] = 'Zobrazen';
+$lang['Hidden'] = 'Skryt';
+$lang['Disable the animation'] = 'Deaktivovat animaci';
+$lang['Photo Description Panel'] = 'Panel s popisem fotografií';
+$lang['Comments Panel'] = 'Panel komentářů';
+?>
