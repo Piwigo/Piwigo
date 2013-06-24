19af3b703cd7bec0acb9bb35908142516e174d1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-24 16:43:06 +0000

    [elegant] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/trunk@23505 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/eo_EO/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Photo Description Panel'] = 'Fotopriskriba panelo';
+$lang['Panels options'] = 'Panelaj opcioj';
+$lang['Main Menu Panel'] = 'Panelo de la ĉefa menuo';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Elektu, kion estu la apriora stato de ĉiu panelo, aŭ malŝaltu la animacio:';
+$lang['Elegant, Configuration Page'] = 'Agordopaĝo de Elegant';
+$lang['Comments Panel'] = 'Panelo por komentoj';
+$lang['Disable the animation'] = 'Malŝalti la animacion';
+$lang['Displayed'] = 'Montrita';
+$lang['Hidden'] = 'Kaŝita';
+?>
