2e923d232aea37db951b62ecc5a27609ac1a00de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-24 16:42:46 +0000

    [elegant] Add el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/trunk@23503 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/el_GR/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Photo Description Panel'] = 'Πάνελ Περιγραφής Φωτογραφίας ';
+$lang['Panels options'] = 'επιλογές πάνελ';
+$lang['Main Menu Panel'] = 'Πάνελ Κύριου μενού';
+$lang['Hidden'] = 'Κρυφό';
+$lang['Elegant, Configuration Page'] = 'Elegant, διαμόρφωσης σελίδας';
+$lang['Displayed'] = 'Εμφανιζόμενο';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Επιλέξτε αυτό που θα θέλατε να είναι η προεπιλεγμένη κατάσταση για κάθε πάνελ, ή απενεργοποιήσετε τα κινούμενα σχέδια:';
+$lang['Disable the animation'] = 'Απενεργοποιήστε τα κινούμενα σχέδια';
+$lang['Comments Panel'] = 'Πάνελ Σχολίων';
+?>
