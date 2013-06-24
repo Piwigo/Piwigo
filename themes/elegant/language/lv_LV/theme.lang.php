da69c29646c9c037cb1a11519b8db6b8b07f2b35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-24 17:17:41 +0000

    [elegant] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/trunk@23518 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/language/lv_LV/theme.lang.php
+++ themes/elegant/language/lv_LV/theme.lang.php
@@ -24,4 +24,9 @@ $lang['Main Menu Panel'] = 'Galvenās Izvēlnes Panelis';
 $lang['Hidden'] = 'Paslēpts';
 $lang['Disable the animation'] = 'atslēgt animāciju';
 $lang['Comments Panel'] = 'Komentāru panelis';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Izvēlēties, kas būtu katra paneļa noklusējuma stāvoklis vai atspējot animācijas:';
+$lang['Elegant, Configuration Page'] = 'Elegants, Konfigurācijas Lapa';
+$lang['Displayed'] = 'Attēlots';
+$lang['Photo Description Panel'] = 'Fotogrāfiju Apraksta Panelis';
+$lang['Panels options'] = 'Paneļa opcijas';
 ?>
