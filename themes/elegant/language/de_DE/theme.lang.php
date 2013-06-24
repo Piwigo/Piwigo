ac75327d145758e8f5af418f25e2293b9e733b0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-24 16:43:52 +0000

    [elegant] Add de_DE, thanks to : fs101299
    git-svn-id: http://piwigo.org/svn/trunk@23510 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/de_DE/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Wählen Sie den default Status eines jeden Panels aus, oder deaktivieren Sie die Animation';
+$lang['Comments Panel'] = 'Kommentar Panel';
+$lang['Disable the animation'] = 'Animation deaktivieren';
+$lang['Displayed'] = 'angezeigt';
+$lang['Elegant, Configuration Page'] = 'Elegant ,Konfigurationsseite';
+$lang['Hidden'] = 'versteckt';
+$lang['Main Menu Panel'] = 'Hauptmenü Panel';
+$lang['Panels options'] = 'Panel Optionen';
+$lang['Photo Description Panel'] = 'Panel Fotobeschreibung';
+?>
