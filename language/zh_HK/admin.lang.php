d03afd4b90e1ab6a657281455841f9bb177957a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-26 04:21:47 +0000

    [trunk_admin] Update zh_HK, thanks to : gurunited
    git-svn-id: http://piwigo.org/svn/trunk@23554 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/admin.lang.php
@@ -0,0 +1,83 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos uploaded'] = '%d 相片已被上傳';
+$lang['%d photos were deleted'] = '%d 相片已被上傳';
+$lang['%d physical'] = '%d 實體';
+$lang['%d physicals'] = '%d 實體';
+$lang['%d second'] = '%d 秒';
+$lang['%d seconds'] = '%d 秒';
+$lang['%d tag'] = '%d 標籤';
+$lang['%d tags'] = '%d 標籤';
+$lang['%d user'] = '%d 用戶';
+$lang['%d user comment rejected'] = '%d 用戶意見已被拒絕';
+$lang['%d user comment validated'] = '%d 用戶意見已被接納';
+$lang['%d user comments rejected'] = '%d 用戶意見已被拒絕';
+$lang['%d user comments validated'] = '%d 用戶意見已被接納';
+$lang['%d user deleted'] = '%d 用戶已被刪除';
+$lang['%d user was not updated.'] = '%d 用戶沒有更新';
+$lang['%d user was updated.'] = '%d 用戶已被更新';
+$lang['%d users'] = '%d 用戶';
+$lang['%d users deleted'] = '%d 用戶已被刪除';
+$lang['%d users were not updated.'] = '%d 用戶沒有更新';
+$lang['%d users were updated.'] = '%d 用戶已被更新';
+$lang['%d mail was not sent.'] = '%s 郵件未被發送';
+$lang['%d mail was sent.'] = '%s 郵件已被發送';
+$lang['%d mails were not sent.'] = '%s 郵件未被發送';
+$lang['%d mails were sent.'] = '%s 郵件已被發送';
+$lang['%d of %d photos selected'] = '%d / %d 相片被選取';
+$lang['%d parameter was updated.'] = '%d 參數已更新';
+$lang['%d parameters were updated.'] = '%d 參數已更新';
+$lang['%d photo was deleted'] = '%d 圖片已被刪除';
+$lang['%d guest'] = '%d 訪客';
+$lang['%d guests'] = '%d 訪客';
+$lang['%d hour'] = '%d 小時';
+$lang['%d hours'] = '%d 小時';
+$lang['%d line filtered'] = '%d 行被過濾';
+$lang['%d lines filtered'] = '%d 行被過濾';
+$lang['%d member'] = '%d 會員';
+$lang['%d members'] = '%d 會員';
+$lang['%d minute'] = '%d 分鐘';
+$lang['%d minutes'] = '%d 分鐘';
+$lang['%d month'] = '%d 月';
+$lang['%d months'] = '%d 月';
+$lang[' and %d virtual'] = '和 %d 虛擬';
+$lang[' and %d virtuals'] = '和 %d 虛擬';
+$lang['%d album including'] = '包含 %d 相簿';
+$lang['%d album moved'] = '%d 相簿已移走';
+$lang['%d albums including'] = '包含 %d 相簿';
+$lang['%d albums moved'] = '%d 相簿已移走';
+$lang['%d anomalies have been detected corrected.'] = '揁察及修正了 %d 個異常';
+$lang['%d anomalies have been detected.'] = '揁察到 %d 個異常';
+$lang['%d anomalies have been ignored.'] = '%d 異常被忽略';
+$lang['%d anomalies have not been corrected.'] = '不能修正 %d 個異常';
+$lang['%d anomaly has been corrected.'] = '修正了 %d 異常';
+$lang['%d anomaly has been detected.'] = '揁察到 %d 異常';
+$lang['%d anomaly has not been corrected.'] = '不能修正 %d 異常';
+$lang['%d association'] = '%d 聯繫';
+$lang['%d associations'] = '%d 聯繫';
+$lang['%d anomaly has been ignored.'] = '%d 異常被忽略';
+$lang['%d day'] = '%d 天';
+$lang['%d days'] = '%d 天';
+$lang['%d group'] = '%d 群組';
+$lang['%d groups'] = '%d 群組';
+?>
