c41ff2e14e8a15e46eb078a205ff67ff241c619f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-27 17:07:53 +0000

    [elegant] Add lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/trunk@23597 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/lt_LT/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Elegant, Configuration Page'] = 'Elegant konfigūracijos puslapis';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Pasirinkite kokia turėtų būti kiekvieno skydelio būsena arba išjunkite animaciją';
+$lang['Comments Panel'] = 'Komentarų skydelis';
+$lang['Disable the animation'] = 'Išjungti animaciją';
+$lang['Displayed'] = 'Rodoma';
+$lang['Hidden'] = 'Paslėpta';
+$lang['Main Menu Panel'] = 'Pagrindinio meniu skydelis';
+$lang['Panels options'] = 'Skydelių parinktys';
+$lang['Photo Description Panel'] = 'Foto aprašymo skydelis';
+?>
