3794beac7b04afcf3121fec88c0b928ee9fb9f17
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-30 10:36:03 +0000

    [elegant] Add nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/trunk@23692 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/nl_NL/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Geef de standaardtoestand van elk paneel, of schakel de animatie uit:';
+$lang['Comments Panel'] = 'Commentaar-paneel';
+$lang['Disable the animation'] = 'Schakel de animatie uit';
+$lang['Displayed'] = 'Weergegeven';
+$lang['Elegant, Configuration Page'] = 'Elegant, Configuratiepagina';
+$lang['Main Menu Panel'] = 'Hoofdmenu-paneel';
+$lang['Panels options'] = 'Paneel-opties';
+$lang['Photo Description Panel'] = 'Afbeeldingenbeschrijvings-paneel';
+$lang['Hidden'] = 'Verborgen';
+?>
