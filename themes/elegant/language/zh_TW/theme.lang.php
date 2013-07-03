165775ea4a76ab2c5eb621a424c6bbe3b92e8c43
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-03 00:13:34 +0000

    [elegant] Add zh_TW, thanks to : unsetlay
    git-svn-id: http://piwigo.org/svn/trunk@23752 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/zh_TW/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = '選擇每個面板的預設狀態，或關閉動畫。';
+$lang['Comments Panel'] = '回應面板';
+$lang['Disable the animation'] = '關閉動畫';
+$lang['Displayed'] = '已顯示';
+$lang['Elegant, Configuration Page'] = '優雅，配置頁面';
+$lang['Hidden'] = '隱藏';
+$lang['Main Menu Panel'] = '主要選單面板';
+$lang['Panels options'] = '面板選項';
+$lang['Photo Description Panel'] = '照片簡介面板';
+?>
