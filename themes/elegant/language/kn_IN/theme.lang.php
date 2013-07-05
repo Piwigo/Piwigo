417711a0dc866ce3bd808f4089196b9e89e1a38b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-05 03:53:36 +0000

    [elegant] Add kn_IN, thanks to : rudramurthy
    git-svn-id: http://piwigo.org/svn/trunk@23781 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/kn_IN/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Elegant, Configuration Page'] = 'ಎಲಿಗ್ಯಾಂಟ್, ಕಾನ್ಫಿಗರೇಶನ್ ಪುಟ';
+$lang['Hidden'] = 'ಬಚ್ಚಿಟ್ಟದ್ದು';
+$lang['Main Menu Panel'] = 'ಮುಖ್ಯ ಆಯ್ಕೆಪಟ್ಟಿ ಪ್ಯಾನೆಲ್';
+$lang['Panels options'] = 'ಪ್ಯಾನೆಲ್ ಗಳ ಆಯ್ಕೆಗಳು';
+$lang['Photo Description Panel'] = 'ಚಿತ್ರದ ವಿವರಣೆ ಪ್ಯಾನೆಲ್';
+$lang['Displayed'] = 'ಪ್ರದರ್ಶಿಸಲಾಗಿದೆ';
+$lang['Disable the animation'] = 'ಅನಿಮೇಶನ್ ಅನ್ನು ನಿಷ್ಕ್ರಿಯಗೊಳಿಸಿ';
+$lang['Comments Panel'] = 'ಟಿಪ್ಪಣಿಗಳ ಪ್ಯಾನೆಲ್';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'ಪ್ರತಿಯೊಂದು ಪ್ಯಾನೆಲ್ ನ ಪೂರ್ವನಿಯೋಜಿತ ಸ್ಥಿತಿ ಏನಿರಬೇಕೆಂದು ಆರಿಸಿ, ಅಥವಾ ಅನಿಮೇಶನ್ ಅನ್ನು ನಿಷ್ಕ್ರಿಯಗೊಳಿಸಿ.';
+?>
