35d2785c9ce90e2c43d10e5ff44b54c3a8bc7227
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-07 20:14:30 +0000

    [elegant] Add ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/trunk@23854 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/ro_RO/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Alege care ar trebui să fie starea implicită pentru fiecare panou, sau dezactivează animația.';
+$lang['Comments Panel'] = 'Panou de comentarii';
+$lang['Disable the animation'] = 'Dezactivează animația';
+$lang['Displayed'] = 'Afișat';
+$lang['Elegant, Configuration Page'] = 'Pagină de configurare pentru Elegant';
+$lang['Hidden'] = 'Ascuns';
+$lang['Main Menu Panel'] = 'Panou meniu principal';
+$lang['Panels options'] = 'Panou de optiuni';
+$lang['Photo Description Panel'] = 'Panou de descrierea fotografiei';
+?>
