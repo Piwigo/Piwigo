86b8b695718700e44d3aa07110be21ca3a517da3
ddtddt <ddtddt@piwigo.org>
2013-07-07 06:01:21 +0000

    [trunk] - Language - Bahasa Indonesia language (id-ID) - Thanks to papua
    git-svn-id: http://piwigo.org/svn/trunk@23819 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/id_ID/common.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Bahasa Indonesia [ID]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=707
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Bahasa Indonesia';
+$lang_info['country'] = 'Indonesia';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'id';
+$lang_info['zero_plural'] = true;
+
+$lang['%d comments'] = '%d komentar';
+$lang['%d hit'] = '%d pengunjung';
+$lang['%d hits'] = '%d pengunjung';
+$lang['%d Kb'] = '%d Kb';
+$lang['%d new comment'] = '%d komentar baru';
+$lang['%d new comments'] = '%d komentar baru';
+$lang['%d new photo'] = '%d foto baru';
+$lang['%d new photos'] = '%d foto baru';
+$lang['%d new user'] = '%d pengguna baru';
+$lang['%d new users'] = '%d pengguna baru';
+$lang['Date'] = 'Tanggal';
+$lang['day'][0] = 'Minggu';
+$lang['day'][1] = 'Senin';
+$lang['day'][2] = 'Selasa';
+$lang['day'][3] = 'Rabu';
+$lang['day'][4] = 'Kamis';
+$lang['day'][5] = 'Jumat';
+$lang['day'][6] = 'Sabtu';
+$lang['month'][10] = 'Oktober';
+$lang['month'][11] = 'November';
+$lang['month'][12] = 'Desember';
+$lang['month'][1] = 'Januari';
+$lang['month'][2] = 'Februari';
+$lang['month'][3] = 'Maret';
+$lang['month'][4] = 'April';
+$lang['month'][5] = 'Mei';
+$lang['month'][6] = 'Juni';
+$lang['month'][7] = 'Juli';
+$lang['month'][8] = 'Agustus';
+$lang['month'][9] = 'September';
+
+
+?>
