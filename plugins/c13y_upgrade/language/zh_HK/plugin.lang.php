e5e2c9d3237fc3f1a4eb49335dda28bca6bc6b35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-11 21:13:57 +0000

    [c13y_upgrade] Add zh_HK, thanks to : hkdigit
    git-svn-id: http://piwigo.org/svn/trunk@23914 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/zh_HK/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['c13y_correction_dbl_email_user'] = '刪除重複的用戶';
+$lang['c13y_correction_obsolete_plugin'] = '"%s"插件已包含在這個版本上，你必須先卸載它。';
+$lang['c13y_dbl_email_user'] = '已有用戶用了相同的電郵地址';
+$lang['c13y_obsolete_plugin'] = '已過時的插件';
+$lang['c13y_upgrade_no_anomaly'] = '升級後沒有檢測到異常，"檢查升級"插件已被停用。';
+?>
