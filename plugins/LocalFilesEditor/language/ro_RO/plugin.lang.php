4ab0cf2418a70778c9b00cdd9801b2df6c5d851a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-12 08:33:57 +0000

    [localfileseditor] Add ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/trunk@23923 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/ro_RO/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_cant_save'] = 'Fișerul curent nu poate fi scris. Cerifică dacă dosarul "local/" poate fi scris (chmod).';
+$lang['locfiledit_parent_directory'] = 'Dosarul părinte';
+$lang['locfiledit_restore_confirm'] = 'Confirmă.
+Restaurarea nu va intra în efect până la urmatoarea salvare.';
+$lang['locfiledit_bak_loaded1'] = 'Copia de siguranță a fost încărcată.';
+$lang['locfiledit_restore'] = 'Restaurează copia de siguranță';
+$lang['locfiledit_save_bak'] = 'Copia de siguranță va fi creată odată cu salvarea.';
+$lang['locfiledit_saved_bak'] = 'Copia de siguranță a fost creată (%s)';
+$lang['locfiledit_webmaster_only'] = 'Numai webmasterii pot crea sau modifica fișiere locale.';
+$lang['locfiledit_new_tpl'] = 'Crează un fișier nou';
+$lang['locfiledit_bak_loaded2'] = 'Trebuie să salvezi un fișier pentru a-l restaura.';
+$lang['locfiledit_newfile'] = 'Acest fișier nu va exista pănâ cand nu vei introduce informațiile de mai jos. Odată ce informația va fi introdusă și salvată, fișierul va fi creat.';
+$lang['locfiledit_onglet_plug'] = 'Plugin personal';
+$lang['locfiledit_choose_file'] = 'Alege fișierul de modificat';
+$lang['locfiledit_edit'] = 'Modifică';
+$lang['locfiledit_empty_filename'] = 'Trebuie să introduci numele fișierului.';
+$lang['locfiledit_empty_page'] = 'Pagină goală';
+$lang['locfiledit_file_already_exists'] = 'Fișierul există deja';
+$lang['locfiledit_filename_error'] = 'Caractere interzise în numele fișierului.';
+$lang['locfiledit_model'] = 'Model';
+$lang['locfiledit_model_error'] = 'Trebuie să alegi un model';
+$lang['locfiledit_new_filename'] = 'Numele fișierului';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Limbi';
+$lang['locfiledit_onglet_localconf'] = 'Configurație locală';
+$lang['locfiledit_onglet_tpl'] = 'Șabloane';
+$lang['locfiledit_save_config'] = 'Fișierul a fost scris cu succes.';
+$lang['locfiledit_save_file'] = 'Salvează fișier';
+$lang['locfiledit_show_default'] = 'Arată fișierul de referințăȘ';
+$lang['locfiledit_syntax_error'] = 'Eroare de sintaxă! Fișierul nu poate fi salvat.';
+?>
