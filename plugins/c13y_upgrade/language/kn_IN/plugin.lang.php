aa3965c128e869e1c2dd56db780a5079dc7ca6e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-18 06:21:09 +0000

    [c13y_upgrade] Add kn_IN, thanks to : rudramurthy
    git-svn-id: http://piwigo.org/svn/trunk@23991 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/kn_IN/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['c13y_correction_dbl_email_user'] = 'ನಕಲಿ ಬಳಕೆದಾರರನ್ನು ಅಳಿಸಿ';
+$lang['c13y_correction_obsolete_plugin'] = 'ಈ ಆವೃತ್ತಿಯಲ್ಲಿ "%s" ಪ್ಲಗ್ ಇನ್ ಒಳಗೊಂಡಿದೆ ಮತ್ತು ನೀವು ಅದನ್ನು ಅನುಸ್ಠಾಪಿಸಲೇ ಬೇಕು';
+$lang['c13y_dbl_email_user'] = 'ಏಕರೂಪದ ಮಿಂಚೆ ವಿಳಾಸವಿರುವ ಬಳಕೆದಾರರು';
+$lang['c13y_obsolete_plugin'] = 'ಹಳತಾಗಿರುವ ಪ್ಲಗ್ ಇನ್ ';
+$lang['c13y_upgrade_no_anomaly'] = 'ಅಪ್ಲಿಕೇಶನ್ ನವೀಕರಣದ ನಂತರ ಯಾವುದೇ ಅಸಮಂಜಸತೆ ಕಂಡುಬಂದಿಲ್ಲ, "ನವೀಕರಣಗಳನ್ನು ಪರೀಕ್ಷಿಸಿ"  ಪ್ಲಗ್ ಇನ್ ಅನ್ನು ನಿಷ್ಕ್ರಿಯಗೊಳಿಸಲಾಗಿದೆ.';
+?>
