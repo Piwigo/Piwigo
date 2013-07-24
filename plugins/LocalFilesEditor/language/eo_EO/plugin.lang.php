64c65824c8039fc1066bbb354e271ee9d811f43d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-24 05:59:15 +0000

    [localfileseditor] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/trunk@24026 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/eo_EO/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_restore'] = 'Restaŭri la savdosieron';
+$lang['locfiledit_restore_confirm'] = 'Bonvolu konfirmi.
+Restaŭrado efektiviĝos post musklako ĉe Konservi.';
+$lang['locfiledit_save_bak'] = 'Savkopio kreiĝos dum konserviĝo.';
+$lang['locfiledit_save_config'] = 'La dosiero sukcese skribiĝis.';
+$lang['locfiledit_save_file'] = 'Konservi dosieron';
+$lang['locfiledit_saved_bak'] = 'Savdosiero kreiĝis (%s)';
+$lang['locfiledit_show_default'] = 'Vidigu referencan dosieron:';
+$lang['locfiledit_syntax_error'] = 'Sintakseraro! Ne eblas konservi la dosieron.';
+$lang['locfiledit_webmaster_only'] = 'Nur retestroj povas krei aŭ modifi lokajn dosierojn.';
+$lang['locfiledit_bak_loaded1'] = 'Savdosiero ŝargita.';
+$lang['locfiledit_bak_loaded2'] = 'Vi devas konservi la dosieron por restaŭri ĝin.';
+$lang['locfiledit_cant_save'] = 'La kuranta dosiero ne skribeblas. Kontrolu ĉu la permesilo de la dosierujo "local/" taŭgas (chmod).';
+$lang['locfiledit_choose_file'] = 'Elektu la dosiero redaktota.';
+$lang['locfiledit_edit'] = 'Redakti';
+$lang['locfiledit_empty_filename'] = 'Vi devas tajpi la dosieran nomon.';
+$lang['locfiledit_empty_page'] = 'Malplena paĝo';
+$lang['locfiledit_file_already_exists'] = 'La dosiero jam ekzistas.';
+$lang['locfiledit_filename_error'] = 'Malpermesitaj signoj en la dosiera nomo.';
+$lang['locfiledit_model'] = 'Modelo';
+$lang['locfiledit_model_error'] = 'Elektu modelon.';
+$lang['locfiledit_new_filename'] = 'Dosiera nomo.';
+$lang['locfiledit_new_tpl'] = 'Krei novan dosieron';
+$lang['locfiledit_newfile'] = 'La dosiero ne ekzistas ĝis informoj tajpiĝas ĉisube. Ĝi kreiĝos ekde informoj entajpiĝas kaj registriĝas.';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Lingvoj';
+$lang['locfiledit_onglet_localconf'] = 'Loka agordo';
+$lang['locfiledit_onglet_plug'] = 'Persona kromaĵo';
+$lang['locfiledit_onglet_tpl'] = 'Ŝablonoj';
+$lang['locfiledit_parent_directory'] = 'Parenca dosierujo';
+?>
