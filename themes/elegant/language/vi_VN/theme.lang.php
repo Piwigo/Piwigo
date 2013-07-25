3cb8acdd9f18c218ccc5865abbb726178316586a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-25 05:51:11 +0000

    [elegant] Add vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/trunk@24028 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/vi_VN/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Chọn trạng thái mặc định cho mỗi bảng, hoặc vô hiệu hóa tính năng hoạt hình';
+$lang['Comments Panel'] = 'Bảng bình luận';
+$lang['Disable the animation'] = 'Vô hiệu hóa tính năng hoạt hình';
+$lang['Displayed'] = 'Được hiển thị';
+$lang['Elegant, Configuration Page'] = 'Elegant, Trang cấu hình';
+$lang['Hidden'] = 'Ẩn';
+$lang['Main Menu Panel'] = 'Bảng Menu chính';
+$lang['Panels options'] = 'Tùy chọn bảng';
+$lang['Photo Description Panel'] = 'Bảng mô tả ảnh';
+?>
