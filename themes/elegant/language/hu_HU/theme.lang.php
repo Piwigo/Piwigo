59b668649a6861cfac76f4aba8441e031e73a434
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-26 05:46:24 +0000

    [elegant] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/trunk@24031 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/hu_HU/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Állítsa be milyen legyen a panel alapértelmezett állapotban, vagy tiltsa le az animációt:';
+$lang['Panels options'] = 'Panelek lehetőségei';
+$lang['Comments Panel'] = 'Hozzászólások panel';
+$lang['Disable the animation'] = 'Animáció kikapcsolása';
+$lang['Displayed'] = 'Megjelenítés';
+$lang['Elegant, Configuration Page'] = 'Elegant, beállítás oldal';
+$lang['Hidden'] = 'Rejtett';
+$lang['Main Menu Panel'] = 'Főmenü panel';
+$lang['Photo Description Panel'] = 'Kép leírás panel';
+?>
