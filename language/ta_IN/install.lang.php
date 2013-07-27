12fe77dc3c91c81d1db19191e1e85dc2b469e579
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-27 05:55:01 +0000

    [trunk_install] Update ta_IN, thanks to : saitjaleel
    git-svn-id: http://piwigo.org/svn/trunk@24040 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/install.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['An alternate solution is to copy the text in the box above and paste it into the file "local/config/database.inc.php" (Warning : database.inc.php must only contain what is in the textarea, no line return or space character)'] = 'ஒரு மாற்று தீர்வு மேலே பெட்டியில் உரை நகலெடுத்து கோப்புகளை ஒட்டவும் உள்ளது."local/config/database.inc.php"(எச்சரிக்கை: database.inc.php மட்டும், textarea என்ன எந்த வரியை திரும்ப அல்லது இடைவெளி எழுத்து கொண்டிருக்க வேண்டும்)';
+$lang['Creation of config file local/config/database.inc.php failed.'] = 'கட்டமைப்பு கோப்பு உருவாக்கம் local/config/database.inc.php தோல்வியடைந்தது.';
+$lang['Default gallery language'] = 'இயல்புநிலை கேலரி மொழி';
+$lang['Download the config file'] = 'Config கோப்பை பதிவிறக்க';
+$lang['Hope to see you back soon.'] = 'நீங்கள் விரைவில் திரும்பி பார்க்க நம்புகிறேன்';
+$lang['Host'] = 'host';
+$lang['Installation'] = 'நிறுவல்';
+$lang['It appears your webhost is currently running PHP %s.'] = 'இது உங்களது webhost தற்போது PHP% கள் இயக்கத்தில் தோன்றுகின்றது.';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'இது பார்வையாளர்களுக்கு காண்பிக்கப்படும். இது இணைய நிர்வாகத்தில் அவசியம்';
+$lang['Just another Piwigo gallery'] = 'மற்றொரு Piwigo கேலரி';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'இதை ரகசியமாக வைத்து, இதை நீங்கள் நிர்வாகத்தில் பலகத்தை அணுகுவதற்கு உதவுகிறது';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'உதவி வேண்டுமா? <a உங்கள் கேள்வி கேட்க href="%s"> Piwigo செய்தி பலகை </ a>';
+$lang['Database configuration'] = 'தரவுத்தள உள்ளமைவை';
+$lang['Database name'] = 'தரவுத்தளத்தின் பெயர்';
+$lang['Database table prefix'] = 'தரவுத்தள அட்டவணைகள் முன்னிணைப்பு';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'சர்வர் இணைப்பு வெற்றி, ஆனால் தரவுத்தளத்தின் இணைப்பு சாத்தியம் இல்லை.';
+$lang['Congratulations, Piwigo installation is completed'] = 'வாழ்த்துக்கள், Piwigo நிறுவல், முடிக்கப்பட்டது';
+$lang['Can\'t connect to server'] = 'சர்வருடன் இணைக்க முடியவில்லை';
+$lang['Basic configuration'] = 'அடிப்படை கட்டமைப்பு';
+$lang['Admin configuration'] = 'நிர்வாகம் கட்டமைப்பு';
+?>
