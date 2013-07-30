8f073f8128fbcc93b25bb5ed1bddb8a7d017056b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-30 19:50:11 +0000

    [elegant] Add ta_IN, thanks to : saitjaleel
    git-svn-id: http://piwigo.org/svn/trunk@24080 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/ta_IN/theme.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Hidden'] = 'மறைந்துள்ள';
+$lang['Main Menu Panel'] = 'முதன்மை மெனுவிலுள்ள குழு';
+$lang['Panels options'] = 'பலகங்கள் விருப்பங்கள்';
+$lang['Photo Description Panel'] = 'புகைப்பட விவரிப்பு பலகம்';
+$lang['Elegant, Configuration Page'] = 'நளினமான, ​​கட்டமைப்பு பக்கம்';
+$lang['Displayed'] = 'காண்பிக்கப்படும்';
+$lang['Comments Panel'] = 'கருத்துரைகள் குழு';
+$lang['Disable the animation'] = 'அனிமேசன் செயலிழக்க';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'தேர்வு செய்யவும் முன்னிருப்பு என்னவாக இருக்க வேண்டும்
+ஒவ்வொரு குழு நிலை,  அல்லது அனிமேஷன் செயலிழக்க:';
+?>
