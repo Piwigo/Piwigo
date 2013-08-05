979464a961f1aff4c61569f865ab80d5fef8c9aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-05 14:30:44 +0000

    [trunk_admin] Update ta_IN, thanks to : saitjaleel
    git-svn-id: http://piwigo.org/svn/trunk@24107 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/admin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d associations'] = '
+';
+$lang['%d guests'] = '% d விருந்தினர்';
+$lang['%d hour'] = '% d மணிநேரம்';
+$lang['%d hours'] = '% d மணிநேரம்';
+$lang['%d guest'] = '% d விருந்தினர்';
+$lang['%d groups'] = '%d குழுக்கள்';
+$lang['%d group'] = '%d குழு';
+$lang['%d days'] = '%d நாட்கள்';
+$lang['%d day'] = '% d நாள்';
+$lang['%d anomalies have been detected.'] = '% d முரண்பாடுகள் கண்டறியப்பட்டது';
+$lang['%d anomalies have been detected corrected.'] = '% d முரண்பாடுகள் கண்டறியப்பட்டு மேலும் சரிசெய்யப்பட்டது';
+$lang['%d albums moved'] = '%d ஆல்பங்கள் நகர்த்தப்பட்டது';
+$lang['%d albums including'] = 'இதில்% d ஆல்பங்கள்';
+$lang['%d album moved'] = '% D ஆல்பம் நகர்த்தப்பட்டது';
+$lang['%d album including'] = '% d ஆல்பம் அடங்கும்';
+$lang[' and %d virtual'] = 'மற்றும் %d நிஜமான';
+$lang[' and %d virtuals'] = 'மற்றும் %d நிஜமான';
+?>
