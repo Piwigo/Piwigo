6049301ea3284d93b43887d5020bf7dfadaff7eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-06 07:50:54 +0000

    [elegant] Add ko_KR, thanks to : ayokyun
    git-svn-id: http://piwigo.org/svn/trunk@24119 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/ko_KR/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = '각 패널의 기본 상태와 애니메이션을 비활성화 할 지 선택하십시오.';
+$lang['Comments Panel'] = '댓글 패널';
+$lang['Disable the animation'] = '애니메이션 비활성화';
+$lang['Displayed'] = '보임';
+$lang['Elegant, Configuration Page'] = 'Elegant, 설정 페이지';
+$lang['Hidden'] = '숨김';
+$lang['Main Menu Panel'] = '메인 메뉴 패널';
+$lang['Panels options'] = '패널 옵션';
+$lang['Photo Description Panel'] = '사진 설명 패널';
+?>
