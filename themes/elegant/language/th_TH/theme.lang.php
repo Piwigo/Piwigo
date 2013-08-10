7c8db86769e8a2d62f59d671d863b4341197ee25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-10 17:49:29 +0000

    [elegant] Add th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/trunk@24148 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/th_TH/theme.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = '
+เลือกสิ่งที่ควรมีในตอนเริ่มต้นสำหรับแต่ละแผงหรือปิดการใช้งานภาพเคลื่อนไหว :';
+$lang['Comments Panel'] = 'แผงความเห็น';
+$lang['Disable the animation'] = 'ปิดการใช้งานภาพเคลื่อนไหว';
+$lang['Displayed'] = 'การแสดงผล';
+$lang['Elegant, Configuration Page'] = 'สง่างามหน้าการกำหนดค่า';
+$lang['Hidden'] = 'ซ่อน';
+$lang['Main Menu Panel'] = 'แผงเมนูหลัก';
+$lang['Panels options'] = 'ตัวเลือกแผง';
+$lang['Photo Description Panel'] = 'แผงรายละเอียดรูปภาพ';
+?>
