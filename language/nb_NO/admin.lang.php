dd06c00c12380ac24d8245ce61f26f03477d0860
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-19 10:12:38 +0000

    [trunk_admin] Update nb_NO, thanks to : abbamust
    git-svn-id: http://piwigo.org/svn/trunk@24187 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nb_NO/admin.lang.php
+++ language/nb_NO/admin.lang.php
@@ -949,4 +949,6 @@ $lang['Type here the name of the new group'] = 'Skriv her navnet på den nye gru
 $lang['between %.2f and %.2f'] = 'mellom %.2f og %.2f';
 $lang['between %d and %d pixels'] = 'mellom %d og %d piksler';
 $lang['Purge user cache'] = 'Rens bruker cache';
+$lang['Resize after upload disabled due to the use of GD as graphic library'] = 'Endring av bildestørrelse er slått av, siden du bruker GD som grafikkgalleri.';
+$lang['The picture dimensions will be reduced to %dx%d pixels.'] = 'Bildets dimensjoner vil bli redusert til %dx%d piksler.';
 ?>
