b96329d54fcfff6cb6a3555c19259f3bdfd1edd9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-10 19:12:19 +0000

    [trunk_upgrade] Update pt_BR, thanks to : msakik
    git-svn-id: http://piwigo.org/svn/trunk@24398 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/upgrade.lang.php
+++ language/pt_BR/upgrade.lang.php
@@ -35,7 +35,7 @@ $lang['As a precaution, following plugins have been deactivated. You must check 
 $lang['Only administrator can run upgrade: please sign in below.'] = 'Apenas administrador pode realizar o upgrade: por favor, faça o login logo abaixo.';
 $lang['You do not have access rights to run upgrade'] = 'Você não tem permissões de acesso para realizar o upgrade';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'Em <i>%s</i>, antes de <b>?></b>, insira:';
-$lang['All sub-albums of private albums become private'] = 'Todas os sub-albuns de álbuns privados tornam-se privados';
+$lang['All sub-albums of private albums become private'] = 'Todas os sub-álbuns de álbuns privados tornam-se privados';
 $lang['User permissions and group permissions have been erased'] = 'Permissões de usuário e de grupo foram apagadas.';
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Apenas o prefixo das miniaturas (thumbnails) e o endereço de email do webmaster foram salvos da configuração anterior.';
 $lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Esta página se propõe a atualizar o banco de dados do seu Piwigo de uma versão antiga para uma nova versão. O assistente de atualização detectou que você está rodando uma <strong>distribuição %s</strong> (ou equivalente).'; 
