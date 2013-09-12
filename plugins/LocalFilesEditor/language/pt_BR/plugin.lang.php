be36ede3408ce6964242cb290b5582a12dde046c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-12 18:59:09 +0000

    [localfileseditor] Update pt_BR, thanks to : msakik
    git-svn-id: http://piwigo.org/svn/trunk@24430 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/LocalFilesEditor/language/pt_BR/plugin.lang.php
+++ plugins/LocalFilesEditor/language/pt_BR/plugin.lang.php
@@ -21,25 +21,25 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['locfiledit_bak_loaded1'] = 'Faça cópia do arquivo baixado.';
-$lang['locfiledit_bak_loaded2'] = 'Você deve salvar o arquivo para reustaurá-lo';
+$lang['locfiledit_bak_loaded2'] = 'Você deve salvar o arquivo para restaurá-lo.';
 $lang['locfiledit_cant_save'] = 'O arquivo atual não tem permissão de escrita. Verifique se o diretório "local/" tem permissão de escrita (chmod).';
-$lang['locfiledit_choose_file'] = 'Escolha o arquivo a ser editado.';
+$lang['locfiledit_choose_file'] = 'Escolha o arquivo a ser editado';
 $lang['locfiledit_edit'] = 'Edite';
 $lang['locfiledit_empty_filename'] = 'Você deve entrar com o nome do arquivo.';
 $lang['locfiledit_empty_page'] = 'Página vazia';
-$lang['locfiledit_file_already_exists'] = 'O arquivo já existe';
+$lang['locfiledit_file_already_exists'] = 'O arquivo já existe.';
 $lang['locfiledit_filename_error'] = 'Caracteres não permitidos no nome do arquivo.';
 $lang['locfiledit_model_error'] = 'Você deve escolher um modelo.';
 $lang['locfiledit_model'] = 'Modelo';
 $lang['locfiledit_new_filename'] = 'Nome do arquivo';
 $lang['locfiledit_new_tpl'] = 'Cria novo arquivo';
-$lang['locfiledit_newfile'] = 'O arquivo não existe e será criado pelo Editor Local de Arquivos';
+$lang['locfiledit_newfile'] = 'O arquivo não existe e será criado pelo Editor Local de Arquivos.';
 $lang['locfiledit_onglet_css'] = 'CSS';
 $lang['locfiledit_onglet_lang'] = 'Idiomas';
 $lang['locfiledit_onglet_localconf'] = 'Configuração local';
 $lang['locfiledit_onglet_plug'] = 'Plugin Pessoal';
 $lang['locfiledit_onglet_tpl'] = 'Templates';
-$lang['locfiledit_parent_directory'] = 'Diretório pai';
+$lang['locfiledit_parent_directory'] = 'Pasta pai';
 $lang['locfiledit_restore_confirm'] = 'Confirme por favor? A restauração não será efetiva até o próximo salvar.';
 $lang['locfiledit_restore'] = 'Restaura a cópia de segurança';
 $lang['locfiledit_save_bak'] = 'Cópia de segurança será criada ao salvar.';
