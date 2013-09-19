065cd2b38b20f3a90820aa632aab1a67ec202109
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-19 18:27:06 +0000

    [trunk_admin] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/trunk@24532 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/admin.lang.php
+++ language/el_GR/admin.lang.php
@@ -957,4 +957,5 @@ $lang['Manage the members'] = 'Διαχειριστείτε τα μέλη';
 $lang['Purge user cache'] = 'Εκκαθάριση cache χρήστη';
 $lang['The picture dimensions will be reduced to %dx%d pixels.'] = 'Το μέγεθος της εικόνας θα μικρίνει κατα  %dx%d pixels.';
 $lang['Resize after upload disabled due to the use of GD as graphic library'] = 'Η Αλλαγή μεγέθους μετά τη μεταφόρτωση έχει απενεργοποιηθεί λόγω της χρήσης της βιβλιοθήκης γραφικών GD ';
+$lang['Select at least one tag'] = 'Επιλέξτε τουλάχιστον μια ετικέτα. ';
 ?>
