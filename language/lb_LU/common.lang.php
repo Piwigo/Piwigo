402f7dd247b95e83810417cf35c4ab177dfbdf30
ddtddt <ddtddt@piwigo.org>
2013-10-03 20:53:40 +0000

    [trunk] - Language - create Luxembourgish - Lëtzebuergesch [lb_LU]
    git-svn-id: http://piwigo.org/svn/trunk@24743 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lb_LU/common.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Lëtzebuergesch [LU]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=715
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Lëtzebuergesch ';
+$lang_info['country'] = 'Luxembourg';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'lb';
+$lang_info['zero_plural'] = true;
+
+
+$lang['day'][0] = 'Sonden';
+$lang['day'][1] = 'Méinden';
+$lang['day'][2] = 'Denschten';
+$lang['day'][3] = 'Mettwoch';
+$lang['day'][4] = 'Donneschten';
+$lang['day'][5] = 'Freiden';
+$lang['day'][6] = 'Samschten';
+?>
