b1b9b1b1fadb2101bf82c407170f1cfbd0fe938d
ddtddt <ddtddt@piwigo.org>
2013-10-16 18:50:50 +0000

    [trunk] - language - new language en_GB Thanks to Cadencia
    git-svn-id: http://piwigo.org/svn/trunk@24940 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/en_GB/admin.lang.php
@@ -0,0 +1,113 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+
+$lang['(this tag will be deleted)'] = '(this keyword will be deleted)';
+$lang['Add a criteria'] = 'Add a criterion';
+$lang['add new photos to caddie'] = "add new photos to collection";
+$lang['Add to caddie'] = "Add to collection";
+$lang['albums added in the database'] = "albums added to the database";
+$lang['Albums authorized thanks to group associations'] = "Albums authorised by group association";
+$lang['All optimizations have been successfully completed.'] = "All optimisations have been successfully completed.";
+$lang['Allow user customization'] = 'Allow user customisation';
+$lang['Are you sure to install this upgrade? You must verify if this version does not need uninstallation.'] = "Are you sure you want to install this upgrade? You must check if this version needs prior uninstallation.";
+$lang['Authorize users to add comments on selected albums'] = "Authorise users to add comments on selected albums";
+$lang['Authorized'] = "Authorised";
+$lang['Caddie management'] = "Collection management";
+$lang['Caddie'] = "Collection";
+$lang['Complementary mail content'] = "Extra mail content";
+$lang['Database synchronization with files'] = "Database synchronization with files";
+$lang['Does not represent'] = "Is not thumbnail for";
+$lang['Duplicate selected tags'] = 'Duplicate selected keywords';
+$lang['Edit selected tags'] = "Edit selected keywords";
+$lang['Edit tags'] = "Edit keyword";
+$lang['Errors caption'] = "Error legend";
+$lang['errors during synchronization'] = "errors during synchronisation";
+$lang['even already synchronized photos'] = "including already synchronised photos";
+$lang['Execution time is out, treatment must be continue [Estimated time: %d seconds].'] = "Permitted execution time exceeded, operation will continue [Estimated time: %d seconds].";
+$lang['Following plugins may not be compatible with the new version of Piwigo:'] = 'The following plugins may not be compatible with the new version of Piwigo:';
+$lang['Following themes may not be compatible with the new version of Piwigo:'] = 'The following themes may not be compatible with the new version of Piwigo:';
+$lang['FTP + Synchronization'] = 'FTP + Synchronisation';
+$lang['Group management'] = "Group management";
+$lang['I decide to update anyway'] = 'I choose to update anyway';
+$lang['Impossible to delete this theme. Other themes depends on it: %s'] = 'This theme cannot be deleted, because other themes depend on it: %s';
+$lang['Instructions to use Piwigo'] = "How to use Piwigo";
+$lang['Last hit'] = "Latest hit";
+$lang['Last import'] = 'Latest import';
+$lang['Last revisions'] = "Latest revisions";
+$lang['Last send'] = "Latest send";
+$lang['Link all album photos to some existing albums'] = "Link all album photos to some existing album";
+$lang['Manage tags'] = "Manage keyword";
+$lang['Manage the members'] = 'Manage members';
+$lang['Merge selected tags'] = 'Merge selected keywords';
+$lang['Merge tags'] = 'Merge keywords';
+$lang['Metadata synchronization results'] = "Metadata synchronisation results";
+$lang['Metadata synchronized from file'] = "Metadata synchronised from file";
+$lang['only perform a simulation (no change in database will be made)'] = "perform a simulation only (nothing will be changed in the database)";
+$lang['Operation in progress'] = 'Operation in progress.';
+$lang['Optimizations have been completed with some errors.'] = "Optimisations have been completed with some errors.";
+$lang['Orphan tags deleted'] = 'Orphan keywords deleted';
+$lang['Photo informations updated'] = "Photo information updated";
+$lang['photos added in the database'] = "photos added to the database";
+$lang['photos candidates for metadata synchronization'] = "candidate photos for metadata synchronisation";
+$lang['photos informations synchronized with files metadata'] = "photo information synchronised with file metadata";
+$lang['Purge never used notification feeds'] = "Purge unused notification feeds";
+$lang['Quick Local Synchronization'] = "Quick Local Synchronisation";
+$lang['reduce to single existing albums'] = "reduce to single existing album";
+$lang['Reinitialize check integrity'] = "Reinitialise integrity check";
+$lang['Remove from caddie'] = 'Remove from collection';
+$lang['remove tags'] = "remove keywords";
+$lang['Repair and optimize database'] = "Repair and optimise database";
+$lang['Replacement of original templates by customized templates from template-extension subfolder'] = "Replacement of original templates by customised templates from the template-extension subfolder";
+$lang['Replacers (customized templates)'] = "Substitutes (customised templates)";
+$lang['Select at least two tags for merging'] = 'Select at least two keywords for merging';
+$lang['Subscribers could be listed (available) only if there is new elements to notify.'] = "Subscribers listed (available) only if there are new elements to notify.";
+$lang['synchronize files metadata with database photos informations'] = "synchronise database photos with file metadata";
+$lang['synchronize files structure with database'] = "synchronise file structure with database";
+$lang['Synchronize metadata'] = "Synchronise metadata";
+$lang['Synchronize'] = "Synchronise";
+$lang['Tag "%s" already exists'] = "Keyword \"%s\" already exists";
+$lang['Tag "%s" is now a duplicate of "%s"'] = "Keyword \"%s\" is now a duplicate of \"%s\"";
+$lang['Tag "%s" was added'] = "Keyword \"%s\" has been added";
+$lang['Tag selection'] = "Keyword selection";
+$lang['Tags <em>%s</em> merged into tag <em>%s</em>'] = 'Keywords <em>%s</em> merged into keyword <em>%s</em>';
+$lang['The %d following tags were deleted'] = "The following %d keywords have been deleted";
+$lang['The anomaly will be ignored until next application version'] = "The anomaly will be ignored until the next version of the application";
+$lang['The following tag was deleted'] = 'The following keyword was deleted';
+$lang['The number of comments a page must be between 5 and 50 included.'] = "The number of comments on a page must be between 5 and 50 inclusive.";
+$lang['The webmaster has subscribed you to receiving notifications by mail.'] = "The webmaster has added you to the mail notification list.";
+$lang['The webmaster has unsubscribed you from receiving notifications by mail.'] = "The webmaster has removed you from the list for receiving notifications by mail.";
+$lang['There is no available subscribers to mail.'] = "There are no subscribers to be notified by mail.";
+$lang['Thumbnails generation in progress...'] = 'Thumbnail generation in progress...';
+$lang['Time to send mail is limited. Others mails are skipped.'] = "The maximum time for sending mail has been exceeded. Remaining emails have been skipped.";
+$lang['Update albums informations'] = "Update album information";
+$lang['Update photos information'] = "Update photo information";
+$lang['User list'] = "User list";
+$lang['Virtual albums to move'] = "Virtual albums to be moved";
+$lang['With no tag'] = 'With no keyword';
+$lang['You are running on development sources, no check possible.'] = "You are running a development version, no check possible.";
+$lang['You have specified <i>$conf[\'order_by\']</i> in your local configuration file, this parameter in deprecated, please remove it or rename it into <i>$conf[\'order_by_custom\']</i> !'] = 'You have specified <i>$conf[\'order_by\']</i> in your local configuration file, this parameter is deprecated, please remove it or rename it to <i>$conf[\'order_by_custom\']</i> !';
+$lang['You need to upgrade your system to take full advantage of the application else the application will not work correctly, or not at all'] = "Please upgrade your system to take full advantage of the application, otherwise it might work improperly, or not at all";
+
+?>
