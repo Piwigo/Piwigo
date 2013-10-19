68e0e368611692efb0ac144d4d03ec333f905262
ddtddt <ddtddt@piwigo.org>
2013-10-19 13:44:34 +0000

    git-svn-id: http://piwigo.org/svn/trunk@25014 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_US/common.lang.php
+++ language/en_US/common.lang.php
@@ -23,7 +23,7 @@
 /*
 Language Name: English [US]
 Version: 2.5.0
-Language URI: http://piwigo.org/ext/extension_view.php?eid=
+Language URI: http://piwigo.org/ext/extension_view.php?eid=717
 Author: Piwigo team
 Author URI: http://piwigo.org
 */
