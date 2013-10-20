eb7f728c692a22e6ee9ad7c1b731ac6be1b0ed5c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-20 16:46:54 +0000

    [elegant] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/trunk@25034 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/nn_NO/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Comments Panel'] = 'Panel for kommentarar';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Vel kva som skal vera standard for kvart panel, eller ikkje vis animering:';
+$lang['Disable the animation'] = 'Ikkje vis animasjonar';
+$lang['Elegant, Configuration Page'] = 'Elegant - innstillingsside';
+$lang['Main Menu Panel'] = 'Panel for hovudmeny';
+$lang['Panels options'] = 'Panelinnstillingar';
+$lang['Photo Description Panel'] = 'Panel for biletskildring';
+$lang['Displayed'] = 'Vist';
+$lang['Hidden'] = 'Gøymd';
+?>
