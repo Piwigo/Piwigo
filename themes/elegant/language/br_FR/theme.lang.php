db93243d1f4aa7e40148a32095fa9ff88385dc68
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-29 23:39:25 +0000

    [elegant] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@25230 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/language/br_FR/theme.lang.php
+++ themes/elegant/language/br_FR/theme.lang.php
@@ -24,4 +24,9 @@ $lang['Comments Panel'] = 'Panell an evezhiadennoù';
 $lang['Main Menu Panel'] = 'Panell ar penn-lañser';
 $lang['Panels options'] = 'Dibarzhioù ar banelloù';
 $lang['Photo Description Panel'] = 'Panell deskrivadur al luc\'hskeudennoù';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Dibabit peseurt stad a rank bezañ an hini dre ziouer evit pep skramm, pe diweredekait ar skeudenn-vev:';
+$lang['Hidden'] = 'Kuzhet';
+$lang['Elegant, Configuration Page'] = 'Cheuc\'h, Pajenn kefluniañ';
+$lang['Disable the animation'] = 'Diweredekaat ar skeudenn-vev';
+$lang['Displayed'] = 'Diskouezet';
 ?>
