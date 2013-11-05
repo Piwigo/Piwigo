550a2514c7dd4742f32b1374b854043975cb63c3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-05 19:33:34 +0000

    [trunk_upgrade] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@25346 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/br_FR/upgrade.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Ar bajenn-mañ a ginnig hizivadur an diaz roadennoù eus ho stumm Piwigo kozh d\'ar stumm nevez. An ameiler hizivaat a soñj emañ <strong>stumm %s</strong> o labourat (pe gevatal).';
+$lang['User permissions and group permissions have been erased'] = 'Implijerien ha strolladoù a zo bet dilemet';
+$lang['You do not have access rights to run upgrade'] = 'Difennet eo deoc\'h hizivaat';
+$lang['Statistics'] = 'Stadegoù';
+$lang['total upgrade time'] = 'Hollad amzer an hizivadur';
+$lang['Upgrade'] = 'Hizivadur';
+$lang['Upgrade from version %s to %s'] = 'Hizivaat eus stumm %s betek %s';
+$lang['Upgrade informations'] = 'Titouroù hizivadur';
+$lang['total SQL time'] = 'Hollad an amzer SQL ';
+$lang['SQL queries'] = 'Rekedoù SQL';
+?>
