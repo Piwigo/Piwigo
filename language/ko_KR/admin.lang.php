418b64e1b6bd1fe9e0955bbd20a5ee644a3f988a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-05 09:57:25 +0000

    [trunk_admin] Update ko_KR, thanks to : ayokyun
    git-svn-id: http://piwigo.org/svn/trunk@25341 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ko_KR/admin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add group'] = '그룹 추가';
+$lang['Add tags'] = '태그 추가';
+$lang['Add/delete a permalink'] = '퍼마링크 추가/삭제';
+$lang['Added by %s'] = '%s이(가) 추가함';
+$lang['%d guest'] = '%d 손님';
+$lang['%d guests'] = '%d 손님';
+$lang['%d hour'] = '%d 시간';
+$lang['%d hours'] = '%d 시간';
+$lang['%d day'] = '%d 일';
+$lang['%d days'] = '%d 일';
+$lang['%d group'] = '%d 그룹';
+$lang['%d groups'] = '%d 그룹';
+?>
