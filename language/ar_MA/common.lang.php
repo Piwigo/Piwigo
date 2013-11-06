12b68909dc5f3173c099f805aae5c5e764fbd098
ddtddt <ddtddt@piwigo.org>
2013-11-06 22:05:12 +0000

    [Trunk] - language - add ar_MA
    git-svn-id: http://piwigo.org/svn/trunk@25371 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_MA/common.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: العربية [MA]
+Version: 2.5.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Arabic';
+$lang_info['country'] = 'Morocco';
+$lang_info['direction'] = 'rtl';
+$lang_info['code'] = 'ar';
+$lang_info['zero_plural'] = true;
+$lang_info['parent']= "ar_SA";
+
+?>
