2aaea8e72fdc0cbf88ee811de324f3c0ac5992e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-18 18:31:53 +0000

    [trunk_public] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/trunk@25554 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/common.lang.php
+++ language/lv_LV/common.lang.php
@@ -418,4 +418,5 @@ $lang['First Page'] = 'Pirmā lapa';
 $lang['Password is missing. Please enter the password.'] = 'Nepareiza parole. Lūdzu atkārtojiet mēģinājumu.';
 $lang['Password confirmation is missing. Please confirm the chosen password.'] = 'Trūkst paroles apstiprinājuma. Lūdzu apstipriniet izvēlēto paroli.';
 $lang['%d photos per page'] = '%d fotogrāfiju lapā';
+$lang['Theme'] = 'Tēma';
 ?>
