de852e71b83a85acf66efade0b90a72e0be59257
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-23 12:18:46 +0000

    [trunk_admin] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@25649 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/br_FR/admin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang[' and %d virtual'] = 'ha %d galloudel';
+$lang[' and %d virtuals'] = 'ha %d galloudel';
+$lang['Add a virtual album'] = 'Ouzhpennañ ur rummad galloudel';
+$lang['Virtual Links'] = 'Liammoù galloudel';
+$lang['Virtual album'] = 'Rummad galloudel';
+$lang['Virtual album added'] = 'Rummad galloudel ouzhpennet';
+$lang['Virtual album deleted'] = 'Rummad galloudel dilimet';
+$lang['Virtual album name'] = 'Anv ar rummad galloudel';
+$lang['Virtual albums to move'] = 'Rummadoù galloudel da zilec\'hiañ';
+$lang['With no virtual album'] = 'Hep rummad galloudel';
+?>
