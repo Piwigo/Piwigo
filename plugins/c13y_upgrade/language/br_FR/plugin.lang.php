dd05fe8554e2bcb9d3ea40dd50d7124622410bb3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-17 21:54:46 +0000

    [c13y_upgrade] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@25991 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/c13y_upgrade/language/br_FR/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['c13y_correction_dbl_email_user'] = 'Dilemel an implijerien doublenn';
+$lang['c13y_correction_obsolete_plugin'] = 'Lugant "%s" a zo bet lakaet e stumm-mañ an arload, ha ret eo deoc\'h distaliañ anezhañ.';
+$lang['c13y_dbl_email_user'] = 'Implijerien gant ar memes postel';
+$lang['c13y_obsolete_plugin'] = 'Lugant diamzeret';
+$lang['c13y_upgrade_no_anomaly'] = 'Fazi ebet a oa dinoet goude hizivadenn an arload, al lugant "Check upgrades" a zo bet diweredekaet.';
+?>
