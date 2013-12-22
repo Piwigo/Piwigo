9a7a7a523b74b20b641df9a930163dd413859f92
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-22 19:40:00 +0000

    [localfileseditor] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@26092 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/br_FR/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_newfile'] = 'Ne vo ket a restr betek ma vo lakaet titouroù dindan. Pa vo lakaet titouroù ha saveteet, krouet e vo ar restr.';
+$lang['locfiledit_model'] = 'Patrom';
+$lang['locfiledit_model_error'] = 'Ret eo deoc\'h dibab ur patrom.';
+$lang['locfiledit_new_filename'] = 'Anv ar restr';
+$lang['locfiledit_new_tpl'] = 'Krouiñ ur restr nevez';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Yezhoù';
+$lang['locfiledit_onglet_localconf'] = 'Kefluniadur lec\'hel';
+$lang['locfiledit_onglet_plug'] = 'Lugant personel';
+$lang['locfiledit_onglet_tpl'] = 'Patromoù';
+$lang['locfiledit_parent_directory'] = 'Kavlec\'h tad';
+$lang['locfiledit_restore'] = 'Adsevel ar restr saveteiñ';
+$lang['locfiledit_restore_confirm'] = 'Kadarnaat ?
+Ne vo ket adsavet betek ar saveteiñ gentañ.';
+$lang['locfiledit_save_bak'] = 'An eilad a vo krouet gant ar saveteerezh.';
+$lang['locfiledit_save_config'] = 'Skrivet mat ar restr';
+$lang['locfiledit_save_file'] = 'Saveteiñ ar restr';
+$lang['locfiledit_saved_bak'] = 'Restr saveteiñ krouet (%s)';
+$lang['locfiledit_show_default'] = 'Diskouez ar restr dave:';
+$lang['locfiledit_syntax_error'] = 'Fazi ereadurezh! Ne c\'hell ket ar restr bezañ saveteet.';
+$lang['locfiledit_webmaster_only'] = 'Ar mistri-gwiad hepken a c\'hell krouiñ pe cheñch restroù lec\'hel.';
+$lang['locfiledit_bak_loaded1'] = 'Restr saveteiñ karget.';
+$lang['locfiledit_bak_loaded2'] = 'Ret eo deoc\'h saveteiñ ar restr a-raok adsevel anezhi.';
+$lang['locfiledit_cant_save'] = 'N\'eo ket posubl skrivañ e restr red. Sellit m\'eo posubl skrivañ e kavlec\'h "local/" (chmod).';
+$lang['locfiledit_choose_file'] = 'Dibabit ar restr da aozañ';
+$lang['locfiledit_edit'] = 'Aozañ';
+$lang['locfiledit_empty_filename'] = 'Ret eo deoc\'h lakaat anv ar restr.';
+$lang['locfiledit_empty_page'] = 'Pajennad goullo';
+$lang['locfiledit_file_already_exists'] = 'Bez zo ar restr-mañ dija.';
+$lang['locfiledit_filename_error'] = 'Arouezennoù berzet a zo e anv ar restr.';
+?>
