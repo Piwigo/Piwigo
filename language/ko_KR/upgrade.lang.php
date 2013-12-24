ef475faf01cc1c30e4c193ca46cee1c09e782298
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-24 08:32:56 +0000

    [trunk_upgrade] Update ko_KR, thanks to : ayokyun
    git-svn-id: http://piwigo.org/svn/trunk@26159 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ko_KR/upgrade.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Only administrator can run upgrade: please sign in below.'] = '관리자만 업그레이드를 수행 할 수 있습니다: 로그인하십시오.';
+$lang['SQL queries'] = 'SQL 쿼리';
+$lang['Statistics'] = '통계';
+$lang['Upgrade'] = '업그레이드';
+$lang['Upgrade from version %s to %s'] = '버전 %s에서 %s로 업그레이드';
+$lang['Upgrade informations'] = '업그레이드 정보';
+$lang['You do not have access rights to run upgrade'] = '업그레이드를 수행 할 권한이 없습니다';
+$lang['total SQL time'] = '총 SQL 수행시간';
+$lang['total upgrade time'] = '총 업그레이드 수행시간';
+?>
