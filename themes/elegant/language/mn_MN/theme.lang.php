085130f22fda7768022a56136d55209c8dc0c250
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-02 10:14:55 +0000

    [elegant] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@26392 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/language/mn_MN/theme.lang.php
+++ themes/elegant/language/mn_MN/theme.lang.php
@@ -24,4 +24,9 @@ $lang['Elegant, Configuration Page'] = 'Тохиргооны хуудас';
 $lang['Comments Panel'] = 'Сэтгэгдэлийн талбар';
 $lang['Disable the animation'] = 'Анимейшнийг хаах ';
 $lang['Hidden'] = 'Нуугдмал';
+$lang['Displayed'] = 'Харуулах';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Хэсэг тус бүрийн тохиргоог сонгох буюу анимейшнийг хаах:';
+$lang['Main Menu Panel'] = 'Үндсэн цэсийн хэсэг';
+$lang['Panels options'] = 'Талбарын тохиргоо';
+$lang['Photo Description Panel'] = 'Зургын тайлбар хэсэг';
 ?>
