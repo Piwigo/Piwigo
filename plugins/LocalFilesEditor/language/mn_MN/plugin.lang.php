c0a1c0f42db17f2a283d32258a62e36d54607099
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-03 06:00:27 +0000

    [localfileseditor] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@26407 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/LocalFilesEditor/language/mn_MN/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_cant_save'] = 'Тухайн файлд бичлэг хийх боломжгүй байна. "local/" хавтас өөрчлөлт хийх боломтой эсэхийг шалгана уу (chmod).';
+$lang['locfiledit_empty_page'] = 'Хуудсыг цэвэрлэх';
+$lang['locfiledit_show_default'] = 'Тайлбар файлыг харуулах:';
+$lang['locfiledit_onglet_plug'] = 'Нэмэлт өргөтгөл';
+$lang['locfiledit_save_bak'] = 'Хадгалах үед нөөц файл үүсэх болно.';
+$lang['locfiledit_parent_directory'] = 'Үндсэн хавтас';
+$lang['locfiledit_onglet_tpl'] = 'Эх загвар';
+$lang['locfiledit_onglet_localconf'] = 'Тохиргооны файл';
+$lang['locfiledit_filename_error'] = 'Файлын нэрэнд зөвшөөрөгдөөгүй тэмдэгт орсон байна.';
+$lang['locfiledit_webmaster_only'] = 'Файлыг зөвхөн вэбмастер үүсгэж, засах боломжтой.';
+$lang['locfiledit_restore'] = 'Нөөц файлаас сэргээх';
+$lang['locfiledit_saved_bak'] = 'Нөөц файлыг амжилттай үүсгэлээ (%s)';
+$lang['locfiledit_syntax_error'] = 'Бичлэгийн алдаа! Файлыг хадгалах боломжгүй.';
+$lang['locfiledit_file_already_exists'] = 'Ийм нэртэй файл байна.';
+$lang['locfiledit_save_config'] = 'Файлыг амжилттай хадгаллаа';
+$lang['locfiledit_save_file'] = 'Хадгалах';
+$lang['locfiledit_choose_file'] = 'Засах файлаа сонгоно уу';
+$lang['locfiledit_edit'] = 'Засах';
+$lang['locfiledit_empty_filename'] = 'Файлын нэрээ оруулна уу.';
+$lang['locfiledit_model'] = 'Загвар';
+$lang['locfiledit_model_error'] = 'Загвараа сонгоно уу';
+$lang['locfiledit_new_filename'] = 'Файлын нэр';
+$lang['locfiledit_new_tpl'] = 'Шинэ файл үүсгэх';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Хэл';
+?>
