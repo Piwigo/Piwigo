60db82e0626afb760fc351c1bbf9ff657bfdf602
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-04 08:21:01 +0000

    [trunk_upgrade] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/trunk@26419 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fa_IR/upgrade.lang.php
+++ language/fa_IR/upgrade.lang.php
@@ -23,21 +23,21 @@
 
 
 
-$lang['Upgrade'] = 'آپگرید';
+$lang['Upgrade'] = 'بروز رسانی';
 $lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'پیشنهاد می‌شود که با استفاده از این صفحه بانک اطلاعاتی نگارش قدیمی Piwigo خود را به نگارش تازه ی آن ارتقا دهید. سیستم آپگرید نگارش کنونی گالری شما را <strong>نگارش %s </strong> تشخیص داده است.';
-$lang['Upgrade from version %s to %s'] = 'آپگرید از نگارش %s به نگارش %s';
+$lang['Upgrade from version %s to %s'] = 'بروزرسانی از نگارش %s به نگارش %s';
 $lang['Statistics'] = "آمار";
-$lang['total upgrade time'] = 'زمان کل آپگرید';
+$lang['total upgrade time'] = 'زمان کل بروز رسانی';
 $lang['total SQL time'] = 'زمان کل SQL';
 $lang['SQL queries'] = 'کوئری(Query) های SQL';
-$lang['Upgrade informations'] = 'اطلاعات آپگرید';
+$lang['Upgrade informations'] = 'اطلاعات بروز رسانی';
 $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'اگر به مشکلی برخوردید به [مدیریت > ابزار ها > نگهداری] مراجعه نمایید.';
 $lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'هشدار، پلاگین های زیر در حال حاضر از کار افتاده اند. برای امنیت بیشتر، قبل از فعال کردن دوباره ی آن ها باید آپگرید های تازه آنها را بررسی کنید:';
-$lang['Only administrator can run upgrade: please sign in below.'] = 'فقط مدیران می توانند سیستم را آپگرید کنند.';
-$lang['You do not have access rights to run upgrade'] = "شما دسترسی لازم برای آپگرید را ندارید";
+$lang['Only administrator can run upgrade: please sign in below.'] = 'فقط مدیران می توانند سیستم را بروز رسانی کنند.';
+$lang['You do not have access rights to run upgrade'] = 'شما دسترسی لازم برای بروز رسانی را ندارید';
 $lang['All sub-albums of private albums become private'] = 'تمام زیرآلبوم های مربوط به آلبوم های شخصی، شخصی شدند';
 $lang['User permissions and group permissions have been erased'] = 'مجوز های کاربران و گروه ها پاک شد';
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'فقط پیشوند عکس های بندانگشتی و آدرس ایمیل مدیرکل از تنظیمات قدیمی باقی خواهد ماند';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'در <i>%s</i>, قبل از <b>?></b>, اضافه کنید:';
-$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'هشدار، قالب های زیر در حال حاضر از کار افتاده اند. برای امنیت بیشتر، قبل از فعال کردن دوباره ی آن ها باید آپگرید های تازه آنها را بررسی کنید:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'هشدار، قالب های زیر در حال حاضر از کار افتاده اند!!. برای امنیت بیشتر، قبل از فعال کردن دوباره ی آن ها باید بروزرسانی تازه آنها را بررسی کنید:';
 ?>
