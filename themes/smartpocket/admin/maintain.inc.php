b071d8dc18a8b2e6aeff96a2e9208ac02653381e
flop25 <flop25@piwigo.org>
2014-01-11 15:35:00 +0000

    smartpocket: Thememaintain class used in maintain.inc.php-> no more issue on 2.6 installation (tested)
    git-svn-id: http://piwigo.org/svn/trunk@26627 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/smartpocket/admin/maintain.inc.php
+++ themes/smartpocket/admin/maintain.inc.php
@@ -1,29 +1,45 @@
 <?php
-
-function theme_activate($id, $version, &$errors)
+class smartpocket_maintain extends ThemeMaintain
 {
-  global $prefixeTable, $conf;
-
-  if (!isset($conf['smartpocket']))
+  private $installed = false;
+  
+  private $default_conf = array(
+    'loop'            => true,//true - false
+    'autohide'            => 5000,//5000 - 0
+  );
+  
+  function activate($theme_version, &$errors=array())
   {
-    $config = array(
-      'loop'            => true,//true - false
-      'autohide'            => 5000,//5000 - 0
-    );
-      
-    $query = "
-INSERT INTO " . CONFIG_TABLE . " (param,value,comment)
-VALUES ('smartpocket' , '".pwg_db_real_escape_string(serialize($config))."' , 'loop#autohide');";
-    pwg_query($query);
+    global $conf, $prefixeTable;
+
+    if (empty($conf['smartpocket']))
+    {
+      $conf['smartpocket'] = serialize($this->default_conf);
+      $query = "
+  INSERT INTO " . CONFIG_TABLE . " (param,value,comment)
+  VALUES ('smartpocket' , '".pwg_db_real_escape_string($conf['smartpocket'])."' , 'loop#autohide');";
+      pwg_query($query);
+    }
+    elseif (count(unserialize( $conf['smartpocket'] ))!=2)
+    {
+      $conff=unserialize($conf['smartpocket']);
+      $config = array(
+        'loop'            => (!empty($conff['loop'])) ? $conff['loop'] :true,
+        'autohide'            => (!empty($conff['autohide'])) ? $conff['autohide'] :5000,
+      );
+      conf_update_param('smartpocket', pwg_db_real_escape_string(serialize($config)));
+      load_conf_from_db();
+    }
+    $this->installed = true;
+  }
+
+  function deactivate()
+  { }
+
+  function delete()
+  {
+    // delete configuration
+    conf_delete_param('smartpocket');
   }
 }
-
-function theme_delete()
-{
-  global $prefixeTable;
-
-  $query = 'DELETE FROM ' . CONFIG_TABLE . ' WHERE param="smartpocket" ;';
-  pwg_query($query);
-}
-
 ?>
--- themes/smartpocket/admin/upgrade.inc.php
+++ themes/smartpocket/admin/upgrade.inc.php
@@ -20,8 +20,8 @@ elseif (count(unserialize( $conf['smartpocket'] ))!=2)
 {
   $conff=unserialize($conf['smartpocket']);
   $config = array(
-    'loop'            => (isset($conff['loop'])) ? $conff['loop'] :true,
-    'autohide'            => (isset($conff['autohide'])) ? $conff['autohide'] :5000,
+    'loop'            => (!empty($conff['loop'])) ? $conff['loop'] :true,
+    'autohide'            => (!empty($conff['autohide'])) ? $conff['autohide'] :5000,
   );
   conf_update_param('smartpocket', pwg_db_real_escape_string(serialize($config)));
   load_conf_from_db();
