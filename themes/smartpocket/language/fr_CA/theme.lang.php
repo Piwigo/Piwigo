a289427604de0f31d692c9a5db4367b01789adfa
ddtddt <ddtddt@piwigo.org>
2014-01-15 21:22:46 +0000

    [trunk] - themes/smartpocket - add fr_CA - Quebec - Québécois Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/trunk@26771 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/smartpocket/language/fr_CA/theme.lang.php
@@ -0,0 +1,7 @@
+<?php
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, Page de configuration';
+$lang['Slideshow Options'] = 'Options du diaporama';
+$lang['Loop the slideshow'] = 'Boucler le diaporama';
+$lang['Autohide the bar of the slideshow'] = 'Cacher automatiquement la barre du diaporama';
+$lang['More Information'] = 'Plus d\'informations';
+?>
