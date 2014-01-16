0aecb41ddf9aff206952db0bee522547b4fbec95
ddtddt <ddtddt@piwigo.org>
2014-01-16 05:53:07 +0000

    [Trunk] - themes/smartpocket - add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/trunk@26786 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/smartpocket/language/cs_CZ/theme.lang.php
@@ -0,0 +1,7 @@
+<?php
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, Stránka s nastavením';
+$lang['Slideshow Options'] = 'Slideshow nastavení';
+$lang['Loop the slideshow'] = 'Opakovat slideshow ve smyčce';
+$lang['Autohide the bar of the slideshow'] = 'Automatické skrytí panelu ve slideshow';
+$lang['More Information'] = 'Další Informace';
+?>
