388871cce5d139d6076e864be11b163343a04182
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 08:45:05 +0000

    [smartpocket] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/trunk@26787 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/smartpocket/language/es_ES/theme.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Autohide the bar of the slideshow'] = 'Ocultar automáticamente la barra de la presentación de diapositivas';
+$lang['Loop the slideshow'] = 'Poner en bucle la presentación de diapositivas';
+$lang['More Information'] = 'Mas información';
+$lang['Slideshow Options'] = 'Opciones de la presentación';
+$lang['Smartpocket, Configuration Page'] = 'Pagina de configración para smartpocket';
+?>
