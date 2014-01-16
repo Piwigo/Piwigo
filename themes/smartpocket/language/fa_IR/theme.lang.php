9466d20de15bfb22c89d0057e00f594c2d4ca984
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 13:41:02 +0000

    [smartpocket] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/trunk@26804 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/smartpocket/language/fa_IR/theme.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Loop the slideshow'] = 'حلقه تصاویر به صورت خودکار';
+$lang['Slideshow Options'] = 'گزینه نمایش به صورت اسلاید';
+$lang['Autohide the bar of the slideshow'] = 'مخفی شونده خودکار نوار از تصاویر به صورت خودکار';
+$lang['More Information'] = 'اطلاعات بیشتر';
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket، پیکربندی صفحه';
+?>
