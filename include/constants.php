41f66ccac66bfd99b5496ed0cbfbedd54579b890
plegall <plg@piwigo.org>
2014-01-31 14:42:31 +0000

    next version is 2.7.0beta1
    git-svn-id: http://piwigo.org/svn/trunk@27094 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.6.0');
+define('PHPWG_VERSION', '2.7.0beta1');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'elegant');
 
