aa0209f21db839678bc3e0d70a5172dafc9e715e
ddtddt <ddtddt@piwigo.org>
2014-03-09 17:55:40 +0000

    [Trunk] - language_ - add Wolof for translate tool
    git-svn-id: http://piwigo.org/svn/trunk@27629 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/wo_SN/common.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Wolof (Sénégal) [SN]
+Version: 2.6.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=737
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Wolof';
+$lang_info['country'] = 'Sénégal';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'wo';
+$lang_info['zero_plural'] = true;
+
+$lang['day'][0] = 'Dibeer ji ';
+$lang['day'][1] = 'Altine';
+$lang['day'][2] = 'Talaata';
+$lang['day'][3] = 'àllarba';
+$lang['day'][4] = 'Alxames';
+$lang['day'][5] = 'àjjuma ';
+$lang['day'][6] = 'Gaawu-aset';
+$lang['month'][10] = 'Tabaski';
+$lang['month'][11] = 'Tamkharit';
+$lang['month'][12] = 'Digui Gamou';
+$lang['month'][1] = 'Gamou';
+$lang['month'][2] = 'Raki Gamou';
+$lang['month'][3] = 'Rakâti Gamou';
+$lang['month'][4] = 'Mâmou Kôr';
+$lang['month'][5] = 'Ndèyou Kôr';
+$lang['month'][6] = 'Baraxlou';
+$lang['month'][7] = 'Kôr';
+$lang['month'][8] = 'Kori';
+$lang['month'][9] = 'Digui Tabaski';
+?>
