5b0a3e226dc3c601813694e70ca6630cf2c75ef7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-12 11:52:04 +0000

    [elegant] Update km_KH, thanks to : osify
    git-svn-id: http://piwigo.org/svn/trunk@27710 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/km_KH/theme.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'ជ្រើសអ្វីដែលគួរតែត្រូវបាន​ស្ថិតនៅជា​លំនាំដើម សម្រាប់បន្ទះនិមួយៗ ឬបិទចលនាតែម្តង៖';
+$lang['Comments Panel'] = 'បន្ទះមតិ';
+$lang['Disable the animation'] = 'បិទចលនា';
+$lang['Displayed'] = 'បានបង្ហាញ';
+$lang['Elegant, Configuration Page'] = 'Elegant, ទំព័រកំណត់រចនា';
+$lang['Hidden'] = 'លាក់';
+$lang['Main Menu Panel'] = 'បន្ទះម៉ឺនុយមេ';
+$lang['Panels options'] = 'បន្ទះជម្រើស';
+$lang['Photo Description Panel'] = 'បន្ទះពណ៌នា​រូបភាព';
+?>
