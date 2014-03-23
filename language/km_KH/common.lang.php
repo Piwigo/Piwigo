2f8018c281bf30daa0c6ed7952bea48d94a252c5
ddtddt <ddtddt@piwigo.org>
2014-03-23 07:48:35 +0000

    [Trunk] - Language - prepare for 2.6.2
    git-svn-id: http://piwigo.org/svn/trunk@27889 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/km_KH/common.lang.php
+++ language/km_KH/common.lang.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: ខ្មែរ [KH]
-Version: 2.6.0
+Version: 2.6.1
 Language URI: http://piwigo.org/ext/extension_view.php?eid=579
 Author: Piwigo team
 Author URI: http://piwigo.org
--- language/mn_MN/common.lang.php
+++ language/mn_MN/common.lang.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: Монгол [MN]
-Version: 2.6.1
+Version: 2.6.2
 Language URI: http://piwigo.org/ext/extension_view.php?eid=696
 Author: Piwigo team
 Author URI: http://piwigo.org
