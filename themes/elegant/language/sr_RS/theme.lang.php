cd437d36c2417f6d620dc1c6a1fdd904c668ee78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-26 17:00:24 +0000

    [elegant] Update sr_RS, thanks to : gpopac
    git-svn-id: http://piwigo.org/svn/trunk@27963 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/sr_RS/theme.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Изаберите подразумеван приказ сваке површи или искључите анимацију:';
+$lang['Comments Panel'] = 'Површ за коментаре';
+$lang['Disable the animation'] = 'Онемогући анимацију';
+$lang['Displayed'] = 'Приказано';
+$lang['Elegant, Configuration Page'] = 'Подешавања Елегантне теме';
+$lang['Hidden'] = 'Сакривено';
+$lang['Main Menu Panel'] = 'Површ са главним менијем';
+$lang['Panels options'] = 'Опције панела';
+$lang['Photo Description Panel'] = 'Површ са описом фотографије';
