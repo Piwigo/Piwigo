35fd7da4307f186f07235176cdd009f5d2bfc9f6
rvelices <rv-github@modusoptimus.com>
2014-04-05 20:06:28 +0000

    type error in prev commit
    git-svn-id: http://piwigo.org/svn/trunk@28090 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/functions_search.inc.php
+++ include/functions_search.inc.php
@@ -1258,7 +1258,7 @@ function get_quick_search_results($q, $options)
 
   $where_clauses = array();
   $where_clauses[]='i.id IN ('. implode(',', $ids) . ')';
-  if (!empty($options['images_where'))
+  if (!empty($options['images_where']))
   {
     $where_clauses[]='('.$images_where.')';
   }
