511138399b9a14909a0857db135ad5f747273c82
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-14 20:57:54 +0000

    [takeatour] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/trunk@28178 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/ca_ES/plugin.lang.php
@@ -0,0 +1,75 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_stp2'] = 'L\'enllaç és aquí. Cliqueu a Següent per a continuar';
+$lang['first_contact_stp11'] = 'Per tant, aquí podeu seleccionar una o més d\'una fotos';
+$lang['first_contact_stp10'] = 'Aquí hi ha l\'enllaç per accedir directament al vostre carret. Aquest és una eina per als administrador que permet a un conjunt de fotos configurar-les o editar-les en el gestor per lots. Podeu afegir fotos al carret des de la pàgina d\'imatges publicades si heu entrat com a usuari administrador, o utilitzant l\'acció \"Afegeix al carret\" en el gestor per lots. ';
+$lang['first_contact_stp16'] = 'aquí hi ha l\'opció per afegir o eliminar la foto dels àlbums. l\'enllaç es virtual, físicament mai no es mou cap foto .';
+$lang['first_contact_stp15'] = 'En aquesta pàgina podeu editar totes les propietats d\'una foto, per exemple...';
+$lang['first_contact_title14'] = ' ';
+$lang['first_contact_title41'] = ' ';
+$lang['first_contact_title10'] = ' ';
+$lang['first_contact_title12'] = ' ';
+$lang['first_contact_title13'] = ' ';
+$lang['first_contact_title16'] = ' ';
+$lang['first_contact_title17'] = ' ';
+$lang['first_contact_title2'] = ' ';
+$lang['first_contact_title23'] = ' ';
+$lang['first_contact_title24'] = ' ';
+$lang['first_contact_title26'] = ' ';
+$lang['first_contact_title27'] = ' ';
+$lang['first_contact_title30'] = ' ';
+$lang['first_contact_title31'] = ' ';
+$lang['first_contact_title32'] = ' ';
+$lang['first_contact_title40'] = ' ';
+$lang['TAT_descrp'] = 'Benvinguts! Aquí teniu llistats totes les possibles visites per descobrir totes les característiques de la galeria Piwigo.<br/> Escolliu una visita i seguiu les instruccions. Cliqueu Següent/Anterior (o utilitzeu les tecles de desplaçament) per navegar durant la visita. Si aneu a una altra pàgina de l\'administració sereu redirigits cap a la pàgina actual de la visita. Si voleu aturar-ho o no podeu acabar la visita, aneu a <em>Connectors » Feu una visita </em> per finalitzar la visita.';
+$lang['takeatour_configpage'] = 'Feu una visita » Llista de visites';
+$lang['first_contact_stp1'] = 'Hola! Jo seré el vostre guia per fer-vos descobrir Piwigo. Seguiu les meves instruccions, per continuar cliqueu a Següent (o utilitzeu les tecles de desplaçament del teclat) per navegar. Si aneu a una altra pàgina de l\'administració se us redirigirà a la pàgina actual de la visita. Si esteu bloquejats o no podeu acabar la visita, aneu a <em>Connectors » Feu una visita</em> i finalitzeu la visita. <br/> Comencem afegint imatges!';
+$lang['first_contact_title1'] = 'Benvinguts a la vostra galeria Piwigo';
+$lang['first_contact_title11'] = 'Com afegir una foto';
+$lang['first_contact_title18'] = 'Gestiona àlbums';
+$lang['first_contact_title19'] = 'Gestiona àlbums';
+$lang['first_contact_title20'] = 'Gestiona àlbums';
+$lang['first_contact_title21'] = 'Altres pestanyes';
+$lang['first_contact_title22'] = 'Pàgina d\'edició d\'àlbum';
+$lang['first_contact_title25'] = 'Permisos d\'àlbum';
+$lang['first_contact_title28'] = 'Configuració';
+$lang['first_contact_title29'] = 'Configuració » Opcions » General';
+$lang['first_contact_title3'] = 'Afegeix una pàgina de foto';
+$lang['first_contact_title34'] = 'Temes';
+$lang['first_contact_title35'] = 'Temes';
+$lang['first_contact_title36'] = 'Temes';
+$lang['first_contact_title37'] = 'Temes';
+$lang['first_contact_title38'] = 'Connectors';
+$lang['first_contact_title39'] = 'Connectors';
+$lang['first_contact_title4'] = 'Som-hi, afegeix una foto!';
+$lang['first_contact_title5'] = ' ';
+$lang['first_contact_title6'] = ' ';
+$lang['first_contact_title7'] = ' ';
+$lang['first_contact_title8'] = ' ';
+$lang['first_contact_title42'] = 'Idiomes';
+$lang['first_contact_title9'] = 'El gestor per lots';
+$lang['First Contact'] = 'Primer contacte';
+$lang['Start the Tour'] = 'Comença la visita';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Feu una visita i descobriu les característiques de la vostra galeria Piwigo » Aneu a les visites disiponibles.';
+$lang['Visit your Piwigo!'] = 'Visita la teva Piwigo!';
+$lang['first_contact_descrp'] = 'Recomanada per a principiants. Aquesta visita introdueix les característiques bàsiques de Piwigo. Comença amb la gestió d\'imatges  i la gestió dels àlbums i permisos i continua amb la personalització de la galeria utilitzant els temes i els connectors.';
