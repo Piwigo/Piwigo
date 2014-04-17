17788c5fdb7f875149a9539c0fb06065ab377675
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-17 16:48:47 +0000

    [takeatour] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/trunk@28211 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/pt_BR/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_stp14'] = 'Vamos ver agora a página de edição de uma foto. Essa página é acessível a partir daqui, mas também a partir da página de imagem pública quando registrado como administrador. Clique em Editar ou ir para a próxima ao lado';
+$lang['first_contact_stp13'] = 'Você pode editar as descrições e mais a partir do gestor de lote no modo unidade. Mas, por enquanto, vamos ficar no modo global,';
+$lang['first_contact_stp12'] = 'em seguida, editá-los usando uma ação. Mas, por exemplo, você não pode por enquanto editar lote de descrições, uma vez que são únicas';
+$lang['first_contact_stp11'] = 'Assim, você pode selecionar a partir daqui uma ou mais fotos';
+$lang['first_contact_stp10'] = 'Aqui está o link para acessar diretamente a seu guia: esta é uma  ferramenta para administradores facilmente adicionar fotos a um conjunto, a fim de editá-las na Gerência em Bloco. Você pode adicionar fotos ao guia da página de imagem pública quando registrado como um administrador, ou usando a ação \"Adicionar ao guia " na Gerência em Bloco';
+$lang['first_contact_stp1'] = 'Olá! Eu vou ser o seu guia para descobrir o Piwigo. Por favor, siga as minhas instruções e clique em Avançar (ou use as setas do seu teclado para navegar). Se você vai para uma outra página da administração, você será redirecionado para a página atual da Turnê. Se você está preso e não pode terminar a turnê, vai em <em> Plugins »Faça uma turnê </em> para finalizar a turnê. <br> Vamos começar adicionando imagens!';
+$lang['first_contact_descrp'] = 'Recomendado para iniciantes, esta turnê vai apresentá-lo ao Piwigo, e suas características básicas. Ele vai começar adicionando imagens, então gerenciá-las. Aturnê continua com a gestão de álbum e permissões, e termina na personalização usando a configuração, os temas e os plugins.';
+$lang['Visit your Piwigo!'] = 'Visite a sua Piwigo!';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Faça uma turnê e descubra as características de sua galeria Piwigo »Ir para turnês disponíveis';
+$lang['TAT_descrp'] = 'Bem-vindo! Aqui estão listados todas as turnês disponíveis para descobrir as características de sua galeria Piwigo. <br> Inicie uma turnê abaixo e siga as instruções. Clique em Próximo/Anterior (ou use as setas do seu teclado) para navegar durante uma turnê. Durante uma turnê, se você vai a uma outra página da administração, você será redirecionado para a página atual da Turnê. Se você está preso e não puder terminar a turnê, vá para <em> Plugins » Faça uma Turnê </em> isso irá finalizar a turnê.';
+$lang['Start the Tour'] = 'Iniciar a Turnê';
+$lang['First Contact'] = 'Primeiro Contato';
