ecc684d43b674d127b0453304b260abbff25f561
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-17 16:48:56 +0000

    [takeatour] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/trunk@28217 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/it_IT/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_stp4'] = 'Primo scegli un album; creane uno se non c\'è nessun album.';
+$lang['first_contact_stp40'] = 'Qui ci sono io! Non disattivarmi ora, ma puoi vedere che è possibile Disattivare o Ripristinare un plugin attivato.';
+$lang['first_contact_stp41'] = 'Puoi vedere nell\'ultima scheda tutti i plugin disponibili per la tua versione di Piwigo. Ci sono centinaia di plugin qui!';
+$lang['first_contact_stp5'] = 'Quindi clicca per selezionare le foto da inviare dal tuo computer';
+$lang['first_contact_title1'] = 'Benvenuto nella tua galleria Piwigo';
+$lang['first_contact_title11'] = 'Come modificare le foto';
+$lang['first_contact_stp19_b'] = 'Tutti gli album sono chiamati \'album virtuali\'. Qui sono elencati tutti i \'top\' album, quelli nella root della tua galleria.';
+$lang['first_contact_stp32'] = 'Adesso salva le tue modifiche';
+$lang['first_contact_stp29'] = 'Qui, in questa prima pagina, potrai impostare la configurazione essenziale e fondamentale della tua galleria.';
+$lang['first_contact_stp30'] = 'Cominciamo modificando il titolo';
+$lang['first_contact_stp22'] = 'Come per la foto, qui è possibile modificare le proprietà di un album';
+$lang['first_contact_stp24'] = 'Certamente, una delle pagine più importanti è la pagina dei permessi di un album privato. Clicca sulla scheda permessi oppure clicca su avanti';
+$lang['first_contact_stp2'] = 'Il link è qui. Clicca su Avanti per continuare';
+$lang['first_contact_stp16'] = 'qui per aggiungere o rimuovere le foto dagli album. Il link è solo virtuale, nessuna foto verrà veramente spostata.';
+$lang['first_contact_stp13'] = 'Puoi modificare le descrizioni ed altro dal batch manager in modalità unità. Ma per ora, restiamo in modalità globale';
+$lang['first_contact_stp15'] = 'Su questa pagina è possibile modificare tutte le proprietà di una foto, ad esempio...';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Fai un percorso e scopri le caratteristiche della tua galleria Piwigo» Vai alle guide disponibili';
+$lang['First Contact'] = 'Primo Contatto';
+$lang['first_contact_stp11'] = 'E\' possibile selezionare da qui una o più foto';
+$lang['Visit your Piwigo!'] = 'Visita il tuo Piwigo!';
+$lang['Start the Tour'] = 'Inizia il Tour';
