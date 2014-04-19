d1b95439e827abdb1e074ff63ee5f3841cf0cc8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-19 05:28:44 +0000

    [takeatour] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/trunk@28232 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/da_DK/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['TAT_descrp'] = 'Velkommen! Her finder man alle tilgængelige opdagelsesture til dit Piwigo-galleris funktionalitet.<br>Start turen herunder, følg dernæst vejledningen. Klik Næste/Foregående (eller anvend pilene på dit tastatur) for at navigere under turen. Hvis du går til en anden administrationsside under turen, vil du blive stillet videre til turens aktuelle side. Hvis du er kørt fast og ikke kan afslutte turen, vil turen blive afsluttet ved at gå til <em>Plugins » Tag på en tur</em>.';
+$lang['first_contact_stp1'] = 'Hej! Jeg er din guide i udforskning af Piwigo. Følg min vejledning, og klik på Næste (eller benyt pilene på dit tastatur) for at navigere.  Hvis du går til en anden administrationsside, vil du blive stillet videre til turens aktuelle side. Hvis du er kørt fast og ikke kan afslutte turen, vil turen blive afsluttet ved at gå til <em>Plugins » Tag på en tur</em>.<br>Lad os begynde med at tilføje billeder!';
+$lang['first_contact_stp10'] = 'Her er et link, som du direkte kan tilgå din kurv med: Det er et værktøj til administratorer, som gør det let at tilføje billeder til et sæt, hvis man ønsker at redigere dem i Batch Manager. Du kan tilføje billeder til kurven fra den offentlige billedside, når du er logget på som administrator, eller benytte handlingen \"Føj til kurv\" i Batch Manager';
+$lang['first_contact_stp11'] = 'Det vil sige, at du herfra kan vælge et eller flere fotografier';
+$lang['first_contact_stp12'] = 'og dernæst redigere dem ved hjælp af en handling. Men du kan eksempelvis ikke - i øjeblikket - batchredigere beskrivelser, da de for det meste er unikke';
+$lang['first_contact_stp13'] = 'Du kan redigere beskrivelser og mere fra Batch Manager i enhedstilstand. Men lige nu bliver vi i den gloable tilstand';
+$lang['first_contact_stp14'] = 'Du ser et enkelt billedes redigeringsside. Siden er tilgængelig herfra, men også fra den offentlige billedside, når man er logget på som administrator. Klik på Rediger og gå til næste punkt';
+$lang['first_contact_stp15'] = 'På den side kan du redigere alle et billedes egenskaber, eksempelvis ...';
+$lang['first_contact_descrp'] = 'Denne tur, som er anbefalet til begyndere, gør dig bekendt med Piwigo og de grundlæggende funktioner. Den begynder med at tilføje billeder, dernæst håndteringen af dem. Turen fortsætter med håndtering af album og rettigheder, og afslutter med tilpasning hvad hjælp af opsætningen, temaerne og plugin\'erne.';
+$lang['First Contact'] = 'Første Kontakt';
+$lang['Start the Tour'] = 'Start turen';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Tag på en tur og opdag funktionalitet i dit Piwigo-galleri » Gå til tilgængelige ture';
+$lang['Visit your Piwigo!'] = 'Besøg din Piwigo!';
