cf00860d39c5da27667ee0207819c8fdd3ddf745
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-20 05:34:50 +0000

    [takeatour] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/trunk@28235 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/el_GR/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['TAT_descrp'] = 'Καλώς ήρθατε! Εδώ παρατίθενται όλες οι διαθέσιμες περιηγήσεις για να ανακαλύψετε τα χαρακτηριστικά της Piwigo γκαλερί σας.<br>
+Ξεκινήστε με μια περιήγηση και στη συνέχεια ακολουθήστε τις οδηγίες. Κάντε κλικ στο Επόμενο / Προηγούμενο (ή χρησιμοποιήστε τα βελάκια του πληκτρολογίου σας) για να πλοηγηθείτε κατά τη διάρκεια της περιήγησης. Κατά τη διάρκεια μιας περιήγησης, αν πάτε σε μια άλλη σελίδα διαχείρισης, θα πρέπει να μεταφερθείτε στην τρέχουσα σελίδα της περιήγησης. Αν κολλήσετε και δεν μπορείτε να ολοκληρώσετε την περιήγηση, πηγαίνοντας στο <em>Plugins » " Ξεκινήστε την περιήγηση" </em> θα ολοκληρώσει την περιοδεία.';
+$lang['Start the Tour'] = 'Ξεκινήστε την περιήγηση ';
+$lang['First Contact'] = 'Πρώτη Επαφή';
