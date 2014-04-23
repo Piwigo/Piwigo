4f1265288c6f041f0df498d1033fea7ab339469a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-23 15:58:54 +0000

    [takeatour] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/trunk@28255 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/lv_LV/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_descrp'] = 'Ieteicams iesācējiem, šī tūre iepazīstina jūs ar Piwigo, tā pamatfunkcijām. Sākas, pievienojot bildes, tad tās pārvalda. Tūre turpinās ar albumu vadību un atļaujām, beidzas ar pielāgošanu, izmantojot konfigurāciju, tēmas un spraudņus.';
+$lang['first_contact_stp29'] = 'Šeit, pirmajā lapā, varat iestatīt svarīgāko un veikt savas galerijas pamatkonfigurāciju.';
+$lang['first_contact_title1'] = 'Laipni lūgti savā Piwigo galerijā';
+$lang['first_contact_title22'] = 'Albūma Izdevuma Lapa';
+$lang['first_contact_title25'] = 'Albūma Atļauja';
+$lang['first_contact_title21'] = 'Citas Cilnes';
+$lang['first_contact_title15'] = 'Attēla izdevuma lapa';
+$lang['first_contact_title19'] = 'Pārvaldīt Albūmus';
+$lang['first_contact_title20'] = 'Pārvaldīt Albūmus';
+$lang['first_contact_title18'] = 'Pārvaldīt albūmus';
+$lang['first_contact_title11'] = 'Kā rediģēt attēlus';
+$lang['takeatour_configpage'] = 'Izvēlies Ekskursu > Ekskursu saraksts';
+$lang['first_contact_title9'] = 'Virknes darbību pārvaldnieks';
+$lang['first_contact_title43'] = 'Pavadīts jauks laiks';
+$lang['first_contact_title42'] = 'Valodas';
+$lang['first_contact_title29'] = 'Konfigurācija->Opcijas->Vispārēji';
+$lang['first_contact_title35'] = 'Tēmas';
+$lang['first_contact_title36'] = 'Tēmas';
+$lang['first_contact_title37'] = 'Tēmas';
+$lang['first_contact_title38'] = 'Spraudņi';
+$lang['first_contact_title39'] = 'Spraudņi';
+$lang['first_contact_title4'] = 'Pievienosim foto!';
+$lang['first_contact_title34'] = 'Tēmas';
+$lang['first_contact_title33'] = 'Viesa iestatījumi';
+$lang['first_contact_title3'] = 'Pievienot Foto Lapu';
+$lang['first_contact_title28'] = 'Konfigurācija';
+$lang['first_contact_stp5'] = 'Tad klikšķiniet, lai izvēlētos attēlus, ko sūtīt no sava datora';
+$lang['first_contact_stp4'] = 'Vispirms izvēlaties albūmu, ja nav izveidojiet to.';
+$lang['first_contact_stp30'] = 'Sāksim nomainot nosaukumu';
+$lang['first_contact_stp32'] = 'Tagad saglabājiet veiktās izmaiņas';
+$lang['first_contact_stp2'] = 'Saite ir šeit. Spiediet Next lai turpinātu';
+$lang['first_contact_stp11'] = 'Šeit Jūs varat izvēlēties vienu vai vairākus attēlus';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Izmantojiet ekskursu un atklājiet savas Piwigo galerijas iespējas, Ejiet uz pieejamajiem ekskursiem';
+$lang['Visit your Piwigo!'] = 'Apmeklējiet savu Piwigo!';
+$lang['Start the Tour'] = 'Sākt Iepazīšanos';
+$lang['First Contact'] = 'Pirmā Iepazīšanās';
