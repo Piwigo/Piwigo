2771cd450140b9cceb414609f3ec52596811e429
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-26 15:26:00 +0000

    [takeatour] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/trunk@28267 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/es_ES/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['First Contact'] = 'Primer contacto';
+$lang['Start the Tour'] = 'Empezar el tour';
+$lang['TAT_descrp'] = '¡Bienvenido! Aquí aparecen listados todos los tours disponibles para descubrir las funcionalidades de tu galería Piwigo. <br>Selecciona un tour aquí abajo y sigue las instrucciones. Haz click en Siguiente/Anterior (o usa las flechas de tu teclado) para navegar durante el tour. Durante el tour, si vas a otra página de la administración, se te redireccionará a la actual página del tour. Si te quedas atascado y no consigues acabar el tour, accediendo a <em>Pulgins » Empezar un tour</em> acabará el tour.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Empieza un tour y descubre las posibilidades de tu galería Piwigo » Accede a los tours disponibles';
+$lang['Visit your Piwigo!'] = '¡Visita tu Piwigo!';
