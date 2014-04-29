6fabe0d268280f54ab3463dac6ef97cce5626a8d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-29 04:45:20 +0000

    [takeatour] Update de_DE, thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/trunk@28291 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/de_DE/plugin.lang.php
@@ -0,0 +1,80 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_stp22'] = 'Hier kannst du die Eigenschaften eines Albums bearbeiten, wie bei den Fotos';
+$lang['first_contact_title10'] = ' ';
+$lang['first_contact_title12'] = ' ';
+$lang['first_contact_title13'] = ' ';
+$lang['first_contact_title41'] = ' ';
+$lang['first_contact_title42'] = 'Sprachen';
+$lang['first_contact_title43'] = 'Es war eine tolle Zeit';
+$lang['first_contact_title5'] = ' ';
+$lang['first_contact_title6'] = ' ';
+$lang['first_contact_title7'] = ' ';
+$lang['first_contact_title8'] = ' ';
+$lang['first_contact_title9'] = 'Der Batch Manager';
+$lang['takeatour_configpage'] = 'Mache eine Tour >> Liste der Touren';
+$lang['first_contact_title14'] = ' ';
+$lang['first_contact_title16'] = ' ';
+$lang['first_contact_title17'] = ' ';
+$lang['first_contact_title2'] = ' ';
+$lang['first_contact_title23'] = ' ';
+$lang['first_contact_title24'] = ' ';
+$lang['first_contact_title26'] = ' ';
+$lang['first_contact_title27'] = ' ';
+$lang['first_contact_title30'] = ' ';
+$lang['first_contact_title31'] = ' ';
+$lang['first_contact_title32'] = ' ';
+$lang['first_contact_title33'] = 'Gäste Einstellung';
+$lang['first_contact_title34'] = 'Themen';
+$lang['first_contact_title35'] = 'Themen';
+$lang['first_contact_title36'] = 'Themen';
+$lang['first_contact_title37'] = 'Themen';
+$lang['first_contact_title38'] = 'Plugins';
+$lang['first_contact_title39'] = 'Plugins';
+$lang['first_contact_title4'] = 'Lass uns ein Foto hin zu fügen';
+$lang['first_contact_title40'] = ' ';
+$lang['first_contact_title15'] = 'Die Bearbeiten Seite eines Fotos';
+$lang['first_contact_title18'] = 'Alben verwalten';
+$lang['first_contact_title19'] = 'Alben verwalten';
+$lang['first_contact_title20'] = 'Alben verwalten';
+$lang['first_contact_title21'] = 'Andere Tabs';
+$lang['first_contact_title22'] = 'Album bearbeiten Seite';
+$lang['first_contact_title25'] = 'Album Berechtigung';
+$lang['first_contact_title28'] = 'Konfiguration';
+$lang['first_contact_title29'] = 'Konfiguration -> Optionen -> Allgemein';
+$lang['first_contact_title3'] = 'Foto Seite hin zu fügen';
+$lang['first_contact_title11'] = 'Wie bearbeite ich ein Foto';
+$lang['first_contact_stp8'] = 'Lass sie uns nun bearbeiten! Klicke auf den Link. Falls du ihn nicht siehst, klicke auf "Prev" um wenigstens 2 Fotos hin zu zu fügen';
+$lang['first_contact_title1'] = 'Willkommen in deiner Piwigo Galerie';
+$lang['first_contact_stp29'] = 'Hier auf dieser ersten Seite stellst du die erforderlichen Basic Konfiguration deiner Galerie ein';
+$lang['first_contact_stp30'] = 'Las uns mit dem ändern des Titels beginnen';
+$lang['first_contact_stp32'] = 'Nun speichere deine Änderungen';
+$lang['first_contact_stp4'] = 'Zuerst wähle ein Album; erstelle eins falls noch nicht vorhanden';
+$lang['first_contact_stp5'] = 'Dann klicke den Button um die Fotos zu wählen und von deinem Computer zu senden';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Mache eine Tour und entdecke die Funktionen deiner Piwigo Galerie >> Gehe zu den Verfügbaren Touren ';
+$lang['First Contact'] = 'Erster Kontakt';
+$lang['Start the Tour'] = 'Tour starten';
+$lang['Visit your Piwigo!'] = 'Besuche dein Piwigo!';
+$lang['first_contact_stp11'] = 'So, von hier kannst du ein oder mehrere Fotos auswählen';
+$lang['first_contact_stp15'] = 'Hier zum Beispiel, kann man alle Eigenschaften eines Fotos bearbeiten';
+$lang['first_contact_stp2'] = 'Der Link ist hier. Klicke "Next" zum weitermachen';
