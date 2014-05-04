1b3b7980e357df2c799eadd3ded26540da6fa35b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-04 05:23:08 +0000

    [trunk_public] Update he_IL, thanks to : ADIRBITON
    git-svn-id: http://piwigo.org/svn/trunk@28334 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/he_IL/common.lang.php
+++ language/he_IL/common.lang.php
@@ -419,4 +419,4 @@ $lang['Go back to the album'] = 'חזור לאלבום';
 $lang['First Page'] = 'עמוד ראשון';
 $lang['Email'] = 'אימייל';
 $lang['%d photos per page'] = '%d תמונות בעמוד';
-?>
+$lang['No results for'] = 'לא נמצאו תוצאות עבור';
