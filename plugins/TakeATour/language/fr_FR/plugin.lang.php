e205063e88001397a0cf16a1e15b1dbb7df501f5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-12 17:55:39 +0000

    [takeatour] Update fr_FR, thanks to : laurent.duretz
    git-svn-id: http://piwigo.org/svn/trunk@28446 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/fr_FR/plugin.lang.php
+++ plugins/TakeATour/language/fr_FR/plugin.lang.php
@@ -110,3 +110,22 @@ $lang['2_7_0_title6'] = ' ';
 $lang['2_7_0_title7'] = ' ';
 $lang['2_7_0_title8'] = ' ';
 $lang['2_7_0_title9'] = ' ';
+$lang['2_7_0_stp3'] = 'La recherche rapide a été retravaillée :<br/>
+- ajout de l\'opérateur OR<br/>
+- possibilité de grouper en utilisant les parenthèses ()<br/>
+- ajout de la recherche par ressource comme :<br/>
+ * #snow - recherche les tags \'snow\'<br/>
+ * ratio:0.9..1.1 - recherche les photos avec un ration largeur/hauteur compris entre 0.9 and 1.1 (a peu près carrées)<br/>
+ * hits:>10 - recherche les photos avec un nombre de visites >10<br/>
+ * size:>10mp - recherche les photos d\'un poids > 10 méga pixels<br/>
+ * file:.jpg - seulement les images jpg<br/>
+- utilisation des formes singulier/pluriel (Anglais/Français pour le moment)<br/>';
+$lang['2_7_0_stp2'] = 'Comme vous pouvez le voir, un nouveau plugin a été ajouté par défaut : Take a Tour. Ce plugin vous aide à découvrir votre Piwigo. Ici sont listées les visites disponibles.<br/>Cette page est accessible dans <em>Plugins » Take A Tour</em>';
+$lang['2_7_0_title2'] = 'Première nouvelle fonctionalité';
+$lang['2_7_0_title3'] = 'Recherche rapide';
+$lang['2_7_0_title1'] = 'Bienvenue sur Piwigo 2.7';
+$lang['2_7_0_title11'] = 'A bientôt !';
+$lang['2_7_0_descrp'] = 'Découvrez ce qui est nouveau avec Piwigo 2.7';
+$lang['2.7 Tour'] = 'Visite 2.7';
+$lang['2_7_0_stp1'] = 'Salut ! Je serai votre guide pour découvrir Piwigo. Merci de suivre mes instructions et de cliquer sur Suivant (ou d\'utiliser les flèches du clavier) pour naviguer. Si vous allez sur une autre page, vous serez redirigé vers la page courante de la visite.';
+$lang['2_7_0_stp11'] = 'Cette visite est terminée.<p style="text-align:center">Profitez de Piwigo 2.7 !</p>Si vous aimez Piwigo et voulez nous soutenir, vous pouvez contribuer en nous aidant sur les traductions, dons, etc. <a href="http://piwigo.org/basics/contribute" target="_blank">Cliquez ici pour nous soutenir</a>';
