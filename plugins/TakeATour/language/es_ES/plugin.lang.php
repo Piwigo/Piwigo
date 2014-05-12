0d92f19c79c3c276b3e7943ec059421dd914db07
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-12 10:04:56 +0000

    [takeatour] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/trunk@28442 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/es_ES/plugin.lang.php
+++ plugins/TakeATour/language/es_ES/plugin.lang.php
@@ -96,3 +96,22 @@ $lang['first_contact_title10'] = ' ';
 $lang['first_contact_title12'] = ' ';
 $lang['first_contact_title5'] = ' ';
 $lang['first_contact_title6'] = ' ';
+$lang['2_7_0_stp3'] = 'La búsqueda rápida se ha rediseñado:<br>
+- añade el operador OR<br>
+- permite la agrupación usando paréntesis ()<br>
+- añade un hueco de separación como:<br>
+*#nieve - busca solamente etiquetas con nieve<br>
+*ratio:0.9..11 - busca fotos con la relación de aspecto entre 0.9 y 1.1 (básicamente cuadrados)<br>
+*hits:>10 - busca fotos con un número de visitas mayor a 10<br>
+*size:>10mp - busca fotos con un alto*ancho mayor a 10 megapixels<br>
+*file:.jpg - solo fotos jpg<br>
+- uso de etiquetas singular/plural (en inglés y francés solo por ahora)<br>';
+$lang['2_7_0_stp2'] = 'Como puedes ver, un nuevo plugin por defecto se ha añadido: Hacer un tour. Este plugin te ayuda a descubrir tu Piwigo. Aquí aparecen listados todos los tours disponibles. <br> Esta página está disponible en <em>Plugins » Hacer un tour</em>';
+$lang['2_7_0_stp11'] = 'El tour se ha acabado. <p style="text-align:center">¡Disfruta tu Piwigo 2.7!</p> Si te gusta y quieres ayudarnos, puedes contribuir ayudando con las traducciones, donando, etc.  <a href="http://piwigo.org/basics/contribute" target="_blank">Haz click aquí para contribuir</a>';
+$lang['2_7_0_stp1'] = '¡Hola! Voy a ser tu guía para descubrir Piwigo. Por favor, sigue mis instrucciones y haz click en Siguiente (o usa las flechas de tu teclado) para navegar. Si vas a otra página, se te redirigirá a la página actual del tour.';
+$lang['2.7 Tour'] = '2.7 Tour';
+$lang['2_7_0_descrp'] = 'Descubre las novedades de Piwigo 2.7';
+$lang['2_7_0_title1'] = 'Bienvenido a Piwigo 2.7';
+$lang['2_7_0_title11'] = '¡Nos vemos pronto!';
+$lang['2_7_0_title2'] = 'Primera nueva funcionalidad';
+$lang['2_7_0_title3'] = 'Búsqueda rápida';
