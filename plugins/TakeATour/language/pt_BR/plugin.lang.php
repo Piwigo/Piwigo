65f164c6cc7bbe72a7a72db9a8722a5837d60bd8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-15 05:19:59 +0000

    [takeatour] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/trunk@28480 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/pt_BR/plugin.lang.php
+++ plugins/TakeATour/language/pt_BR/plugin.lang.php
@@ -128,3 +128,25 @@ $lang['2_7_0_title10'] = ' ';
 $lang['2_7_0_title4'] = ' ';
 $lang['2_7_0_title5'] = ' ';
 $lang['2_7_0_stp10'] = ' ';
+$lang['2_7_0_stp11'] = 'Este tour é longo. <p Style="text-align:center"> Aproveite o seu Piwigo 2.7! </P> Se você gosta Piwigo e quer nos ajudar, você pode contribuir, ajudando em traduções, doando, etc <a href="http://piwigo.org/basics/contribute" target="_blank">Clique aqui para nos apoiar</a>';
+$lang['2_7_0_stp2'] = 'Como você pode ver, um novo plugin padrão foi adicionado: Faça um tour. Este plugin ajuda você a descobrir o seu Piwigo. Aqui é onde estão listados todos os ptours disponíveis. <br> Esta página está disponível em Plugins <em> »Faça um tour </em>';
+$lang['2_7_0_stp3'] = 'A busca rápida foi retrabalhada: <br>
+- Adicionar operador OU <br>
+- Permitir o agrupamento usando parenteses () <br>
+- Adicionar a pesquisa como namespace : <br>
+* #neve - Pesquisas apenas nomes e marcas para neve <br>
+* Relação: 0.9 .. 1.1 - procura fotos com relação largura/altura entre 0,9 e 1,1 (quase quadrados) <br>
+* Visitas:>10 - procura fotos com o número de visitas >10 <br>
+* Tamanho:> 10MP - procura fotos nas quais a largura * altura >10 mega pixels <br>
+* Arquivo:. jpg - apenas imagens jpg <br>
+- Usar inflexões singular/plural (Inglês/francês só por agora) <br>';
+$lang['Prev'] = 'Anterior';
+$lang['2_7_0_title1'] = 'Bem-vindo ao Piwigo 2.7';
+$lang['2_7_0_title11'] = 'Vejo você em breve!';
+$lang['2_7_0_title2'] = 'Primeira novidade';
+$lang['2_7_0_title3'] = 'Pesquisa rápida';
+$lang['End tour'] = 'Fim do tour ';
+$lang['Next '] = 'Próxima';
+$lang['2_7_0_stp1'] = 'Olá! Eu vou ser o seu guia para descobrir o Piwigo. Por favor, siga as minhas instruções e clique em Avançar (ou use as setas do seu teclado para navegar). Se você vai para uma outra página, você será redirecionado para a página atual do Tour.';
+$lang['2_7_0_descrp'] = 'Descubra o que há de novo no Piwigo 2.7';
+$lang['2.7 Tour'] = '2.7 Tour';
