39fc0a9f9a5248783eebea7bd05d45d5318a3409
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-27 05:22:53 +0000

    [takeatour] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/trunk@28544 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/pt_PT/plugin.lang.php
+++ plugins/TakeATour/language/pt_PT/plugin.lang.php
@@ -67,3 +67,59 @@ $lang['2_7_0_title8'] = '
 $lang['2_7_0_title9'] = '
 ';
 $lang['2_7_0_stp10'] = ' ';
+$lang['first_contact_title1'] = 'Benvindo(a) à sua galeria';
+$lang['first_contact_title11'] = 'Como editar fotos';
+$lang['first_contact_title15'] = 'A página de edição de uma imagem';
+$lang['first_contact_title18'] = 'Administrar Albuns';
+$lang['first_contact_title19'] = 'Administrar Albuns';
+$lang['first_contact_title20'] = 'Administrar Albuns';
+$lang['first_contact_title21'] = 'outras abas';
+$lang['first_contact_title22'] = 'Página de edição do Album';
+$lang['first_contact_title25'] = 'Permissão do album';
+$lang['first_contact_title28'] = 'Configuração';
+$lang['first_contact_title29'] = 'Configuração->Opções->Geral';
+$lang['first_contact_title3'] = 'Adicionar página da foto';
+$lang['first_contact_title33'] = 'Definições de visitantes';
+$lang['first_contact_title34'] = 'Temas';
+$lang['first_contact_title35'] = 'Temas';
+$lang['first_contact_title36'] = 'Temas';
+$lang['first_contact_title37'] = 'Temas';
+$lang['first_contact_title38'] = 'Extensões';
+$lang['first_contact_title39'] = 'Extensões';
+$lang['first_contact_title4'] = 'Permitir adicionar foto!';
+$lang['first_contact_title42'] = 'Idiomas';
+$lang['first_contact_title43'] = 'Tem sido um momento aprazível';
+$lang['first_contact_title9'] = 'Administrar miniaturização';
+$lang['takeatour_configpage'] = 'Faça Uma Viagem » Lista de demonstrações';
+$lang['2_7_0_stp1'] = 'Olá! Eu vou ser o seu guia para descobrir Piwigo. Por favor, siga as minhas instruções e clique em Avançar (ou use as setas do seu teclado para navegar). Se vai para uma outra página,  será redirecionado(a) para a página atual da Apresentação.';
+$lang['2_7_0_stp11'] = 'Esta apresentação terminou. <p Style="text-align:center"> Aprecie o seu Piwigo 2.7!</p> Se você gosta Piwigo e nos quer ajudar, pode contribuir, colaborando nas traduções, doando, etc <a href="http://piwigo.org/basics/contribute"target="_blank"> Clique aqui para colaborar</a>';
+$lang['2_7_0_stp2'] = 'Como você pode constatar, uma nova extensão padrão foi adicionada: Faça uma viagem. Esta extenção ajudá-lo(a)-á a descobrir o seu Piwigo. Aqui é onde estão listados todas as possibilidades disponíveis. <br>Esta página está disponível nas extensões <em>Faça uma viagem</em>';
+$lang['2_7_0_stp3'] = 'A busca rápida foi reformulada:<br> 
+- Adicionar OU operador<br> 
+- Permitir o agrupamento usando parêntesis ()<br> 
+- Adicionar espaço de pesquisa como:<br> 
+  *#Neve - Pesquisar apenas etiquetas para neve<br> 
+  * Rácio:0.9..1.1 - procurar fotos com rácio largura/altura entre 0,9 e 1,1 (quase quadradas)<br> 
+  * Cliquess:>10 - procura fotos com o número de cliques>10<br> 
+  * Tamanho:>10MP - procura fotos nas quais a largura*altura>10 mega pixels<br> 
+  * Arquivo:.Jpg - apenas imagens jpg<br> 
+- Usar inflexões singular/plural (Inglês/francês só por agora) <br>';
+$lang['first_contact_stp30'] = 'Comecemos por alterar o título';
+$lang['first_contact_stp32'] = 'Agora salve as suas alterações';
+$lang['first_contact_stp2'] = 'O link é aqui. Clique em avançar para continuar';
+$lang['first_contact_stp22'] = 'Como para uma foto, aqui pode editar as propriedades de um album';
+$lang['2_7_0_title2'] = 'Primeira nova funcionalidade';
+$lang['2_7_0_title3'] = 'Busca rápida';
+$lang['first_contact_stp11'] = 'Então pode selecionar daqui uma ou mais fotos';
+$lang['first_contact_stp15'] = 'Nesta página pode editar todas as propriedades de uma foto, por exemplo...';
+$lang['first_contact_stp16'] = 'aqui para adicionar ou remover uma foto dos albuns. O link é virtual, nunca serão movidas fotos fisicamente.';
+$lang['2.7 Tour'] = 'Apresentação do 2.7';
+$lang['2_7_0_descrp'] = 'Descobre o que há de novo no Piwigo 2.7';
+$lang['2_7_0_title1'] = 'Bem vindo(a) ao Piwwigo 2.7';
+$lang['2_7_0_title11'] = 'Até breve!';
+$lang['End tour'] = 'Fim da apresentação';
+$lang['First Contact'] = 'Primeiro contacto';
+$lang['Next '] = 'Próximo';
+$lang['Prev'] = 'Anterior';
+$lang['Start the Tour'] = 'Iniciar apresentação';
+$lang['Visit your Piwigo!'] = 'Visitar o seu Piwigo';
