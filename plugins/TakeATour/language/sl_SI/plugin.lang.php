f98edc7a2e423bed534977107e4cb892180cb946
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-01 19:15:20 +0000

    [takeatour] Update sl_SI, thanks to : newron
    git-svn-id: http://piwigo.org/svn/trunk@28584 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/sl_SI/plugin.lang.php
+++ plugins/TakeATour/language/sl_SI/plugin.lang.php
@@ -68,3 +68,30 @@ $lang['2_7_0_title8'] = '
 ';
 $lang['2_7_0_title9'] = '
 ';
+$lang['TAT_descrp'] = '
+Dobrodošli! Tu so navedene vse razpoložljive turneje, da odkrijete značilnosti vašege galerije Piwigo. <br> Naložite turnejo spodaj, nato pa sledite navodilom. Kliknite Naprej / Nazaj (ali uporabite puščice tipkovnici) za navigacijo med turnejo. Med turnejo, če greste na drugi strani uprave, boste preusmerjeni na trenutn stran turneje. Če ste obtičali in ne more končati turneje, pojdite na<em> Dodatki » Pričnite Turnejo </ em> s tem boste končali turnejo.';
+$lang['Start the Tour'] = 'Prični z Turnejo';
+$lang['Prev'] = 'Nazaj';
+$lang['Next '] = 'Naprej';
+$lang['First Contact'] = 'Prvi Kontakt';
+$lang['End tour'] = 'Konec turneje';
+$lang['2_7_0_title3'] = 'Hitro iskanje';
+$lang['2_7_0_title2'] = 'Prva nova funkcija';
+$lang['2_7_0_title11'] = 'Vidimo se kmalu!';
+$lang['2_7_0_title1'] = 'Dobrodošli v Piwigo 2,7';
+$lang['2_7_0_stp3'] = 'Hitro iskanje popravkom: <br>
+- Dodajanje ali upravljaj <br>
+- Omogoči združevanje z uporabo oklepajev () <br>
+- Dodajanje iskanje z presledkom, kot so: <br>
+  * # Sneg - Išče samo imena tag za sneg <br>
+  * Razmerje: 0.9 .. 1.1 - išče fotografij z razmerjem med širino / višino med 0,9 in 1,1 (skoraj kvadratov) <br>
+  * Pogledov:> 10 - išče fotografij s številom obiskov> 10 obiskov
+  * Velikost:> 10MP - poišče fotografije, kjer je širina * višina> 10 milijonov slikovnih točk, <br>
+  * Slika:. Jpg - samo jpg slike <br>
+- Uporaba ednina / množina trendov, (angleščina / francoščina samo za zdaj) <br>';
+$lang['2_7_0_stp2'] = 'Kot lahko vidite, je bila dodan novi privzet plugin: Oglejte si predstavitev. Ta plugin vam pomaga, da odkrijete svoj Piwigo. Tu so navedene vse razpoložljive turneje. <br> Ta stran je na voljo v <em> Plugininih »Začni turnejo </em>';
+$lang['2_7_0_stp11'] = '
+Ta turneja je končana. <p style="text-align:center"> Uživajte v Piwigo 2,7! </p> Če vam je všeč Piwigo in želite, da nas podpirate, lahko prispevate s pomočjo za prevode, donacijami, itd. <a href= "http://piwigo.org/basics/contribute" target = "_blank"> Kliknite tukaj, da nas podpirate </a>';
+$lang['2.7 Tour'] = '2.7 Turneja';
+$lang['2_7_0_stp1'] = 'Pozdravljeni! Jaz bom vaš vodič, da odkrijete Piwigo. Prosimo, da sledite mojim navodilom, in kliknite Naprej (ali uporabite puščice tipkovnici) za navigacijo. Če greste na drugo stran, boste preusmerjeni na trenutni strani Tura.';
+$lang['2_7_0_descrp'] = 'Odkrijte kaj je novega v Piwigo 2,7';
