0cba9b4b644ccbbec15f968751b942d1f4080724
ddtddt <ddtddt@piwigo.org>
2014-06-02 10:29:50 +0000

    [Trunk] - language_ - add Gujarati - ગુજરાતી[IN] language for translate tool Thanks to huz_akh for first translation
    git-svn-id: http://piwigo.org/svn/trunk@28590 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gu_IN/common.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: ગુજરાતી[IN]
+Version: 2.6.0
+Language URI: http://piwigo.org/ext/extension_view.php?eid=767
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+$lang_info['language_name'] = 'ગુજરાતી';
+$lang_info['country'] = 'હિન્દુસ્તાન';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'gu';
+$lang_info['zero_plural'] = true;
+
+$lang['Date'] = 'તારીખ';
+$lang['day'][0] = 'રવિવાર';
+$lang['day'][1] = 'સોમવાર';
+$lang['day'][2] = 'મંગળવાર';
+$lang['day'][3] = 'બુધવાર';
+$lang['day'][4] = 'ગુરુવાર';
+$lang['day'][5] = 'શુક્રવાર';
+$lang['day'][6] = 'શનિવાર';
+$lang['month'][10] = 'ઑક્ટ્બર';
+$lang['month'][11] = 'નવેમ્બર';
+$lang['month'][12] = 'ડિસેમ્બર';
+$lang['month'][1] = 'જાન્યુઆરી';
+$lang['month'][2] = 'ફેબ્રુઆરી';
+$lang['month'][3] = 'માર્ચ';
+$lang['month'][4] = 'એપ્રિલ';
+$lang['month'][5] = 'મે';
+$lang['month'][6] = 'જૂન';
+$lang['month'][7] = 'જુલાઈ';
+$lang['month'][8] = 'ઑગસ્ટ';
+$lang['month'][9] = 'સપ્ટેમ્બર';
+?>
