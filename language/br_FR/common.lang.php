ceaa7de36dd6772f9fa98741cf1101f9da2bf8ea
ddtddt <ddtddt@piwigo.org>
2014-06-03 10:29:08 +0000

    [Trunk] - Language - prepare for 2.6.3
    git-svn-id: http://piwigo.org/svn/trunk@28619 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/br_FR/common.lang.php
+++ language/br_FR/common.lang.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: Brezhoneg [FR]
-Version: 2.6.1
+Version: 2.6.2
 Language URI: http://piwigo.org/ext/extension_view.php?eid=698
 Author: Piwigo team
 Author URI: http://piwigo.org
