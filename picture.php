55619222fbf0032b94630c9a86e793476040db95
mistic100 <mistic@piwigo.org>
2014-06-05 07:25:28 +0000

    don't update images.lastmodified field when incrementing hits count
    git-svn-id: http://piwigo.org/svn/trunk@28640 68402e56-0260-453c-a942-63ccdbb3a9ee

--- picture.php
+++ picture.php
@@ -436,10 +436,11 @@ else
 // don't increment if adding a comment
 if (trigger_change('allow_increment_element_hit_count', $inc_hit_count, $page['image_id'] ) )
 {
+  // avoiding auto update of "lastmodified" field
   $query = '
 UPDATE
   '.IMAGES_TABLE.'
-  SET hit = hit+1
+  SET hit = hit+1, lastmodified = lastmodified
   WHERE id = '.$page['image_id'].'
 ;';
   pwg_query($query);
