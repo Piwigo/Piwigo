963bcd33dc7915054b17d2ab157558f910a2264f
ddtddt <ddtddt@piwigo.org>
2014-06-10 19:18:30 +0000

    [2.6] - themes -Language - prepare for 2.6.3
    git-svn-id: http://piwigo.org/svn/branches/2.6@28670 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themes/elegant/language/he_IL/theme.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Panels options'] = 'אפשרויות לוחות';
+$lang['Elegant, Configuration Page'] = 'דף תצורה, מהודר';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'בחר את ברירת המחדל עבור כל לוח, או בטל את ההנפשה:';
+$lang['Disable the animation'] = 'בטל את ההנפשות';
+$lang['Comments Panel'] = 'לוח תגובות';
+$lang['Main Menu Panel'] = 'לוח תפריט ראשי';
+$lang['Photo Description Panel'] = 'לוח תיאור תמונה';
+$lang['Displayed'] = 'מוצג';
+$lang['Hidden'] = 'מוסתר';
