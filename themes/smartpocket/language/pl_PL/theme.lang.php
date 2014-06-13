1157c3717341410a389557ae217400cf12b0288d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-13 15:29:10 +0000

    [smartpocket] Update pl_PL, thanks to : billy88
    git-svn-id: http://piwigo.org/svn/trunk@28683 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/smartpocket/language/pl_PL/theme.lang.php
+++ themes/smartpocket/language/pl_PL/theme.lang.php
@@ -24,4 +24,4 @@ $lang['Autohide the bar of the slideshow'] = 'Ukrywaj belkę pokazu slajdów';
 $lang['Loop the slideshow'] = 'Zapętl pokaz slajdów';
 $lang['More Information'] = 'Więcej informacji';
 $lang['Slideshow Options'] = 'Opcje pokazu slajdów';
-?>
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, strona konfiguracyjna';
