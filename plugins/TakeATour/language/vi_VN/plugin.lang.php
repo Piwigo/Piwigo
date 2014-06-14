4edd40080c17bc8217ec2b77da0b9ae7ee430c04
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-14 05:18:37 +0000

    [takeatour] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/trunk@28698 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/vi_VN/plugin.lang.php
+++ plugins/TakeATour/language/vi_VN/plugin.lang.php
@@ -68,3 +68,4 @@ $lang['2_7_0_title2'] = 'Tính năng mới đầu tiên';
 $lang['2_7_0_title3'] = 'Tìm nhanh';
 $lang['Prev'] = 'Trước đó';
 $lang['Next '] = 'Kế tiếp';
+$lang['2_7_0_stp1'] = 'Xin chào! Tôi sẽ là hướng dẫn viên để giúp bạn khám phá Piwigo. Vui lòng làm theo những hướng dẫn của tôi và ấn vào Tiếp tục (hoặc dùng các phím mũi tên trên bàn phím) để đi tiếp. Nếu đi đến trang khác, bạn sẽ bị quay lại trang hiện tại của Tour';
