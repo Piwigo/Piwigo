42c5f8bd14f8e4ebc8a8ddf715f24dc93190d1a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-21 05:31:10 +0000

    [takeatour] Update sv_SE, thanks to : Blytung
    git-svn-id: http://piwigo.org/svn/trunk@28754 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/sv_SE/plugin.lang.php
+++ plugins/TakeATour/language/sv_SE/plugin.lang.php
@@ -54,3 +54,8 @@ $lang['first_contact_title5'] = ' ';
 $lang['first_contact_title6'] = ' ';
 $lang['first_contact_title7'] = ' ';
 $lang['first_contact_title8'] = ' ';
+$lang['2.7 Tour'] = '2.7 Översikt';
+$lang['2_7_0_descrp'] = 'Upptäck nyheterna i Piwigo 2.7';
+$lang['2_7_0_stp1'] = 'Hej! Jag kommer att vara din guide på upptäcktsfärden genom Piwigo. Följ mina instruktioner, och klicka på Nästa eller använd piltangenterna för att navigera. Om du går vilse till någon annan sida, kommer du att omdirigeras till den aktuella sidan i den här översikten.';
+$lang['2_7_0_stp11'] = 'Resan genom denna översikt är över.<p style="text-align:center">Ha det kul med Piwigo 2.7!</p>Om du uppskattar Piwigo och vill ge ditt stöd, så kan du bidra genom översättningar, donationer, etc. <a href="http://piwigo.org/basics/contribute" target="_blank">Klicka här för att ge ditt bidrag</a>';
+$lang['2_7_0_stp2'] = 'Som du kan se har ett nytt standardplugin lagts till: Take A Tour. Det hjälper dig att utforska möjligheterna med Piwigo. Just här finns alla tillgängliga guider listade.<br>Denna sida är tillgänglig under <em>Insticksmoduler» Take A Tour</em>';
