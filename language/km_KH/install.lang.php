d13f764c2d022a4b84b9f1e4d2e2166eeaea67b0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-25 10:08:13 +0000

    [trunk_install] Update km_KH, thanks to : osify
    git-svn-id: http://piwigo.org/svn/trunk@28789 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/km_KH/install.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Password ']['confirm'] = 'ពាក្យ​សម្ងាត់​ [បញ្ជាក់]';
+$lang['PHP 5 is required'] = 'PHP 5 ត្រូវ​ការជា​ចាំបាច់';
+$lang['Database configuration'] = 'ការ​កំណត់​លើ​មូលដ្ឋាន​ទិន្នន័យ';
+$lang['Download the config file'] = 'ទាញ​យក​ឯកសារកំណត់';
+$lang['Admin configuration'] = 'ការ​កំណត់​ផ្នែក​រដ្ឋបាល';
+$lang['Can\'t connect to server'] = 'មិន​អាចភ្ជាប់​ទៅ​ម៉ាស៊ីន​មេ​បាន​ទេ';
+$lang['Basic configuration'] = 'ការ​កំណត់ត្រឹម​មូលដ្ឋាន';
+$lang['Installation'] = 'ការបញ្ចូល';
+$lang['Host'] = 'ម៉ាស៊ីនមេ';
+$lang['Default gallery language'] = 'ភាសាវិចិត្រសាលលំនាំដើម';
+$lang['Database table prefix'] = 'បុព្វបទតារាងមូលដ្ឋាន​ទិន្ន​ន័យ';
+$lang['Database name'] = 'ឈ្មោះ​មូលដ្ឋាន​ទិន្ន​ន័យ';
