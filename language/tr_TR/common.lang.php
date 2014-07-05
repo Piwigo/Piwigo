1b486d887a2a962f1f0a727269ad47c723cd7c3f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-05 19:03:31 +0000

    [trunk_public] Update tr_TR, thanks to : Nakre
    git-svn-id: http://piwigo.org/svn/trunk@28961 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/common.lang.php
+++ language/tr_TR/common.lang.php
@@ -422,3 +422,6 @@ $lang['No results for'] = 'Sonuç bulunamadı:';
 $lang['Photo description'] = 'Fotoğraf açıklamaları';
 $lang['Photo title'] = 'Fotoğraf başlığı';
 $lang['Apply on properties'] = 'Niteliklerine uygula';
+$lang['Album name, A &rarr; Z'] = 'Albüm adı, A &rarr; Z';
+$lang['Album name, Z &rarr; A'] = '
+Albüm adı, Z &rarr; A';
