be610d6a1b2bc4ad287d2bdaab44cc2e566fdd85
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-07 10:01:04 +0000

    [trunk_public] Update km_KH, thanks to : osify
    git-svn-id: http://piwigo.org/svn/trunk@28988 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/km_KH/common.lang.php
+++ language/km_KH/common.lang.php
@@ -423,3 +423,5 @@ $lang['Apply on properties'] = 'ដាក់លើ properties';
 $lang['No results for'] = 'មិនមានលទ្ធផលសំរាប់';
 $lang['Photo description'] = 'ពិពណ៌នារូបថត';
 $lang['Photo title'] = 'ឈ្មោះរូបថត';
+$lang['Album name, A &rarr; Z'] = 'ឈ្មោះអាល់ប៊ុម A &rarr; Z';
+$lang['Album name, Z &rarr; A'] = 'ឈ្មោះអាល់ប៊ុម Z &rarr; A';
