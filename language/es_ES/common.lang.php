6755b3e356998f4b821ba473fb489ff426721beb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-27 08:51:28 +0000

    [trunk_public] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/trunk@29084 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/common.lang.php
+++ language/es_ES/common.lang.php
@@ -433,3 +433,4 @@ $lang['Photo description'] = 'Descripción de la foto';
 $lang['Photo title'] = 'Título de la foto';
 $lang['Album name, Z &rarr; A'] = 'Nombre del album, Z &rarr; A';
 $lang['Album name, A &rarr; Z'] = 'Nombre del album, A &rarr; Z';
+$lang['Link: %s'] = 'Enlace: %s';
