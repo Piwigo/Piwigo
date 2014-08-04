5c5490e03e03730e5ebc7866c873919168ba5c73
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-04 16:27:23 +0000

    [trunk_public] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/trunk@29167 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/common.lang.php
+++ language/vi_VN/common.lang.php
@@ -425,3 +425,4 @@ $lang['Photo description'] = 'Mô tả hình ảnh';
 $lang['Photo title'] = 'Tiêu đề hình ảnh';
 $lang['Album name, A &rarr; Z'] = 'Tên album, A &rarr; Z';
 $lang['Album name, Z &rarr; A'] = 'Tên album, Z &rarr; A';
+$lang['Link: %s'] = 'Liên kết: %s';
