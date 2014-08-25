a653b8608126b868010f69b263528667864479c0
plegall <plg@piwigo.org>
2014-08-25 18:56:40 +0000

    feature 3105: also display link in install notification email
    git-svn-id: http://piwigo.org/svn/trunk@29270 68402e56-0260-453c-a942-63ccdbb3a9ee

--- install.php
+++ install.php
@@ -495,6 +495,8 @@ else
         get_l10n_args('Welcome to your new installation of Piwigo!', ''),
         get_l10n_args('', ''),
         get_l10n_args('Here are your connection settings', ''),
+        get_l10n_args('', ''),
+        get_l10n_args('Link: %s', get_absolute_root_url()),
         get_l10n_args('Username: %s', $admin_name),
         get_l10n_args('Password: %s', $admin_pass1),
         get_l10n_args('Email: %s', $admin_mail),
