e7e34fe1b916665ce7cc6753cb2af92faf3dae61
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-04 17:08:03 +0000

    [takeatour] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/trunk@29403 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/lv_LV/plugin.lang.php
+++ plugins/TakeATour/language/lv_LV/plugin.lang.php
@@ -153,3 +153,28 @@ $lang['2_7_0_stp3'] = 'Ātrā meklēšana ir pārstrādāta:<br>
 - izmanto vienskaitļa / daudzskaitļa locījumus (angļu / franču tikai no šī brīža)<br>';
 $lang['2_7_0_stp11'] = 'Šī ekskursija nu ir galā.<p style="text-align:center">Baudiet savu Piwigo 2.7!</p>Ja jums palīk Piwigo un vēlaties mūs atbalstīt, to varat darīt palīdzot ar tulkojumiem, ziedojot, utt. <a href="http://piwigo.org/basics/contribute" target="_blank">Klikšķiniet lai atbalstītu mūs</a>';
 $lang['privacy_stp13'] = 'Tagad klikšķiniet uz ptivate.';
+$lang['privacy_stp8'] = 'Atlasiet vienu vai vairākas bildes...';
+$lang['privacy_title6'] = 'Privātuma Līmenis';
+$lang['privacy_title7'] = 'Privātuma Līmenis';
+$lang['privacy_title8'] = 'Privātuma Līmenis';
+$lang['privacy_title9'] = 'Privātuma Līmenis';
+$lang['privacy_title12'] = 'Albumu atļaujas';
+$lang['privacy_title11'] = 'Albumu atļaujas';
+$lang['privacy_title10'] = 'Albumu atļaujas';
+$lang['privacy_title1'] = '
+Laipni gaidīti Privātuma Tūrē';
+$lang['privacy_title22'] = 'Vietējā Konfigurācija: Oriģinālā aizsardzība';
+$lang['privacy_title23'] = '
+Vietējā Konfigurācija: Konfidencialitātes Līmenis';
+$lang['privacy_title2'] = 'Palīdzība jūsu Piwigo';
+$lang['privacy_title24'] = 'Tas bija lieliski';
+$lang['privacy_title5'] = 'Atļaujas > Grupas';
+$lang['privacy_title4'] = 'Atļaujas';
+$lang['privacy_title3'] = 'Atļaujas';
+$lang['privacy_title21'] = 'Publiskie foto';
+$lang['privacy_title20'] = 'Publiskie foto';
+$lang['privacy_title19'] = 'Lietotāji';
+$lang['privacy_title15'] = 'padoms';
+$lang['privacy_title18'] = 'Grupas';
+$lang['privacy_title17'] = 'Grupas';
+$lang['privacy_stp11'] = 'Tagad klikšķiniet uz Permissions (atļaujas) cilnes';
