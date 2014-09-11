be35b776bc0eefde5d114f613921cd8786900d70
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-11 07:01:22 +0000

    [trunk_admin] Update fi_FI, thanks to : lrd
    git-svn-id: http://piwigo.org/svn/trunk@29468 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fi_FI/admin.lang.php
+++ language/fi_FI/admin.lang.php
@@ -498,3 +498,4 @@ $lang['Automatic sort order'] = 'Automaattinen järjestys';
 $lang['Available on'] = 'Saatavilla';
 $lang['Average rate'] = 'Pisteiden keskiarvo';
 $lang['Batch Manager'] = 'Massahallintatyökalu';
+$lang['<em>Piwigo for Android</em> application empowers you to connect your Android phone or table to your Piwigo gallery, create some albums and upload several photos at once.'] = '<em>Piwigo Androidille</em> applikaatio mahdollistaa yhdistämisen Android laitteestasi Piwigo galleriaan albumeiden luontia ja kuvien lisäämistä varten.';
