0f9644e392cbbbbf6bf49aeb7602084384b82da7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-12 18:25:28 +0000

    [takeatour] Update fr_FR, thanks to : plg
    git-svn-id: http://piwigo.org/svn/trunk@29526 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/fr_FR/plugin.lang.php
+++ plugins/TakeATour/language/fr_FR/plugin.lang.php
@@ -6,13 +6,13 @@ $lang['Prev'] = 'Préc';
 $lang['Next '] = 'Suiv';
 $lang['End tour'] = 'Finir';
 $lang['takeatour_configpage'] = 'Visite guidée » Liste des visites';
-$lang['TAT_descrp'] = 'Bienvenue ! Sont listées ici toutes les visites guidées disponibles pour découvrir les fonctionnalités de votre galerie Piwigo.<br />Démarrez une visite ci-dessous puis suivez les instructions. Cliquez sur Suivant/Précédent (ou utilisez les flèches du clavier) pour suivre le visite. Pendant la visite, si vous affichez une autre page d\'administration, vous serez automatiquement redirigé vers la page courante de la visite. Si vous êtes coincés, afficher <em>Plugins » Take A Tour</em> terminera la visite.';
+$lang['TAT_descrp'] = 'Bienvenue ! Sont listées ici toutes les visites guidées disponibles pour découvrir les fonctionnalités de votre galerie Piwigo.<br /><br/>Démarrez une visite ci-dessous puis suivez les instructions. Cliquez sur Suivant/Précédent (ou utilisez les flèches du clavier) pour suivre le visite. Pendant la visite, si vous affichez une autre page d\'administration, vous serez automatiquement redirigé vers la page courante de la visite. Si vous êtes coincés, afficher <em>Plugins » Take A Tour</em> terminera la visite.';
 
 $lang['First Contact'] = 'Premier contact';
-$lang['first_contact_descrp'] = 'Cette visite est une introduction à Piwigo et ses fonctionnalités de base. Elle est recommandée pour les débutants. Elle débuter par l\'ajout d\'images puis leur gestion. La visite continue avec la gestion des albums, les permissions et se termine sur la personnalisation à travers la configuration, les thèmes et plugins. ';
+$lang['first_contact_descrp'] = 'Cette visite est une introduction à Piwigo et ses fonctionnalités de base. Elle est recommandée pour les débutants. Elle débute par l\'ajout de photos puis leur gestion. La visite continue avec la gestion des albums et se termine sur une touche de personnalisation avec la configuration, les thèmes et les plugins. ';
 
 $lang['2_7_0_descrp'] = 'Découvrez ce qui est nouveau avec Piwigo 2.7';
 $lang['2.7 Tour'] = 'Visite 2.7';
 
 $lang['Privacy'] = 'Vie privée';
-$lang['privacy_descrp'] = 'Cette visite vous présente toutes les manières de protéger vos photos de l\'accès public avec des systèmes d\'autorisation par album et par image, et vous protège de la copie à l\'aide de protection d\'image.';
+$lang['privacy_descrp'] = 'Cette visite vous explique comment protéger vos photos de l\'accès public avec des systèmes de permissions par album et par photo, et vous protège de la copie à l\'aide de la protection d\'image.';
