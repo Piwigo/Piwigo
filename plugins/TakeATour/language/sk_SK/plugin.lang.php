0c6b3df333635d81ca18d7bb1ec5aff5fdffbb04
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-12 06:29:01 +0000

    [takeatour] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/trunk@29494 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/sk_SK/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['TAT_descrp'] = 'Vitajte! Tu sú uvedené všetky dostupné postupy, ako spoznať vlastnosti galérie Piwigo. <br><br>Spustite prehliadku nižšie a postupujte podľa pokynov. Kliknite na tlačidlo Nasledujúce/Predchádzajúce (alebo pomocou šipiek na vašej klávesnici) pre navigáciu počas prehliadky. Počas prehliadky, keď idete na inú stránku administrácie, budete presmerovaný na aktuálnu stránku prehliadky. Ak neviete ukončiť prehliadku, kliknite na <em>Doplnky » Urobte si prehliadku</em> a prehliadka skončí.';
+$lang['first_contact_descrp'] = 'Doporučené pre začiatočníkov, táto prehliadka vám predstaví Piwigo, jeho základné funkcie. Začne pridávním fotky, následne jej spravovaním. Prehliadka pokračuje so správou albumu a oprávneniami a končí prispôsobením pomocou nastavení tém a doplnkov.';
+$lang['privacy_descrp'] = 'Táto prehliadka predstavuje všetky spôsoby, ako chrániť svoje fotografie pred prístupom verejnosti na základe povolenia systémov v albume, na fotografiu a kopírovanie pomocou ochrany fotky.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Urobte si vprehliadku a objavte funkcie Vašej Piwigo galérie » Prejsť na dostupné prehliadky';
+$lang['takeatour_configpage'] = 'Urobte si prehliadku » Zoznam prehliadok';
+$lang['Start the Tour'] = 'Začnite prehliadku';
+$lang['Visit your Piwigo!'] = 'Navštívte Vaše Piwigo!';
+$lang['Prev'] = 'Predchádzajúce';
+$lang['Next '] = 'Nasledujúce';
+$lang['First Contact'] = 'Prvý kontakt';
+$lang['End tour'] = 'Koniec prehliadky';
+$lang['2_7_0_descrp'] = 'Zistite, čo je nové v Piwigo 2.7';
+$lang['2.7 Tour'] = '2.7 Tour';
