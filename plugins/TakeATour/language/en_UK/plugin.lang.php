fe9618761401e328eaf8a56de47584d8e8072031
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-12 12:44:01 +0000

    [takeatour] Update en_UK, thanks to : Tadjio
    git-svn-id: http://piwigo.org/svn/trunk@29507 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/en_UK/plugin.lang.php
+++ plugins/TakeATour/language/en_UK/plugin.lang.php
@@ -2,13 +2,13 @@
 $lang['Start the Tour'] = 'Start the Tour';
 $lang['Visit your Piwigo!'] = 'Visit your Piwigo!';
 $lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Take a tour and discover the features of your Piwigo gallery » Go to the available tours';
-$lang['TAT_descrp'] = 'Welcome! Here are listed all the available tours to discover the features of your Piwigo gallery.<br><br>Launch a tour below, then follow the instructions. Click Next/Prev (or use the arrows of your keyboard) to navigate during a tour. During a tour, if you go to an other page of the administration, you will be redirected to the current page of the Tour. If you are stuck and can\'t end the tour, going to <em>Plugins » Take A Tour</em> will end the tour.';
+$lang['TAT_descrp'] = 'Welcome! Here is a list of all the available tours to help you discover the features of your Piwigo gallery.<br><br>Launch a tour below, then follow the instructions. Click Next/Prev (or use the arrows of your keyboard) to navigate during a tour. While you are in a tour, if you go to another page of the administration, you will be redirected to the current page of the Tour. If you are stuck and can\'t end the tour, going to <em>Plugins » Take A Tour</em> will end the tour.';
 $lang['takeatour_configpage'] = 'Take A Tour » List of Tours';
 $lang['Prev'] = 'Prev';
 $lang['Next '] = 'Next';
 $lang['End tour'] = 'End tour';
 $lang['First Contact'] = 'First Contact';
-$lang['first_contact_descrp'] = 'Recommended for beginners, this tour introduce you to Piwigo and its basic features. It will start by adding photos, then manage them. The tour continues with album management and ends on the customization using configuration, themes and plugins.';
+$lang['first_contact_descrp'] = 'Recommended for beginners, this tour introduce you to Piwigo and its basic features. It will start by showing you how to add photos, then manage them. The tour continues with album management and ends with customization using configuration, themes and plugins.';
 $lang['2.7 Tour'] = '2.7 Tour';
 $lang['2_7_0_descrp'] = 'Discover What\'s New in Piwigo 2.7';
 $lang['Privacy'] = 'Privacy';
