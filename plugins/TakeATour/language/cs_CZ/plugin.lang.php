1a3dbf11d1ac598c976a633906f0164b99e3a13c
ddtddt <ddtddt@piwigo.org>
2014-09-15 19:47:56 +0000

    [Trunk] - Plugin - TakeATour add cs_CZ - Česky – Czech Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/trunk@29550 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/cs_CZ/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['Start the Tour'] = 'Spustit ukázku';
+$lang['Visit your Piwigo!'] = 'Prozkoumejte Piwigo!';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Vydej se na průzkum a objev nové vlastnosti tvé Piwigo fotogalerie » Přejít na dostupné ukázky';
+$lang['TAT_descrp'] = 'Vítejte! Zde vidíte seznam všech aktuálních ukázek které vám mohou pomoci lépe pochopit a využít funkce vaší Piwigo fotogalerie.<br><br>Spusťte ukázku níže, poté následujte instrukce. Klikněte na Další/Předchozí (nebo lze použít tlačítka šipek na vaší pc klávesnici) pro posun v průběhu ukázky. Když si prohlížíte ukázku, pokud přejdete na jinou stránku administrace, budete přesměrováni zpět na aktuální stránku ukázky. Pokud nechcete pokračovat a nemůžete ukončit ukázku, přejděte na <em>Pluginy » Take A Tour</em> což ukončí probíhající ukázku.';
+$lang['takeatour_configpage'] = 'Prohlédni si » Seznam ukázek';
+$lang['Prev'] = 'Předchozí';
+$lang['Next '] = 'Další';
+$lang['End tour'] = 'Konec ukázky';
+$lang['First Contact'] = 'První Kontakt';
+$lang['first_contact_descrp'] = 'Doporučené pro začátečníky, tato ukázka vás seznámí s Piwigo a jeho základními funkcemi. Na začátek vás tato ukázka seznámí zejména s tím jak přidávat fotky a spravovat je. Ukázka končí prací kolem správy album a úpravami jeho základního nastavení, šablon vzhledu a modulů.';
+$lang['2.7 Tour'] = 'Ukázka 2.7';
+$lang['2_7_0_descrp'] = 'Zjisti co je nového v Piwigo 2.7';
+$lang['Privacy'] = 'Soukromí';
+$lang['privacy_descrp'] = 'Tato ukázka prezentuje vše okolo ochrany vašich fotografií před veřejným přístupem díky možnostem nastavit oprávnění pro přístup na každé album a fotku a zamezení snadnému kopírování použitím aktivní ochrany obrázků.';
