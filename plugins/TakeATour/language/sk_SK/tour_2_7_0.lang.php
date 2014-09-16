9e127e4a899a207ad377a358ae770dc029044249
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-16 18:11:57 +0000

    [tour_2_7_0] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/trunk@29565 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/sk_SK/tour_2_7_0.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp11'] = 'Táto prehliadka je na konci.<p style ="text-align: center">Užite si Piwigo 2.7!</p>Ak sa vám páči Piwigo a chcete nás podporiť, môžete prispieť tým, že urobíte preklady, finančne nás podporíte, atď.<a href="http://piwigo.org/basics/contribute"target="_blank"> Kliknite tu, aby ste nás podporili</a>Vďaka!';
+$lang['2_7_0_stp2'] = 'Ako môžete vidieť, nový predvolený doplnok bol pridaný: Pozrite sa na Tour. Tento doplnok vám pomôže objaviť svoje Piwigo. Tu je miesto, kde sú uvedené všetky dostupné možnosti.<br>Táto stránka je k dispozícii v <em>Doplnky » Možnosti Tour </em>';
+$lang['2_7_0_stp2b'] = 'Nový formulár pre nahrávanie v Piwigo 2.7 je založený na HTML5 miesto Flashu, to znamená, že má lepšiu kompatibilitu s modernými webovými prehliadačmi, rovnako ako zvýšenie spoľahlivosti na pomalé pripojenie. A drag & drop bude fotky vyberať oveľa jednoduchšie z vášho prehliadača súborov!';
+$lang['2_7_0_stp4'] = 'Stránka vyhľadávania bola prepracovaná: schopnosť vybrať vlastnosti, na ktoré sa vzťahuje hľadané slovo, zoznam autorov nájdených v databáze, auto-kompletný zoznam kľúčových slov, auto-kompletný zoznam albumov ...';
+$lang['2_7_0_stp9'] = 'Vyhľadávací filter bol pridaný do manažéra dávky. Dodáva sa s výkonným syntax vyhľadávaním fotiek na termíny, značky, veľkosti, názvu súboru a tiež možnosti, ako je presná zhoda vyhľadávania a/alebo zoskupovanie, vylúčenie. Môžete si prečítať podrobnú nápovedu neskôr. Syntax je rovnaká, platí pre rýchle vyhľadávanie poľa v ponuke verejnej stránky ďalej.';
+$lang['2_7_0_stp10'] = 'Filter na veľkosť súboru bol tiež pridaný do manažéra dávky. Môžete si vybrať rozsah s minimálnou a maximálnou hodnotou.';
+$lang['2_7_0_stp1'] = 'Dobrý deň! Budem vaším sprievodcom pri objavovaní Piwigo. Prosím, postupujte podľa mojich inštrukcií a kliknite na tlačidlo Ďalej (alebo pomocou šípok na vašej klávesnici) pre navigáciu. Ak sa vydáte na druhú stranu, budete presmerovaný na aktuálnu stránku na Tour.';
+$lang['2_7_0_stp6'] = 'Označovací box pre uchopenie albumu, kľúčové slová atď sa zmenil na nový užívateľsky príjemný. Otestujte ho tu';
+$lang['2_7_0_stp5'] = 'Môžete nastaviť dobu, okrem dátumu, hodiny a minúty pomocou posuvníkov';
+$lang['2_7_0_stp7'] = 'Teraz môžete triediť albumu podľa dátumu vytvorenia fotografií alebo dátumu vloženia';
+$lang['2_7_0_stp8'] = 'Jednoduché kroky na vyprázdnenie caddy jedným kliknutím.';
+$lang['2_7_0_title10'] = 'Filter veľkosti súboru';
+$lang['2_7_0_title11'] = 'Uvidíme sa čoskoro!';
+$lang['2_7_0_title2'] = 'Prvá nová funkcia';
+$lang['2_7_0_title2b'] = 'Nový odosielací formulár';
+$lang['2_7_0_title4'] = 'Vyhľadávacia stránka';
+$lang['2_7_0_title6'] = 'Vyberte boxy';
+$lang['2_7_0_title8'] = 'Vyprázdniť caddy';
+$lang['2_7_0_title9'] = 'Vyhľadávací filter';
+$lang['2_7_0_title7'] = 'Zotriediť albumy podľa dátumu';
+$lang['2_7_0_title5'] = 'Čas';
+$lang['2_7_0_title1'] = 'Vitajte v Piwigo 2.7';
