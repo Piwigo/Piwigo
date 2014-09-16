7a1fc967ef2e774fcdeba9c8babadd08f2cadbe8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-16 22:34:51 +0000

    [privacy] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/trunk@29572 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/it_IT/tour_privacy.lang.php
+++ plugins/TakeATour/language/it_IT/tour_privacy.lang.php
@@ -1,3 +1,7 @@
 <?php
 $lang['privacy_title14b'] = '';
 $lang['privacy_stp14b'] = 'Fatto importante: il webmaster e gli amministratori non sono onnisciente durante la navigazione nella parte pubblica, ma possono accedere ad ogni album e alle foto sulla parte amministrativa.';
+$lang['privacy_stp14'] = 'Quindi utenti e gruppi di utenti può essere concesso di accedere all\'album.';
+$lang['privacy_stp13'] = 'Ora clicca su privato.';
+$lang['privacy_stp11'] = 'Ora clicca sulla scheda autorizzazioni';
+$lang['privacy_stp12'] = 'In quella pagina, puoi scegliere se l\'album sarà disponibile per chiunque o se sia limitato ad alcuni utenti';
