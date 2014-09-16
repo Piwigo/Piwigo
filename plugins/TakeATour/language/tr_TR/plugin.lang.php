b104686eb043bbf28801ba7bf9cb5efbad23df5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-16 18:11:59 +0000

    [takeatour] Update tr_TR, thanks to : Nakre
    git-svn-id: http://piwigo.org/svn/trunk@29566 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/tr_TR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2.7 Tour'] = '2.7 Turu';
+$lang['2_7_0_descrp'] = 'Piwigo 2.7 yeniliklerini keşfet';
+$lang['End tour'] = 'Turu bitir';
+$lang['Next '] = 'Sonraki';
+$lang['Prev'] = 'Önceki';
+$lang['Privacy'] = 'Gizlilik';
+$lang['Start the Tour'] = 'Turu Başlat';
+$lang['Visit your Piwigo!'] = 'Piwigonuzu ziyaret edin';
