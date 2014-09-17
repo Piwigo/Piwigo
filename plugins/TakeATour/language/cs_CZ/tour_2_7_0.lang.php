91df7870cb34e09d55dc2ebf5dc13e2ce9681c3a
ddtddt <ddtddt@piwigo.org>
2014-09-17 20:08:39 +0000

    [Trunk] - Plugin - TakeATour add 2.7 cs_CZ - Česky – Czech Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/trunk@29608 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/cs_CZ/tour_2_7_0.lang.php
@@ -0,0 +1,23 @@
+<?php
+$lang['2_7_0_title1'] = 'Vítejte ve fotogalerii Piwigo 2.7';
+$lang['2_7_0_stp1'] = 'Dobrý den! Já budu váš průvodce po aplikaci Piwigo. Prosím následujte mých pokynů a klikněte na Další (nebo použijte šipky na vaší klávesnici) pro posun a navigaci. Pokud půjdete na jinou stránku administrace, budete přesměrování na poslední aktuální stránku ukázky.';
+$lang['2_7_0_title2'] = 'Nové funkce';
+$lang['2_7_0_stp2'] = 'Jak můžete vidět, přibyl nový základní plugin: Take A Tour. Tento plugin vám pomůže s průzkumem app Piwigo. Zde najdete seznam aktuálích ukázek.<br>Seznam nejdete <em>Pluginy » Take A Tour</em>';
+$lang['2_7_0_title4'] = 'Vyhledávač';
+$lang['2_7_0_stp4'] = 'Stránka s vyhledáváním byla přepracována: možnost zvolit určité dopady a klíčová slova na které je brán zvýšený důraz při vyhledávání, seznam autorů v databázi, auto-doplnění ze seznamu štítků, auto-doplnění ze seznamu alb...';
+$lang['2_7_0_title5'] = 'Čas';
+$lang['2_7_0_stp5'] = 'Nyní lze nastavit datum a čas v minutách a hodinách pomocí posuvníku';
+$lang['2_7_0_title6'] = 'Volby pro výběr';
+$lang['2_7_0_stp6'] = 'Bloky pro výběr album, štítků, atd. byli změněny na nový více pro uživatele přívětivý. Otestujte níže';
+$lang['2_7_0_title7'] = 'Řazení alb podle datum';
+$lang['2_7_0_stp7'] = 'Nyní lze seřadit alba na základě data vytvoření fotek a nebo podle data uveřejnění';
+$lang['2_7_0_title8'] = 'Vyprazdnění výběru';
+$lang['2_7_0_stp8'] = 'Snadný rychlý odkaz pro vyprázdnění výběru na jedno kliknutí.';
+$lang['2_7_0_title9'] = 'Vyhledávací filtr';
+$lang['2_7_0_stp9'] = 'Filtr byl implentován do batch manageru. Umí nyní vyhledávat pomocí datum, štítků, rozlišení, názvu souboru a také umožňuje volby logického typu zcela souhlasí, a/nebo, množiny, vyloučení. Můžete si přečíst detailní popis později. Stejné možnosti nyní nabízí také samotné pole pro veřejné vyhledávání na webu v menu.';
+$lang['2_7_0_title10'] = 'Filtr podle velikosti souboru';
+$lang['2_7_0_stp10'] = 'Filtr podle velikosti souboru byl také přidán do batch manageru. Lze zvolit rozsah od min a max a opačně.';
+$lang['2_7_0_title11'] = 'Naviděnou u další verze!';
+$lang['2_7_0_stp11'] = 'Tato ukázka skončila.<p style="text-align:center">Užijte si vaše nové Piwigo 2.7!</p>Pokud jste si oblíbili fotogalerii Piwigo a chcete nám pomoci, můžete přispět do jeho překladu do češtiny nebo také přispět finančně, apod. <a href="http://piwigo.org/basics/contribute" target="_blank">KLikněte pro možnosti jak nám pomoci</a> Děkujeme!';
+$lang['2_7_0_title2b'] = 'Nový upload formulář';
+$lang['2_7_0_stp2b'] = 'Nový formulář pro import souborů fotografií v Piwigo 2.7 je vytvořen na technologii HTML5 a předtím byl použit Flash, což má jednu podstatnou výhodu v lepší kompatibilitě s novými typy prohlížečů zejména na mobilních zařízeních. Soubory lze vkládat pouhým přetažením z vaší pracovní plochy diky fci drag & drop která umožní pohodlnější označení a vložení vícero fotografií najednou!';
