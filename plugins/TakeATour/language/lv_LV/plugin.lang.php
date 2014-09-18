6eac7dad94c0afca70fec7b592c9dcba34ed466b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-18 05:59:55 +0000

    [takeatour] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/trunk@29612 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/lv_LV/plugin.lang.php
+++ plugins/TakeATour/language/lv_LV/plugin.lang.php
@@ -36,3 +36,4 @@ $lang['2.7 Tour'] = '2.7 Apskats';
 $lang['2_7_0_descrp'] = 'Atklājiet, kas jauns Piwigo 2.7';
 
 $lang['privacy_descrp'] = 'Šis apskats iepazīstina ar visiem veidiem, kā aizsargāt savu foto no publiskas piekļuves, izmantojot atļauju sistēmu vienam albūmam un vienam attēlam, un pret kopēšanu, izmantojot attēlu aizsardzību.';
+$lang['Privacy'] = 'Privātums';
