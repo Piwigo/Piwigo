c702a6cc2e165cde16f2ba7877cbc3ef10ca474f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-20 13:09:38 +0000

    [takeatour] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/trunk@29668 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/ca_ES/plugin.lang.php
+++ plugins/TakeATour/language/ca_ES/plugin.lang.php
@@ -20,10 +20,17 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['TAT_descrp'] = 'Benvinguts! Aquí teniu llistats totes les possibles visites per descobrir totes les característiques de la galeria Piwigo.<br/> Escolliu una visita i seguiu les instruccions. Cliqueu Següent/Anterior (o utilitzeu les tecles de desplaçament) per navegar durant la visita. Si aneu a una altra pàgina de l\'administració sereu redirigits cap a la pàgina actual de la visita. Si voleu aturar-ho o no podeu acabar la visita, aneu a <em>Connectors » Feu una visita </em> per finalitzar la visita.';
+$lang['TAT_descrp'] = 'Benvinguts! Aquí teniu llistades les possibles visites que podeu realitzar per descobrir totes les característiques de la galeria Piwigo.<br/><br/> Escolliu una visita i seguiu les instruccions. Durant la visita cliqueu Següent/Anterior (o utilitzeu les tecles de desplaçament) per navegar. Mentres hi hagi activa una visita, si aneu a una altra pàgina de l\'administració sereu redirigits cap a la pàgina de la visita. Si voleu aturar-ho o no podeu acabar la visita, aneu a <em>Connectors » Feu una visita </em> per finalitzar-la.';
 $lang['takeatour_configpage'] = 'Feu una visita » Llista de visites';
 $lang['First Contact'] = 'Primer contacte';
 $lang['Start the Tour'] = 'Comença la visita';
 $lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Feu una visita i descobriu les característiques de la vostra galeria Piwigo » Aneu a les visites disiponibles.';
 $lang['Visit your Piwigo!'] = 'Visita la teva Piwigo!';
 $lang['first_contact_descrp'] = 'Recomanada per a principiants. Aquesta visita introdueix les característiques bàsiques de Piwigo. Comença amb la gestió d\'imatges  i la gestió dels àlbums i permisos i continua amb la personalització de la galeria utilitzant els temes i els connectors.';
+$lang['2.7 Tour'] = 'Visita 2.7';
+$lang['2_7_0_descrp'] = 'Descobreix que hi ha de nou a Piwigo 2.7';
+$lang['End tour'] = 'Fi de la visita';
+$lang['Next '] = 'Següent';
+$lang['Prev'] = 'Anterior';
+$lang['Privacy'] = 'Privacitat';
+$lang['privacy_descrp'] = 'Aquesta visita presenta tots els camins per protegir les vostres fotos de l\'accès públic afegint permisos en els àlbums i fotos, i evitant la còpia d\'imatges protegides.';
