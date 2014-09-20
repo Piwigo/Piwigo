e1163fb2f559d0beb166855794de56977e27f6d5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-20 14:26:42 +0000

    [tour_2_7_0] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/trunk@29670 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/ca_ES/tour_2_7_0.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp6'] = 'La caixa de selecció d\'àlbums, etiquetes, etc ha canviat en una de nova molt més entenedora. Prova-la a sota';
+$lang['2_7_0_stp4'] = 'La pàgina de cerca s\'ha refet: habilitat de seleccionar propietats en les quals la paraula clau s\'apliqui, llista d\'autors trobats en la base de dades, llista de auto-compleció d\'etiquetes, llista d\'auto-compleció d\'àlbums...';
+$lang['2_7_0_stp2b'] = 'El nou formulari de càrrega de Piwigo 2.7 està basat en HTML5 en comptes de Flash, això significa una millor compatibilitat amb els navegadors web moderns i una millor confiança amb connexions lentes. L\'opció de clicar i arrastrar farà molt més fàcil la selecció de fotos! ';
+$lang['2_7_0_stp9'] = 'S\'ha afegit un filtre en el gestor per lots. Inclou una potent sintaxi de cerca de fotos per dates, etiquetes, mides, nom d\'arxius i també opcions de cerca com exact, match, AND/OR, grouping, exclusion. Més tard podeu llegir l\'ajuda detallada. La mateixa sintaxi s\'aplica en el camp de cerca ràpida en menú del costat públic.';
+$lang['2_7_0_stp5'] = 'Podeu indicar l\'hora i la data, utilitzant els sliders Hora i Minut';
+$lang['2_7_0_stp7'] = 'Ara podreu ordenar els àlbums en la data de creació o d\'enviament de les fotos.';
+$lang['2_7_0_stp8'] = 'Acció senzilla per buidar el carret en un únic clic';
+$lang['2_7_0_title2b'] = 'Nova pujada de';
+$lang['2_7_0_title8'] = 'Buida el carret';
+$lang['2_7_0_title9'] = 'Filtre de cerca';
+$lang['2_7_0_stp10'] = 'S\'ha afegit un filtre de mida de fitxer en el gestor per lots. Podeu seleccionar un rang amb un valor mínim i màxim.';
+$lang['2_7_0_stp2'] = 'Com podeu veure, s\'ha afegit un nou connector: Exploreu. Aquest connector us ajudarà a descobrir Piwigo. Aquí hi ha una llista de totes les possibles exploracions. <br/> Aquesta pàgina està disponible a <em>Connectors » Exploreu </em>';
+$lang['2_7_0_stp11'] = 'Aquesta exploració s\'ha acabat <p style="text-align:center"> Gaudiu de Piwigo 2.7</p> Si us agrada Piwigo i voleu donar-nos suport, podeu contribuir ajudant a traduir, donant, etc. <a href="http://piwigo.org/basics/contribute" target="_blank">Cliqueu aquí per donar suport </a>  Gràcies!';
+$lang['2_7_0_title10'] = 'Filtre de mida de fitxer';
+$lang['2_7_0_title11'] = 'Ens veiem aviat!';
+$lang['2_7_0_title2'] = 'Primera nova funcionalitat';
+$lang['2_7_0_title4'] = 'Pàgina de cerca';
+$lang['2_7_0_title5'] = 'Temps';
+$lang['2_7_0_title6'] = 'Selecciona caixes';
+$lang['2_7_0_title7'] = 'Ordena els àlbums per data';
+$lang['2_7_0_title1'] = 'Benvingut a Piwigo 2.7';
+$lang['2_7_0_stp1'] = 'Hola!jo seré la vostra guia per descobrir Piwigo. Si us plau segueix les meves instruccions, i clica a Següent (o utilitza les fletxes de desplaçament del teclat) per navegar. Si aneu a una altra pàgina sereu redirigits a la pàgina on havíeu deixat l\'exploració.';
