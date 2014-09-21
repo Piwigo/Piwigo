f708f78574160ae609b961e10682f42980cb546f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-21 14:59:36 +0000

    [admin_tools] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/trunk@29705 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/AdminTools/language/br_FR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Revert'] = 'Nullañ';
+$lang['Closed icon position'] = 'Lec\'h an arlun serret';
+$lang['Combine JS&CSS'] = 'Kenstrollañ JS ha CSS';
+$lang['Debug languages'] = 'Dizreinañ ar yezhoù';
+$lang['Debug template'] = 'Dizreinañ ar patrom';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Reiñ da perc\'henn al luc\'hskeudennoù tu du aozañ buan ha pa vefent ket merourien.';
+$lang['Open toolbar by default'] = 'Digeriñ ar varrenn vinvioù dre ziouer';
+$lang['Properties page'] = 'Pajenn ar perzhioù';
+$lang['Quick edit'] = 'Aozañ buan';
+$lang['Save'] = 'Enrollañ';
+$lang['Save visit in history'] = 'Enrollañ ar weladenn er roll-istor';
+$lang['Saved'] = 'Enrollet';
+$lang['Show SQL queries'] = 'Diskouez rekedoù SQL';
+$lang['View as'] = 'Gwelet evel';
+$lang['Viewing as <b>%s</b>.'] = 'Gwelet evel <b>%s</b>.';
+$lang['left'] = 'tu kleiz';
+$lang['right'] = 'tu dehoù';
