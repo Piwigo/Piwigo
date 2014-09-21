c61d07795119529e165c17c4639bbbd210621145
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-21 17:35:50 +0000

    [takeatour] Update et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/trunk@29706 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/et_EE/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['TAT_descrp'] = 'Tervitus! Siin on nimekiri kõigist olemasolevatest ülevaadetest, mis aitavad Sul tundma õppida Piwigo galerii funktsioone. Vajuta Järgmine/Eelmine (või kasuta klaviatuuril noole klahve) ülevaate käigus navigeerimiseks. Ülevaatest mõnele teisele administreerimise lehele minnes suunatakse Sind tagasi samale Ülevaate leheküljele. Kui olete ummikus ja ei saa ülevaadet lõpetada, siis minnes <em>Plugins » Vaata Ülevaadet</em> lõpetab ülevaate';
+$lang['2.7 Tour'] = '2.7 ülevaade';
+$lang['2_7_0_descrp'] = 'Avasta mis on uut Piwigo 2.7';
+$lang['End tour'] = 'Ülevaate lõpp';
+$lang['First Contact'] = 'Esimene kontakt';
+$lang['Next '] = 'Järgmine';
+$lang['Prev'] = 'Eelmine';
+$lang['Privacy'] = 'Privaatsus';
+$lang['Start the Tour'] = 'Alusta ülevaadet';
