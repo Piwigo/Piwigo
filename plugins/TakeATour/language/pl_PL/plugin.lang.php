524cf062a3fc0a73503680479292a7d04171686e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-21 14:59:30 +0000

    [takeatour] Update pl_PL, thanks to : Greybrow
    git-svn-id: http://piwigo.org/svn/trunk@29701 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/pl_PL/plugin.lang.php
+++ plugins/TakeATour/language/pl_PL/plugin.lang.php
@@ -20,16 +20,19 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['takeatour_configpage'] = 'Wybierz trasę » Lista tras';
-$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Rozpocznij zwiedzanie i odkrywaj możliwości swojej galeri Piwigo » Przejdź do dostępnych wycieczek';
+$lang['takeatour_configpage'] = 'Wybierz przewodnik » Lista przewodników';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Rozpocznij zwiedzanie i odkrywaj możliwości swojej galeri Piwigo » Przejdź do dostępnych przewodników';
 $lang['End tour'] = 'Zakończ zwiedzanie';
 $lang['Next '] = 'Dalej';
 $lang['Prev'] = 'Wstecz';
-$lang['Start the Tour'] = 'Rozpocznij zwiedzanie';
-$lang['Visit your Piwigo!'] = 'Odwiedź swoje Piwigo!';
+$lang['Start the Tour'] = 'Rozpocznij korzystanie z przewodnika';
+$lang['Visit your Piwigo!'] = 'Odwiedź swoją galerię Piwigo!';
 
 $lang['First Contact'] = 'Pierwszy kontakt';
-$lang['first_contact_descrp'] = 'Tę trasę polecamy szczególnie początkuącym, wprowadzi cię do Piwigo i zaprezentuje podstawowe cechy. Rozpocznie się od dodawania zdjęć i zarządzania nimi. Następnie przybliży zarządzanie albumami i uprawieniami do nich, a na koniec dostosowywanie przy uyżyciu konfiguracji, skórek i wtyczek.';
+$lang['first_contact_descrp'] = 'Ten przewodnik polecamy szczególnie początkującym, wprowadzi cię do Piwigo i zaprezentuje podstawowe cechy. Rozpocznie się od dodawania zdjęć i zarządzania nimi. Następnie przybliży zarządzanie albumami i uprawieniami do nich, a na koniec dostosowywanie przy użyciu konfiguracji, skórek i wtyczek.';
 
 $lang['2.7 Tour'] = 'Przewodnik po 2.7';
 $lang['2_7_0_descrp'] = 'Zobacz co nowego w Piwigo 2.7';
+$lang['TAT_descrp'] = 'Witamy! Oto lista przewodników, które pomogą Ci odkryć możliwości galerii Piwigo.<br><br>Uruchom jeden z przewodników poniżej i zastosuj się do dalszych instrukcji. Możesz korzystać do nawigacji po przewodniku z myszki, lub strzałek na klawiaturze. Będąc w przewodniku, jeśli przejdziesz na inną stronę panelu administracji, przewodnik przeskoczy również do odpowiedniej strony. Żeby zakończyć wyświetlanie przewodnika kliknij na <em>Wtyczki > Przewodniki</em>.';
+$lang['privacy_descrp'] = 'Ten przewodnik prezentuje sposoby zabezpieczenia Twoich zdjęć przed publicznym dostępem, poprzez zezwolenia dostępu do albumów i pojedynczych zdjęć, oraz zabezpieczenia przed kopiowaniem.';
+$lang['Privacy'] = 'Prywatność';
