380678b250e2c7274db185e8104d167c8e1b2d6c
ddtddt <ddtddt@piwigo.org>
2014-09-21 19:16:51 +0000

    [2.6] - theme - prepare for 2.7.0
    git-svn-id: http://piwigo.org/svn/trunk@29712 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themes/elegant/themeconf.inc.php
+++ themes/elegant/themeconf.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Theme Name: elegant
-Version: 2.6.2
+Version: 2.7.0
 Description: Dark background, grayscale.
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=685
 Author: Piwigo team
