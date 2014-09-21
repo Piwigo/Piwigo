0db0ace0a9dceaefe814685d529fd90b5e38b143
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-21 20:09:26 +0000

    [privacy] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/trunk@29715 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/it_IT/tour_privacy.lang.php
+++ plugins/TakeATour/language/it_IT/tour_privacy.lang.php
@@ -5,3 +5,5 @@ $lang['privacy_stp14'] = 'Quindi utenti e gruppi di utenti può essere concesso 
 $lang['privacy_stp13'] = 'Ora clicca su privato.';
 $lang['privacy_stp11'] = 'Ora clicca sulla scheda autorizzazioni';
 $lang['privacy_stp12'] = 'In quella pagina, puoi scegliere se l\'album sarà disponibile per chiunque o se sia limitato ad alcuni utenti';
+$lang['privacy_stp18'] = 'Per gestire le associazioni tra gli utenti e gruppi, vai alla pagina <em>Utenti » Gestione</em>';
+$lang['privacy_stp15'] = 'Prima di spiegare i gruppi, un suggerimento: questo è il link alla pagina per impostare come privato/pubblico diversi album contemporaneamente. Clicca su di esso';
