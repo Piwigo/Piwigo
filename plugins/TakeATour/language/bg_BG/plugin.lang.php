75ad4766f35956bc9d8dcf35e4f7e85a24073f51
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-21 07:48:00 +0000

    [takeatour] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/trunk@29695 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/bg_BG/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Next '] = 'Напред';
+$lang['Prev'] = 'Назад';
+$lang['TAT_descrp'] = 'Добре дошли! Тук е списъка с всички налични описания с помощ за да откриети новите възможности на Piwigo. <br><br> Стартирайте помощника и следвайте инструкциите. Натискайте Напред/Назад или ползвайте стрелките за навигация в обиколката. Ако ви е омръзнала и не желаете да завършите обиколката, отидете на Добавки в Админ меню - Направете обиколка!';
+$lang['privacy_descrp'] = 'Тази обиколка показва всички начини да защитите вашите снимки от публичен достъп без позволение на ниво снимка, албум и прочие. Използване на защити против копиране.';
+$lang['first_contact_descrp'] = 'Препоръчително за начинаещи, тази обиколка ще ви запознае с Piwigo и неговите основни възможности. Ще започне с това как да добавяте снимки и да ги редактирате. Също ще видите създаване и редакция на албуми и накрая конфигуриране и настройки на теми и приложиния добавки.';
+$lang['takeatour_configpage'] = 'Направете обиколка - списък';
+$lang['Visit your Piwigo!'] = 'Посетете вашата Piwigo!';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Запознайте се и разгледайте новите възможности на вашата Piwigo галерия. ';
+$lang['Start the Tour'] = 'Започни прегледа';
+$lang['Privacy'] = 'Защита на данни';
+$lang['First Contact'] = 'За първи път';
+$lang['2.7 Tour'] = '2.7 версия преглед';
+$lang['End tour'] = 'Край на прегледа';
+$lang['2_7_0_descrp'] = 'Вижти какво е ново в Piwigo 2.7';
