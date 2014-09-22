a04685d0cc2f123600506e594614be50dc5903f1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-22 18:22:45 +0000

    [takeatour] Update nb_NO, thanks to : abbamust
    git-svn-id: http://piwigo.org/svn/trunk@29746 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/nb_NO/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2.7 Tour'] = '2.7 Omvisning';
+$lang['2_7_0_descrp'] = 'Se hva som er nytt i Piwigo 2.7';
+$lang['End tour'] = 'Avslutt omvisning';
+$lang['First Contact'] = 'Første kontakt';
+$lang['Next '] = 'Neste';
+$lang['Prev'] = 'Forrige';
+$lang['Privacy'] = 'Privat';
+$lang['Start the Tour'] = 'Start omvinsning';
+$lang['Visit your Piwigo!'] = 'Besøk din Piwigo!';
