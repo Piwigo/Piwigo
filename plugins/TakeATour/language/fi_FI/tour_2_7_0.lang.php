765feec9ea2de31ccb6200e227f773329f33a249
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-22 04:53:23 +0000

    [tour_2_7_0] Update fi_FI, thanks to : lrd
    git-svn-id: http://piwigo.org/svn/trunk@29728 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/fi_FI/tour_2_7_0.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp8'] = 'Helppo toimintolinkki kuvakorin tyhjentämiseen yhden linkin klikkauksella.';
+$lang['2_7_0_title8'] = 'Tyhjennä kuvakori';
+$lang['2_7_0_stp9'] = 'Hakusuodatin on lisätty massahallintatyökaluun. Siinä on tehokas tapa etsiä kuvia päivämäärien, koon, tiedoston nimen ja asettaa hakuun asetuksia, kuten tarkat osumat, JA/TAI haku, ryhmittely ja pois jättäminen. Voit lukea yksityiskohtaisen ohjeen myöhemmin. Sama syntaksi on käytössä myös julkisen puolen pikahakukentässä.';
+$lang['2_7_0_title9'] = 'Haku suodatin';
+$lang['2_7_0_stp1'] = 'Hei! Olen oppaasi Piwigoon tutustumisessa. Ole hyvä ja seuraa ohjeitani ja paina Seuraavaa (tai käytä näppäimistösi nuolinappeja) navigoidaksesi. Jos menet toiselle sivulle, sinut palautetaan kiertueen nykyiselle sivulle.';
+$lang['2_7_0_stp10'] = 'Suodatin kuvien koolle on lisätty massahallintatyökaluun. Voit valita alueen minimi ja maksimikoolle.';
+$lang['2_7_0_stp11'] = 'Tämä esittely on ohi.<p style="text-align:center">Nauti Piwigo 2.7:stäsi!</p>Jos pidät Piwigosta ja haluat tukea meitä, voit auttaa tekemällä käännöstyötä, lahjoittamalla rahaa ja niin edelleen. <a href="http://piwigo.org/basics/contribute" target="_blank">Klikkaa tästä auttaaksesi meitä.</a> Kiitos!';
+$lang['2_7_0_stp2'] = 'Kuten ehkä huomasitkin, uusi "Katso esittely"-laajennus on lisätty. Tämä auttaa sinua löytämään Piwigosi ominaisuuksia. Ohessa on kaikki saatavilla olevat esittelyt.<br> Tämä sivu löytyy <em>Laajennukset » Katso esitely</em>';
+$lang['2_7_0_stp2b'] = 'Piwigo 2.7 uusi lisäys lomake perustuu HTML5:n Flashin sijaan, tarkoittaen parempaa yhteensopivuutta nykyisten selaimien kanssa ja parempaa luotettavuutta hitaiden yhteyksien yli. Lisäksi pudota & lisää toiminto tekee kuvien valinnasta tiedostoselaimesta paljon helpompaa!';
+$lang['2_7_0_stp4'] = 'Hakutulossivu on uudistettu: Kyky asettaa ominaisuuksia, joihin hakusana kohdistetaan, lista julkaisujoita tietokannasta, automaattinen tagien täydennys, automaattinen albumien täydennys...';
+$lang['2_7_0_stp5'] = 'Voit asettaa ajan päivämäärän lisäksi, käyttäen Tunti ja Minuutti säätimiä';
+$lang['2_7_0_stp6'] = 'Monivalinta ikkuna albumeiden, tagien yms. valintaa varten on muutettu uuteen, käyttäjäystävällisempään. Testaa alempana';
+$lang['2_7_0_stp7'] = 'Nyt voit järjestää albumisi kuvien luomis- tai lisäämis päivämäärän perusteella';
+$lang['2_7_0_title1'] = 'Tervetuloa Piwigo 2.7:n';
+$lang['2_7_0_title10'] = 'Tiedoston koko suodatin';
+$lang['2_7_0_title11'] = 'Nähdään pian!';
+$lang['2_7_0_title2'] = 'Ensimmäinen uusi ominaisuus';
+$lang['2_7_0_title2b'] = 'Uusi lisäys lomake';
+$lang['2_7_0_title4'] = 'Hakusivu';
+$lang['2_7_0_title5'] = 'Aika';
+$lang['2_7_0_title6'] = 'Valinta ikkunat';
+$lang['2_7_0_title7'] = 'Järjestele albumit päivämäärän mukaan';
