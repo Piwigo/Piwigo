775bab4d97630c2ba0b56e2facc303d1cc955e41
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-23 21:29:26 +0000

    [tour_2_7_0] Update et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/trunk@29766 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/et_EE/tour_2_7_0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp2b'] = 'Piwigo 2.7 uus üleslaadimise vorm põhineb Flash\'i asemel HTML5\'l, mis tähendab paremat ühildumist tänapäevaste veebibrauseritega ja samas ka tõhusamat töökindlust aeglase ühenduse puhul. Ning lohista & aseta muudab fotode valimise failide brauserist veelgi mugavamaks!';
+$lang['2_7_0_stp2'] = 'Nagu sa näed on lisatud uus vaikimisi plugin: Ülevaade. See plugin aitab tutvuda sinu Piwigoga. Siin on nimekiri kõigist olemasolevatest ülevaadetest. <br>See leht on saadaval <em>Pluginad » Ülevaade</em>';
+$lang['2_7_0_stp1'] = 'Tere! Olen su teejuht Piwigo\'s. Palun järgi mu juhiseid ja vajuta Järgmine (või kasuta nooli oma klaviatuuril) edasi liikumiseks. Kui sa lahkud mõnele teisele lehele, suunatakse sind Ülevaatesse tagasi pöördudes samale lehele, kust lahkusid. ';
