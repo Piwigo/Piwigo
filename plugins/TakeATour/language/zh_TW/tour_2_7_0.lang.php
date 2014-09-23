dad5c67a75862e7e30f6350430400f43929962a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-23 16:53:24 +0000

    [tour_2_7_0] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/trunk@29765 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/zh_TW/tour_2_7_0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp11'] = '這次的教學到此為止。<p style="text-align:center">享受你的Piwigo 2.7 </p>
+如果你喜歡Piwigo，想支持我們，可以通過翻譯，捐贈等貢獻幫助。
+<a href="http://piwigo.org/basics/contribute" target="_blank">
+點擊這裡支持我們</a>謝謝！';
