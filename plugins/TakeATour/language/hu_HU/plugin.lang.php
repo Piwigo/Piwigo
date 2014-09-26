ba160199ae0512f6c55c78c1e91c871834c4572e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-26 05:32:25 +0000

    [takeatour] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/trunk@29796 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/hu_HU/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2.7 Tour'] = '2.7 túra';
+$lang['2_7_0_descrp'] = 'Fedezze fel mi az új a Piwigo 2.7-ben';
+$lang['End tour'] = 'Túra vége';
+$lang['Next '] = 'Tovább';
+$lang['Prev'] = 'Vissza';
+$lang['Start the Tour'] = 'Túra indítása';
+$lang['takeatour_configpage'] = 'Felfedező túra » Túra lista';
