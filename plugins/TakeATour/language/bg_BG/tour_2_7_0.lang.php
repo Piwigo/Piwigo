319071b84c896bbe7a7c098735bc4b84ebd5332b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-28 17:01:24 +0000

    [tour_2_7_0] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/trunk@29832 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/bg_BG/tour_2_7_0.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp5'] = 'Може да настройте часа на определената дата, ползвайки слайдера за час и минути';
+$lang['2_7_0_stp10'] = 'Добъвен е също към менажера и филтър за големина на файлове. 
+Може да избирате минимална и маскимална големина.';
+$lang['2_7_0_stp8'] = 'Бързо изчистване на кошницата само с един клик.';
+$lang['2_7_0_stp7'] = 'Сега може да сортирате вашите албуми по дата на публикация или по дата на създаване';
+$lang['2_7_0_title5'] = 'Време';
+$lang['2_7_0_title4'] = 'Страниза за търсене';
+$lang['2_7_0_title2b'] = 'Форма за ново качване';
+$lang['2_7_0_title2'] = 'Първи новости';
+$lang['2_7_0_title11'] = 'До скоро !!!';
+$lang['2_7_0_title10'] = 'Филтър за размер файл';
+$lang['2_7_0_title1'] = 'Добре дошли в Piwigo 2.7';
+$lang['2_7_0_title7'] = 'Сортиране албум по дата';
+$lang['2_7_0_title6'] = 'Избери скрипт';
+$lang['2_7_0_title8'] = 'Изпразване на кошницата';
+$lang['2_7_0_title9'] = 'Филтър за търсене';
