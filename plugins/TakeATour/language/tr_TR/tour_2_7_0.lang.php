6f456078c48b4a6de377ce2ad33facbf0eeac920
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-28 06:56:29 +0000

    [tour_2_7_0] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/trunk@29823 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/tr_TR/tour_2_7_0.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp7'] = 'Şimdi albümlerinizi fotoğraf yaratma veya gönderim tarihine göre sıralayabilirsiniz';
+$lang['2_7_0_stp8'] = 'Bir tıklamayla sepeti boşaltacak kolay aksiyon bağlantısı';
+$lang['2_7_0_title2'] = 'İlk yeni özellik';
+$lang['2_7_0_title10'] = 'Dosya büyüklüğü filtresi';
+$lang['2_7_0_title8'] = 'Sepeti boşalt';
+$lang['2_7_0_title9'] = 'Arama filtresi';
+$lang['2_7_0_title1'] = 'Piwigo 2.7 \'ye hoş geldiniz';
+$lang['2_7_0_title11'] = 'Tekrar görüşmek üzere!';
+$lang['2_7_0_title2b'] = 'Yeni yükleme formu';
+$lang['2_7_0_title4'] = 'Arama Sayfası';
+$lang['2_7_0_title5'] = 'Zaman';
+$lang['2_7_0_title6'] = 'Seçim kutuları';
+$lang['2_7_0_title7'] = 'Tarihe göre albüm sıralama';
