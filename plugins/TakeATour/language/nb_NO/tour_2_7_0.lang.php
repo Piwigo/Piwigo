cdab6677c9bae9768d6989e9b6f09ad489c5826c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-06 05:50:50 +0000

    [tour_2_7_0] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/trunk@29889 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/nb_NO/tour_2_7_0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_stp2'] = 'Som du kan se, har det blitt lagt til et nytt standard tillegsprogram: Ta en Omvisning. Dette tillegsprogrammet hjelper deg å finne mer ut av din Piwigo. Her er det en listet med alle tilgjengelige omvisninger. Denne siden er tilgjengelig under Tillegsprogrammer » Ta en omvisning';
+$lang['2_7_0_stp11'] = 'Denne omvisningen er over <p style="text-align:center">. Nyt Piwigo 2.7!</p>Hvis du liker Piwigo og ønsker å støtte oss, kan du bidra ved å hjelpe med oversettelser, donere, etc.<a! href = "http://piwigo.org/basics/contribute"target="_blank"> Klikk her for å støtte oss</a>Takk!';
+$lang['2_7_0_stp10'] = '
+Et filter på filstørrelse har også blitt lagt til batch manager. Du kan velge et område med et minimum og maksimum verdi.';
+$lang['2_7_0_stp1'] = 'Hei! Jeg vil være din guide på denne omvisningen når du skal oppdage Piwigo. Følg mine instruksjoner, og klikk på Neste (eller bruke pilene på tastaturet) for å navigere. Hvis du går til en annen side, vil du bli omdirigert til den gjeldende siden av omvisningen.';
