5e489a45ac7d40d135d6e7bc8a2befcf1d48e9ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-09 19:41:38 +0000

    [first_contact] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/trunk@29951 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/nb_NO/tour_first_contact.lang.php
@@ -0,0 +1,73 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_stp41'] = 'Du kan se på den siste kategorien alle plugins tilgjengelig for din versjon av Piwigo. Flere hundre tillegsprogrammer finne det!';
+$lang['first_contact_stp40'] = 'Det er meg her! Ikke deaktivere meg nå, men du kan se at du kan Deaktivere eller Gjenopprett et aktivert tillegsprogram.';
+$lang['first_contact_stp4'] = 'Først velger du et album; opprette et hvis det ikke er noen album ennå.';
+$lang['first_contact_stp39'] = 'På den første siden, er alle tillegsprogrammene oppført.<br><br>Øverst, er alle tillegsprogrammene som er aktivert oppført, og som i dag kjører.<br><br>Nederst, er alle tillegsprogrammer som er installert i /tillegsprogram mappen, men deaktivert.<br><br> Å slette et tillegsprogram betyr at alle spor av programtillegget vil bli fjernet (filer, opsjoner etc). For de fleste av tillegsprogrammene, vil en deaktivering beholde dataene som er registrert av tillegsprogrammet.';
+$lang['first_contact_stp38'] = 'Bruk av tillegsprogrammer er en svært enkle måter å tilpasse din Piwigo. De kan gjøre nesten alt du kan tenke deg fra små tekst tillegg til å fullføre funksjoner, som tillegsprogrammet Fellesskapet som lar ikke-administrator brukere å laste opp uten å ta del i administrasjonen.';
+$lang['first_contact_stp37'] = 'Hvordan installere nye temaer, du kan direkte laste dem ned fra kategorien \'Legg til et tema\'. Bare temaene merket som kompatibel med din versjon av Piwigo vises.<br><br>La oss nå utforske tillegsprogrammer! Klikk på <em>Tillegsprogrammer »Behandle</em>';
+$lang['first_contact_stp35'] = 'Sett din favoritt tema som standard tema.';
+$lang['first_contact_stp34'] = 'Velge et tema er det første skrittet for å tilpasse Piwigo. Temaer som er installert, er oppført her<br>Mer enn ett tema kan aktiveres: brukere kan endre sitt tema ved å velge et blant de som er aktiverte her, hvis alternativet \'Tillat bruker tilpasning\' er krysset av på alternativ siden.';
+$lang['first_contact_stp32'] = 'Lagre endringen nå';
+$lang['first_contact_stp33'] = 'Bare et siste ord om alternativene, denne \'Gjeste innstillinger\' siden har som oppgave å sette preferansene til uregistrerte besøkende. Hver gang Piwigo refererer til "gjest" som betyr uregistrerte besøkende<br>La oss fortsette med Konfigurasjon: Klikk på <em> Konfigurasjon »Temaer</em> eller bare trykke Neste.';
+$lang['first_contact_stp31'] = 'Så banneren, som vil bli vist på toppen av hver side. Som du kan se, er HTML-tagger tillatt her. Du kan også bruke% galleri_titel% tag for å vise tittelen du nettopp har skrevet ovenfor.';
+$lang['first_contact_stp30'] = 'La oss starte med å endre på tittelen';
+$lang['first_contact_title15'] = 'Redigerings siden til et bilde';
+$lang['first_contact_title18'] = 'Behandle album';
+$lang['first_contact_title19'] = 'Behandle Album';
+$lang['first_contact_title20'] = 'Behandle Album';
+$lang['first_contact_title21'] = 'Andre Tabs';
+$lang['first_contact_title22'] = 'Redigerings Siden til et Albums ';
+$lang['first_contact_title28'] = 'Konfigurasjon';
+$lang['first_contact_title29'] = '<em>Konfigurasjon » alternativer » Generelt</em>';
+$lang['first_contact_title3'] = 'Legg til en Bilde Side';
+$lang['first_contact_title33'] = '
+Gjeste innstillinger';
+$lang['first_contact_title34'] = 'Tema';
+$lang['first_contact_title35'] = 'Tema';
+$lang['first_contact_title37'] = 'Tema';
+$lang['first_contact_title38'] = 'Tillegsprogrammer';
+$lang['first_contact_title39'] = 'Tillegsprogrammer';
+$lang['first_contact_title4'] = 'La oss legge til et bilde!';
+$lang['first_contact_title43'] = 'Det har vært en flott tid';
+$lang['first_contact_title9'] = 'Batch Manager';
+$lang['first_contact_stp3'] = 'Denne første kategorien er der du legger til bilder direkte fra nettleseren. Den andre kategoriene er om de andre måtene å laste opp bilder på';
+$lang['first_contact_stp29'] = 'Her, på den første siden, kan du sette avgjørende og grunnleggende konfigurasjon i galleriet.';
+$lang['first_contact_stp28'] = 'Nå vil vi se på de mulighetene som er tilgjengelig for å få Piwigo til å virke slik du ønsker. Klikk på Configuration->Alternativer eller bare trykke Neste.';
+$lang['first_contact_stp23'] = 'Låse et album betyr at bare administratorer vil være i stand til å se dette albumet: svært nyttig når du forbereder albumet før publisering';
+$lang['first_contact_stp22'] = 'Som for et bilde, her kan du redigere egenskapene til et album';
+$lang['first_contact_stp21'] = 'Den neste kategorien er en side hvor du kan flytte de virtuelle album<br>i permalink kategorien kan du lage en bestemt url for et album<br>Men la oss nå redigere et album: Klikk på Rediger et album';
+$lang['first_contact_stp20'] = 'Når musen holdes over et album, lenker og informasjon vises. Når du drar og slipper et album -en avrundet ramme-, vil du kunne endre dens posisjon og deretter kan du lagre eller avbryte den manuell handlingen.';
+$lang['first_contact_stp2'] = 'Linken er her. Klikk på den eller klikk neste for å forsette';
+$lang['first_contact_stp19_b'] = 'Her er en liste over \'root\' albumer.';
+$lang['first_contact_stp19'] = 'Her er en listet over "root" albumer. Hvis du ser et album med en stiplet bakgrunn, betyr dette at det er et fysisk album fra en FTP-synkronisering. Fysiske albumer kan ikke flyttes eller slettes herfra. De andre albumene kalles virtuelle albumer.';
+$lang['first_contact_stp1'] = 'Hei! Jeg vil være din guide på denne omvisningen når du skal oppdage Piwigo. Følg mine instruksjoner, og klikk på Neste (eller bruk pilene på tastaturet) for å navigere. Hvis du går til en annen side i administrasjonen, vil du bli omdirigert til den gjeldende siden av omvisningen. Hvis du står fast og kan ikke avslutte turen, gå til <em> Tillegsprogrammer »ta en omvisning </ em> dette vil avslutte omvisningen. <br> La oss begynne med å legge til bilder!';
+$lang['first_contact_stp18'] = 'La oss nå se hvordan man behandler albumer. Klikk<em>På album »Behandle</em>eller klikk Neste';
+$lang['first_contact_stp17'] = 'Her kan du velge bildet som et miniatyrbilde til en eller flere albumer. Bildet trenger ikke å være assosiert med albumet for å være albumets miniatyrbilde. Dersom bildet er assosiert med albumet, er det enda enklere å angi det som albums miniatyrbilde fra den offentlig side til bildet (tips: bruk tillegsprogrammet Admin Verktøy for å gjøre dette enklere).';
+$lang['first_contact_stp16'] = '...i hvilket album (er) er bildet.<br><br>Så her kan du legge til eller fjerne bildet fra albumer. Koblingen er virtuell, ingen bilder vil fysisk bli flyttet noensinne.';
+$lang['first_contact_stp11'] = 'Herfra kan du velge ett eller flere bilder';
+$lang['first_contact_stp12'] = 'deretter redigere dem ved hjelp av en handling';
+$lang['first_contact_stp13'] = 'Du kan redigere beskrivelser og mer fra batch manager i enkeltmodus. Men for nå, la oss bli i den globale modusen';
+$lang['first_contact_stp14'] = 'Nå vil vi se på <b> rediger side </ b> av ett bilde. Den siden er tilgjengelig herfra, men også fra det offentlige fotoside når du er logget på som administrator.<br><br>Merk miniatyrbilde og klikk deretter på rediger.';
+$lang['first_contact_stp15'] = 'På denne siden kan du redigere alle egenskapene til et bilde, for eksempel...';
+$lang['first_contact_stp10'] = 'Her er en link som gir deg direkte tilgang til din caddy: dette er et verktøy for administratorer som enkelt kan velge bilder når de surfer på den offentlige delen og ønsker å redigere bildene i Batch Manager. <br><br> Du kan legge til bilder i caddy fra den offentlige fotoside når du er logget på som administrator, eller ved hjelp av handlingen "Legg til caddy" i Batch Manager.';
