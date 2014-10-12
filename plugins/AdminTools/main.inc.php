6b2f7b9634967e646271d38d3611188ca30c4d2f
ddtddt <ddtddt@piwigo.org>
2014-10-12 06:24:15 +0000

    [Trunk] - plugin - language - prepare 2.7.1
    git-svn-id: http://piwigo.org/svn/trunk@30012 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/AdminTools/main.inc.php
+++ plugins/AdminTools/main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Admin Tools
-Version: 2.7.0
+Version: 2.7.1
 Description: Do some admin task from the public pages
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=720
 Author: Piwigo team
