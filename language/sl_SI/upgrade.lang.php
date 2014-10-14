5e004538cb64ff33d59fd6e67f369f42d9c1c315
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-14 19:37:41 +0000

    [trunk_upgrade] Update sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/trunk@30069 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sl_SI/upgrade.lang.php
+++ language/sl_SI/upgrade.lang.php
@@ -28,7 +28,7 @@ $lang['Statistics'] = 'Statistika';
 $lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Ta stran predlaga, da nadgradite podatkovno bazo vaše Piwigo različice na novejšo različico. Čarovnik nadgradnje je zaznal, da trenutno uporabljate <strong>različico %s</strong> (ali enakovredno).';
 $lang['Upgrade'] = 'Nadgradi';
 $lang['Upgrade from version %s to %s'] = 'Nadgradi iz različice %s na različico %s';
-$lang['Upgrade informations'] = 'Informacije nadgradnje';
+$lang['Upgrade informations'] = 'Povzetek posodobitve';
 $lang['User permissions and group permissions have been erased'] = 'Dovoljenja uporabnikov in skupin so bila izbrisana';
 $lang['You do not have access rights to run upgrade'] = 'Nimate dovoljenja za izvedbo nadgradnje';
 $lang['total SQL time'] = 'skupen SQL čas';
@@ -37,4 +37,3 @@ $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'V <i>%s</i>, pred <b>?></b>,
 $lang['Only administrator can run upgrade: please sign in below.'] = 'Nadgradnjo lahko opravi le Administrator. Prosimo prijavite se spodaj:';
 $lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Kot previdnostni ukrep so sledeče preobleke onemogočene. Preveriti morate posodobitve za te preobleke in jih nato ponovno omogočiti:';
 $lang['All sub-albums of private albums become private'] = 'Vsi pod-albumi zasebnega albuma postanejo zasebni';
-?>
