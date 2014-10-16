55c02de13740bfe26850540baf8ecf4a183d08f3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-16 22:07:08 +0000

    [trunk_install] Update br_FR, thanks to : ddtddt
    git-svn-id: http://piwigo.org/svn/trunk@30171 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/br_FR/install.lang.php
+++ language/br_FR/install.lang.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 $lang['Note you can change your configuration by yourself and restart Piwigo after that.'] = 'Posubl eo deoc\'h cheñch ho-unan kefluniadur PHP, ha adloc\'hañ Piwigo da c\'houde.';
 $lang['Download the config file'] = 'Pellgargañ ar restr gefluniañ';
-$lang['Try to configure PHP 5'] = 'Esaeit kefluniañ PHP 5';
+$lang['Try to configure PHP 5'] = 'Esaeit kefluniañ PHP 5.2';
 $lang['Basic configuration'] = 'Kefluniadur diazez';
 $lang['Database configuration'] = 'Kefluniadur an diaz roadennoù';
 $lang['An alternate solution is to copy the text in the box above and paste it into the file "local/config/database.inc.php" (Warning : database.inc.php must only contain what is in the textarea, no line return or space character)'] = 'Un diskoulm all a zo eilañ an destenn eus boest a-us ha pegañ anezhañ da restr "local/config/database.inc.php" (Diwallit: ret eo lakaat e-barzh database.inc.php ar pezh a zo er framm-testenn, distro d\'al linenn ebet, hag arouezenn esaouiñ ebet)';
@@ -34,15 +34,15 @@ $lang['User'] = 'Implijer';
 $lang['please enter your password again'] = 'Lakait ho ger-tremen adarre';
 $lang['user password given by your host provider'] = 'ger-tremen roet gant ho pourvezer ostiz';
 $lang['Password ']['confirm'] = 'Ger-tremen [evit kadarnaat]';
-$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo n\'eo ket deuet a-benn da gefluniañ PHP 5.';
-$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigo a c\'hell cheñch ho kefluniadur da PHP 5 en ur grouiñ pe kemmañ ur restr .htaccess.';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo n\'eo ket deuet a-benn da gefluniañ PHP 5.2';
+$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigo a c\'hell cheñch ho kefluniadur da PHP 5.2 en ur grouiñ pe kemmañ ur restr .htaccess.';
 $lang['Creation of config file local/config/database.inc.php failed.'] = 'C\'hwitet eo krouadur ar restr gefluniañ local/config/database.inc.php.';
 $lang['You can download the config file and upload it to local/config directory of your installation.'] = 'Posubl eo deoc\'h pellgargañ ar restr gefluniañ, hag ezkargañ anezhañ da kavlec\'h local/config ho staliadur.';
 $lang['Congratulations, Piwigo installation is completed'] = 'Gourc\'hemennoù, echu eo staliadur Piwigo';
 $lang['Installation'] = 'Staliadur';
 $lang['Start Install'] = 'Kregiñ gant ar staliadur';
 $lang['Welcome to your new installation of Piwigo!'] = 'Degemer mat en ho staliadur nevez Piwigo !';
-$lang['You may referer to your hosting provider\'s support and see how you could switch to PHP 5 by yourself.'] = 'Goulennit da skor ho pourvezer ostiz penaos e c\'hellit implij PHP 5.';
+$lang['You may referer to your hosting provider\'s support and see how you could switch to PHP 5 by yourself.'] = 'Goulennit da skor ho pourvezer ostiz penaos e c\'hellit implij PHP 5.2';
 $lang['It appears your webhost is currently running PHP %s.'] = 'PHP %s a zo o labourat war hoc\'h ostiz.';
 $lang['localhost or other, supplied by your host provider'] = 'localhost pe anv all, roet gant ho pourvezer ostiz';
 $lang['also given by your host provider'] = 'roet gant ho pourvezer ostiz ivez';
@@ -52,7 +52,7 @@ $lang['Can\'t connect to server'] = 'Ne oa ket posubl kevreañ ouzh ar servijer'
 $lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'Sikour ho peus ezhom? Savit ho goulennoù war <a href="%s">Forom Piwigo</a>.';
 $lang['verification'] = 'gwiriadur';
 $lang['Sorry!'] = 'Digarezit !';
-$lang['PHP 5 is required'] = 'Ret eo kaout PHP 5';
+$lang['PHP 5 is required'] = 'Ret eo kaout PHP 5.2';
 $lang['Just another Piwigo gallery'] = 'Ur palier-luc\'hskeudennoù Piwigo all';
 $lang['Hope to see you back soon.'] = 'Ken ar wech all !';
 $lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Lennit hor foromoù evit kaout sikour: %s';
@@ -64,4 +64,3 @@ $lang['Database table prefix'] = 'Rakger taolennoù an diaz roadennoù';
 $lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'Ar rakger-mañ a vo ouzhpennet da anvioù taolennoù an diaz roadennoù (Gwelloc\'h e vo mererezh ho taolennoù)';
 $lang['Database name'] = 'Anv an diaz roadennoù';
 $lang['Connection to server succeed, but it was impossible to connect to database'] = 'Kevreet mat ouzh ar servijer omp, met ne oa ket posubl kevreañ ouzh an diaz roadennoù.';
-?>
