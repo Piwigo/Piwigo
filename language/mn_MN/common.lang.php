4400a05c8cecfabcd7b5e89762b91ec539cc2522
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-22 19:38:19 +0000

    [trunk_public] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/trunk@30258 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/mn_MN/common.lang.php
+++ language/mn_MN/common.lang.php
@@ -423,3 +423,4 @@ $lang['Apply on properties'] = 'Тохиргоог хадгалах';
 $lang['No results for'] = 'Ийм үр дүн олдсонгүй';
 $lang['Album name, A &rarr; Z'] = 'Цомогын нэр, A &rarr; Z';
 $lang['Album name, Z &rarr; A'] = 'Цомогын нэр, Z &rarr; A';
+$lang['Link: %s'] = 'Холбоос: %s';
