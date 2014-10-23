b22e747bc13dcdca20fb02ef4aa3548cfe2162e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-23 06:06:47 +0000

    [first_contact] Update vi_VN, thanks to : thanhphan467
    git-svn-id: http://piwigo.org/svn/trunk@30265 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/vi_VN/tour_first_contact.lang.php
+++ plugins/TakeATour/language/vi_VN/tour_first_contact.lang.php
@@ -22,3 +22,12 @@
 // +-----------------------------------------------------------------------+
 $lang['first_contact_stp1'] = 'Hi! Tôi sẽ hướng dẫn bạn về piwigo. Hãy làm theo hướng dẫn, ấn nút Next(Hoặc sử dụng phím mũi tên trên bàn phím) để điều hướng. Nếu bạn đi đến một trang khác thuộc quyền của bạn, bạn sẽ được chuyển đến trang hiện tại, nếu bạn không thể kết thúc phần tìm hiểu bạn hãy vào <em>Plugin >> Take A tour </em> để kết thúc . Nào, giờ ta bắt đầu bằng việc Thêm hình ảnh!';
 $lang['first_contact_stp2'] = 'Liên kết ở đây, ấn vào đây hoặc ấn vào Next để tiếp tục';
+$lang['first_contact_stp18'] = 'Để thấy làm sao quản lý album. Ấn vào <em>On album » Manage</em> or chọn Next';
+$lang['first_contact_stp22'] = 'Như cho hình ảnh, tại đây có thể chỉnh sữa thuộc tính của album';
+$lang['first_contact_stp28'] = 'Giờ chúng ta thấy những lựa chọn để cấu hình Piwigo hoạt động theo cách của bạn. Ấn vào cấu hình -> lựa chọn hoặc chỉ ấn Next';
+$lang['first_contact_stp30'] = 'Hãy bắt đầu để thay đổi tiêu đề';
+$lang['first_contact_stp16'] = '...trong đó một album( hoặc nhiều album) là hình ảnh.<br><br> vì vậy bạn có thể thêm hoặc bớt hình ảnh trong albums. liên kết không có thật. Không hình ảnh sẽ không hiển thị khi di chuyển qua nó.';
+$lang['first_contact_stp13'] = 'Bạn có thể chỉnh sữa mô tả và nhiều hơn tại quản lý chung ở chế độ single. Nhưng cho hiện tại, bạn hãy để chế độ global';
+$lang['first_contact_stp12'] = 'Sau đó chỉnh sửa bằng cách sử dụng một hành động';
+$lang['first_contact_stp11'] = 'Bạn có thể chọn một hoặc nhiều hình ảnh tại đây
+';
