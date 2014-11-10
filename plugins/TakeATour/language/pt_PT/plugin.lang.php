57385d271540be1db81a4cd224556160781d020d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-10 07:06:17 +0000

    [takeatour] Update pt_PT, thanks to ANO
    git-svn-id: http://piwigo.org/svn/trunk@30406 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/pt_PT/plugin.lang.php
+++ plugins/TakeATour/language/pt_PT/plugin.lang.php
@@ -31,3 +31,8 @@ $lang['First Contact'] = 'Primeiro contacto';
 
 $lang['2.7 Tour'] = 'Apresentação do 2.7';
 $lang['2_7_0_descrp'] = 'Descobre o que há de novo no Piwigo 2.7';
+$lang['Privacy'] = 'Privacidade';
+$lang['TAT_descrp'] = 'Bem-vindo(a)! Aqui está uma lista de todas as possibilidades disponíveis para ajudar a descobrir as possibilidades da sua galeria Piwigo.<br><br> Inicie uma visita abaixo e siga as instruções. Clique em Próximo/anterior (ou use as setas do seu teclado) para navegar durante uma visita. Enquanto está numa visita, se for para outra página da administração, será redirecionado(a) para a página da visita que efetuava. Se está bloqueado(a) e não pode terminar o passeio, vá para <em>Plugins» Fazer uma visita</em> e terminará a visita.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Dê uma vista de olhos e descubra as possibilidades da sua galeria Piwigo >> Veja as possibilidades de verificação existentes.';
+$lang['first_contact_descrp'] = 'Recomendado para iniciantes, esta visita introduze-o no Piwigo e suas possibilidades básicas. Começa por lhe mostrar como adicionar fotos, e depois administra-las. A visita continua com a gestão do album e termina com personalização usando configuração, temas e extenções.';
+$lang['privacy_descrp'] = 'Esta visita apresenta-lhe todas as hipóteses para proteger as suas fotos de acesso público com permissões por album e foto, e de copias usando proteções de imagem.';
