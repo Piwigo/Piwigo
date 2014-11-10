a9dd06e03d314f694ac401070833df77fea8e550
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-10 12:04:33 +0000

    [admin_tools] Update cs_CZ, thanks to marfig
    git-svn-id: http://piwigo.org/svn/trunk@30410 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/AdminTools/language/cs_CZ/plugin.lang.php
+++ plugins/AdminTools/language/cs_CZ/plugin.lang.php
@@ -13,4 +13,8 @@ $lang['Save visit in history'] = 'Ukládat návštevy do historie';
 $lang['Show SQL queries'] = 'Zobrazit SQL queries';
 $lang['View as'] = 'Zobrazit jako';
 
-?>
+$lang['Closed icon position'] = 'Poloha ikony pro zavření';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Udělit přístup do rychlé editace vlastníkům fotek i když nejsou admin';
+$lang['Open toolbar by default'] = 'Výchozí otevření panelu nástrojů';
+$lang['left'] = 'levý';
+$lang['right'] = 'pravý';
