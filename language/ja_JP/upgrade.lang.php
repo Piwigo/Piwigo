ab6567a23f4ffc0c4ebe0648f48540c91949401f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-12 07:04:07 +0000

    [trunk_upgrade] Update ja_JP, thanks to ma2tak
    git-svn-id: http://piwigo.org/svn/trunk@30442 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ja_JP/upgrade.lang.php
+++ language/ja_JP/upgrade.lang.php
@@ -35,10 +35,9 @@ $lang['Upgrade informations'] = '更新情報';
 $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = '問題がある場合、[管理 > ツール > メンテナンス] でメンテナンスチェックを実行してください。';
 $lang['Only administrator can run upgrade: please sign in below.'] = '管理者のみアップグレードを実行できます: 以下でログインしてください。';
 $lang['You do not have access rights to run upgrade'] = 'あなたには、アップグレードを実行する権限がありません。';
-$lang['All sub-albums of private albums become private'] = 'プライベートカテゴリのすべてのサブカテゴリがプライベートにされました。';
+$lang['All sub-albums of private albums become private'] = 'プライベートアルバムに含まれるすべてのサブアルバムがプライベートにされました。';
 $lang['User permissions and group permissions have been erased'] = 'ユーザパーミッションおよびグループパーミッションが削除されました。';
 $lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = '以前の設定より、サムネイル接頭辞およびウェブマスターのメールアドレスのみ保存されました。';
 $lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = '予防措置として、次のプラグインは無効になりました。プラグインを有効にする前に、プラグインのアップグレードを確認してください。:';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i>に、 <b>?></b>の前に入力:';
 $lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = '予防措置として、次のテーマが無効になりました。テーマを有効にする前に、アップグレードを確認してください。：';
-?>
