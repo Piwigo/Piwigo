25071b3f7b4988c65643e88e4750e2b6623a40dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-13 20:49:51 +0000

    [tour_2_7_0] Update nb_NO, thanks to paulen
    git-svn-id: http://piwigo.org/svn/trunk@30482 68402e56-0260-453c-a942-63ccdbb3a9ee

--- plugins/TakeATour/language/nb_NO/tour_2_7_0.lang.php
+++ plugins/TakeATour/language/nb_NO/tour_2_7_0.lang.php
@@ -21,11 +21,11 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['2_7_0_stp2'] = 'Som du kan se, har det blitt lagt til et nytt standard tillegsprogram: Ta en Omvisning. Dette tillegsprogrammet hjelper deg å finne mer ut av din Piwigo. Her er det en listet med alle tilgjengelige omvisninger. Denne siden er tilgjengelig under Tillegsprogrammer » Ta en omvisning';
-$lang['2_7_0_stp11'] = 'Denne omvisningen er over <p style="text-align:center">. Nyt Piwigo 2.7!</p>Hvis du liker Piwigo og ønsker å støtte oss, kan du bidra ved å hjelpe med oversettelser, donere, etc.<a! href = "http://piwigo.org/basics/contribute"target="_blank"> Klikk her for å støtte oss</a>Takk!';
-$lang['2_7_0_stp10'] = 'Et filter på filstørrelse har også blitt lagt til batch manager. Du kan velge et område med et minimum og maksimum verdi.';
+$lang['2_7_0_stp11'] = 'Denne omvisningen er over. <p style="text-align:center"> Nyt Piwigo 2.7!</p>Hvis du liker Piwigo og ønsker å støtte oss, kan du bidra ved å hjelpe med oversettelser, donere, etc.  <a href="http://piwigo.org/basics/contribute" target="_blank"> Klikk her for å støtte oss </a> Takk!';
+$lang['2_7_0_stp10'] = 'Et filter på filstørrelse har også blitt lagt til batch behandler. Du kan velge et område med et minimum og maksimum verdi.';
 $lang['2_7_0_stp1'] = 'Hei! Jeg vil være din guide på denne omvisningen når du skal oppdage Piwigo. Følg mine instruksjoner, og klikk på Neste (eller bruk pilene på tastaturet) for å navigere. Hvis du går til en annen side, vil du bli omdirigert til den gjeldende siden av omvisningen.';
 $lang['2_7_0_title9'] = 'Søke filter';
-$lang['2_7_0_title8'] = 'Tømm din caddy';
+$lang['2_7_0_title8'] = 'Tøm din kurv';
 $lang['2_7_0_title6'] = 'Velg bokser';
 $lang['2_7_0_title7'] = 'Sorter albumer etter dato';
 $lang['2_7_0_title5'] = 'Tid';
@@ -35,10 +35,10 @@ $lang['2_7_0_title2'] = 'Først ny funksjon';
 $lang['2_7_0_title11'] = 'Ser deg senere!';
 $lang['2_7_0_title10'] = 'Fil størrelses filter';
 $lang['2_7_0_title1'] = 'Velkommen til Piwigo 2.7';
-$lang['2_7_0_stp9'] = 'Et søke filter har blitt lagt til batch manager. Den har en kraftig syntaks for å søke etter bilder på datoer, tags, størrelse, filnavn og også et søke alternativer som eksakt match, OG/ELLER, gruppering, utelukkelse. Du kan lese detaljert hjelp senere. Den samme syntaks finner du ogå under raskt søk-feltet under menyen på den offentlig siden.';
-$lang['2_7_0_stp8'] = 'Enkel handlings link til å tømme caddie i bare ett klikk.';
+$lang['2_7_0_stp9'] = 'Et søke filter har blitt lagt til batch behandler. Den har en kraftig syntaks for å søke etter bilder på datoer, taggs, størrelse, filnavn og også et søke alternative som eksakt match, OG/ELLER, gruppering, utelukkelse. Du kan lese detaljert hjelp senere. Den samme syntaks finner du ogå under raskt søk-feltet under menyen på den offentlig siden.';
+$lang['2_7_0_stp8'] = 'Enkel handlings link for å tømme kurven med bare ett klikk.';
 $lang['2_7_0_stp7'] = 'Nå kan du sortere dine albumer basert på bildenes  opprettelsesdato eller postet dato';
 $lang['2_7_0_stp6'] = 'Boksen for å velge album,koder etc er endret til en mere brukervennlig. Test det nedenfor';
 $lang['2_7_0_stp5'] = 'Du kan stille inn tiden i tillegg til dato, ved hjelp av time og minutt glidere';
-$lang['2_7_0_stp4'] = 'Søke siden er omarbeidet: muligheten til å velge egenskaper der ordesøk gjelder, liste over forfattere som finnes i databasen, autofullfør-listen over koder, autofullfør-listen over albumer.';
+$lang['2_7_0_stp4'] = 'Søke siden er omarbeidet: muligheten til å velge egenskaper der ordsøk gjelder, liste over forfattere som finnes i databasen, autofullfør-listen over koder, autofullfør-listen over albumer.';
 $lang['2_7_0_stp2b'] = 'Det nye opplastingsskjemaet i Piwigo 2.7 er basert på HTML5 i stedet for Flash, dette betyr en bedre kompatibilitet med moderne nettlesere samt en økt pålitelighet på trege nettforbindelser. Og en dra & slipp funksjon vil gjøre bilde utvalget mye lettere fra din filbehandler!';
