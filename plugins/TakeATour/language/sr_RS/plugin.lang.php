9e120862c51e00ef4319bd13f0b0b6ab8ac11f54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-17 06:23:04 +0000

    [takeatour] Update sr_RS, thanks to gpopac
    git-svn-id: http://piwigo.org/svn/trunk@30508 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/sr_RS/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['takeatour_configpage'] = 'Крени у обилазак » Списак обиласка';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Крените у обилазак занимљивих могућности Piwigo галерије » Иди на доступне обиласке';
+$lang['Visit your Piwigo!'] = 'Посетитеве вашу Piwigo галерију!';
+$lang['first_contact_descrp'] = 'Ово упутство је препоручљиво за почетнике пошто вас води кроз основне могућности Piwigo галерије. Обилазак почиње додавањем нових слика и њиховим управљањем, а затим вас води кроз управљање албума и фино подешавање ваше галерије, тема и додатака.';
+$lang['privacy_descrp'] = 'Овај обилазак ће вам показати све начине за заштиту приступа вашим фотографијама, укључујући дозволу по албуму, по слици и забрану копирања преко заштите слика.';
+$lang['2.7 Tour'] = 'Обилазак верзије 2.7';
+$lang['2_7_0_descrp'] = 'Откријте шта је ново у Piwigo верзији 2.7';
+$lang['End tour'] = 'Крај обиласка';
+$lang['First Contact'] = 'Први контакт';
+$lang['Next '] = 'Наредно';
+$lang['Prev'] = 'Претходно';
+$lang['Privacy'] = 'Приватност';
+$lang['Start the Tour'] = 'Започни обилазак';
+$lang['TAT_descrp'] = 'Добродошли! Овде се налази списак доступних упутстава која ће вас провести кроз Piwigo галерију.<br><br>Покрените неки од обиласка, а затим следите упутства. За кретање кликните на Наредно/Претходно или користите стрелице на вашој тастатури. Док сте у обиласку, одлазак на другу администраторску страницу ће вас преусмерити на тренутну страницу обилсака. Уколико се изгубите и не можете да напустите обилазак, идите на <em>Додаци » Крени у обилазак</em>.';
