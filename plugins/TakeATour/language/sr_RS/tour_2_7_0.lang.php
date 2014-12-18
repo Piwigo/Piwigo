f9da1a073837e54ee16ae273702c42dc44684746
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-18 21:28:24 +0000

    Update sr_RS, thanks to knjigor
    git-svn-id: http://piwigo.org/svn/trunk@30781 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/sr_RS/tour_2_7_0.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_title9'] = 'Филтер за претрагу';
+$lang['2_7_0_title10'] = 'Филтер величине фајла';
+$lang['2_7_0_title11'] = 'Видимо се ускоро!';
+$lang['2_7_0_title2b'] = 'Нова форма за додавање';
+$lang['2_7_0_title4'] = 'Страница за претрагу';
+$lang['2_7_0_title5'] = 'Време';
+$lang['2_7_0_title7'] = 'Поређај албуме по датуму';
+$lang['2_7_0_title1'] = 'Добро дошли у Piwigo 2.7';
