b1e0fd4b1371abd6f7093f3471c3d8390583bfb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-22 05:58:48 +0000

    Update sk_SK, thanks to xpace
    git-svn-id: http://piwigo.org/svn/trunk@30801 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/sk_SK/tour_first_contact.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_title15'] = 'Upravenie stránky fotky';
+$lang['first_contact_title18'] = 'Spravuj albumy';
+$lang['first_contact_title19'] = 'Správa Albumov';
+$lang['first_contact_title20'] = 'Správa Albumov';
+$lang['first_contact_title21'] = 'Ostatné Taby';
+$lang['first_contact_title22'] = 'Stránka Úpravy Albumu';
+$lang['first_contact_title28'] = 'Konfigurácia';
+$lang['first_contact_title3'] = 'Stránka Pridania Fotky';
+$lang['first_contact_title34'] = 'Témy';
+$lang['first_contact_title35'] = 'Témy';
+$lang['first_contact_title37'] = 'Témy';
+$lang['first_contact_title4'] = 'Pridajme fotku!';
+$lang['first_contact_title9'] = 'Správca Skupín';
+$lang['first_contact_stp35'] = 'Nastav svoju obľúbenú tému ako prednastavenú.';
+$lang['first_contact_stp4'] = 'Najsôr vyber album, alebo vytvor jeden ak žiadny neexistuje.';
+$lang['first_contact_stp5'] = 'Potom klikni na tlačítko pre výber fotiek, ktoré chceš poslať, alebo zober a presuň svoje súbory.';
+$lang['first_contact_stp6'] = 'Môžeš vidieť, že fotky sa listujú. Ak si pripravený/á klikni na tlačítko Začni Nahrávať';
+$lang['first_contact_stp8'] = 'Upravme ich! Klikni sa na linku.';
+$lang['first_contact_title1'] = 'Vitaj vo svojej Piwigo galérii';
+$lang['first_contact_title11'] = 'Ako upraviť fotku';
+$lang['first_contact_stp29'] = 'Tuto, na prvej strane, nastavíš hlavnú konfiguráciu svojej galérie.';
+$lang['first_contact_stp1'] = 'Ahoj! Budem tvojim sprievodcom objavovania Piwiga. Nasleduj moje inštrukcie, a klikni Ďalej (alebo použi šípky na klávesnici) pre navigáciu. Ak prejdeš na ďalšiu stranu administrácie, budeš presmerovaný/á na túto stranu tejto prehliadky. Ak sa zasekneš a nemôžeš ukončiť prehliadku, prejdením na <em>Pluginy » Začni Prehliadku</em> ukončí prehliadku.<br>Začnime pridávaním fotiek!';
+$lang['first_contact_stp18'] = 'Pozrime sa teraz ako spravovať albumy. Klikni <em>Na album » Spravovať</em> alebo stlač Ďalej';
+$lang['first_contact_stp19_b'] = 'Tu sú vylistované \'koreňové\' albumy.';
+$lang['first_contact_stp2'] = 'Linka sa nachádza tu. Klikni na ňu alebo klikni na Ďalej pre pokračovanie';
+$lang['first_contact_stp22'] = 'Ako pre fotku, tu môžeš upraviť vlastnosti albumu';
+$lang['first_contact_stp30'] = 'Začneme zmenou titulky';
+$lang['first_contact_stp32'] = 'Teraz ulož svoje zmeny';
+$lang['first_contact_stp11'] = 'Odtialto môžeš zvoliť jednu alebo viac fotiek';
+$lang['first_contact_stp12'] = 'potom ich môžeš upravovať použitím akcie';
+$lang['first_contact_stp13'] = 'Môžeš upravovať popisy a iné pomocou manažéra skupín v samostatnom móde. Ale zatiaľ zostaneme v globálnom móde. ';
+$lang['first_contact_stp15'] = 'Na tejto stránke môžeš editovať všetky vlastnosti fotky, napríklad...';
+$lang['first_contact_stp16'] = '...v ktorom/ktorých albume/albumoch sa táto fotka nachádza.<br><br>Takže tu môžeš pridať alebo odobrať túto fotku z albumov. Táto linka je virtuálna, takže žiadne fotky sa v skutočnosti nepresúvajú.';
