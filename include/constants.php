aa19337e9812fd0cebb641f918045be435990aa3
plegall <plg@piwigo.org>
2015-01-08 13:23:56 +0000

    next release is 2.6.5
    git-svn-id: http://piwigo.org/svn/branches/2.6@30869 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.6.4');
+define('PHPWG_VERSION', '2.6.5');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'elegant');
 
