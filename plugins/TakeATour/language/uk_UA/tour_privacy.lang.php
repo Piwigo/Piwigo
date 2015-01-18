99e0547b76df0520865e178ce545ae2b1608123c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-18 18:21:29 +0000

    Update uk_UA, thanks to animan
    git-svn-id: http://piwigo.org/svn/trunk@30894 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/uk_UA/tour_privacy.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp6'] = 'Коли Ви завантажуєте зображення, Ви можете змінити рівень конфіденційності прямо під час завантаження...';
+$lang['privacy_stp8'] = 'Виберіть одно або декілька зображень...';
+$lang['privacy_title22'] = 'Локальна конфігурація: оригінальний захист';
+$lang['privacy_title24'] = 'Це був прекрасний час';
+$lang['privacy_title1'] = 'Ласкаво просимо в тур Конфіденційності';
+$lang['privacy_title10'] = 'Дозволи альбому';
+$lang['privacy_title11'] = 'Дозволи альбому';
+$lang['privacy_title12'] = 'Дозволи альбому';
+$lang['privacy_title2'] = 'Допомога всередині вашого Piwigo';
+$lang['privacy_title5'] = 'Дозволи > Групи';
+$lang['privacy_title9'] = 'Рівень конфіденційності';
+$lang['privacy_title15'] = 'Порада';
+$lang['privacy_title17'] = 'Групи';
+$lang['privacy_title18'] = 'Групи';
+$lang['privacy_title19'] = 'Користувачі';
+$lang['privacy_title20'] = 'Загальнодоступні зображення';
+$lang['privacy_title21'] = 'Загальнодоступні зображення';
+$lang['privacy_title3'] = 'Дозволи';
+$lang['privacy_title4'] = 'Дозволи';
+$lang['privacy_title6'] = 'Рівень конфіденційності';
+$lang['privacy_title7'] = 'Рівень конфіденційності';
+$lang['privacy_title8'] = 'Рівень конфіденційності';
+$lang['privacy_stp11'] = 'Тепер перейдіть на вкладку дозволи';
+$lang['privacy_stp13'] = 'Тепер натисніть кнопку приватність.';
+$lang['privacy_stp3'] = 'Знайдіть хвилинку, щоб ознайомитися з інформацією нижче.';
