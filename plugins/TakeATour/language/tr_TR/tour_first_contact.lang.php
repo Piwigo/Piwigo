061d4d8a050aad2704e3eaf163f654e62249b4c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-02 19:39:11 +0000

    Update tr_TR, thanks to Nakre
    git-svn-id: http://piwigo.org/svn/trunk@30927 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/tr_TR/tour_first_contact.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_title28'] = 'Yapılandırma';
+$lang['first_contact_title33'] = 'Misafir ayarları';
+$lang['first_contact_title4'] = 'Haydi bir fotoğraf ekleyelim!';
+$lang['first_contact_title18'] = 'Albüm yönetimi';
+$lang['first_contact_title19'] = 'Albüm yönetimi';
+$lang['first_contact_title20'] = 'Albüm yönetimi';
+$lang['first_contact_title34'] = 'Temalar';
+$lang['first_contact_title35'] = 'Temalar';
+$lang['first_contact_title37'] = 'Temalar';
+$lang['first_contact_title38'] = 'Eklentiler';
+$lang['first_contact_title39'] = 'Eklentiler';
