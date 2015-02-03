55e4358fa3e7fd8b870b1954af1cf6f98dfd2c38
ddtddt <ddtddt@piwigo.org>
2015-02-03 06:52:32 +0000

    [extensions] - TakeATour - add fr_CA - Quebec - Québécois Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/trunk@30931 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/fr_CA/tour_2_7_0.lang.php
@@ -0,0 +1,23 @@
+<?php
+$lang['2_7_0_stp2'] = 'Comme vous pouvez le voir, un nouveau greffon a été ajouté par défaut : Take a Tour. Ce greffon vous aide à découvrir votre Piwigo. Ici sont listées les visites disponibles.<br/><br/>Cette page est accessible dans <em>Plugins » Take A Tour</em>';
+$lang['2_7_0_title2'] = 'Première nouvelle fonctionalité';
+$lang['2_7_0_title1'] = 'Bienvenue sur Piwigo 2.7';
+$lang['2_7_0_title11'] = 'À bientôt !';
+$lang['2_7_0_stp1'] = 'Salut ! Je serai votre guide pour découvrir Piwigo. Merci de suivre mes instructions et de cliquer sur Suivant (ou d\'utiliser les flèches du clavier) pour naviguer. Si vous allez sur une autre page, vous serez redirigé vers la page courante de la visite.';
+$lang['2_7_0_stp11'] = 'Cette visite est terminée.<p style="text-align:center">Profitez bien de Piwigo 2.7 !</p>Si vous aimez Piwigo et voulez nous soutenir, vous pouvez contribuer en nous aidant sur les traductions, dons, etc. <a href="http://fr.piwigo.org/basics/contribute" target="_blank">Cliquez ici pour nous soutenir</a>';
+$lang['2_7_0_stp5'] = 'Vous pouvez inscrire l\'heure en plus de la date en utilisant les curseurs Heures et Minutes';
+$lang['2_7_0_title6'] = 'Boîtes de sélection';
+$lang['2_7_0_title5'] = 'Heure';
+$lang['2_7_0_title4'] = 'Page de recherche';
+$lang['2_7_0_stp4'] = 'La page de recherche a été retravaillée : il est possible de sélectionner plusieurs propriétés sur lesquelles la recherche va s\'appliquer. Nouvelles listes auto-complétées des auteurs, des mots-clefs, des albums, etc.';
+$lang['2_7_0_stp6'] = 'La boîte de sélection pour choisir les albums, les mots-clefs, etc. a changé en faveur d`\'une nouvelle plus conviviale. Testez-la ci-dessous.';
+$lang['2_7_0_title7'] = 'Tri des albums par date';
+$lang['2_7_0_title8'] = 'Vider le panier';
+$lang['2_7_0_title9'] = 'Filtre de recherche';
+$lang['2_7_0_title2b'] = 'Nouveau formulaire d\'ajout';
+$lang['2_7_0_stp9'] = 'Un filtre de recherche a été ajouté au gestionnaire par lot. Il embarque une syntaxe très puissante pour trouver vos photos par dates, mots-clefs, poids, nom de fichier, etc. et aussi des options comme la correspondance exacte, le ET/OU (AND/OR), le regroupement ou l\'exclusion. Vous pourrez lire l\'aide détaillée plus tard. La même syntaxe s\'applique à la recherche rapide dans le menu côté galerie.';
+$lang['2_7_0_stp8'] = 'Nouveau lien tout simple pour vider votre panier en 1 clic.';
+$lang['2_7_0_stp7'] = 'Dorénavant vous pouvez trier vos albums selon la date de création ou la date d\'ajout des photos qu\'il contient.';
+$lang['2_7_0_stp2b'] = 'Le nouveau formulaire d\'ajout de photos de Piwigo 2.7 exploite le HTML5 en remplacement de Flash, ce qui signifie une meilleure compatibilité avec les navigateurs web modernes tout en augmentant la fiabilité des transferts sur les connexions Internet les plus lentes. De plus, le glisser/déposer à partir de votre explorateur de fichiers va rendre la sélection de photos bien plus facile !';
+$lang['2_7_0_title10'] = 'Filtre par poids';
+$lang['2_7_0_stp10'] = 'Un filtre sur le poids de la photo a également été ajouté au gestionnaire par lot. Vous pouvez sélectionner une plage avec une valeur minimum et maximum.';
