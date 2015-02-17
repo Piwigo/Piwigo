2921c25e0024617d05186e9baf3d4927828991f7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-17 09:40:18 +0000

    Update tr_TR, thanks to Nakre
    git-svn-id: http://piwigo.org/svn/trunk@30977 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/tr_TR/tour_privacy.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['privacy_title24'] = 'Bu harika zaman geçti.';
+$lang['privacy_title1'] = 'Gizlilik Turuna Hoşgeldiniz';
+$lang['privacy_stp11'] = 'Şimdi İzinler sekmesine tıklayınız.';
+$lang['privacy_stp13'] = 'Şimdi Özele tıklayınız.';
+$lang['privacy_stp3'] = 'Aşağıdaki bilgileri okumak için bir dakikanızı ayırın.';
+$lang['privacy_title6'] = 'Gizlilik Seviyesi';
+$lang['privacy_title7'] = 'Gizlilik Seviyesi';
+$lang['privacy_title8'] = 'Gizlilik Seviyesi';
+$lang['privacy_title9'] = 'Gizlilik Seviyesi';
+$lang['privacy_title3'] = 'İzinler';
+$lang['privacy_title4'] = 'İzinler';
+$lang['privacy_title10'] = 'Albüm izinleri';
+$lang['privacy_title11'] = 'Albüm izinleri';
+$lang['privacy_title12'] = 'Albüm izinleri';
+$lang['privacy_title15'] = 'İpucu';
+$lang['privacy_title17'] = 'Gruplar';
+$lang['privacy_title18'] = 'Gruplar';
+$lang['privacy_title19'] = 'Kullanıcılar';
