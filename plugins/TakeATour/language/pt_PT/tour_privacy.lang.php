a76e3c6ac25093cef8fa417eb86ea9c3d7ef9c0c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-25 07:17:38 +0000

    Update pt_PT, thanks to vn
    git-svn-id: http://piwigo.org/svn/trunk@31005 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/pt_PT/tour_privacy.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp1'] = 'Olá! Eu vou ser o seu guia para descobrir como pode proteger as suas fotos no Piwigo. Por favor siga as minhas instruções, e prima Próximo (Next) - ou use as setas do teclado - para navegar. Se for a outra página da administração, será redireccionado para a página em que se encontra. Se estiver com dificuldade e não puder terminar, vá a <em>Plugins » Take A Tour</em> para concluir.<br>Comecemos!';
