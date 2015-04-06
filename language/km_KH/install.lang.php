a00c7bb253ae02395ed2dca6b53a2b075392e417
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-06 04:47:24 +0000

    Update km_KH, thanks to Vanndy
    git-svn-id: http://piwigo.org/svn/trunk@31073 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/km_KH/install.lang.php
+++ language/km_KH/install.lang.php
@@ -41,3 +41,25 @@ $lang['Welcome to your new installation of Piwigo!'] = 'ស្វាគមន៍
 $lang['Welcome to my photo gallery'] = 'ស្វាគមន៍​មកកាន់​វិចិត្រសាលរូបភាព​របស់​ខ្ញុំ';
 $lang['Just another Piwigo gallery'] = 'គ្រាន់តែជា​វិចិត្រសាលផ្សេងទៀតរបស់​ភីវីហ្គូ';
 $lang['Congratulations, Piwigo installation is completed'] = 'អប់​អរ​សាទរ, ការ​បញ្ចូល​ភីវីហ្គូ​បាន​សម្រេច';
+$lang['also given by your host provider'] = 'ផ្គត់ផ្គង់ដោយអ្នកផ្គត់ផ្គង់ hosting របស់អ្នកផងដែរ';
+$lang['user login given by your host provider'] = 'ការចូលរបស់អ្នកប្រើប្រាស់ បានផ្តល់ដោយអ្នកផ្តល់ host របស់អ្នក';
+$lang['localhost or other, supplied by your host provider'] = 'localhost ឬផ្សេងទៀត ដែលបានផ្គត់ផ្គង់ដោយអ្នកផ្តល់ host របស់អ្នក';
+$lang['database tables names will be prefixed with it (enables you to manage better your tables)'] = 'ឈ្មោះតារាង database នឹងត្រូវបានដាក់បុព្វបទជាមួយវា (អនុញ្ញាតឱ្យអ្នកគ្រប់គ្រងតារាងរបស់អ្នកល្អប្រសើរជាងមុន)';
+$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigo អាចព្យាយាមដើម្បីប្តូរការកំណត់រចនាសម្ព័ន្ធរបស់អ្នកទៅ PHP 5.2 ដោយការបង្កើត ឬកែប្រែឯកសារ .htaccess';
+$lang['You can download the config file and upload it to local/config directory of your installation.'] = 'អ្នកអាចទាញយក​ឯកសាររចនាសម្ព័ន្ធ ហើយផ្ទុកឡើងទៅកាន់ថត local/config នៅក្នុងការតំឡើងរបស់អ្នក';
+$lang['Visitors will be able to contact site administrator with this mail'] = 'ភ្ញៀវនឹងអាចប្រើអ៊ីម៉ែលនេះដើម្បីទាក់ទងអ្នកគ្រប់គ្រងតំបន់បណ្ដាញ';
+$lang['Note you can change your configuration by yourself and restart Piwigo after that.'] = 'ចំណាំថាអ្នកអាចប្តូររចនាសម្ព័ន្ធដោយខ្លួនអ្នក ហើយបន្ទាប់មកចាប់ផ្តើម Piwigo សារជាថ្មី';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'ត្រូវការជំនួយ? សូមសួរសំណួររបស់អ្នកនៅលើ <a href="%s">ក្តារសារ Piwigo</a>';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'រក្សាទុកវាជាសម្ងាត់, វាអនុញ្ញាតឱ្យអ្នកដើម្បីចូលដំណើរការ administration panel';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'វានឹងត្រូវបានបង្ហាញដល់ភ្ញៀវ។ វាជាការចាំបាច់សម្រាប់ការគ្រប់គ្រងវ៉ិបសាយ';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo គឺមិនអាចកំណត់ PHP 5.2';
+$lang['It appears your webhost is currently running PHP %s.'] = 'នៅលើម៉ាស៊ីនបណ្តាញរបស់អ្នកបច្ចុប្បន្នកំពុងដំណើរការ PHP​ %s';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'ចូរកុំស្ទាក់ស្ទើរក្នុងការពិគ្រោះយោបល់នៅវេទិការបស់យើងសម្រាប់ជំនួយណាមួយ: %s';
+$lang['Creation of config file local/config/database.inc.php failed.'] = 'ការបង្កើតរចនាសម្ព័ន្ធឯកសារ local/config/database.inc.php បរាជ័យ';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'ការភ្ជាប់ទៅកាន់ម៉ាស៊ីនមេបានជោគជ័យ តែការភ្ជាប់ទៅកាន់ database​ គឺមិនអាចធ្វើទៅបាន';
+$lang['webmaster login can\'t contain characters \' or "'] = '
+ការចូលរបស់អ្នកគ្រប់គ្រងបណ្ដាញមិនអាចមានតួអក្សរ \' ឬ​ "';
+$lang['user password given by your host provider'] = 'ពាក្យសម្ងាត់របស់អ្នកប្រើប្រាស់់ដែលបានផ្គត់ផ្គង់ដោយអ្នកផ្តល់​ host របស់អ្នក';
+$lang['verification'] = 'ការផ្ទៀងផ្ទាត់';
+$lang['enter a login for webmaster'] = 'សូមបញ្ចូលឈ្មោះអ្នកគ្រប់គ្រងបណ្ដាញ';
+$lang['please enter your password again'] = 'សូមបញ្ចូលលេខសម្ងាត់របស់អ្នកម្តងទៀត';
