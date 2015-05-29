27e60234d667c34cdd7764010e2217deb0a930fd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-05-29 20:41:14 +0000

    Add he_IL, thanks to ymoran00
    git-svn-id: http://piwigo.org/svn/trunk@31187 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ plugins/TakeATour/language/he_IL/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2.7 Tour'] = 'סיור 2.7';
+$lang['2_7_0_descrp'] = 'גלה מה חדש ב-Piwigo 2.7';
+$lang['End tour'] = 'סוף סיור';
+$lang['First Contact'] = 'איש קשר ראשון';
+$lang['Next '] = 'הבא';
+$lang['Prev'] = 'הקודם';
+$lang['Privacy'] = 'פרטיות';
+$lang['Start the Tour'] = 'התחל את הסיור';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'צא לסיור וגלה את הפית׳רים של גלריית Piwigo שלך >> צא לסיורים זמינים';
+$lang['Visit your Piwigo!'] = 'בקר ב-Piwigo שלך!';
+$lang['takeatour_configpage'] = 'צא לסיור >> רשימת סיורים';
+$lang['TAT_descrp'] = 'ברוך הבא! הנה רשימה של כל הסיורים הקיימים לעזור לך לגלות את הפיצ׳רים של גלריית Piwigo שלך.<br><br>הפעל סיור למטה, ואז עקוב אחר ההוראות. ךלץ הבא/הקודם (או השתמש בחיצי המקלדת) לנווט במהלך סיור. כאשר אתה בסיור, אם אתה הולך לדף ניהול אחר, תועבר לדף העכשווי של הסיור. אם אתה תקוע ולא מצליח לסיים את הסיור, הגעה ל<em>תוספים >> קח סיור</em> תסיים את הסיור.';
