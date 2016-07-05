c72a414a4832d2f768161b87188f16cb151f6cc9
plegall <plg@piwigo.org>
2016-07-05 14:02:01 +0200

    cheery-pick 8 commits from translation
    git log --reverse --oneline  translation ^master --pretty=format:"git cherry-pick --no-commit %H" c7465d6..39ad66c | bash

---
+++ language/id_ID/install.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['verification'] = 'Verifikasi';
+$lang['Need help ? Ask your question on <a href="%s">Piwigo message board</a>.'] = 'Butuh bantuan? Ajukan pertannyaan anda ke <a href="%s">Piwigo message board</a>.';
+$lang['Note you can change your configuration by yourself and restart Piwigo after that.'] = 'Catat bahwa anda dapat mengubah pengaturan anda sendiri kemudian mulai ulang Piwigo.';
+$lang['PHP 5 is required'] = 'PHP 5.2 diperlukan';
+$lang['Password ']['confirm'] = 'Kata sandi [konfirmasi]';
+$lang['Piwigo may try to switch your configuration to PHP 5 by creating or modifying a .htaccess file.'] = 'Piwigo mungkin akan mencoba mengganti pengaturan anda ke PHP 5.2 dengan membuat atau mengubah sebuah berkan .httaccess';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo tidak dapat mengatur PHP 5.2';
+$lang['Sorry!'] = 'Maaf!';
+$lang['Start Install'] = 'Mulai pemasangan';
+$lang['Try to configure PHP 5'] = 'Cobalah untuk mengatur PHP 5.2';
+$lang['User'] = 'Pengguna';
+$lang['Visitors will be able to contact site administrator with this mail'] = 'Pengunjung akan dapat menggunakan email ini untuk menghubungi administrator situs';
+$lang['Welcome to my photo gallery'] = 'Selamat datang ke foto galeriku';
+$lang['Welcome to your new installation of Piwigo!'] = 'Selamat datang ke instalasi baru dari Piwigo!';
+$lang['Admin configuration'] = 'Pengaturan administrasi';
+$lang['Basic configuration'] = 'Pengaturan dasar';
+$lang['Can\'t connect to server'] = 'Tidak dapat terhubung ke server';
+$lang['Congratulations, Piwigo installation is completed'] = 'Selamat, Instalasi piwigo telah selesai';
+$lang['Connection to server succeed, but it was impossible to connect to database'] = 'Koneksi ke server berhasil, tetapi koneksi ke database tidak memungkinkan.';
+$lang['Creation of config file local/config/database.inc.php failed.'] = 'Pembuatan berkas pengaturan local/config.database.inc.php gagal.';
+$lang['Database configuration'] = 'Pengaturan database';
+$lang['Database name'] = 'Nama database';
+$lang['Database table prefix'] = 'Prefiks tabel database';
+$lang['Don\'t hesitate to consult our forums for any help : %s'] = 'Jangan ragu untuk berkosultasi ke forum kami untuk bantuan apapun: %s';
+$lang['Download the config file'] = 'Unduh berkas pengaturan';
+$lang['Hope to see you back soon.'] = 'Berharap akan bertemu anda kembali.';
+$lang['Installation'] = 'Pemasangan';
