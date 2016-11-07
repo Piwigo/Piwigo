ef25e72486a914664fa989832f349e75af9b3d3e
plegall <plg@piwigo.org>
2016-11-07 14:43:37 +0100

    fixes #515 adds check on mysql extension loaded
    Especially on PHP 7 where only mysqli can be available.

--- install.php
+++ install.php
@@ -133,7 +133,26 @@ $dbhost = (!empty($_POST['dbhost'])) ? $_POST['dbhost'] : 'localhost';
 $dbuser = (!empty($_POST['dbuser'])) ? $_POST['dbuser'] : '';
 $dbpasswd = (!empty($_POST['dbpasswd'])) ? $_POST['dbpasswd'] : '';
 $dbname = (!empty($_POST['dbname'])) ? $_POST['dbname'] : '';
-$dblayer = extension_loaded('mysqli') ? 'mysqli' : 'mysql';
+
+// dblayer
+if (extension_loaded('mysqli'))
+{
+  $dblayer = 'mysqli';
+}
+else
+{
+  if (version_compare(PHP_VERSION, '7') >= 0)
+  {
+    fatal_error('PHP extension "mysqli" is not loaded');
+  }
+
+  if (!extension_loaded('mysql'))
+  {
+    fatal_error('No PHP extension installed for mysql, Piwigo needs one');
+  }
+
+  $dblayer = 'mysql';
+}
 
 $admin_name = (!empty($_POST['admin_name'])) ? $_POST['admin_name'] : '';
 $admin_pass1 = (!empty($_POST['admin_pass1'])) ? $_POST['admin_pass1'] : '';
