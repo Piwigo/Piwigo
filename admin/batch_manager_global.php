419538a1497519cc1b1af9ff130aee7a7e9af359
plegall <plg@piwigo.org>
2016-11-10 11:51:07 +0100

    fixes #545, add dimensions/filesize on batch manager thumbnail tooltips

--- admin/batch_manager_global.php
+++ admin/batch_manager_global.php
@@ -697,6 +697,8 @@ SELECT id,path,representative_ext,file,filesize,level,name,width,height,rotation
       $ttitle.= ' ('.$row['file'].')';
     }
 
+    $ttitle.= '<br>'.$row['width'].'&times;'.$row['height'].' pixels, '.sprintf('%.2f', $row['filesize']/1024).'MB';
+
     $template->append(
       'thumbnails', array_merge($row,
       array(
