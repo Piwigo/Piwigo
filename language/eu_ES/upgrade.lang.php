899ef16802c2127f5eb515ca1ca3ef0558002980
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-12-10 06:01:44 +0100

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/upgrade.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['total upgrade time'] = 'guztizko eguneraketa denbora';
+$lang['You do not have access rights to run upgrade'] = 'Ez duzu sarbide-eskubiderik eguneraketa exekutatzeko';
+$lang['total SQL time'] = 'guztizko SQL denbora';
+$lang['User permissions and group permissions have been erased'] = 'Erabiltzaileak eta talde baimenak ezabatu egin dira';
+$lang['Upgrade informations'] = 'Eguneratu informazioa';
+$lang['Upgrade from version %s to %s'] = 'Eguneratu %s bertsiotik %s(e)ra';
+$lang['This page proposes to upgrade your database corresponding to your old version of Piwigo to the current version. The upgrade assistant thinks you are currently running a <strong>release %s</strong> (or equivalent).'] = 'Datu-basea Piwigo bertsio berrira eguneratu dezazun proposatzen dizugu. Eguneratze-laguntzailearen arabera <strong>%s bertsioa</strong> (edo baliokidea) exekutatzen ari zara.';
+$lang['Upgrade'] = 'Eguneratu';
+$lang['Statistics'] = 'Estatistikak';
+$lang['SQL queries'] = 'SQL kontsultak';
+$lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you encounter any problem.'] = 'Burutu mantentze-azterketa [Administrazioa>Tresnak>Mantentzea] arazorik aurkitzen baduzu.';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Koadro txikien aurrizkia eta web arduradunaren posta elektroniko helbidea baino ez dira gorde aurreko konfiguraziotik';
+$lang['Only administrator can run upgrade: please sign in below.'] = 'Administrariek baino ezin dute eguneraketak exekutatu: hasi saioa azpian.';
+$lang['In <i>%s</i>, before <b>?></b>, insert:'] = '<i>%s</i>(e)an, <b>?></b>(e)ren aurretik, txertatu:';
+$lang['All sub-albums of private albums become private'] = 'Album pribatuetako azpi-album guztiak pribatu bihurtzen dira';
+$lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Hurrengo pluginak ezgaitu egin dira badaezpada. Egiaztatu eguneratu daiteken pluginik dagoen berriro gaitu baino lehen:';
+$lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Hurrengo gaiak ezgaitu egin dira badaezpada. Egiaztatu eguneratu daiteken gairik dagoen berriro gaitu baino lehen:';
