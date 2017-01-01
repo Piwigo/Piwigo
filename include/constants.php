239cb041811228adce5168fa223d313df2d332d8
plegall <plg@piwigo.org>
2017-01-01 19:46:42 +0100

    next version is 2.8.5

--- include/constants.php
+++ include/constants.php
@@ -22,7 +22,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.8.4');
+define('PHPWG_VERSION', '2.8.5');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 define('PHPWG_DEFAULT_TEMPLATE', 'elegant');
 
