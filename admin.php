32ca0a088d807f420521aafdc67010027f53d2d5
plegall <plg@piwigo.org>
2017-01-27 16:49:40 +0100

    fixes #590, "history" menu goes to current month

--- admin.php
+++ admin.php
@@ -174,7 +174,7 @@ $template->assign(
     'USERNAME' => $user['username'],
     'ENABLE_SYNCHRONIZATION' => $conf['enable_synchronization'],
     'U_SITE_MANAGER'=> $link_start.'site_manager',
-    'U_HISTORY_STAT'=> $link_start.'stats',
+    'U_HISTORY_STAT'=> $link_start.'stats&amp;year='.date('Y').'&amp;month='.date('n'),
     'U_FAQ'=> $link_start.'help',
     'U_SITES'=> $link_start.'remote_site',
     'U_MAINTENANCE'=> $link_start.'maintenance',
