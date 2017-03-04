5146d6dcbfb8173bac768c0997706eef5401f56b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-04 15:31:57 +0100

    Update et_EE, thanks to Pelagis

--- language/et_EE/common.lang.php
+++ language/et_EE/common.lang.php
@@ -448,3 +448,4 @@ $lang['Photo title'] = 'Foto pealkiri';
 $lang['Album name, A &rarr; Z'] = 'Albumine nimed, A &rarr;Z';
 $lang['Album name, Z &rarr; A'] = 'Albumine nimed, Z &rarr;A';
 $lang['Link: %s'] = 'Link: %s';
+$lang['Your authentication key is no longer valid.'] = 'Teie autentimise võti enam ei kehti';
