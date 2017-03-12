d60403d45540ca4a631d2136b355aa83b036c180
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-12 12:07:59 +0100

    Update et_EE, thanks to Pelagis

--- language/et_EE/common.lang.php
+++ language/et_EE/common.lang.php
@@ -449,3 +449,5 @@ $lang['Album name, A &rarr; Z'] = 'Albumine nimed, A &rarr;Z';
 $lang['Album name, Z &rarr; A'] = 'Albumine nimed, Z &rarr;A';
 $lang['Link: %s'] = 'Link: %s';
 $lang['Your authentication key is no longer valid.'] = 'Teie autentimise võti enam ei kehti';
+$lang['Invalid username or password!'] = 'Vale kasutajanimi või salasõna!';
+$lang['generate random password'] = 'Genereeri juhuslik salasõna';
