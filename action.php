c633b99f5c1252dab8d2f6b138edd3153eda8e16
Adrian Siemieniak <sauron@ordugh.org>
2017-03-22 22:22:46 +0100

    Some (most) of downloaded pictures came corrupted (completelly different binary file, but of the right size). After investigation, it looks like we need to "ob_clean — Clean (erase) the output buffer" and "flush — Flush system output buffer" before doing readfile().

--- action.php
+++ action.php
@@ -235,7 +235,9 @@ if (ini_get('safe_mode') == 0)
 {
   @set_time_limit(0);
 }
-
+// Without clean and flush there may be some image download problems, or image can be corrupted after download
+ob_clean();
+flush();
 @readfile($file);
 
 ?>
