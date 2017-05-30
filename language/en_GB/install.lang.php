e72634c7f71e93f4b424944b49db3acb94fcf110
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-30 06:21:09 +0200

    Update en_GB, thanks to Pat2309

--- language/en_GB/install.lang.php
+++ language/en_GB/install.lang.php
@@ -21,3 +21,31 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Congratulations, Piwigo installation is completed'] = 'Congratulations.  Piwigo installation is completed';
+$lang['Welcome to your new installation of Piwigo!'] = 'Welcome to your new installation of Piwigo!';
+$lang['Welcome to my photo gallery'] = 'Welcome to my photo gallery';
+$lang['Can\'t connect to server'] = 'Cannot connect to server';
+$lang['Password ']['confirm'] = 'Password [confirm]';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo was not able to configure PHP 5.2';
+$lang['Sorry!'] = 'Sorry!';
+$lang['Start Install'] = 'Start installation';
+$lang['Try to configure PHP 5'] = 'Try to configure PHP 5.2';
+$lang['also given by your host provider'] = 'also supplied by your hosting provider';
+$lang['enter a login for webmaster'] = 'please enter the webmaster username';
+$lang['localhost or other, supplied by your host provider'] = 'localhost or other, supplied by your host provider';
+$lang['please enter your password again'] = 'please enter your password again';
+$lang['user login given by your host provider'] = 'user login supplied by your host provider';
+$lang['user password given by your host provider'] = 'user password supplied by your host provider';
+$lang['verification'] = 'verification';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'Keep it confidential, it enables you to access the administration panel';
+$lang['Just another Piwigo gallery'] = 'Just another Piwigo gallery';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'It will be shown to visitors. It is necessary for website administration';
+$lang['Installation'] = 'Installation';
+$lang['Host'] = 'Host';
+$lang['Hope to see you back soon.'] = 'Hope to see you back soon.';
+$lang['Download the config file'] = 'Download the config file';
+$lang['Database name'] = 'Database name';
+$lang['Database table prefix'] = 'Database tables prefix';
+$lang['Default gallery language'] = 'Default gallery language';
+$lang['Database configuration'] = 'Database configuration';
+$lang['Admin configuration'] = 'Administration configuration';
+$lang['Basic configuration'] = 'Basic configuration';
