513a73729482dab024f8642513832a93b6962ace
Quallenauge <Hamsi2k@freenet.de>
2017-06-28 23:04:16 +0200

    Respect $conf['gallery_url'] when determining root url.

--- include/functions_url.inc.php
+++ include/functions_url.inc.php
@@ -15,6 +15,14 @@
 function get_root_url()
 {
   global $page;
+  global $conf;
+  if (!empty($conf['gallery_url']))
+  {
+  	if (url_is_remote($conf['gallery_url']) or $conf['gallery_url'][0]=='/' )
+  	{
+  		return $conf['gallery_url'];
+  	}
+  }
   if ( ($root_url = @$page['root_path']) == null )
   {// TODO - add HERE the possibility to call PWG functions from external scripts
     $root_url = PHPWG_ROOT_PATH;
