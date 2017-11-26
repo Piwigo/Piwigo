b156da71fecdd8bf38aacddf1280e5585b3c93f4
Dave Anderson <nlclothing@gmail.com>
2017-11-26 18:08:04 -0500

    Modified pwg.tags.getImages to allow returning images that are not tagged

--- include/ws_functions/pwg.tags.php
+++ include/ws_functions/pwg.tags.php
@@ -93,57 +93,79 @@ function ws_tags_getAdminList($params, &$service)
  */
 function ws_tags_getImages($params, &$service)
 {
-  // first build all the tag_ids we are interested in
-  $tags = find_tags($params['tag_id'], $params['tag_url_name'], $params['tag_name']);
-  $tags_by_id = array();
-  foreach ($tags as $tag)
-  {
-    $tags['id'] = (int)$tag['id'];
-    $tags_by_id[ $tag['id'] ] = $tag;
-  }
-  unset($tags);
-  $tag_ids = array_keys($tags_by_id);
-
-  $where_clauses = ws_std_image_sql_filter($params);
-  if (!empty($where_clauses))
-  {
-    $where_clauses = implode(' AND ', $where_clauses);
-  }
-
-  $order_by = ws_std_image_sql_order($params, 'i.');
-  if (!empty($order_by))
-  {
-    $order_by = 'ORDER BY '.$order_by;
-  }
-  $image_ids = get_image_ids_for_tags(
-    $tag_ids,
-    $params['tag_mode_and'] ? 'AND' : 'OR',
-    $where_clauses,
-    $order_by
-    );
-
-  $count_set = count($image_ids);
-  $image_ids = array_slice($image_ids, $params['per_page']*$params['page'], $params['per_page'] );
-
+  $untagged = false;
+  $image_ids = array();
   $image_tag_map = array();
-  // build list of image ids with associated tags per image
-  if (!empty($image_ids) and !$params['tag_mode_and'])
-  {
-    $query = '
+
+  // if "null" was supplied as the first item in tag_id, get a list of untagged images
+  if(isset($params['tag_id'][0]) && strtolower($params['tag_id'][0]) == "null") {
+    $query ='SELECT id FROM '.IMAGES_TABLE.' LEFT JOIN '.IMAGE_TAG_TABLE.' it ON id=it.image_id INNER JOIN '.IMAGE_CATEGORY_TABLE.' ic ON id=ic.image_id WHERE it.image_id IS NULL';
+    $query.= get_sql_condition_FandF(
+      array(
+        'forbidden_categories' => 'category_id',
+        'visible_categories' => 'category_id',
+        'visible_images' => 'id'
+        ),
+      "\n  AND"
+      );
+
+    $image_ids = query2array($query, null, 'id');
+    $tag_ids = array("null");
+    $untagged = true;
+
+  // else build all the tag_ids we are interested in
+  } else {
+    $tags = find_tags($params['tag_id'], $params['tag_url_name'], $params['tag_name']);
+    $tags_by_id = array();
+    foreach ($tags as $tag)
+    {
+      $tags['id'] = (int)$tag['id'];
+      $tags_by_id[ $tag['id'] ] = $tag;
+    }
+    unset($tags);
+    $tag_ids = array_keys($tags_by_id);
+
+    $where_clauses = ws_std_image_sql_filter($params);
+    if (!empty($where_clauses))
+    {
+      $where_clauses = implode(' AND ', $where_clauses);
+    }
+
+    $order_by = ws_std_image_sql_order($params, 'i.');
+    if (!empty($order_by))
+    {
+      $order_by = 'ORDER BY '.$order_by;
+    }
+    $image_ids = get_image_ids_for_tags(
+      $tag_ids,
+      $params['tag_mode_and'] ? 'AND' : 'OR',
+      $where_clauses,
+      $order_by
+      );
+
+    $image_ids = array_slice($image_ids, $params['per_page']*$params['page'], $params['per_page'] );
+
+    // build list of image ids with associated tags per image
+    if (!empty($image_ids) and !$params['tag_mode_and'])
+    {
+      $query = '
 SELECT image_id, GROUP_CONCAT(tag_id) AS tag_ids
   FROM '. IMAGE_TAG_TABLE .'
   WHERE tag_id IN ('. implode(',', $tag_ids) .')
-    AND image_id IN ('. implode(',', $image_ids) .')
+  AND image_id IN ('. implode(',', $image_ids) .')
   GROUP BY image_id
 ;';
-    $result = pwg_query($query);
+      $result = pwg_query($query);
 
-    while ($row = pwg_db_fetch_assoc($result))
-    {
-      $row['image_id'] = (int)$row['image_id'];
-      $image_tag_map[ $row['image_id'] ] = explode(',', $row['tag_ids']);
+      while ($row = pwg_db_fetch_assoc($result))
+      {
+        $row['image_id'] = (int)$row['image_id'];
+        $image_tag_map[ $row['image_id'] ] = explode(',', $row['tag_ids']);
+      }
     }
   }
+
+  $count_set = count($image_ids);
 
   $images = array();
   if (!empty($image_ids))
@@ -175,29 +197,32 @@ SELECT *
       }
       $image = array_merge( $image, ws_std_get_urls($row) );
 
-      $image_tag_ids = ($params['tag_mode_and']) ? $tag_ids : $image_tag_map[$image['id']];
       $image_tags = array();
-      foreach ($image_tag_ids as $tag_id)
+      if (!$untagged)
       {
-        $url = make_index_url(
-          array(
-            'section'=>'tags',
-            'tags'=> array($tags_by_id[$tag_id])
-            )
-          );
-        $page_url = make_picture_url(
-          array(
-            'section'=>'tags',
-            'tags'=> array($tags_by_id[$tag_id]),
-            'image_id' => $row['id'],
-            'image_file' => $row['file'],
-            )
-          );
-        $image_tags[] = array(
-          'id' => (int)$tag_id,
-          'url' => $url,
-          'page_url' => $page_url,
-          );
+        $image_tag_ids = ($params['tag_mode_and']) ? $tag_ids : $image_tag_map[$image['id']];
+        foreach ($image_tag_ids as $tag_id)
+        {
+          $url = make_index_url(
+            array(
+              'section'=>'tags',
+              'tags'=> array($tags_by_id[$tag_id])
+              )
+            );
+          $page_url = make_picture_url(
+            array(
+              'section'=>'tags',
+              'tags'=> array($tags_by_id[$tag_id]),
+              'image_id' => $row['id'],
+              'image_file' => $row['file'],
+              )
+            );
+          $image_tags[] = array(
+            'id' => (int)$tag_id,
+            'url' => $url,
+            'page_url' => $page_url,
+            );
+        }
       }
 
       $image['tags'] = new PwgNamedArray($image_tags, 'tag', ws_std_get_tag_xml_attributes() );
