1e8aad5b63e481936c3795858eb0133906a8aa15
Orestes Sanchez <orestes@acm.org>
2018-08-13 16:38:46 +0200

    Call non-static method using object.

--- include/template.class.php
+++ include/template.class.php
@@ -583,7 +583,7 @@ class Template
         'AAAA_DEBUG_TOTAL_TIME__' => get_elapsed_time($t2, get_moment())
         )
         );
-      Smarty_Internal_Debug::display_debug($this->smarty);
+      $this->smarty->_debug->display_debug($this->smarty);
     }
   }
 
