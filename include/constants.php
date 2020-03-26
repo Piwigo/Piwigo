ed487e4b24bc161f479d04545360fbf9dd36534a
plegall <plg@piwigo.org>
2020-03-26 19:50:36 +0100

    next version is 2.10.2

--- include/constants.php
+++ include/constants.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.10.1');
+define('PHPWG_VERSION', '2.10.2');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 
 // this constant is only used in the upgrade process, the true default theme
