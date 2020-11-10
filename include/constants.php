ebab72b539d4d8992d830c402d1605840690d868
plegall <plg@piwigo.org>
2020-11-10 12:17:19 +0100

    next version is 11.0.0beta5 (and not 2.11.0beta5)

--- include/constants.php
+++ include/constants.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '2.11.0beta4');
+define('PHPWG_VERSION', '11.0.0beta5');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 
 // this constant is only used in the upgrade process, the true default theme
