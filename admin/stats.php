246f6fdf4085f00d8d5c18044d0df29f49e0cbdc
plegall <plg@piwigo.org>
2021-01-06 10:09:10 +0100

    stats: l10n everything and use existing keys as much as possible

--- admin/stats.php
+++ admin/stats.php
@@ -381,6 +381,8 @@ if (count(get_last(60, 'year')) > 1 )
   );
 }
 
+ksort($lang['month']);
+
 $template->assign(array(
   'compareYears' => get_month_of_last_years($conf['stat_compare_year_displayed']),
   'monthStats' => get_month_stats(),
@@ -388,7 +390,9 @@ $template->assign(array(
   'lastDays' => $last_days,
   'lastMonths' => $last_months,
   'lastYears' => $last_years,
-  'langCode' => strval($user['language'])
+  'langCode' => strval($user['language']),
+  'month_labels' => join('~', $lang['month']),
+  'ADMIN_PAGE_TITLE' => l10n('History'),
 ));
 
 $template->assign_var_from_handle('ADMIN_CONTENT', 'stats');
