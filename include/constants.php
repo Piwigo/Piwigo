fec62fc52b54f023db81ee9333727fbfaf4ef17d
plegall <plg@piwigo.org>
2021-05-14 14:53:18 +0200

    next release is 11.5.0

--- include/constants.php
+++ include/constants.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '11.4.0');
+define('PHPWG_VERSION', '11.5.0');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 
 // this constant is only used in the upgrade process, the true default theme
