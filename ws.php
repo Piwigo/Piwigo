ddbdbd93bb2fa7dd833b36c35a361cc504723c81
Matthieu Leproux <leproux.matthieu@gmail.com>
2021-07-28 16:52:44 +0200

    fixes #1381 orphans calculation is done only when needed by ajax call

--- ws.php
+++ ws.php
@@ -594,6 +594,18 @@ function ws_addDefaultMethods( $arr )
     );
 
   $service->addMethod(
+      'pwg.images.calculateOrphansOnAlbumDeletion',
+      'ws_images_calculateOrphansOnAlbumDeletion',
+      array(
+        'category_id' =>  array('type'=>WS_TYPE_ID,
+                                'flags'=>WS_PARAM_FORCE_ARRAY),
+        ),
+      'Return the number of orphan photos if an album is deleted.',
+      $ws_functions_root . 'pwg.php',
+      array('admin_only'=>true)
+    );
+
+  $service->addMethod(
       'pwg.categories.getAdminList',
       'ws_categories_getAdminList',
       null,
