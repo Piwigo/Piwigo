7e3a8446a69d821a3fcfbcd2109550af6bf06e45
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-08-31 22:45:01 +0200

    Update tr_TR, thanks to LazBoy

--- language/tr_TR/common.lang.php
+++ language/tr_TR/common.lang.php
@@ -416,3 +416,5 @@ $lang['Invalid username or password!'] = 'Geçersiz kullanıcı adı veya şifre
 $lang['generate random password'] = 'rastgele şifre oluştur';
 $lang['Password: ********** (no copy by email)'] = 'Şifre: ********** (not edin, eposta ile kopyası gönderilmiyor)';
 $lang['Related albums'] = 'İlgili albümler';
+$lang['Album name, 1 &rarr; 5 &rarr; 10 &rarr; 100'] = 'Albüm adı, 1 &rarr; 5 &rarr; 10 &rarr; 100';
+$lang['Album name, 100 &rarr; 10 &rarr; 5 &rarr; 1'] = 'Albüm adı, 100 &rarr; 10 &rarr; 5 &rarr; 1';
