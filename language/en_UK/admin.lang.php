91035ae5b98e0728f3b94a8d60dd7dec5734f902
Matthieu Leproux <leproux.matthieu@gmail.com>
2021-10-04 14:24:09 +0200

    related to #1480 switch help icon translated in edit album

--- language/en_UK/admin.lang.php
+++ language/en_UK/admin.lang.php
@@ -1244,3 +1244,4 @@ $lang['Your selection'] = 'Your selection';
 $lang['action succesfully performed.'] = 'action succesfully performed.';
 $lang['%s lines'] = "%s lines";
 $lang['%s line'] = "%s line";
+$lang['Locked albums are disabled for maintenance. Only administrators can view them in the gallery. Lock this album will also lock his Sub-albums'] = 'Locked albums are disabled for maintenance. Only administrators can view them in the gallery. Lock this album will also lock his Sub-albums';
