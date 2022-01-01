7c27f9624aa41385cbf5422d4b299afb33d1d643
plegall <plg@piwigo.org>
2022-01-01 19:00:30 +0100

    next release is 12.2.0

--- include/constants.php
+++ include/constants.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '12.1.0');
+define('PHPWG_VERSION', '12.2.0');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 
 // this constant is only used in the upgrade process, the true default theme
