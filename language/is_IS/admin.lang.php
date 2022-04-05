b8cb3b1554cd8797b7f8e8fe4a5a3e57c32dbce9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-05 07:10:11 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/admin.lang.php
@@ -0,0 +1,93 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d user added'] = '%d notanda bætt við';
+$lang['%d user'] = '%d notandi';
+$lang['%d tags moved'] = '%d merki færð';
+$lang['%d tags edited'] = '%d merkjum breytt';
+$lang['%d tags deleted'] = '%d merkjum eytt';
+$lang['%d tags added'] = '%d merkjum bætt við';
+$lang['%d tags'] = '%d merki';
+$lang['%d tag moved'] = '%d merki fært';
+$lang['%d tag edited'] = '%d merki breytt';
+$lang['%d tag deleted'] = '%d merki eytt';
+$lang['%d tag added'] = '%d merki bætt við';
+$lang['%d tag'] = '%d merki';
+$lang['%d sub-albums'] = '%d undiralbúm';
+$lang['%d seconds'] = '%d sekúndur';
+$lang['%d second'] = '%d sekúnda';
+$lang['%d photos were deleted'] = '%d myndum eytt';
+$lang['%d photos uploaded'] = '%d myndir innfluttar';
+$lang['%d photos moved'] = '%d myndir færðar';
+$lang['%d photos edited'] = '%d myndum breytt';
+$lang['%d photos deleted'] = '%d myndum eytt';
+$lang['%d photos added'] = '%d myndum bætt við';
+$lang['%d photos'] = '%d myndir';
+$lang['%d photo was deleted'] = '%d mynd eytt';
+$lang['%d photo moved'] = '%d mynd færð';
+$lang['%d photo edited'] = '%d mynd breytt';
+$lang['%d photo deleted'] = '%d mynd eytt';
+$lang['%d photo added'] = '%d mynd bætt við';
+$lang['%d parameters were updated.'] = '%d notandabreytur uppfærðar.';
+$lang['%d parameter was updated.'] = '%d færibreyta uppfærð.';
+$lang['%d of %d users selected'] = '%d af %d notendum valdir';
+$lang['%d of %d photos selected'] = '%d af %d myndum valdar';
+$lang['%d months'] = '%d mánuðir';
+$lang['%d month'] = '%d mánuður';
+$lang['%d minutes'] = '%d mínútur';
+$lang['%d minute'] = '%d mínúta';
+$lang['%d members'] = '%d meðlimir';
+$lang['%d member'] = '%d meðlimur';
+$lang['%d mails were sent.'] = '%d póstar hafa verið sendir.';
+$lang['%d mails were not sent.'] = '%d póstar hafa ekki verið sendir.';
+$lang['%d mail was sent.'] = '%d póstur hefur verið sendur.';
+$lang['%d mail was not sent.'] = '%d póstur hefur ekki verið sendur.';
+$lang['%d lines filtered'] = '%d línur síaðar';
+$lang['%d line filtered'] = '%d lína síuð';
+$lang['%d hours'] = '%d klukkustundir';
+$lang['%d hour'] = '%d klukkustund';
+$lang['%d guests'] = '%d gestir';
+$lang['%d guest'] = '%d gestur';
+$lang['%d groups moved'] = '%d hópar færðir';
+$lang['%d groups edited'] = '%d hópum breytt';
+$lang['%d groups deleted'] = '%d hópum eytt';
+$lang['%d groups added'] = '%d hópum bætt við';
+$lang['%d groups'] = '%d hópar';
+$lang['%d group moved'] = '%d hópur færður';
+$lang['%d group edited'] = '%d hópi breytt';
+$lang['%d group added'] = '%d hópi bætt við';
+$lang['%d group deleted'] = '%d hópi eytt';
+$lang['%d group'] = '%d hópur';
+$lang['%d days'] = '%d dagar';
+$lang['%d day'] = '%d dagur';
+$lang['%d checksums were added'] = '%d tékkum var bætt við';
+$lang['%d anomaly has not been corrected.'] = '%d frávik hafa ekki verið lagfærð.';
+$lang['%d anomaly has been ignored.'] = '%d frávik hafa verið hunsuð.';
+$lang['%d anomaly has been detected.'] = '%d frávik hafa verið uppgötvuð.';
+$lang['%d anomaly has been corrected.'] = '%d frávik hafa verið lagfærð.';
+$lang['%d anomalies have not been corrected.'] = '%d frávik hafa ekki verið lagfærð.';
+$lang['%d anomalies have been ignored.'] = '%d frávik hafa verið hunsuð.';
+$lang['%d anomalies have been detected.'] = '%d frávik hafa fundist.';
+$lang['%d anomalies have been detected corrected.'] = '%d frávik hafa fundist og leiðrétt.';
+$lang['%d albums moved'] = '%d albúm færð';
+$lang['%d albums including'] = '
+%d albúmum þar á meðal';
+$lang['%d albums edited'] = '%d albúmum breytt';
+$lang['%d albums deleted'] = '
+%d albúmum eytt';
+$lang['%d album moved'] = '%d albúm færð';
+$lang['%d albums added'] = '%d albúmum bætt við';
+$lang['%d album including'] = '%d albúm þar á meðal';
+$lang['%d album edited'] = '%d albúmi breytt';
+$lang['%d album deleted'] = '%d albúmi eytt';
+$lang['%d album added'] = '%d albúmi bætt við';
+$lang['%d Activity'] = '%d Starfsemi';
+$lang['%d Activities'] = '%d Starfsemi';
+$lang[' and %d virtuals'] = 'og %d sýndarmynd';
+$lang[' and %d virtual'] = 'og %d sýndarmynd';
+$lang[' (copy)'] = '(afrit)';
+$lang[' (copy %s)'] = '(afrit %s)';
