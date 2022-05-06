04f27822f81d36d5816e362dc850046c897341df
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-06 06:16:01 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/upgrade.lang.php
+++ language/nl_NL/upgrade.lang.php
@@ -19,9 +19,9 @@ $lang['Perform a maintenance check in [Administration>Tools>Maintenance] if you 
 $lang['As a precaution, following plugins have been deactivated. You must check for plugins upgrade before reactiving them:'] = 'Als voorzorgsmaatregel zijn de volgende plug-ins gedeactiveerd. Voordat je ze reactiveert moet je deze plug-ins op upgrades controleren:';
 $lang['Only administrator can run upgrade: please sign in below.'] = 'Alleen een beheerder kan een upgrade uitvoeren: hieronder kun je inloggen.';
 $lang['You do not have access rights to run upgrade'] = 'Je bent niet bevoegd om een upgrade uit te voeren';
-$lang['All sub-albums of private albums become private'] = 'Alle sub-albums van privé albums worden privé';
+$lang['All sub-albums of private albums become private'] = 'Alle sub-albums van privé-albums worden privé';
 $lang['User permissions and group permissions have been erased'] = 'Gebruikers- en groepsrechten zijn verwijderd';
-$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Alleen het voorvoegsel (prefix) van de klikplaatjes en het e-mail-adres van de webbeheerder zijn bewaard gebleven van de vorige configuratie';
+$lang['Only thumbnails prefix and webmaster mail address have been saved from previous configuration'] = 'Alleen het voorvoegsel (prefix) van de klikplaatjes en het e-mail-adres van de webbeheerder van de vorige configuratie zijn bewaard gebleven ';
 $lang['In <i>%s</i>, before <b>?></b>, insert:'] = 'In <i>%s</i>, voor <b>?></b>, voeg tussen:';
 $lang['As a precaution, following themes have been deactivated. You must check for themes upgrade before reactiving them:'] = 'Als voorzorgsmaatregel zijn de volgende thema\'s gedeactiveerd. Controleer of er nieuwe versies van deze thema\'s bestaan, voordat je ze reactiveert:';
 $lang['Discover what\'s new in Piwigo %s'] = 'Ontdek wat er nieuw is in Piwigo %s';
