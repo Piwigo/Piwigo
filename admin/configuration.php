735b670599345690fde7af0004648b77a69d210d
Matthieu Leproux <matthieu.leproux@gmail.com>
2022-07-19 11:57:12 +0200

    related to #1693 solve deprecated trim and str_replace

--- admin/configuration.php
+++ admin/configuration.php
@@ -350,7 +350,7 @@ switch ($page['section'])
     {
       $out = array();
       $order_by = trim($conf['order_by_inside_category']);
-      $order_by = str_replace('ORDER BY ', null, $order_by);
+      $order_by = str_replace('ORDER BY ', false, $order_by);
       $order_by = explode(', ', $order_by);
     }
 
