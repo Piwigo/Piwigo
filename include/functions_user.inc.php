d9027d2defb51d480e9a5abee380395f8b00fa21
zag-adka <56411907+zag-adka@users.noreply.github.com>
2023-01-10 12:56:40 -0400

    Fixes #1254 fix quote escaping in password

--- include/functions_user.inc.php
+++ include/functions_user.inc.php
@@ -1168,7 +1168,7 @@ SELECT '.$conf['user_fields']['id'].' AS id,
 ;';
 
   $row = pwg_db_fetch_assoc(pwg_query($query));
-  if (isset($row['id']) and $conf['password_verify']($password, $row['password'], $row['id']))
+  if (isset($row['id']) and $conf['password_verify'](stripslashes($password), $row['password'], $row['id']))
   {
     $user_found = true;
   }
@@ -1184,7 +1184,7 @@ SELECT '.$conf['user_fields']['id'].' AS id,
     ;';
 
     $row = pwg_db_fetch_assoc(pwg_query($query));
-    if (isset($row['id']) and $conf['password_verify']($password, $row['password'], $row['id']))
+    if (isset($row['id']) and $conf['password_verify'](stripslashes($password), $row['password'], $row['id']))
     {
       $user_found = true;
     }
