99731cafa1f3d7322a6662e568d939dc785a429d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-03 06:16:50 +0100

    Update es_MX, thanks to grantiago

---
+++ language/es_MX/admin.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d anomalies have been detected.'] = 'Se han detectado %d anomalías.';
+$lang['%d anomalies have been detected corrected.'] = 'Se han detectado y corregido %d anomalías.';
+$lang['%d albums including'] = '%d álbumes incluidos';
+$lang['%d albums edited'] = '%d álbumes editados';
+$lang['%d albums deleted'] = '%d álbumes eliminados';
+$lang['%d albums added'] = '%d álbumes agregados';
+$lang['%d album including'] = '%d álbum incluido';
+$lang['%d album edited'] = '%d álbum editado';
+$lang['%d album deleted'] = '%d álbum eliminado';
+$lang['%d album added'] = '%d álbum agregado';
+$lang['%d Activity'] = '%d Actividad';
+$lang['%d Activities'] = '%d actividades';
+$lang[' and %d virtuals'] = 'y %d virtuales';
+$lang[' and %d virtual'] = 'y %d virtuales';
+$lang[' (copy)'] = '(copiar)';
+$lang[' (copy %s)'] = '(copiar %s)';
