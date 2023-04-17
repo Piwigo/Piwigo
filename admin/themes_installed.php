ea53750bfe7e822b4e1bf62c8847a5d409a3102d
plegall <plg@piwigo.org>
2023-04-17 10:44:32 +0200

    fixes #1900 recent version of PHP seems to prefer -1 over 0 on compare function for sorting

--- admin/themes_installed.php
+++ admin/themes_installed.php
@@ -151,7 +151,7 @@ function cmp($a, $b)
   if($a['STATE'] == $b['STATE'])
     return strcasecmp($a['NAME'], $b['NAME']); 
   else
-    return ($s[$a['STATE']] >= $s[$b['STATE']] ? 1 : 0);
+    return ($s[$a['STATE']] >= $s[$b['STATE']] ? 1 : -1);
 }
 usort($tpl_themes, 'cmp');
 
