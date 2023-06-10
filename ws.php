da7ebc2a32f55518b23f40eb45a7b1e756cbd8f9
plegall <plg@piwigo.org>
2023-06-10 15:29:48 +0200

    issue #1869 new API method pwg.images.filteredSearch.update

--- ws.php
+++ ws.php
@@ -1365,28 +1365,52 @@ enabled_high, registration_date, registration_date_string, registration_date_sin
     );
 
     $service->addMethod(
-      'pwg.gallery.getSearch',
-      'ws_gallery_getSearch',
+      'pwg.images.filteredSearch.update',
+      'ws_images_filteredSearch_update',
       array(
-        'search_id' => array('default'=>null),
+        'search_id' => array(
+          'type' => WS_TYPE_ID,
         ),
+        'allwords' => array(
+          'flags' => WS_PARAM_OPTIONAL,
+          'info' => 'query to search by words',
+        ),
+        'allwords_mode' => array(
+          'flags' => WS_PARAM_OPTIONAL,
+          'info' => 'AND (by default) | OR',
+        ),
+        'allwords_fields' => array(
+          'flags' => WS_PARAM_OPTIONAL|WS_PARAM_FORCE_ARRAY,
+          'info' => 'values among [name, comment, tags, file, cat-title, cat-desc]',
+        ),
+        'tags' => array(
+          'flags' => WS_PARAM_OPTIONAL|WS_PARAM_FORCE_ARRAY,
+          'type' => WS_TYPE_ID,
+        ),
+        'tags_mode' => array(
+          'flags' => WS_PARAM_OPTIONAL,
+          'info' => 'AND (by default) | OR',
+        ),
+        'categories' => array(
+          'flags' => WS_PARAM_OPTIONAL|WS_PARAM_FORCE_ARRAY,
+          'type' => WS_TYPE_ID,
+        ),
+        'categories_withsubs' => array(
+          'flags' => WS_PARAM_OPTIONAL,
+          'type' => WS_TYPE_BOOL,
+          'info' => 'false, by default',
+        ),
+        'authors' => array(
+          'flags' => WS_PARAM_OPTIONAL|WS_PARAM_FORCE_ARRAY,
+        ),
+        'added_by' => array(
+          'flags' => WS_PARAM_OPTIONAL|WS_PARAM_FORCE_ARRAY,
+          'type' => WS_TYPE_ID,
+        ),
+      ),
       '',
-      $ws_functions_root . 'pwg.php'
-      );
-
-    $service->addMethod(
-      'pwg.gallery.updateSearch',
-      'ws_gallery_updateSearch',
-      array(
-        'search_id' => array('default'=>null),
-        'params' => array(
-          'default'=>null,
-          'flags'=> WS_PARAM_FORCE_ARRAY
-        ),
-        ),
-      '',
-      $ws_functions_root . 'pwg.php'
-      );
+      $ws_functions_root . 'pwg.images.php'
+    );
 }
 
 ?>
