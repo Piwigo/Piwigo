045330140b96582df567c3e35bb7dd3f861af0b6
plegall <plg@piwigo.org>
2024-01-11 10:58:50 +0100

    (cp e9d487c) add Armenian
    new language Armenian

---
+++ language/hy_AM/common.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+/*
+Language Name: Հայերեն (Hayerēn) [AM]
+Version: auto
+Language URI: http://piwigo.org/ext/extension_view.php?eid=969
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+// Langage informations
+$lang_info['language_name'] = 'Հայերեն';
+$lang_info['country'] = 'Հայաստան';
+$lang_info['direction'] = 'ltr';
+$lang_info['code'] = 'hy';
+$lang_info['zero_plural'] = false;
+
+
+
