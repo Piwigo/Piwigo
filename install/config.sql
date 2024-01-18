d7aaf887d44553dbed42210482e034dd6b585660
plegall <plg@piwigo.org>
2024-01-18 12:19:42 +0100

    fixes #2089 hide search in this button, by default

--- install/config.sql
+++ install/config.sql
@@ -75,6 +75,6 @@ INSERT INTO piwigo_config (param,value) VALUES ('picture_caddie_icon','true');
 INSERT INTO piwigo_config (param,value) VALUES ('picture_representative_icon','true');
 INSERT INTO piwigo_config (param,value) VALUES ('show_mobile_app_banner_in_admin','true');
 INSERT INTO piwigo_config (param,value) VALUES ('show_mobile_app_banner_in_gallery','false');
-INSERT INTO piwigo_config (param,value) VALUES ('index_search_in_set_button','true');
+INSERT INTO piwigo_config (param,value) VALUES ('index_search_in_set_button','false');
 INSERT INTO piwigo_config (param,value) VALUES ('index_search_in_set_action','true');
 INSERT INTO piwigo_config (param,value) VALUES ('upload_detect_duplicate','true');
