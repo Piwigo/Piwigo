ac98cfdcc27f3763e40d5ead5d3daa60f465e926
plegall <plg@piwigo.org>
2024-04-24 16:27:31 +0200

    fixes #2155 register more PHP functions for templates

--- include/template.class.php
+++ include/template.class.php
@@ -118,6 +118,9 @@ class Template
     $this->smarty->registerPlugin('modifier', 'ucfirst', 'ucfirst');
     $this->smarty->registerPlugin('modifier', 'strstr', 'strstr');
     $this->smarty->registerPlugin('modifier', 'stristr', 'stristr');
+    $this->smarty->registerPlugin('modifier', 'trim', 'trim');
+    $this->smarty->registerPlugin('modifier', 'md5', 'md5');
+    $this->smarty->registerPlugin('modifier', 'strtolower', 'strtolower');
     $this->smarty->registerPlugin('modifier', 'str_ireplace', 'str_ireplace');
     $this->smarty->registerPlugin('modifier', 'explode', array('Template', 'mod_explode') );
     $this->smarty->registerPlugin('modifier', 'ternary', array('Template', 'mod_ternary') );
