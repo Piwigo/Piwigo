016ad34ce9f574093507edc6f5be1ffb633802b8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-07 18:54:16 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/whats_new_15.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A new interface for user management'] = 'En ny grænseflade til brugerhåndtering';
+$lang['Brand new features : Activities logs and PDF reader'] = 'Helt nye funktioner: Aktivitetslogninger og PDF-viser';
+$lang['Enhanced interface for batch manager and album selector'] = 'Forbedret grænseflade til batchhåndtering og albumudvælgelse';
+$lang['Even more filters for the gallery search engine'] = 'Endnu flere filtre til galleriets søgemaskine';
