647bffb75c47eb03878c011d85720bb6be4d9bd5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-07 18:54:22 +0100

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/whats_new_15.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A new interface for user management'] = 'Nové rozhranie pre správu používateľov';
+$lang['Brand new features : Activities logs and PDF reader'] = 'Úplne nové funkcie: Denníky aktivít a čítačka PDF';
+$lang['Enhanced interface for batch manager and album selector'] = 'Vylepšené rozhranie pre správcu dávok a výber albumu';
+$lang['Even more filters for the gallery search engine'] = 'Ešte viac filtrov pre vyhľadávač galérie';
