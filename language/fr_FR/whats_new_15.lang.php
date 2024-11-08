68eaa10b7fc9cfbd840aba54faf3ea4f0254003a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-08 18:31:32 +0100

    Update fr_FR, thanks to plg

--- language/fr_FR/whats_new_15.lang.php
+++ language/fr_FR/whats_new_15.lang.php
@@ -8,5 +8,5 @@
 
 $lang['A new interface for user management'] = 'Une nouvelle interface pour la gestion des utilisateurs';
 $lang['Brand new features : Activities logs and PDF reader'] = 'Nouvelles fonctionnalités : activités systèmes et lecteur PDF';
-$lang['Enhanced interface for batch manager and album selector'] = 'Design amélioré pour la gestion par lot et la sélection d\'album';
+$lang['Enhanced interface for batch manager and album selector'] = 'Design amélioré pour la gestion par lot et le sélecteur d\'album';
 $lang['Even more filters for the gallery search engine'] = 'De nouveaux filtres dans le moteur de recherche';
