a7ac9bfb9f5c9e430b8d37a7157ff6a5e603b964
ddtddt <ddtddt@piwigo.org>
2024-12-19 16:37:53 +0100

    Update common.lang.php
    review welcome on ...

--- language/de_DE/common.lang.php
+++ language/de_DE/common.lang.php
@@ -303,7 +303,6 @@ $lang['Cookies are blocked or not supported by your browser. You must enable coo
 $lang['Edit a comment'] = 'Einen Kommentar bearbeiten';
 $lang['(!) This comment requires validation'] = '(!) Dieser Kommentar erfordert Überprüfung.';
 $lang['Welcome'] = 'Willkommen';
-$lang['Welcome to your Piwigo photo gallery!'] = 'Willkommen!';
 $lang['... or browse your empty gallery'] = '...oder durchstöbern Sie Ihre leere Galerie.';
 $lang['... or please deactivate this message, I will find my way by myself'] = '...oder diese Meldung übergehen. Ich finde mich selbst zurecht.';
 $lang['Hello %s, your Piwigo photo gallery is empty!'] = 'Hallo %s, Ihre Piwigo-Fotogalerie ist leer!';
