85354de9eb29065257ebfcb3a1e2be7df67ba085
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-01-16 06:27:36 +0100

    Update fa_IR, thanks to Garcia

---
+++ language/fa_IR/whats_new_15.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A new interface for user management'] = 'یک رابط کاربری جدید برای مدیریت کاربران';
+$lang['Brand new features : Activities logs and PDF reader'] = 'قابلیت‌های جدید: تاریخچه‌ی فعالیت‌ها و پی‌دی‌اف خوان';
+$lang['Enhanced interface for batch manager and album selector'] = 'بهبود رابط کاربری بخش مدیریت دسته‌ای و انتخاب‌کننده‌ی آلبوم';
+$lang['Even more filters for the gallery search engine'] = 'فیلترهای بیشتر برای موتور جستجوی گالری';
