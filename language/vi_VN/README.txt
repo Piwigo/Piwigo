d79e80bbeff5a188841335f0b3edc58a881e86a4
hoyakaka <sprite.sprout@gmail.com>
2025-07-01 16:15:02 +0900

    メンテナンスしない言語を一旦削除

--- language/vi_VN/README.txt
+++
@@ -1,6 +0,0 @@
-Piwigo Vietnamese language files
-author: Tien Ho Quang
-website: http://www.quangtien.net
-e-mail: 
-
-update : 28/01/2010
--- language/vi_VN/whats_new_15.lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-// +-----------------------------------------------------------------------+
-// | This file is part of Piwigo.                                          |
-// |                                                                       |
-// | For copyright and license information, please view the COPYING.txt    |
-// | file that was distributed with this source code.                      |
-// +-----------------------------------------------------------------------+
-$lang['A new interface for user management'] = 'Giao diện mới để quản lý người dùng';
