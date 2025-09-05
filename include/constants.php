cd8c9b7db36f4bc3045672f960a9b382e17ae6ff
plegall <plg@piwigo.org>
2025-09-05 14:23:44 +0200

    issue #2405 increase PHP required version from 7.1 to 7.4 for the upcoming Smarty update

--- include/constants.php
+++ include/constants.php
@@ -20,7 +20,7 @@ defined('PWG_COMBINED_DIR') or define('PWG_COMBINED_DIR', $conf['data_location']
 defined('PWG_DERIVATIVE_DIR') or define('PWG_DERIVATIVE_DIR', $conf['data_location'].'i/');
 
 // Required versions
-define('REQUIRED_PHP_VERSION', '7.1.0');
+define('REQUIRED_PHP_VERSION', '7.4.0');
 
 // Access codes
 define('ACCESS_FREE', 0);
