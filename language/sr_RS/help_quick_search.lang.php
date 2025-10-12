a5879f370c251b65565ada9d4559b2dceb4f7233
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-12 12:04:28 +0200

    Update sr_RS, thanks to gpopac

---
+++ language/sr_RS/help_quick_search.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photos after Jan 1st 2008'] = 'фотографије након 1. јануара 2008. године';
+$lang['photos from 2003 to 2008'] = 'фотографије од 2003. до 2008. године';
+$lang['photos from may 2003'] = 'фотографије из маја 2008. године';
+$lang['photos posted in 2003'] = 'фотографије додате 2003. године';
+$lang['photos taken in 2003'] = 'фотографије снимљене 2003. године';
+$lang['return photos less than 700px high'] = 'фотографије ниже од 700 пиксела';
+$lang['return photos wider than 500px'] = 'фотографије шире од 500 пиксела';
+$lang['return photos with at least 1,000 views'] = 'фотографије које имају бар 1000 прегледа';
+$lang['returns photos of 12 megapixels or more'] = 'фотографије које су бар 12 мегапиксела или веће';
+$lang['returns photos of 5 megapixels'] = 'фотографије од 5 мегапиксела';
+$lang['up to 100 (inclusive)'] = 'до 100 (укључујуће)';
+$lang['will give you all photos with at least one note.'] = 'даће вам све фотографије са бар једном белешком.';
+$lang['will give you photos without notes.'] = 'даће вам све фотографије без белешки.';
+$lang['Searches photos by taken date.'] = 'Претрага фотографија снимљених одређеног датума.';
+$lang['Searches photos by width/height ratio.'] = 'Претрага фотографија по односу ширине/висине.';
+$lang['Searches photos with a given width or height.'] = 'Претрага фотографија на основу ширине или висине.';
+$lang['Supported numeric operators'] = 'Подржане нумеричке операције';
+$lang['The quick search engine allows you to use boolean operators to refine your search. By default, the search applies to all keywords. Searches are not case sensitive.'] = 'Брза претрага вам дозвољава да користите логичке операције како би побољшали претрагу. Претрага се подразумевано примењује на све кључне речи. Претрага не разликује велика и мала слова.';
+$lang['These operators can be used with numeric fields such as'] = 'Ове операције се могу користити са нумеричким пољима као што су';
+$lang['Use quotes to search for an exact keyword or phrase.'] = 'Користите наводнике да потражите тачну кључну реч или израз.';
+$lang['With keywords'] = 'Са кључним речима';
+$lang['between 10 and 100 (inclusive)'] = 'између 10 и 100 (укључиво)';
+$lang['equals 100'] = 'једнако 100';
+$lang['finds files between 1MB and 10MB.'] = 'налази датотеке између 1MB и 10MB.';
+$lang['finds panoramas'] = 'налази панораме';
+$lang['finds photo 123 to 126 (it may find between 0 and 4 photos, because photos can be deleted)'] = 'налази фотографије од 123 до 126 (може да нађе од 0 до 4 фотографија, зато што неке фотографије могу бити обрисане)';
+$lang['finds photos from compact cameras in portrait or landscape modes'] = 'налази фотографије из компактних фото апарата које су усправне или положене';
+$lang['from 100 and above'] = 'од 100 навише';
+$lang['greater than 100'] = 'више од 100';
+$lang['less than 100'] = 'мање од 100';
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = 'Додајте цртицу „-“ или „NOT“ пре речи да је искључите из претраге. NOT служи као филтер приликом претраге, тако да претрага не може да садржи само речи за искључивање из претраге. Не можете да комбинујете OR и NOT (Иван OR NOT Марко није исправно)';
+$lang['Add an OR between words.'] = 'Додајте „OR“ између речи.';
+$lang['Exact search'] = 'Тачна претрага';
+$lang['Exclude'] = 'Искључи';
+$lang['Grouping'] = 'Груписање';
+$lang['Here is a list of actions you can perform:'] = 'Ево списка радњи које можете извршити:';
+$lang['OR inclusive'] = 'OR за укључивање';
+$lang['Others'] = 'Остало';
+$lang['Same principle as for'] = 'Исти принцип као за';
+$lang['Search using extended syntax'] = 'Претрага проширеном синтаксом';
+$lang['Searches by author.'] = 'Претрага по аутору.';
+$lang['Searches by file name.'] = 'Претрага по имену датотеке.';
+$lang['Searches for photos by number of views.'] = 'Претрага фотографија по броју прегледа.';
+$lang['Searches only for photos with the given words in title or description.'] = 'Претрага фотографија по наслову или опису.';
+$lang['Searches only in tag names without looking at photo titles or descriptions.'] = 'Претрага имена ознака без наслова или описа слика.';
+$lang['Searches photos by file size.'] = 'Претрага фотографија по величини датотеке.';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Претрага фотографија по нумеричкој идентификацији у Пивигу.';
+$lang['Searches photos by posted date.'] = 'Претрага фотографија по датуму додавања.';
+$lang['Searches photos by rating.'] = 'Претрага фотографија по оцени';
+$lang['Searches photos by size in pixels.'] = 'Претрага фотографија по величини у пикселима.';
