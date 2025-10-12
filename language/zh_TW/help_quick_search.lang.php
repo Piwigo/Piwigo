43bd2e4b8277fbba69130d94b9718d7604d66dac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-12 16:58:24 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/help_quick_search.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['will give you all photos with at least one note.'] = '將提供您所有至少附有一則註記的照片。';
+$lang['will give you photos without notes.'] = '將提供您不含註記的照片。';
+$lang['between 10 and 100 (inclusive)'] = '在 10 到 100 之間（包含頭尾數字）';
+$lang['equals 100'] = '等於 100';
+$lang['finds files between 1MB and 10MB.'] = '搜尋大小為 1MB 到 10MB 的檔案。';
+$lang['finds panoramas'] = '搜尋全景照片';
+$lang['finds photo 123 to 126 (it may find between 0 and 4 photos, because photos can be deleted)'] = '搜尋照片 123 至 126（可能找到 0 至 4 張照片，因為照片可能已被刪除）';
+$lang['finds photos from compact cameras in portrait or landscape modes'] = '從輕便相機中搜尋直式或橫式的照片';
+$lang['from 100 and above'] = '大於等於 100';
+$lang['greater than 100'] = '大於 100';
+$lang['less than 100'] = '小於 100';
+$lang['photos after Jan 1st 2008'] = '2008 年 1 月 1 日後的照片';
+$lang['photos from 2003 to 2008'] = '2008 年到 2003 年的照片';
+$lang['photos from may 2003'] = '2003 年 5 月的照片';
+$lang['photos posted in 2003'] = '2003 年張貼的照片';
+$lang['photos taken in 2003'] = '2003 年拍攝的照片';
+$lang['return photos less than 700px high'] = '高度小於 700px 的照片';
+$lang['return photos wider than 500px'] = '寬度大於 500px 的照片';
+$lang['return photos with at least 1,000 views'] = '回傳至少被瀏覽 1,000 次的照片';
+$lang['returns photos of 12 megapixels or more'] = '回傳超過 1,200 萬畫素的照片';
+$lang['returns photos of 5 megapixels'] = '回傳 500 萬畫素的照片';
+$lang['up to 100 (inclusive)'] = '大於等於 100';
+$lang['Same principle as for'] = '與此一樣的原則 ';
+$lang['Search using extended syntax'] = '使用延伸的語法搜尋';
+$lang['Searches by author.'] = '依作者搜尋。';
+$lang['Searches by file name.'] = '依檔案名稱搜尋。';
+$lang['Searches for photos by number of views.'] = '依瀏覽次數搜尋照片。';
+$lang['Searches only for photos with the given words in title or description.'] = '依照片標題或是描述文字包含的字詞搜尋。';
+$lang['Searches only in tag names without looking at photo titles or descriptions.'] = '僅依標籤名稱搜尋；不看照片的標題或描述文字。';
+$lang['Searches photos by file size.'] = '依檔案大小搜尋。';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = '依照片的 Piwigo 編號 ID 搜尋。';
+$lang['Searches photos by posted date.'] = '依張貼日期搜尋。';
+$lang['Searches photos by rating.'] = '依評價搜尋照片。';
+$lang['Searches photos by size in pixels.'] = '依相素大小搜尋照片。';
+$lang['Searches photos by taken date.'] = '依拍照日期搜尋。';
+$lang['Searches photos by width/height ratio.'] = '依照片的寬高比例搜尋照片。';
+$lang['Searches photos with a given width or height.'] = '依指定的寬高搜尋照片。';
+$lang['Supported numeric operators'] = '支援的數值運算符號';
+$lang['The quick search engine allows you to use boolean operators to refine your search. By default, the search applies to all keywords. Searches are not case sensitive.'] = '快速搜尋引擎允許您使用布林運算子來精確搜尋。預設情況下，搜尋將套用至所有關鍵字。搜尋結果不區分大小寫。';
+$lang['These operators can be used with numeric fields such as'] = '這些運算子可用於數值欄位，例如';
+$lang['Use quotes to search for an exact keyword or phrase.'] = '使用引號來搜尋精確的關鍵字或短語。';
+$lang['With keywords'] = '使用關鍵字';
+$lang['Exact search'] = '精準搜尋';
+$lang['Exclude'] = '排除';
+$lang['Grouping'] = '組成群組';
+$lang['Here is a list of actions you can perform:'] = '以下是您可以進行的動作列表：';
+$lang['OR inclusive'] = 'OR 包含';
+$lang['Others'] = '其他';
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = '若要將某個字詞排除在搜尋結果之外，請在該字詞前加上減號 (-) 或是 NOT 關鍵字。請注意：NOT 所扮演的角色是篩選運算符號，因此您無法建立「只包含 NOT 運算符號」的搜尋要求。您也無法合併使用 OR 和 NOT（john OR NOT bill 是不合法的）。';
+$lang['Add an OR between words.'] = '在字詞間加入 OR 運算符號。';
