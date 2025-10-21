688b81c5b129a1e40b1ec584fd94b8dca2241457
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 08:06:27 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/help_quick_search.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Exclude'] = 'Escludi';
+$lang['Grouping'] = 'Raggr';
+$lang['Searches by author.'] = 'Ricerca per autore';
+$lang['Searches by file name.'] = 'Ricerca per nome file';
+$lang['Searches photos by file size.'] = 'Ricerca fotografie per dimensione file';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Ricerca fotografie mediante il proprio numero identificativo all\'interno di Piwigo ';
+$lang['Searches photos by posted date.'] = 'Ricerca fotografie per data di pubblicazione';
+$lang['Searches photos by rating.'] = 'Ricerca fotografie per valutazione';
+$lang['Searches photos by size in pixels.'] = 'Ricerca fotografie per dimensione in pixel';
+$lang['Add an OR between words.'] = 'Aggiungi un OR fra le parole';
