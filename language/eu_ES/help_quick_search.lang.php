475c26abc3ae85fb8ce1f186030fe6ec6a3d52de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-23 06:34:36 +0200

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/help_quick_search.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Searches photos by file size.'] = 'Argazkiak bilatzen ditu fitxategi-tamainaren arabera.';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Argazkiak bilatzen ditu Piwigon duten identifikazio zenbakiaren arabera.';
+$lang['Searches photos by posted date.'] = 'Argazkiak bilatzen ditu bidalitako dataren arabera.';
+$lang['Searches for photos by number of views.'] = 'Bistaratze zenbakiaren arabera bilatzen ditu argazkiak.';
+$lang['return photos with at least 1,000 views'] = 'erakutsi 1,000 bistaratzetik gorako argazkiak';
+$lang['Grouping'] = 'Multzokatu';
+$lang['Here is a list of actions you can perform:'] = 'Hona burutu ditzakezun ekintzen zerrenda:';
+$lang['OR inclusive'] = 'OR barne';
+$lang['Others'] = 'Besterik';
+$lang['Same principle as for'] = 'Honen printzipio bera';
+$lang['Search using extended syntax'] = 'Bilatu sintaxi hedatua erabiliz';
+$lang['Searches by author.'] = 'Egileagatik bilatzen du.';
+$lang['Searches by file name.'] = 'Fitxategi-izenagatik bilatzen du.';
+$lang['Add an OR between words.'] = 'Gehitu OR hitzen artean';
+$lang['Exact search'] = 'Bilaketa zehatza';
+$lang['Exclude'] = 'Baztertu';
