541a7e0677742d7b3c859167c5fe7158ba05924e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-03 11:39:38 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/help_quick_search.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Searches photos by taken date.'] = 'Søger efter fotografier ud fra optagelsesdato.';
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = 'Tilføj en bindestreg (-) eller NOT før et ord, for at fravælge det fra søgningen. Bemærk at NOT fungerer som et filter, så du kan ikke have en søgning, der kun indeholder NOT-operatorer. Du kan ikke kombinere OR med NOT ("john OR NOT bent" er ikke gyldigt)';
+$lang['OR inclusive'] = 'OR inklusive';
+$lang['photos from may 2003'] = 'fotografier fra maj 2003';
+$lang['photos posted in 2003'] = 'fotografier offentliggjort i 2003';
+$lang['photos taken in 2003'] = 'fotografier optaget i 2003';
+$lang['return photos less than 700px high'] = 'fotografier mindre end 700 pixels høje';
+$lang['return photos wider than 500px'] = 'fotografier bredere end 500 pixels';
+$lang['return photos with at least 1,000 views'] = 'finder fotografier med mindst 1.000 visninger';
+$lang['returns photos of 12 megapixels or more'] = 'finder fotografier på 12 megapixels eller mere';
+$lang['returns photos of 5 megapixels'] = 'finder fotografier på 5 megapixels';
+$lang['up to 100 (inclusive)'] = 'op til 100 (inklusive)';
+$lang['will give you all photos with at least one note.'] = 'finder alle fotografier med mindst én bemærkning';
+$lang['will give you photos without notes.'] = 'finder fotografier uden bemærkninger';
+$lang['Searches photos by width/height ratio.'] = 'Søger efter fotografier ud fra forhold mellem bredde og højde.';
+$lang['Searches photos with a given width or height.'] = 'Søger efter fotografier ud fra en angivet bredde eller højde.';
+$lang['Supported numeric operators'] = 'Understøttede numeriske operatorer';
+$lang['The quick search engine allows you to use boolean operators to refine your search. By default, the search applies to all keywords. Searches are not case sensitive.'] = 'Den hurtige søgemaskine gør det muligt at anvende boolske operatorer til at gøre søgningen mere præcis. Som standard gælder søgningen alle nøgleord. Søgninger kender ikke forskel på små og store bogstaver.';
+$lang['These operators can be used with numeric fields such as'] = 'Disse operatorer kan anvendes med numeriske felter så som';
+$lang['Use quotes to search for an exact keyword or phrase.'] = 'Anvend anførselsdtegn for at søge efter et specifikt nøgleord eller sætning.';
+$lang['With keywords'] = 'Med nøgleord';
+$lang['between 10 and 100 (inclusive)'] = 'mellem 10 og 100 (inklusive)';
+$lang['equals 100'] = 'præcis 100';
+$lang['finds files between 1MB and 10MB.'] = 'finder filer på mellem 1 og 10 megabyte.';
+$lang['finds panoramas'] = 'finder panoramaer';
+$lang['finds photo 123 to 126 (it may find between 0 and 4 photos, because photos can be deleted)'] = 'finder fotografi 123 til 126 (der findes 0 til 4 fotografier, da der kan være slettede fotografier)';
+$lang['finds photos from compact cameras in portrait or landscape modes'] = 'finder fotografier fra kompaktkameraer i portræt- eller landskabstilstand';
+$lang['from 100 and above'] = 'fra 100 eller flere';
+$lang['greater than 100'] = 'større end 100';
+$lang['less than 100'] = 'mindre end 100';
+$lang['photos after Jan 1st 2008'] = 'fotografier fra 1. januar 2008';
+$lang['photos from 2003 to 2008'] = 'fotografier fra 2003 til 2008';
+$lang['Add an OR between words.'] = 'Tilføj et OR (eller) mellem ord.';
+$lang['Exact search'] = 'Nøjagtig søgning';
+$lang['Exclude'] = 'Fravælg';
+$lang['Grouping'] = 'Gruppér';
+$lang['Here is a list of actions you can perform:'] = 'Her er en liste over handlinger, som du kan udføre:';
+$lang['Others'] = 'Andre';
+$lang['Same principle as for'] = 'Samme princip som for';
+$lang['Search using extended syntax'] = 'Søg vha. udvidet syntaks';
+$lang['Searches by author.'] = 'Søger efter forfatter.';
+$lang['Searches by file name.'] = 'Søger efter filnavn.';
+$lang['Searches for photos by number of views.'] = 'Søger efter fotografier ud fra antallet af visninger.';
+$lang['Searches only for photos with the given words in title or description.'] = 'Søger kun efter fotografier med de angivne ord i titlen eller beskrivelsen.';
+$lang['Searches only in tag names without looking at photo titles or descriptions.'] = 'Søger kun efter tag-navne uden at kigge på fotografiers titler eller beskrivelser.';
+$lang['Searches photos by file size.'] = 'Søger efter fotografier ud fra filstørrelser.';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Søger efter fotografier ud fra numerisk identifikation i Piwigo.';
+$lang['Searches photos by posted date.'] = 'Søger efter fotografier ud fra dato for offentliggørelse';
+$lang['Searches photos by rating.'] = 'Søger efter fotografier ud fra vurdering.';
+$lang['Searches photos by size in pixels.'] = 'Søger efter fotografier ud fra størrelse i pixels.';
