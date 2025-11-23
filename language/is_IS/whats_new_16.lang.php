52a92b1a87355855db63caa1a038e3de9b73df77
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-23 18:11:11 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = 'Glæný innskráning og stillingar með stöðluðum síðum!';
+$lang['Two factor authentication is here'] = 'Tvíþátta auðkenning er hér';
+$lang['UI redesigns that make a difference'] = 'Endurhannað notendaviðmót sem skiptir máli';
