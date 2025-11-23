a2c1468cc78e4b0fc07d0a9e1bbbce7c18b3db8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-23 18:11:13 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = 'Standart sayfalarda yepyeni giriş ve tercihler!';
+$lang['Two factor authentication is here'] = 'İki faktörlü kimlik doğrulama burada';
+$lang['UI redesigns that make a difference'] = 'Fark yaratan kullanıcı arayüzü yeniden tasarımları';
