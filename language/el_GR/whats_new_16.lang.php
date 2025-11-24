1dc97c87a60d7c969bcc4e2f84063ab39e2e2348
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-24 13:30:21 +0100

    Update el_GR, thanks to dimitri

---
+++ language/el_GR/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = 'Εντελώς νέα διαδικασία σύνδεσης και προτιμήσεις με τις Τυπικές σελίδες!';
+$lang['Two factor authentication is here'] = 'Η επαλήθευση δύο παραγόντων είναι εδώ';
+$lang['UI redesigns that make a difference'] = 'Επανασχεδιασμοί του Γραφικού Περιβάλλοντος που κάνουν τη διαφορά';
