3b1b44365feddbab7173ee4b9dbb3034a3b54036
plegall <plg@piwigo.org>
2025-11-24 15:47:22 +0100

    add files whats_new_16.lang.php (wrongly added on master)

--- language/en_UK/whats_new_16.lang.php
+++ language/en_UK/whats_new_16.lang.php
@@ -6,6 +6,6 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 
-$lang['Brand new login & preferences with Standard pages !'] = 'Brand new login & preferences with Standard pages !';
+$lang['Brand new login & preferences with Standard pages !'] = 'Brand new login & preferences with Standard pages!';
 $lang['Two factor authentication is here'] = 'Two factor authentication is here';
 $lang['UI redesigns that make a difference'] = 'UI redesigns that make a difference';
---
+++ language/it_IT/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = 'Login e Preferenze realizzate tramite pagine standard!';
+$lang['Two factor authentication is here'] = 'Con autenticazione a due fattori';
+$lang['UI redesigns that make a difference'] = 'Interfaccia grafica ridisegnata, che fa la differenza';
