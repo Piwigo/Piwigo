07eb1ec91e5836b2ff9fcda39f73e7789e447141
plegall <plg@piwigo.org>
2025-11-24 17:01:36 +0100

    next release is 16.0.0

--- include/constants.php
+++ include/constants.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 
 // Default settings
-define('PHPWG_VERSION', '16.0.0RC3');
+define('PHPWG_VERSION', '16.0.0');
 define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
 
 // this constant is only used in the upgrade process, the true default theme
