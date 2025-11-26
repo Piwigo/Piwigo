c16154080e22666f0296b48f6a9d424742b805dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-26 16:43:30 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = 'דפים חדשים לכניסה והעדפות עם עמודים סטנדרטיים!';
+$lang['Two factor authentication is here'] = 'אימות דו-שלבי כאן';
+$lang['UI redesigns that make a difference'] = 'עיצובי ממשק משתמש שעושים את ההבדל';
