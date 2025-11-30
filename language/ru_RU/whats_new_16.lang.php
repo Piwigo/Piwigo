8153a96a9ad31a78fe19747574c486f67399c5e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-30 18:08:24 +0100

    Update ru_RU, thanks to abravorus

---
+++ language/ru_RU/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Two factor authentication is here'] = 'Реализована двухфакторная авторизация (2FA)';
+$lang['Brand new login & preferences with Standard pages !'] = 'Совершенно новый вид страниц входа и настроек с использованием "Стандартных страниц"!';
+$lang['UI redesigns that make a difference'] = 'Переработан пользовательский интерфейс и Вы увидите разницу';
