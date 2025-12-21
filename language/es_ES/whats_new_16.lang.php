d4f88cf44194b96f32cc07d2c6df80d3b4b50a0d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-21 21:15:51 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Brand new login & preferences with Standard pages !'] = '¡Página de inicio de sesión totalmente nueva y preferencias con páginas estándar!';
+$lang['Two factor authentication is here'] = 'Ha llegado la autenticación de dos factores';
+$lang['UI redesigns that make a difference'] = 'Rediseños de la interfaz de usuario que marcan la diferencia';
