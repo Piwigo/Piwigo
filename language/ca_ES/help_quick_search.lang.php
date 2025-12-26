10ca45ad0c7cfddb39c701ef61e825c88595e596
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-26 07:07:33 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/help_quick_search.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = 'Afegiu un guió (-) o NOT abans d\'una paraula per excloure-la de la cerca. Tingueu en compte que NOT actua com a operador de filtre, de manera que no podeu fer una cerca que contingui només operadors NOT. No podeu combinar OR amb NOT (és a dir, John OR NOT Bill no és vàlid)';
+$lang['Add an OR between words.'] = 'Afegeix un OR entre paraules.';
+$lang['Exact search'] = 'Cerca exacta';
+$lang['Exclude'] = 'Excloure';
+$lang['Grouping'] = 'Agrupació';
+$lang['Here is a list of actions you can perform:'] = 'Aquí teniu una llista de les accions disponibles:';
+$lang['OR inclusive'] = 'OR inclusiu';
+$lang['Others'] = 'Altres';
+$lang['Same principle as for'] = 'Mateix principi que per a';
+$lang['Search using extended syntax'] = 'Cerca mitjançant la sintaxi estesa';
+$lang['Searches by author.'] = 'Cerca per autor.';
+$lang['Searches by file name.'] = 'Cerca per nom de fitxer.';
+$lang['Searches for photos by number of views.'] = 'Cerca fotos per nombre de visualitzacions.';
+$lang['Searches only for photos with the given words in title or description.'] = 'Cerca només fotos amb les paraules donades al títol o descripció.';
+$lang['Searches only in tag names without looking at photo titles or descriptions.'] = 'Cerca només a les etiquetes sense mirar els títols ni les descripcions de les fotos.';
+$lang['Searches photos by file size.'] = 'Cerca fotos per mida del fitxer.';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Cerca fotos pel seu identificador numèric a Piwigo.';
+$lang['Searches photos by posted date.'] = 'Cerca fotos per data de publicació.';
+$lang['Searches photos by rating.'] = 'Cerca fotos per puntuació.';
+$lang['Searches photos by size in pixels.'] = 'Cerca fotos per mida en píxels.';
+$lang['Searches photos by taken date.'] = 'Cerca fotos per la data en què es van fer.';
+$lang['Searches photos by width/height ratio.'] = 'Cerca fotos per ràtio amplada/alçada.';
+$lang['Searches photos with a given width or height.'] = 'Busca fotos amb una amplada o alçada determinades.';
+$lang['Supported numeric operators'] = 'Operadors numèrics suportats';
+$lang['The quick search engine allows you to use boolean operators to refine your search. By default, the search applies to all keywords. Searches are not case sensitive.'] = 'El motor de cerca ràpida us permet utilitzar operadors booleans per refinar la cerca. Per defecte, la cerca s\'aplica a totes les paraules clau. Les cerques no distingeixen entre majúscules i minúscules.';
+$lang['These operators can be used with numeric fields such as'] = 'Aquests operadors es poden utilitzar amb camps numèrics com ara';
+$lang['Use quotes to search for an exact keyword or phrase.'] = 'Utilitzeu cometes per cercar una paraula clau o frase exacta.';
+$lang['With keywords'] = 'Amb paraules clau';
+$lang['between 10 and 100 (inclusive)'] = 'entre 10 i 100 (inclusivament)';
+$lang['equals 100'] = 'igual a 100';
+$lang['finds files between 1MB and 10MB.'] = 'troba fitxers entre 1MB i 10MB.';
+$lang['finds panoramas'] = 'cerca panorames';
+$lang['finds photo 123 to 126 (it may find between 0 and 4 photos, because photos can be deleted)'] = 'cerca les fotos de la número 123 a la 126 (pot trobar entre 0 i 4 fotos, perquè les fotos es poden suprimir)';
+$lang['finds photos from compact cameras in portrait or landscape modes'] = 'cerca fotos de càmeres compactes en mode retrat o paisatge';
+$lang['from 100 and above'] = 'a partir de 100';
+$lang['greater than 100'] = 'més gran que 100';
+$lang['less than 100'] = 'menor que 100';
+$lang['photos after Jan 1st 2008'] = 'fotos posteriors a l\'1 de gener de 2008';
+$lang['photos from 2003 to 2008'] = 'fotos del 2003 al 2008';
+$lang['photos from may 2003'] = 'fotos de maig de 2003';
+$lang['photos posted in 2003'] = 'fotos publicades el 2003';
+$lang['photos taken in 2003'] = 'fotos fetes el 2003';
+$lang['return photos less than 700px high'] = 'fotos de menys de 700px d\'alçada';
+$lang['return photos wider than 500px'] = 'fotos més amples de 500px';
+$lang['return photos with at least 1,000 views'] = 'retorna fotos amb almenys 1000 visites';
+$lang['returns photos of 12 megapixels or more'] = 'retorna fotos de 12 megapíxels o més';
+$lang['returns photos of 5 megapixels'] = 'retorna fotos de 5 megapíxels';
+$lang['up to 100 (inclusive)'] = 'fins a 100 (inclòs)';
+$lang['will give you all photos with at least one note.'] = 'us donarà totes les fotos amb almenys una nota.';
+$lang['will give you photos without notes.'] = 'us donarà les fotos sense notes.';
