896b7f2b118eedecac4f40dff8a824ad3ef1f6d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-13 19:17:05 +0100

    Update hu_HU, thanks to ugyes

---
+++ language/hu_HU/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['UI redesigns that make a difference'] = 'Felhasználói felület újragondolva – érezhető különbséggel';
+$lang['Two factor authentication is here'] = 'Megérkezett a kétlépcsős hitelesítés';
+$lang['Brand new login & preferences with Standard pages !'] = 'Teljesen új bejelentkezés és beállítások a Standard oldalakkal!';
