146b01722d370f030794f298a3c12409ac23f6a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-13 19:16:55 +0100

    Update hu_HU, thanks to ugyes

---
+++ language/hu_HU/help_quick_search.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = 'Adjon egy kötőjelet (-) vagy a NOT szót egy szó elé, hogy kizárja a keresésből. Fontos: a NOT szűrő operátorként működik, ezért nem lehet olyan keresése, ami csak NOT operátorokat tartalmaz. Az OR nem kombinálható a NOT-tal (például: john OR NOT bill nem érvényes)';
+$lang['up to 100 (inclusive)'] = '100-ig (beleértve)';
+$lang['photos from may 2003'] = '2003. májusi fotók';
+$lang['photos from 2003 to 2008'] = '2003 és 2008 közötti képek';
+$lang['photos after Jan 1st 2008'] = '2008. január 1. utáni képek';
+$lang['less than 100'] = 'kisebb, mint 100';
+$lang['greater than 100'] = 'nagyobb, mint 100';
+$lang['between 10 and 100 (inclusive)'] = '10 és 100 között (beleértve)';
+$lang['With keywords'] = 'Kulcsszavakkal';
+$lang['Searches by file name.'] = 'Fájlnév szerinti keresések.';
+$lang['Searches by author.'] = 'Szerző szerinti keresések.';
+$lang['Add an OR between words.'] = 'Adjon egy OR-t a szavak közé.';
+$lang['Grouping'] = 'Csoportosítás';
+$lang['Exclude'] = 'Kizárás';
+$lang['Exact search'] = 'Pontos keresés';
