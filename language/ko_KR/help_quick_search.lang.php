d484ec09fa273cc27cbfba44e3762c03c99a02a2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-29 19:11:19 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/help_quick_search.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photos posted in 2003'] = '2003년에 게시된 사진';
+$lang['photos taken in 2003'] = '2003년에 촬영된 사진';
+$lang['return photos less than 700px high'] = '높이가 700px보다 작은 사진';
+$lang['return photos wider than 500px'] = '너비가 500px보다 큰 사진';
+$lang['return photos with at least 1,000 views'] = '조회수 1,000회 이상 사진 반환';
+$lang['returns photos of 12 megapixels or more'] = '1200만 화소 이상 사진 반환';
+$lang['returns photos of 5 megapixels'] = '500만 화소 사진 반환';
+$lang['up to 100 (inclusive)'] = '100 이하(포함)';
+$lang['will give you all photos with at least one note.'] = '메모가 하나 이상 있는 모든 사진 찾기';
+$lang['will give you photos without notes.'] = '메모가 없는 사진 찾기';
+$lang['Searches photos by taken date.'] = '촬영 날짜별로 사진을 검색합니다.';
+$lang['Searches photos by width/height ratio.'] = '너비/높이 비율로 사진을 검색합니다.';
+$lang['Searches photos with a given width or height.'] = '지정된 너비 또는 높이의 사진을 검색합니다.';
+$lang['Supported numeric operators'] = '지원되는 수치 연산자';
+$lang['The quick search engine allows you to use boolean operators to refine your search. By default, the search applies to all keywords. Searches are not case sensitive.'] = '빠른 검색 엔진을 통해 부울 연산자를 사용하여 검색을 세분화할 수 있습니다. 기본적으로 검색은 모든 키워드에 적용됩니다. 검색은 대소문자를 구분하지 않습니다.';
+$lang['These operators can be used with numeric fields such as'] = '이러한 연산자는 다음과 같은 수치 필드에 사용할 수 있습니다.';
+$lang['Use quotes to search for an exact keyword or phrase.'] = '따옴표를 사용하여 정확한 키워드나 문구를 검색합니다.';
+$lang['With keywords'] = '키워드 사용';
+$lang['between 10 and 100 (inclusive)'] = '10과 100 사이 (포함)';
+$lang['equals 100'] = '100과 같음';
+$lang['finds files between 1MB and 10MB.'] = '1MB에서 10MB 사이의 파일을 찾습니다.';
+$lang['finds panoramas'] = '파노라마 사진을 찾습니다.';
+$lang['finds photo 123 to 126 (it may find between 0 and 4 photos, because photos can be deleted)'] = '사진 123부터 126까지 찾습니다(사진이 삭제될 수 있으므로 0개에서 4개 사이의 사진을 찾을 수 있음).';
+$lang['finds photos from compact cameras in portrait or landscape modes'] = '세로 또는 가로 모드의 컴팩트 카메라 사진을 찾습니다.';
+$lang['from 100 and above'] = '100 이상';
+$lang['greater than 100'] = '100보다 큼';
+$lang['less than 100'] = '100보다 작음';
+$lang['photos after Jan 1st 2008'] = '2008년 1월 1일 이후의 사진';
+$lang['photos from 2003 to 2008'] = '2003년부터 2008년까지의 사진';
+$lang['photos from may 2003'] = '2003년 5월의 사진';
+$lang['Add a dash (-) or NOT before a word to exclude from search. Note that NOT acts as a filtering operator so you cannot have a search containing only NOT operators. You cannot combine OR with NOT (john OR NOT bill is not valid)'] = '검색에서 제외할 단어 앞에 대시(-) 또는 NOT을 추가합니다. NOT은 필터링 연산자로 작동하므로 NOT 연산자만으로 구성된 검색은 불가능합니다. OR과 NOT을 함께 사용할 수 없습니다(예: john OR NOT bill은 유효하지 않음)';
+$lang['Add an OR between words.'] = '단어 사이에 OR을 추가합니다.';
+$lang['Exact search'] = '정확한 검색';
+$lang['Exclude'] = '제외';
+$lang['Grouping'] = '그룹화';
+$lang['Here is a list of actions you can perform:'] = '다음은 수행 가능한 작업 목록입니다:';
+$lang['OR inclusive'] = 'OR 포함';
+$lang['Others'] = '기타';
+$lang['Same principle as for'] = '다음과 동일한 원리입니다:';
+$lang['Search using extended syntax'] = '확장 구문을 사용한 검색';
+$lang['Searches by author.'] = '작성자로 검색합니다.';
+$lang['Searches by file name.'] = '파일명으로 검색합니다.';
+$lang['Searches for photos by number of views.'] = '조회수별 사진 검색.';
+$lang['Searches only for photos with the given words in title or description.'] = '제시된 단어가 제목이나 설명에 포함된 사진만 검색합니다.';
+$lang['Searches only in tag names without looking at photo titles or descriptions.'] = '사진 제목이나 설명을 확인하지 않고 태그 이름에서만 검색합니다.';
+$lang['Searches photos by file size.'] = '파일 크기로 사진을 검색합니다.';
+$lang['Searches photos by its numeric identifier in Piwigo.'] = 'Piwigo 내 숫자 식별자로 사진을 검색합니다.';
+$lang['Searches photos by posted date.'] = '게시된 날짜별로 사진을 검색합니다.';
+$lang['Searches photos by rating.'] = '평점별 사진 검색.';
+$lang['Searches photos by size in pixels.'] = '픽셀 단위의 크기로 사진을 검색합니다.';
