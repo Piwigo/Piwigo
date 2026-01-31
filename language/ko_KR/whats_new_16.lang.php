6ac0b99697af38aeb44c99db9dc136e3975c00f8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-31 06:47:27 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/whats_new_16.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Two factor authentication is here'] = '2단계 인증이 도입되었습니다';
+$lang['UI redesigns that make a difference'] = '차이를 만들어내는 사용자 인터페이스 재설계';
+$lang['Brand new login & preferences with Standard pages !'] = '새롭게 선보이는 로그인 및 기본 설정 기능과 표준 페이지!';
